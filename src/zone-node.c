0fc1f5b3f92b003cec5f0023e8e00dd35f7d142a
Lubos Slovak <lubos.slovak@nic.cz>
2010-03-24 22:06:31 +0100

    Zone node implementation.
    Lightweight module holding only an array of pointers to RRs and
      with basic operations: create, destroy, add, find and
      destructor for using in the overlying data structure.

--- src/zone-node.c
+++ src/zone-node.c
@@ -1,21 +1,78 @@
 #include "zone-node.h"
 
 #include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include "common.h"
+#include "dns-simple.h"
 
 /*----------------------------------------------------------------------------*/
 
-zn_node *zn_create()
+zn_node *zn_create( uint count )
 {
     zn_node *node = malloc(sizeof(zn_node));
+
+    if (node != NULL) {
+        if (count > 0) {
+            node->records = malloc(count * sizeof(dnss_rr *));
+            if (node->records == NULL) {
+                fprintf(stderr, "zn_create(): Allocation failed!\n");
+                free(node);
+                return NULL;
+            }
+            node->max_count = count;
+            memset(node->records, 0, count * sizeof(dnss_rr *));
+        } else {
+            node->records = NULL;
+        }
+        node->count = 0;
+    }
+
     return node;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int zn_add_rr( zn_node *node, dnss_rr *rr )
+{
+    assert(node->count <= node->max_count);
+    if (node->count == node->max_count) {
+        // no place
+        return -1;
+    }
+
+    node->records[node->count++] = rr;
+    return 0;
+}
+
+/*----------------------------------------------------------------------------*/
+
+const dnss_rr *zn_find_rr( zn_node *node, uint16_t type )
+{
+    uint i = 0;
+    while (i < node->count && node->records[i]->rrtype != type) {
+        assert(node->records[i] != NULL);
+        ++i;
+    }
+
+    if (i == node->count) {
+        return NULL;
+    }
+
+    assert(node->records[i]->rrtype == type);
+    return node->records[i];
 }
 
 /*----------------------------------------------------------------------------*/
 
 void zn_destroy( zn_node **node )
 {
+    for (uint i = 0; i < (*node)->count; ++i) {
+        dnss_destroy_rr(&(*node)->records[i]);
+    }
+    free((*node)->records);
     free(*node);
-    node = NULL;
+    *node = NULL;
 }
 
 /*----------------------------------------------------------------------------*/
