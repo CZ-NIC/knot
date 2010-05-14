5b11b0bb92b61ce959cb00eecac49090fca60bdc
Marek Vavrusa <marek@vavrusa.com>
2010-05-14 21:37:35 +0200

    Implemented dispatchers and managers for TCP and UDP in cute_server.

--- src/server/server.c
+++ src/server/server.c
@@ -43,26 +43,33 @@ cute_server *cute_create()
     debug_server("Done\n\n");
     debug_server("Creating Socket Manager structure..\n");
 
-    server->tcp_mgr = NULL;
-    server->udp_mgr = sm_create(server->nameserver);
-    if (server->udp_mgr == NULL ) {
-        ns_destroy(&server->nameserver);
-        zdb_destroy(&server->zone_db);
-        free(server);
-        return NULL;
+    for(int i = 0; i < 2; i++) {
+        server->manager[i] = sm_create(server->nameserver);
+        if (server->manager[i] == NULL ) {
+            ns_destroy(&server->nameserver);
+            zdb_destroy(&server->zone_db);
+            free(server);
+            return NULL;
+        }
     }
+
+    // Register socket handlers
+    sm_register_handler(server->manager[UDP], &sm_udp_handler);
+    sm_register_handler(server->manager[TCP], &sm_tcp_handler);
 
     debug_server("Done\n\n");
     debug_server("Creating Dispatcher structure..\n");
 
-    server->dispatcher = dpt_create(DEFAULT_THR_COUNT, sm_listen_udp,
-                                    server->udp_mgr);
-    if (server->dispatcher == NULL) {
-        sm_destroy(&server->udp_mgr);
-        ns_destroy(&server->nameserver);
-        zdb_destroy(&server->zone_db);
-        free(server);
-        return NULL;
+    for(int i = 0; i < 2; i++) {
+        server->dispatcher[i] = dpt_create(DEFAULT_THR_COUNT, &sm_listen, server->manager[i]);
+        if (server->dispatcher[i] == NULL) {
+            sm_destroy(&server->manager[UDP]);
+            sm_destroy(&server->manager[TCP]);
+            ns_destroy(&server->nameserver);
+            zdb_destroy(&server->zone_db);
+            free(server);
+            return NULL;
+        }
     }
 
     debug_server("Done\n\n");
@@ -80,20 +87,45 @@ int cute_start( cute_server *server, const char *filename )
     }
 
     debug_server("Opening sockets..\n");
-    if (sm_open_socket(server->udp_mgr, DEFAULT_PORT, UDP) != 0) {
+    if (sm_open_socket(server->manager[UDP], DEFAULT_PORT, UDP) != 0) {
+	printf("[failed]\n");
         return -1;
     }
+    printf("TCP(%d) ", DEFAULT_PORT); fflush(stdout);
+    if (sm_open_socket(server->manager[TCP], DEFAULT_PORT, TCP) != 0) {
+        printf("[failed]\n");
+        return -1;
+    }
+    printf("\n");
 
     debug_server("Starting the Dispatcher..\n");
-    return dpt_start(server->dispatcher);
+
+    // Start dispatchers
+    int ret = 0;
+    ret = dpt_start(server->dispatcher[TCP]);
+    printf("   TCP handler: %u threads started.\n", server->dispatcher[TCP]->thread_count);
+    ret += dpt_start(server->dispatcher[UDP]);
+    printf("   UDP handler: %u threads started.\n", server->dispatcher[UDP]->thread_count);
+    if(ret < 0)
+        return ret;
+
+    // Wait for dispatchers to finish
+    /// \todo Intercept SIGINT for clean exit.
+    ret = dpt_wait(server->dispatcher[TCP]);
+    printf("TCP handler finished.\n");
+    ret += dpt_wait(server->dispatcher[UDP]);
+    printf("UDP handler finished.\n");
+    return ret;
 }
 
 /*----------------------------------------------------------------------------*/
 
 void cute_destroy( cute_server **server )
 {
-    dpt_destroy(&(*server)->dispatcher);
-    sm_destroy(&(*server)->udp_mgr);
+    dpt_destroy(&(*server)->dispatcher[UDP]);
+    dpt_destroy(&(*server)->dispatcher[TCP]);
+    sm_destroy(&(*server)->manager[UDP]);
+    sm_destroy(&(*server)->manager[TCP]);
     ns_destroy(&(*server)->nameserver);
     zdb_destroy(&(*server)->zone_db);
     free(*server);
