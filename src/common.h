5183121dea0001d66635364a934be83b13f43740
Marek Vavrusa <marek@vavrusa.com>
2010-06-02 12:55:47 +0200

    Initial syslog-based logging facility.

--- src/common.h
+++ src/common.h
@@ -1,10 +1,19 @@
 #ifndef COMMON
 #define COMMON
 
-#include "print.h"
+/* Common types and macros.
+ */
 
 typedef unsigned int uint;
 
+#define PROJECT_NAME "CuteDNS" // Project name
+#define PROJECT_VER  0x000001  // 0xMMIIRR (MAJOR,MINOR,REVISION)
 #define ERR_ALLOC_FAILED fprintf(stderr, "Allocation failed.\n")
+
+/* Common includes.
+ */
+
+#include "print.h"
+#include "log.h"
 
 #endif // COMMON
