ef0aefc66b37cddec91caa5f31dc16304f10e5d0
Marek Vavrusa <marek@vavrusa.com>
2010-06-02 12:55:47 +0200

    Initial syslog-based logging facility.

--- src/main.c
+++ src/main.c
@@ -8,18 +8,23 @@
 int main( int argc, char **argv )
 {
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <filename>.\n", argv[0]);
+        print_msg(LOG_ERR, "Usage: %s <filename>.\n", argv[0]);
         return -1;
     }
 
+    // Open log
+    log_open(LOG_UPTO(LOG_ERR), LOG_MASK(LOG_ERR)|LOG_MASK(LOG_WARNING));
+
+    // Start server
     cute_server *server = cute_create();
 
     int res;
     if ((res = cute_start(server, argv[1])) != 0) {
-        fprintf (stderr, "Problem starting the server, exiting..\n");
+        log_error("Problem starting the server, exiting..\n");
     }
 
+    // Stop server and close log
     cute_destroy(&server);
-
+    log_close();
     return res;
 }
