53e31b5d0abc76cdffe3651fbfd28018a83c9341
Marek Vavrusa <marek@vavrusa.com>
2010-06-03 13:03:55 +0200

    Updated print helpers with generic output function.

--- src/other/print.h
+++ src/other/print.h
@@ -1,8 +1,23 @@
-#ifndef PRINT
-#define PRINT
+#ifndef __print_h__
+#define __print_h__
+
+/*----------------------------------------------------------------------------*/
+/* Downloaded hex_print, bit_print from http://www.digitalpeer.com/id/print   */
+/* Updated with generic printf handler.                                       */
+/*----------------------------------------------------------------------------*/
+
+typedef int (*printf_t)(const char* fmt, ...);
+
+/* Hex-value printing.
+ */
 
 void hex_print( const char *data, int length );
+void hex_printf( const char *data, int length, printf_t print_handler );
+
+/* Bit-value printing.
+ */
 
 void bit_print( const char *data, int length );
+void bit_printf( const char *data, int length, printf_t print_handler );
 
-#endif  // PRINT
+#endif  // __print_h__
