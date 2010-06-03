52952275c40295922ea5ad3185dd7bac51a59f64
Marek Vavrusa <marek@vavrusa.com>
2010-06-03 13:04:48 +0200

    Improved error allocation reporting.

--- src/common.h
+++ src/common.h
@@ -1,19 +1,22 @@
 #ifndef COMMON
 #define COMMON
 
-/* Common types and macros.
+/* Common types and constants.
  */
 
 typedef unsigned int uint;
 
 #define PROJECT_NAME "CuteDNS" // Project name
 #define PROJECT_VER  0x000001  // 0xMMIIRR (MAJOR,MINOR,REVISION)
-#define ERR_ALLOC_FAILED fprintf(stderr, "Allocation failed.\n")
 
 /* Common includes.
  */
 
 #include "print.h"
 #include "log.h"
+
+/* Common macros.
+ */
+#define ERR_ALLOC_FAILED log_error("Allocation failed at %s:%d (%s ver.%x)\n", __FILE__, __LINE__, PROJECT_NAME, PROJECT_VER)
 
 #endif // COMMON
