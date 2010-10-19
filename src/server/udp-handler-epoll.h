8f8b868abfcf9a179888cd89255cf22ea6aabec0
Marek Vavrusa <marek@vavrusa.com>
2010-10-19 09:45:20 +0200

    Added UDP epoll model for later comparison.

---
+++ src/server/udp-handler-epoll.h
@@ -0,0 +1,19 @@
+/*!
+  * \file udp-handler.h
+  *
+  * UDP sockets threading model.
+  *
+  * The master socket locks one worker thread at a time
+  * and saves events in it's own backing store for asynchronous processing.
+  * The worker threads work asynchronously in thread pool.
+  */
+
+#ifndef UDPHANDLER_EPOLL_H
+#define UDPHANDLER_EPOLL_H
+
+#include "socket-manager.h"
+
+void *udp_epoll_master( void *obj );
+void *udp_epoll_worker( void *obj );
+
+#endif
