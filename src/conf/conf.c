8da9a1fd8cb04edba7c31d89678764a1eff7393b
Ondřej Surý <ondrej@sury.org>
2010-11-01 17:25:12 +0100

    cf-test compileable, but not lexxer yet

--- src/conf/conf.c
+++ src/conf/conf.c
@@ -1,5 +1,18 @@
+/**
+ *
+ **/
+
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <setjmp.h>
 
 #include "conf.h"
+
+static jmp_buf conf_jmpbuf;
+
+struct config *new_config;
 
 struct config *
 config_alloc(char *name)
@@ -12,6 +25,18 @@ config_alloc(char *name)
 int
 config_parse(struct config *c)
 {
+	if (setjmp(conf_jmpbuf))
+		return 1;
 
-	
+	new_config = c;
+	cf_parse();
+	return 0;
+}
+
+void
+cf_error(char *msg)
+{
+	fputs(msg, stderr);
+	fputc('\n', stderr);
+	longjmp(conf_jmpbuf, 1);
 }
