281e51e3e3ba219df2320182aa8b25db30f29215
Jan Kadlec <jan@hp4-jankadlec.(none)>
2010-11-08 15:22:59 +0100

    has now backward identification of flows. Works with tcp. As a first try, I'd say it's pretty much complete. I don't really know what to add that wouldn't make the server run too slowly. ref: #41

---
+++ src/stat/gatherer.c
@@ -0,0 +1,57 @@
+/*
+ * File:     gath.c
+ * Date:     08.11.2010 10:18
+ * Author:   jan
+ * Project:  
+ * Description:   
+ */
+
+#include <malloc.h>
+#include <pthread.h>
+
+#include "gatherer.h"
+#include "common.h"
+
+gatherer_t *new_gatherer( )
+{
+    gatherer_t *ret;
+
+    if ((ret=malloc(sizeof(gatherer_t)))==NULL) {
+               //err memry 
+        return NULL;
+    }
+
+    pthread_mutex_init(&ret->mutex_read, NULL);
+
+    for (int i = 0; i < FREQ_BUFFER_SIZE; i++) {
+        ret->freq_array[i]=0;
+    }
+
+    ret->qps = 0.0;
+    ret->udp_qps = 0.0;
+    ret->tcp_qps = 0.0;
+
+    ret->mean_latency = 0.0;
+    ret->udp_mean_latency = 0.0;
+    ret->tcp_mean_latency = 0.0;
+
+    ret->udp_latency = 0;
+    ret->tcp_latency = 0;
+
+    ret->udp_queries = 0;
+    ret->tcp_queries = 0;
+
+    return ret;
+}
+
+void gatherer_free( gatherer_t *gath )
+{
+    for (int i = 0; i < ; i++) {
+        free(gath->flow_array[i].addr);
+    }
+    free(gath);
+}
+
+
+
+/* end of file gath.c */
