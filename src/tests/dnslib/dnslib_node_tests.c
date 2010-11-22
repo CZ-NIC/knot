e858b690d0c9383ea6c22ea5db5b9d8ca749ae47
Jan Kadlec <jan@hp4-jankadlec.(none)>
2010-11-22 11:21:23 +0100

    Added tests for adding/getting rrset into node tests.

--- src/tests/dnslib/dnslib_node_tests.c
+++ src/tests/dnslib/dnslib_node_tests.c
@@ -28,24 +28,48 @@ struct test_node {
 	uint size;
 };
 
-static struct test_node
-		test_nodes[TEST_NODES] = {
+static struct test_node	test_nodes[TEST_NODES] = {
     {{(uint8_t *)"\3www\7example\3com", 17}, (dnslib_node_t *)0xBADDCAFE}
 };
 
+static dnslib_rrset_t rrsets[1] = {
+    {{(uint8_t *)"\3www\7example\3com", 17}, 1, 1, 3600, NULL, NULL, NULL, 0}
+};
 
 static int test_node_create()
 {
     dnslib_node_t *tmp;
-    for (int i = 0; i < TEST_NODES; i++) {
+    int errors = 0;
+    for (int i = 0; i < TEST_NODES && !errors; i++) {
         tmp = dnslib_node_new(&test_nodes[i].owner, test_nodes[i].parent);
         if (tmp == NULL || tmp->owner != &test_nodes[i].owner || 
             tmp->parent != test_nodes[i].parent || tmp->rrsets == NULL) {
-            return 0;
+            errors++;
+            diag("Failed to create node structure");
         }
         dnslib_node_free(&tmp);
     }
-    return 1;
+    return (errors == 0);
+}
+
+static int test_node_add_rrset()
+{
+    dnslib_node_t *tmp;
+    dnslib_rrset_t *rrset;
+    int errors = 0;
+    for (int i = 0; i < TEST_NODES && !errors; i++) {
+        tmp = dnslib_node_new(&test_nodes[i].owner, test_nodes[i].parent);
+        rrset = &rrsets[0];
+        if (dnslib_node_add_rrset(tmp, rrset)!=0) {
+            errors++;
+            diag("Failed to insert rrset into node");
+        }
+        if (dnslib_node_get_rrset(tmp, rrset->type) == NULL) {
+            errors++;
+            diag("Failed to get rrset from node");
+        }
+    }
+    return (errors == 0);
 }
 
 static const int DNSLIB_NODE_TEST_COUNT = 1;
@@ -64,6 +88,10 @@ static int dnslib_node_tests_run(int argc, char *argv[])
 {
 
   ok(test_node_create(), "node: create");
+
+  //skip
+
+  ok(test_node_add_rrset(), "node: add");
 
 	return 0;
 }
