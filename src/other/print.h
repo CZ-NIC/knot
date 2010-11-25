87b520dfad17af7e213bda420d6ecccddd2150f7
Lubos Slovak <lubos.slovak@nic.cz>
2010-11-25 17:14:48 +0100

    Print functions formatted.
    refs #150

--- src/other/print.h
+++ src/other/print.h
@@ -1,23 +1,34 @@
-#ifndef __print_h__
-#define __print_h__
+/*!
+ * \file print.h
+ *
+ * \author Marek Vavrusa <marek.vavrusa@nic.cz>
+ *
+ * \brief Custom printing functions.
+ *
+ * Downloaded hex_print, bit_print from http://www.digitalpeer.com/id/print
+ * Updated with generic printf handler.
+ *
+ * \addtogroup utils
+ * @{
+ */
 
-/*----------------------------------------------------------------------------*/
-/* Downloaded hex_print, bit_print from http://www.digitalpeer.com/id/print   */
-/* Updated with generic printf handler.                                       */
-/*----------------------------------------------------------------------------*/
+#ifndef _CUTEDNS_PRINT_H_
+#define _CUTEDNS_PRINT_H_
 
-typedef int (*printf_t)(const char* fmt, ...);
+typedef int (*printf_t)(const char *fmt, ...);
 
 /* Hex-value printing.
  */
 
-void hex_print( const char *data, int length );
-void hex_printf( const char *data, int length, printf_t print_handler );
+void hex_print(const char *data, int length);
+void hex_printf(const char *data, int length, printf_t print_handler);
 
 /* Bit-value printing.
  */
 
-void bit_print( const char *data, int length );
-void bit_printf( const char *data, int length, printf_t print_handler );
+void bit_print(const char *data, int length);
+void bit_printf(const char *data, int length, printf_t print_handler);
 
-#endif  // __print_h__
+#endif  /* _CUTEDNS_PRINT_H_ */
+
+/*! @} */
