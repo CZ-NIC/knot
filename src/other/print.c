9f3bd9d02c855ca47f3b665f2b3dba7fe1d056f5
Lubos Slovak <lubos.slovak@nic.cz>
2010-11-25 17:14:48 +0100

    Print functions formatted.
    refs #150

--- src/other/print.c
+++ src/other/print.c
@@ -1,45 +1,40 @@
-#include "print.h"
 #include <stdio.h>
 
-void hex_printf( const char *data, int length, printf_t print_handler )
+#include "print.h"
+
+void hex_printf(const char *data, int length, printf_t print_handler)
 {
-    int ptr = 0;
-    for(;ptr < length;ptr++)
-    {
-        print_handler("0x%02x ",(unsigned char)*(data+ptr));
-    }
-    print_handler("\n");
+	int ptr = 0;
+	for (; ptr < length; ptr++) {
+		print_handler("0x%02x ", (unsigned char)*(data + ptr));
+	}
+	print_handler("\n");
 }
 
-void hex_print( const char *data, int length)
+void hex_print(const char *data, int length)
 {
-    hex_printf(data, length, &printf);
+	hex_printf(data, length, &printf);
 }
 
-void bit_printf( const char *data, int length, printf_t print_handler )
+void bit_printf(const char *data, int length, printf_t print_handler)
 {
-    unsigned char mask = 0x01;
-    int ptr = 0;
-    int bit = 0;
-    for(;ptr < length;ptr++)
-    {
-        for(bit = 7;bit >= 0;bit--)
-        {
-            if ((mask << bit) & (unsigned char)*(data+ptr))
-            {
-                print_handler("1");
-            }
-            else
-            {
-                print_handler("0");
-            }
-        }
-        print_handler(" ");
-    }
-    print_handler("\n");
+	unsigned char mask = 0x01;
+	int ptr = 0;
+	int bit = 0;
+	for (; ptr < length; ptr++) {
+		for (bit = 7; bit >= 0; bit--) {
+			if ((mask << bit) & (unsigned char)*(data + ptr)) {
+				print_handler("1");
+			} else {
+				print_handler("0");
+			}
+		}
+		print_handler(" ");
+	}
+	print_handler("\n");
 }
 
-void bit_print( const char *data, int length )
+void bit_print(const char *data, int length)
 {
-    bit_printf(data, length, &printf);
+	bit_printf(data, length, &printf);
 }
