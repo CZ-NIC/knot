2bd2300924228f1e5d948e4cf14f814f79e954f0
Lubos Slovak <lubos.slovak@nic.cz>
2010-11-25 17:20:09 +0100

    Log formatted + minor changes.
    - includes
    - indentation
    - spaces
    - pointers
    - file comment
    - doxygen tags
    - doxygen grouping
    - doxygen comment format
    - ifdefs
    
    Slightly modified log_msg macro (used do {} while (0) for
      multi-line macro and added braces to the if statement).
    
    refs #150

--- src/other/log.c
+++ src/other/log.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
+
 #include "log.h"
 #include "common.h"
 
@@ -9,41 +10,44 @@ static int _LOG_MASK = 0;
 
 int log_open(int print_mask, int log_mask)
 {
-    setlogmask(log_mask);
-    openlog(PROJECT_NAME, LOG_CONS|LOG_PID|LOG_NDELAY,LOG_LOCAL1); /// \todo May change to LOG_DAEMON.
-    _LOG_MASK = print_mask;
-    _LOG_OPEN = 1;
-    return 0;
+	setlogmask(log_mask);
+
+	/// \todo May change to LOG_DAEMON.
+	openlog(PROJECT_NAME, LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+	_LOG_MASK = print_mask;
+	_LOG_OPEN = 1;
+	return 0;
 }
 
 int log_close()
 {
-    _LOG_OPEN = 0;
-    _LOG_MASK = 0;
-    closelog();
-    return 0;
+	_LOG_OPEN = 0;
+	_LOG_MASK = 0;
+	closelog();
+	return 0;
 }
 
 int log_isopen()
 {
-   return _LOG_OPEN;
+	return _LOG_OPEN;
 }
 
-int print_msg(int level, const char* msg, ...)
+int print_msg(int level, const char *msg, ...)
 {
-    // Get output stream
-    va_list ap;
-    FILE* stream = stdout;
-    if(level & (LOG_ERR|LOG_WARNING|LOG_CRIT|LOG_ALERT))
-        stream = stderr;
+	// Get output stream
+	va_list ap;
+	FILE *stream = stdout;
+	if (level & (LOG_ERR | LOG_WARNING | LOG_CRIT | LOG_ALERT)) {
+		stream = stderr;
+	}
 
-    // Check mask
-    int ret = 0;
-    if(level & _LOG_MASK) {
-        va_start(ap, msg);
-        ret = vfprintf(stream, msg, ap);
-        va_end (ap);
-    }
+	// Check mask
+	int ret = 0;
+	if (level & _LOG_MASK) {
+		va_start(ap, msg);
+		ret = vfprintf(stream, msg, ap);
+		va_end(ap);
+	}
 
-    return ret;
+	return ret;
 }
