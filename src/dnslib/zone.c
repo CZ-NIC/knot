33a2dea6f8469f4a61918e8f9edde76b3e182d7b
Lubos Slovak <lubos.slovak@nic.cz>
2010-11-30 17:54:57 +0100

    Zone structure and API.
    Very basic API for a zone structure. Now the zone consists of
      only a linked list of nodes, first node is the zone apex.
    
    Basic operations:
     - create zone
     - delete zone
     - add node to zone
     - find node in zone
    
    Will change according to needs and to use some smarter structures.
    
    refs #165.

---
+++ src/dnslib/zone.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <assert.h>
+
+#include "zone.h"
+#include "common.h"
+#include "node.h"
+#include "dname.h"
+
+/*----------------------------------------------------------------------------*/
+
+dnslib_zone_t *dnslib_zone_new(dnslib_node_t *apex)
+{
+	if (apex == NULL) {
+		return NULL;
+	}
+
+	dnslib_zone_t *zone = (dnslib_zone_t *)malloc(sizeof(dnslib_zone_t));
+	if (zone == NULL) {
+		ERR_ALLOC_FAILED;
+		return NULL;
+	}
+
+	zone->apex = apex;
+	return zone;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int dnslib_zone_add_node(dnslib_zone_t *zone, dnslib_node_t *node)
+{
+	if (zone == NULL || node == NULL) {
+		return -1;
+	}
+
+	// assert or just check??
+	assert(zone->apex != NULL);
+
+	dnslib_node_t *n = zone->apex;
+	while (n->next != NULL) {
+		n = n->next;
+	}
+	n->next = node;
+	node->next = NULL;
+
+	return 0;
+}
+
+/*----------------------------------------------------------------------------*/
+
+dnslib_node_t *dnslib_zone_get_node(dnslib_zone_t *zone,
+                                    const dnslib_dname_t *name)
+{
+	if (zone == NULL || name == NULL) {
+		return NULL;
+	}
+
+	dnslib_node_t *n = zone->apex;
+	while (n != NULL && dnslib_dname_compare(n->owner, name) != 0) {
+		n = n->next;
+	}
+	return n;
+}
+
+/*----------------------------------------------------------------------------*/
+
+const dnslib_node_t *dnslib_zone_find_node(dnslib_zone_t *zone,
+                                           const dnslib_dname_t *name)
+{
+	return dnslib_zone_get_node(zone, name);
+}
+
+/*----------------------------------------------------------------------------*/
+
+void dnslib_zone_free(dnslib_zone_t **zone, int free_nodes)
+{
+	if (zone == NULL || *zone == NULL) {
+		return;
+	}
+
+	if (free_nodes && (*zone)->apex != NULL) {
+		dnslib_node_t *n = (*zone)->apex;
+		(*zone)->apex = NULL;
+		dnslib_node_t *next = n->next;
+		while (next != NULL) {
+			dnslib_node_free(&n);
+			n = next;
+			next = n->next;
+		}
+	}
+
+	free(*zone);
+	*zone = NULL;
+}
