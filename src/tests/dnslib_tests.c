c4d7ee501841272cac2bbe6e853e681d66094830
Lubos Slovak <lubos.slovak@nic.cz>
2010-12-06 15:07:04 +0100

    Added tests for Zone API + minor changes.
    The tests are not tested yet :)
    
    Changed dname tests to count all results together.
    Changed dnslib tests to count all results together.
    
    refs #31, #174

--- src/tests/dnslib_tests.c
+++ src/tests/dnslib_tests.c
@@ -4,6 +4,7 @@
 #include "dnslib/dnslib_rdata_tests.c"
 #include "dnslib/dnslib_node_tests.c"
 #include "dnslib/dnslib_rrset_tests.c"
+#include "dnslib/dnslib_zone_tests.c"
 
 static int dnslib_tests_count(int argc, char *argv[]);
 static int dnslib_tests_run(int argc, char *argv[]);
@@ -27,22 +28,48 @@ static int dnslib_tests_count(int argc, char *argv[])
    return dnslib_dname_tests_count(argc, argv)
 		   + dnslib_rdata_tests_count(argc, argv)
 		   + dnslib_rrset_tests_count(argc, argv)
-		   + dnslib_node_tests_count(argc, argv);
+		   + dnslib_node_tests_count(argc, argv)
+		   + dnslib_zone_tests_count(argc, argv);
 }
 
 /*! Run all scheduled tests for given parameters.
  */
 static int dnslib_tests_run(int argc, char *argv[])
 {
-	int res = 0;
-	// dname tests
+	int res = 0,
+	    res_total = 0;
+
 	note("Testing module: dname");
-	res = dnslib_dname_tests_run(argc, argv);	
+	ok((res = dnslib_dname_tests_run(argc, argv)), "dnslib: Module dname");
+	res_total += res;
+
 	note("Testing module: rdata");
-	res += dnslib_rdata_tests_run(argc, argv);
-	note("Testing module: node");
-	res += dnslib_node_tests_run(argc, argv);
+	ok((res = dnslib_rdata_tests_run(argc, argv)), "dnslib: Module rdata");
+	res_total += res;
+
+	skip(!res, 3);
+
 	note("Testing module: rrset");
-	res += dnslib_rrset_tests_run(argc, argv);
-	return res;
+	ok((res = dnslib_rrset_tests_run(argc, argv)), "dnslib: Module rrset");
+	res_total += res;
+
+	skip(!res, 2);
+
+	note("Testing module: node");
+	ok((res = dnslib_node_tests_run(argc, argv)), "dnslib: Module node");
+	res_total += res;
+
+	skip(!res, 1);
+
+	note("Testing module: zone");
+	ok((res = dnslib_zone_tests_run(argc, argv)), "dnslib: Module zone");
+	res_total += res;
+
+	endskip; // NODE failed
+
+	endskip; // RRSET failed
+
+	endskip; // DNAME or RDATA failed
+
+	return res_total;
 }
