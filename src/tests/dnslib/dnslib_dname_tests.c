652d3cec2657e55a5da74f911dfc7cca8344ee50
Lubos Slovak <lubos.slovak@nic.cz>
2010-12-07 19:18:01 +0100

    Added dname tests.
    Tests inspired by (mostly taken from) branch zoneparser, but
      some changes were made (fixes and improvements).
    
    New tests for:
      - Creating non-FQDN domain names
      - Concatenating domain names
      - Chopping leftmost label
      - Checking if dname is a FQDN
      - Checking if one dname is subdomain of other
    
    Changed way of couting the return value of the test module.
    
    TODO: test for subdomains fails, but it seems that problem is
          in function dnslib_dname_new_from_str() in case of
          non-FQDN names.
    
    refs #173

--- src/tests/dnslib/dnslib_dname_tests.c
+++ src/tests/dnslib/dnslib_dname_tests.c
@@ -23,9 +23,11 @@ unit_api dnslib_dname_tests_api = {
  */
 
 // C will not accept const int in other const definition
-enum { TEST_DOMAINS_OK = 4 };
+enum { TEST_DOMAINS_OK = 6 };
 
 enum { TEST_DOMAINS_BAD = 2 };
+
+enum { TEST_DOMAINS_NON_FQDN = 6 };
 
 static dnslib_node_t *NODE_ADDRESS = (dnslib_node_t *)0xDEADBEEF;
 
@@ -35,13 +37,28 @@ struct test_domain {
 	uint size;
 };
 
+/*! \warning Do not change the order in those, if you want to test some other
+ *           feature with new dname, add it at the end of these arrays.
+ */
 static const struct test_domain
 		test_domains_ok[TEST_DOMAINS_OK] = {
 	{ "abc.test.domain.com.", "\3abc\4test\6domain\3com", 21 },
 	{ "some.test.domain.com.", "\4some\4test\6domain\3com", 22 },
 	{ "xyz.test.domain.com.", "\3xyz\4test\6domain\3com", 21 },
-	{ "some.test.domain.com.", "\4some\4test\6domain\3com", 22 }
+	{ "some.test.domain.com.", "\4some\4test\6domain\3com", 22 },
+	{ "test.domain.com.", "\4test\6domain\3com", 17 },
+	{ ".", "\0", 1 }
 };
+
+static const struct test_domain // sizes are strlen()s here
+	test_domains_non_fqdn[TEST_DOMAINS_NON_FQDN] = {
+		{"www", NULL, 3},
+		{"example", NULL, 7},
+		{"com", NULL, 3},
+		{"www.example.com", NULL, 15},
+		{"some", NULL, 4},
+		{"example.com", NULL, 11}
+	};
 
 static const struct test_domain
 		test_domains_bad[TEST_DOMAINS_BAD] = {
@@ -87,7 +104,8 @@ static int test_dname_delete()
 	return 0;
 }
 
-static int check_domain_name(const dnslib_dname_t *dname, int i)
+static int check_domain_name(const dnslib_dname_t *dname, int i,
+                             int check_node)
 {
 	int errors = 0;
 
@@ -112,12 +130,13 @@ static int check_domain_name(const dnslib_dname_t *dname, int i)
 		     size, test_domains_ok[i].wire);
 		++errors;
 	}
-	// check node
-	if (dnslib_dname_node(dname) != NODE_ADDRESS) {
-		diag("Node pointer in the created domain name is wrong: %p"
-		     " (should be %p)",
-		     dnslib_dname_node(dname), NODE_ADDRESS);
-		++errors;
+	if (check_node) {
+		if (dnslib_dname_node(dname) != NODE_ADDRESS) {
+			diag("Node pointer in the created domain name is wrong:"
+			     "%p (should be %p)",
+			     dnslib_dname_node(dname), NODE_ADDRESS);
+			++errors;
+		}
 	}
 
 	return errors;
@@ -135,11 +154,123 @@ static int test_dname_create_from_str()
 	dnslib_dname_t *dname = NULL;
 
 	for (int i = 0; i < TEST_DOMAINS_OK && errors == 0; ++i) {
+		//note("testing domain: %s", test_domains_ok[i].str);
 		dname = dnslib_dname_new_from_str(test_domains_ok[i].str,
 		          strlen(test_domains_ok[i].str), NODE_ADDRESS);
-		errors += check_domain_name(dname, i);
+		errors += check_domain_name(dname, i, 1);
 		dnslib_dname_free(&dname);
 	}
+
+	return (errors == 0);
+}
+
+static int check_non_fqdn(dnslib_dname_t *dname, int i)
+{
+	int errors = 0;
+	if (dname->size != test_domains_non_fqdn[i].size + 1) {
+		diag("size of created name is wrong: should be %d is %d",
+		     test_domains_non_fqdn[i].size + 1 , dname->size);
+		errors++;
+	}
+
+	char *tmp = dnslib_dname_to_str(dname);
+
+	if (strcmp(test_domains_non_fqdn[i].str, tmp) != 0) {
+		diag("created name is wrong: should be: %s is %s",
+		     test_domains_non_fqdn[i].str, tmp);
+		errors++;
+	}
+
+	free(tmp);
+
+	return errors;
+}
+
+static int test_dname_create_from_str_non_fqdn()
+{
+	int errors = 0;
+	dnslib_dname_t *dname = NULL;
+
+	for (int i = 0; i < TEST_DOMAINS_NON_FQDN; ++i) {
+		//note("testing domain: %s", test_domains_non_fqdn[i].str);
+		dname = dnslib_dname_new_from_str(test_domains_non_fqdn[i].str,
+		          strlen(test_domains_non_fqdn[i].str), NULL);
+		errors += check_non_fqdn(dname, i);
+		dnslib_dname_free(&dname);
+	}
+
+	return (errors == 0);
+}
+
+static int test_dname_cat()
+{
+	int errors = 0;
+
+	dnslib_dname_t *d1, *d2, *d3;
+
+	d1 = dnslib_dname_new_from_str(test_domains_non_fqdn[0].str,
+	                            strlen(test_domains_non_fqdn[0].str), NULL);
+	d2 = dnslib_dname_new_from_str(test_domains_non_fqdn[1].str,
+	                            strlen(test_domains_non_fqdn[1].str), NULL);
+	d3 = dnslib_dname_new_from_str(test_domains_non_fqdn[2].str,
+	                            strlen(test_domains_non_fqdn[2].str), NULL);
+
+	dnslib_dname_cat(d1, d2);
+	dnslib_dname_cat(d1, d3);
+
+	errors += check_non_fqdn(d1, 3);
+
+	dnslib_dname_free(&d1);
+	dnslib_dname_free(&d2);
+	dnslib_dname_free(&d3);
+
+	d1 = dnslib_dname_new_from_str(test_domains_non_fqdn[4].str,
+	                               strlen(test_domains_non_fqdn[4].str),
+	                               NODE_ADDRESS);
+
+	d2 = dnslib_dname_new_from_str(test_domains_ok[4].str,
+	                               strlen(test_domains_ok[4].str),
+	                               NODE_ADDRESS);
+
+	dnslib_dname_cat(d1, d2);
+
+	errors += check_domain_name(d1, 1, 1);
+
+	dnslib_dname_free(&d1);
+	dnslib_dname_free(&d2);
+
+	return (errors == 0);
+}
+
+static int test_dname_left_chop()
+{
+	int errors = 0;
+
+	dnslib_dname_t *d1;
+
+	d1 = dnslib_dname_new_from_str(test_domains_ok[1].str,
+	                               strlen(test_domains_ok[1].str),
+	                               NODE_ADDRESS);
+
+	dnslib_dname_t *chopped;
+
+	chopped = dnslib_dname_left_chop(d1);
+
+	errors += check_domain_name(chopped, 4, 0);
+
+	dnslib_dname_free(&d1);
+	dnslib_dname_free(&chopped);
+
+	d1 = dnslib_dname_new_from_str(test_domains_non_fqdn[3].str,
+	                               strlen(test_domains_non_fqdn[3].str),
+	                               NODE_ADDRESS);
+
+	chopped = dnslib_dname_left_chop(d1);
+
+	errors += check_non_fqdn(chopped, 5);
+
+	dnslib_dname_free(&d1);
+	dnslib_dname_free(&chopped);
 
 	return (errors == 0);
 }
@@ -161,7 +292,7 @@ static int test_dname_create_from_wire()
 		dname = dnslib_dname_new_from_wire(
 		            (uint8_t *)test_domains_ok[i].wire,
 		            test_domains_ok[i].size, NODE_ADDRESS);
-		errors += check_domain_name(dname, i);
+		errors += check_domain_name(dname, i, 1);
 		dnslib_dname_free(&dname);
 	}
 
@@ -259,7 +390,137 @@ static int test_dname_compare()
 	return (errors == 0);
 }
 
-static const int DNSLIB_DNAME_TEST_COUNT = 7;
+static int test_dname_is_fqdn()
+{
+	int errors = 0;
+
+	dnslib_dname_t *dname;
+
+	for (int i = 0; i < TEST_DOMAINS_OK && !errors; ++i) {
+		dname = dnslib_dname_new_from_wire(
+		                (uint8_t *)test_domains_ok[i].wire,
+		                test_domains_ok[i].size, NODE_ADDRESS);
+		errors += !dnslib_dname_is_fqdn(dname);
+		dnslib_dname_free(&dname);
+	}
+
+	for (int i = 0; i < TEST_DOMAINS_NON_FQDN && !errors; ++i) {
+		dname = dnslib_dname_new_from_str(test_domains_non_fqdn[i].str,
+		          strlen(test_domains_non_fqdn[i].str), NULL);
+		errors += dnslib_dname_is_fqdn(dname);
+		dnslib_dname_free(&dname);
+	}
+
+	return (errors == 0);
+}
+
+static int test_dname_is_subdomain()
+{
+	int errors = 0;
+
+	dnslib_dname_t *dnames_fqdn[TEST_DOMAINS_OK];
+	dnslib_dname_t *dnames_non_fqdn[TEST_DOMAINS_NON_FQDN];
+
+	for (int i = 0; i < TEST_DOMAINS_OK; ++i) {
+		dnames_fqdn[i] = dnslib_dname_new_from_wire(
+		                (uint8_t *)test_domains_ok[i].wire,
+		                test_domains_ok[i].size, NULL);
+		assert(dnames_fqdn[i] != NULL);
+	}
+
+	for (int i = 0; i < TEST_DOMAINS_NON_FQDN; ++i) {
+		dnames_non_fqdn[i] = dnslib_dname_new_from_str(
+		                test_domains_non_fqdn[i].str,
+		                test_domains_non_fqdn[i].size, NULL);
+		assert(dnames_non_fqdn[i] != NULL);
+	}
+
+	// fqdn names 0 - 3 should be subdomains of name 4
+	note("Subdomains 1");
+	dnslib_dname_t *parent = dnames_fqdn[4];
+	for (int i = 0; i < 3; ++i) {
+		if (!dnslib_dname_is_subdomain(dnames_fqdn[i], parent)) {
+			diag("Name %s was not considered subdomain of %s.",
+			     dnslib_dname_name(dnames_fqdn[i]),
+			     dnslib_dname_name(parent));
+			++errors;
+		}
+	}
+
+	note("Subdomains 2");
+	// fqdn names 0 - 4 should be subdomains of name 5 (root)
+	parent = dnames_fqdn[5];
+	for (int i = 0; i < 4; ++i) {
+		if (!dnslib_dname_is_subdomain(dnames_fqdn[i], parent)) {
+			diag("Name %s was not considered subdomain of %s.",
+			     dnslib_dname_name(dnames_fqdn[i]),
+			     dnslib_dname_name(parent));
+			++errors;
+		}
+	}
+
+	note("Subdomains 3");
+	// non-fqdn names 3 and 5 should be subdomains of non-fqdn name 2
+	parent = dnames_non_fqdn[2];
+	if (!dnslib_dname_is_subdomain(dnames_non_fqdn[3], parent)) {
+		diag("Name %s was not considered subdomain of %s.",
+		     dnslib_dname_name(dnames_non_fqdn[3]),
+		     dnslib_dname_name(parent));
+		++errors;
+	}
+	if (!dnslib_dname_is_subdomain(dnames_non_fqdn[5], parent)) {
+		diag("Name %s was not considered subdomain of %s.",
+		     dnslib_dname_name(dnames_non_fqdn[5]),
+		     dnslib_dname_name(parent));
+		++errors;
+	}
+
+	note("Subdomains 4");
+	// non-fqdn name 5 should be subdomain of non-fqdn name 3
+	parent = dnames_non_fqdn[3];
+	if (!dnslib_dname_is_subdomain(dnames_non_fqdn[5], parent)) {
+		diag("Name %s was not considered subdomain of %s.",
+		     dnslib_dname_name(dnames_non_fqdn[5]),
+		     dnslib_dname_name(parent));
+		++errors;
+	}
+
+	note("Subdomains 5");
+	// identical names should not be considered subdomains
+	if (dnslib_dname_is_subdomain(dnames_fqdn[0], dnames_fqdn[0])) {
+		diag("Name %s was considered subdomain of itself.",
+		     dnslib_dname_name(dnames_fqdn[0]));
+		++errors;
+	}
+	if (dnslib_dname_is_subdomain(dnames_fqdn[1], dnames_fqdn[3])) {
+		diag("Name %s was considered subdomain of %s.",
+		     dnslib_dname_name(dnames_fqdn[1]),
+		     dnslib_dname_name(dnames_fqdn[3]));
+		++errors;
+	}
+
+	note("Subdomains 6");
+	// fqdn name should not be considered subdomain of non-fqdn name
+	if (dnslib_dname_is_subdomain(dnames_fqdn[1], dnames_non_fqdn[2])) {
+		diag("Name %s was considered subdomain of %s.",
+		     dnslib_dname_name(dnames_fqdn[1]),
+		     dnslib_dname_name(dnames_non_fqdn[2]));
+		++errors;
+	}
+
+	note("Subdomains 7");
+	// parent name should not be considered subdomain of its subdomain
+	if (dnslib_dname_is_subdomain(dnames_fqdn[4], dnames_fqdn[0])) {
+		diag("Name %s was considered subdomain of %s.",
+		     dnslib_dname_name(dnames_fqdn[4]),
+		     dnslib_dname_name(dnames_fqdn[0]));
+		++errors;
+	}
+
+	return (errors == 0);
+}
+
+static const int DNSLIB_DNAME_TEST_COUNT = 12;
 
 /*! This helper routine should report number of
  *  scheduled tests for given parameters.
@@ -276,40 +537,56 @@ static int dnslib_dname_tests_run(int argc, char *argv[])
 	int res = 0,
 	    res_str = 0,
 	    res_wire = 0,
-	    res_total = 0;
+	    res_str_non_fqdn = 0,
+	    res_final = 1;
 
 	res = test_dname_create();
 	ok(res, "dname: create empty");
-	res_total += res;
+	res_final *= res;
 
-	skip(!res, 5);
+	skip(!res, 9);
 
 	todo();
 
 	ok((res = test_dname_delete()), "dname: delete");
-	res_total += res;
+	//res_final *= res;
 
 	endtodo;
 
 	ok((res_str = test_dname_create_from_str()), "dname: create from str");
 	ok((res_wire = test_dname_create_from_wire()),
 	   "dname: create from wire");
-	res_total += res_str;
-	res_total += res_wire;
+	ok((res_str_non_fqdn = test_dname_create_from_str_non_fqdn()),
+	   "dname: create from str non fqdn");
+	res_final *= res_str;
+	res_final *= res_wire;
+	res_final *= res_str_non_fqdn;
 
-	skip(!res_str || !res_wire, 1);
+	skip(!res_str || !res_wire || !res_str_non_fqdn, 2);
 
 	ok((res = test_dname_to_str()), "dname: convert to str");
-	res_total += res;
+	res_final *= res;
 
 	lives_ok(test_faulty_data(); , "dname: faulty data test");
 
-	endskip;	/* !res_str || !res_wire */
+	endskip;  /* !res_str || !res_wire */
 
 	ok((res = test_dname_compare()), "dname: compare");
-	res_total += res;
+	res_final *= res;
 
-	endskip;	/* create failed */
+	ok((res = test_dname_cat()), "dname: cat");
+	res_final *= res;
 
-	return res_total;
+	ok((res = test_dname_is_fqdn()), "dname: fqdn");
+	res_final *= res;
+
+	ok((res = test_dname_left_chop()), "dname: left chop");
+	res_final *= res;
+
+	ok((res = test_dname_is_subdomain()), "dname: is subdomain");
+	res_final *= res;
+
+	endskip;  /* create failed */
+
+	return res_final;
 }
