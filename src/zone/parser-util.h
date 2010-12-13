db3f58f5f00b6813f4e7e1c723822f6c82043c08
Jan Kadlec <jan@hp4-jankadlec.(none)>
2010-12-13 17:22:34 +0100

    More reformatting. Refs #204

--- src/zone/parser-util.h
+++ src/zone/parser-util.h
@@ -22,19 +22,21 @@ size_t strlcpy(char *dst, const char *src, size_t siz);
 int inet_pton4(src, dst);
 int inet_pton6(src, dst);
 int my_b32_pton(const char *src, uint8_t *target, size_t tsize);
-const char * inet_ntop(int af, const void *src, char *dst, size_t size);
-const char * inet_ntop4(const u_char *src, char *dst, size_t size);
-const char * inet_ntop6(const u_char *src, char *dst, size_t size);
+const char *inet_ntop(int af, const void *src, char *dst, size_t size);
+const char *inet_ntop4(const u_char *src, char *dst, size_t size);
+const char *inet_ntop6(const u_char *src, char *dst, size_t size);
 int inet_pton(af, src, dst);
 void b64_initialize_rmap();
 int b64_pton_do(char const *src, uint8_t *target, size_t targsize);
 int b64_pton_len(char const *src);
 int b64_pton(char const *src, uint8_t *target, size_t targsize);
 void set_bit(uint8_t bits[], size_t index);
-uint32_t strtoserial(const char* nptr, const char** endptr);
+uint32_t strtoserial(const char *nptr, const char **endptr);
 void write_uint32(void *dst, uint32_t data);
 uint32_t strtottl(const char *nptr, const char **endptr);
-dnslib_lookup_table_t *dnslib_lookup_by_name(dnslib_lookup_table_t *table, const char *name);
-dnslib_lookup_table_t *dnslib_lookup_by_id(dnslib_lookup_table_t *table, int id);
+dnslib_lookup_table_t *dnslib_lookup_by_name(dnslib_lookup_table_t *table,
+                                             const char *name);
+dnslib_lookup_table_t *dnslib_lookup_by_id(dnslib_lookup_table_t *table,
+                                           int id);
 
 #endif /* _CUTEDNS_PARSER_UTIL_H_ */
