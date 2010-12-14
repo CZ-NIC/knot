c45b524cf0acd221b3e1350b0fa2cf2dbbfad9c1
Lubos Slovak <lubos.slovak@nic.cz>
2010-12-14 18:21:17 +0100

    Added files for response and packet.
    refs #143

---
+++ src/dnslib/response.h
@@ -0,0 +1,22 @@
+/*!
+ * \file response.h
+ *
+ * \author Lubos Slovak <lubos.slovak@nic.cz>
+ *
+ * \brief Structure for holding response data and metadata.
+ *
+ * This structure can be used to pass all data needed for response creation
+ * inside and between various processing functions of the application.
+ *
+ * \addtogroup dnslib
+ * @{
+ */
+
+#ifndef _CUTEDNS_DNSLIB_RESPONSE_H_
+#define _CUTEDNS_DNSLIB_RESPONSE_H_
+
+
+
+#endif /* _CUTEDNS_DNSLIB_RESPONSE_H_ */
+
+/*! @} */
