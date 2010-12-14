9e25b85d7af68242fbe2dc3c393ba2508f50b61c
Lubos Slovak <lubos.slovak@nic.cz>
2010-12-14 18:21:17 +0100

    Added files for response and packet.
    refs #143

---
+++ src/dnslib/packet.h
@@ -0,0 +1,19 @@
+/*!
+ * \file packet.h
+ *
+ * \author Lubos Slovak <lubos.slovak@nic.cz>
+ *
+ * \brief Functions for manipulating and parsing raw data in DNS packets.
+ *
+ * \addtogroup dnslib
+ * @{
+ */
+
+#ifndef _CUTEDNS_DNSLIB_PACKET_H_
+#define _CUTEDNS_DNSLIB_PACKET_H_
+
+
+
+#endif /* _CUTEDNS_DNSLIB_PACKET_H_ */
+
+/*! @} */
