029d5ca65895c877ef4a7009c282d336aeb62a68
Lubos Slovak <lubos.slovak@nic.cz>
2010-12-15 15:31:26 +0100

    Major changes to response structure + first API.
    - Added structure for DNS header.
    - Removed response wire format pointer and size from structure.
    - Modified way of storing Answer, Authority and Additional
      sections.
    - Outlined some basic API.
    
    refs #143

--- src/dnslib/response.h
+++ src/dnslib/response.h
@@ -20,6 +20,7 @@
 #include "dname.h"
 #include "rrset.h"
 
+/*----------------------------------------------------------------------------*/
 /*!
  * MTU <= 4500B
  * IP header has 24 B
@@ -37,6 +38,7 @@
  */
 static const size_t MAX_RRS_IN_RESPONSE = 405;
 
+/*----------------------------------------------------------------------------*/
 /*!
  * \todo NSID
  */
@@ -48,6 +50,7 @@ struct dnslib_edns_data {
 
 typedef struct dnslib_edns_data dnslib_edns_data_t;
 
+/*----------------------------------------------------------------------------*/
 /*!
  *
  */
@@ -59,14 +62,27 @@ struct dnslib_compressed_dnames {
 
 typedef struct dnslib_compressed_dnames dnslib_compressed_dnames_t;
 
+/*----------------------------------------------------------------------------*/
 /*!
- * \note Current size of the structure:
- *         32bit machine: 18 + 405 x 3 x 4 = 4878 B
- *         64bit machine: 34 + 405 x 3 x 8 = 9754 B
  *
- * \todo Consider dynamically resizing the arrays for RRSets. With well picked
- *       default sizes we could avoid much reallocations (or any at all) and
- *       keep the space reasonably small.
+ */
+struct dnslib_header {
+	uint8_t id[2];    /*!< ID stored in network byte order. */
+	uint8_t flags1;   /*!< First octet of header flags. */
+	uint8_t flags2;   /*!< Second octet of header flags. */
+	uint16_t qdcount; /*!< Number of Question RRs, in host byte order. */
+	uint16_t ancount; /*!< Number of Answer RRs, in host byte order. */
+	uint16_t nscount; /*!< Number of Authority RRs, in host byte order. */
+	uint16_t arcount; /*!< Number of Additional RRs, in host byte order. */
+};
+
+typedef struct dnslib_header dnslib_header_t;
+
+/*----------------------------------------------------------------------------*/
+/*!
+ * \note QNAME, Answer, Authority and Additonal sections are by default put to
+ *       preallocated space after the structure with default sizes. If the
+ *       space is not enough, more space is allocated dynamically.
  */
 struct dnslib_response {
 	/*!
@@ -76,25 +92,66 @@ struct dnslib_response {
 	 */
 	dnslib_dname_t *qname;
 	uint16_t qtype;
+	uint16_t qclass;
 
-	size_t max_size;  /*!< Maximum allowed size of the response. */
+	short max_size;  /*!< Maximum allowed size of the response. */
 
-	uint8_t *resp_wire;
-	size_t resp_size;
+	dnslib_rrset_t *answer;
+	dnslib_rrset_t *authority;
+	dnslib_rrset_t *additional;
 
-	/*! \todo Consider using one array. It would save space but cause
-	 *        more difficult converting to wire format.
+	short max_ancount; /*!< Allocated space for Answer RRsets. */
+	short max_nscount; /*!< Allocated space for Authority RRsets. */
+	short max_arcount; /*!< Allocated space for Additional RRsets. */
+
+	dnslib_header_t header;
+
+	/*!
+	 * \brief EDNS data parsed from query.
+	 *
+	 * \todo Do we need this actually??
 	 */
-	dnslib_rrset_t *answer[MAX_RRS_IN_RESPONSE];
-	dnslib_rrset_t *authority[MAX_RRS_IN_RESPONSE];
-	dnslib_rrset_t *additional[MAX_RRS_IN_RESPONSE];
-
-	dnslib_edns_data_t edns;
+	dnslib_edns_data_t edns_query;
+	const uint8_t *edns_wire;
+	short edns_size;
 
 	dnslib_compressed_dnames_t compression;
+
+	dnslib_dname_t *tmp_dnames; /*!< Synthetized domain names. */
+	short tmp_dname_count;      /*!< Count of synthetized domain names. */
 };
 
 typedef struct dnslib_response dnslib_response_t;
+
+/*----------------------------------------------------------------------------*/
+
+dnslib_response_t *dnslib_response_new_empty(const uint8_t *edns_wire,
+                                             short edns_size);
+
+dnslib_response_t *dnslib_response_new_from_query(const uint8_t *query_wire,
+                                                  size_t size,
+                                                  const uint8_t *edns_wire,
+                                                  short edns_size);
+
+/*!
+ * \param tc Set to <> 0 if omitting this RRSet should result in the TC bit set.
+ *           Otherwise set to 0.
+ */
+int dnslib_response_add_rrset_answer(dnslib_response_t *response,
+                                     const dnslib_rrset_t *rrset, int tc);
+
+int dnslib_response_add_rrset_authority(dnslib_response_t *response,
+                                        const dnslib_rrset_t *rrset, int tc);
+
+int dnslib_response_add_rrset_aditional(dnslib_response_t *response,
+                                        const dnslib_rrset_t *rrset, int tc);
+
+/*
+ * TODO: some functions for setting RCODE and flags!
+ */
+
+int dnslib_response_to_wire(dnslib_response_t *response,
+                            uint8_t **resp_wire, size_t *resp_size);
 
 #endif /* _CUTEDNS_DNSLIB_RESPONSE_H_ */
 
