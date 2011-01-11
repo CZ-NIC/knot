9b9ae119745cc40c809a904c3c0303ebb823f657
Lubos Slovak <lubos.slovak@nic.cz>
2011-01-11 18:10:27 +0100

    Added missing files.

---
+++ src/dnslib/debug.h
@@ -0,0 +1,23 @@
+/*
+ * File:     debug.h
+ * Date:     02.12.2010 13:45
+ * Author:   jan
+ * Project:  
+ * Description:   
+ */
+
+
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#include <stdint.h>
+#include "dnslib.h"
+
+void dnslib_rdata_dump(dnslib_rdata_t *rdata, uint32_t type);
+void dnslib_rrset_dump(dnslib_rrset_t *rrset);
+void dnslib_node_dump(dnslib_node_t *node, void *void_param);
+void dnslib_zone_dump(dnslib_zone_t *zone);
+
+#endif
+
+/* end of file debug.h */
---
+++ src/dnslib/dnslib.h
@@ -0,0 +1,28 @@
+/*
+ * File:     dnslib.h
+ * Date:     02.12.2010 12:37
+ * Author:   jan
+ * Project:  
+ * Description:   
+ */
+
+
+#ifndef __DNSLIB_H__
+#define __DNSLIB_H__
+
+#include "consts.h"
+#include "descriptor.h"
+#include "dname.h"
+#include "edns.h"
+#include "node.h"
+#include "packet.h"
+#include "rdata.h"
+#include "response.h"
+#include "rrset.h"
+#include "rrsig.h"
+#include "zone.h"
+#include "zonedb.h"
+
+#endif
+
+/* end of file dnslib.h */
