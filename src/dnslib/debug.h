fded24eb3b8dafe1f82a2d1907fc13706f56c07c
Lubos Slovak <lubos.slovak@nic.cz>
2011-01-12 14:42:28 +0100

    Fixed optional dump functions (again)

--- src/dnslib/debug.h
+++ src/dnslib/debug.h
@@ -14,29 +14,13 @@
 #include <stdint.h>
 #include "dnslib.h"
 
-#if defined(DNSLIB_ZONE_DEBUG) || defined(DNSLIB_RDATA_DEBUG)
 void dnslib_rdata_dump(dnslib_rdata_t *rdata, uint32_t type);
-#else
-inline void dnslib_rdata_dump(dnslib_rdata_t *rdata, uint32_t type) {};
-#endif
 
-#if defined(DNSLIB_ZONE_DEBUG) || defined(DNSLIB_RRSET_DEBUG)
 void dnslib_rrset_dump(dnslib_rrset_t *rrset);
-#else
-inline void dnslib_rrset_dump(dnslib_rrset_t *rrset) {};
-#endif
 
-#if defined(DNSLIB_ZONE_DEBUG) || defined(DNSLIB_NODE_DEBUG)
 void dnslib_node_dump(dnslib_node_t *node, void *void_param);
-#else
-inline void dnslib_node_dump(dnslib_node_t *node, void *void_param) {};
-#endif
 
-#if defined(DNSLIB_ZONE_DEBUG)
 void dnslib_zone_dump(dnslib_zone_t *zone);
-#else
-inline void dnslib_zone_dump(dnslib_zone_t *zone) {};
-#endif
 
 #endif
 
