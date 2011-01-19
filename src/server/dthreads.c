6e62b308e810ca3b9ab0e5876f11ac2f5daac83c
Marek Vavrusa <marek@vavrusa.com>
2011-01-19 19:12:44 +0100

    Increased thread count hint for smoother performance.

--- src/server/dthreads.c
+++ src/server/dthreads.c
@@ -896,7 +896,7 @@ int dt_optimal_size()
 #ifdef _SC_NPROCESSORS_ONLN
 	int ret = (int) sysconf(_SC_NPROCESSORS_ONLN);
 	if (ret >= 1) {
-		return ret + 1;
+		return ret + 2;
 	}
 #endif
 	log_info("server: failed to estimate the number of online CPUs");
