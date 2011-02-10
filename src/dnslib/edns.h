e42319875a9fab157d35e3d9f1d18308e37e33ec
Lubos Slovak <lubos.slovak@nic.cz>
2011-02-10 17:28:02 +0100

    EDNS - fixes & new API
    - Fixed representation of extended RCODE and version (are 1-byte
      integers).
    - Added new API functions:
      - new_from_wire() (moved implementation from response)
      - get_flags()
      - do() and set_do()
    
    refs #458, #459

--- src/dnslib/edns.h
+++ src/dnslib/edns.h
@@ -24,15 +24,16 @@
  */
 struct dnslib_opt_rr {
 	uint16_t payload;    /*!< UDP payload. */
-	uint16_t ext_rcode;  /*!< Extended RCODE. */
+	uint8_t ext_rcode;  /*!< Extended RCODE. */
 
 	/*!
 	 * \brief Supported version of EDNS.
 	 *
 	 * Set to EDNS_NOT_SUPPORTED if not supported.
 	 */
-	uint16_t version;
+	uint8_t version;
 
+	uint16_t flags; /*!< EDNS flags. */
 	short size;
 };
 
@@ -46,13 +47,16 @@ enum dnslib_edns_offsets {
 	DNSLIB_EDNS_OFFSET_RDATA = 11
 };
 
-static const uint16_t EDNS_NOT_SUPPORTED = 65535;
+static const uint8_t EDNS_NOT_SUPPORTED = 255;
 
 static const short DNSLIB_EDNS_MIN_SIZE = DNSLIB_EDNS_OFFSET_RDATA;
 
 /*----------------------------------------------------------------------------*/
 
 dnslib_opt_rr_t *dnslib_edns_new();
+
+int dnslib_edns_new_from_wire(dnslib_opt_rr_t *opt_rr, const uint8_t *wire,
+                              size_t max_size);
 
 uint16_t dnslib_edns_get_payload(const dnslib_opt_rr_t *opt_rr);
 
@@ -65,6 +69,12 @@ void dnslib_edns_set_ext_rcode(dnslib_opt_rr_t *opt_rr, uint8_t ext_rcode);
 uint8_t dnslib_edns_get_version(const dnslib_opt_rr_t *opt_rr);
 
 void dnslib_edns_set_version(dnslib_opt_rr_t *opt_rr, uint8_t version);
+
+uint16_t dnslib_edns_get_flags(const dnslib_opt_rr_t *opt_rr);
+
+int dnslib_edns_do(const dnslib_opt_rr_t *opt_rr);
+
+void dnslib_edns_set_do(dnslib_opt_rr_t *opt_rr);
 
 short dnslib_edns_to_wire(const dnslib_opt_rr_t *opt_rr, uint8_t *wire,
                          short max_size);
