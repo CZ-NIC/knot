2e0a98301e6c1ee84d01ba6b08ba599b7730e36e
Lubos Slovak <lubos.slovak@nic.cz>
2011-02-10 17:28:02 +0100

    EDNS - fixes & new API
    - Fixed representation of extended RCODE and version (are 1-byte
      integers).
    - Added new API functions:
      - new_from_wire() (moved implementation from response)
      - get_flags()
      - do() and set_do()
    
    refs #458, #459

--- src/dnslib/edns.c
+++ src/dnslib/edns.c
@@ -6,6 +6,10 @@
 #include "common.h"
 #include "descriptor.h"
 
+enum dnslib_edns_consts {
+	DNSLIB_EDNS_DO_MASK = (uint16_t)0x8000
+};
+
 /*----------------------------------------------------------------------------*/
 
 dnslib_opt_rr_t *dnslib_edns_new()
@@ -16,6 +20,64 @@ dnslib_opt_rr_t *dnslib_edns_new()
 	opt_rr->size = DNSLIB_EDNS_MIN_SIZE;
 
 	return opt_rr;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int dnslib_edns_new_from_wire(dnslib_opt_rr_t *opt_rr, const uint8_t *wire,
+                              size_t max_size)
+{
+	const uint8_t *pos = wire;
+	int parsed = 0;
+
+	if (pos == NULL || max_size == 0 || opt_rr == NULL) {
+		return -1;
+	}
+
+	if (max_size < DNSLIB_EDNS_MIN_SIZE) {
+		debug_dnslib_edns("Not enough data to parse ENDS.\n");
+		return -2;
+	}
+
+	// owner of EDNS OPT RR must be root (0)
+	if (*pos != 0) {
+		debug_dnslib_edns("EDNS packet malformed (expected root "
+		                  "domain as owner).\n");
+		return -3;
+	}
+	pos += 1;
+
+	// check the type of the record (must be OPT)
+	if (dnslib_wire_read_u16(pos) != DNSLIB_RRTYPE_OPT) {
+		debug_dnslib_edns("EDNS packet malformed (expected OPT type"
+		                  ".\n");
+		return -2;
+	}
+	pos += 2;
+
+	opt_rr->payload = dnslib_wire_read_u16(pos);
+	debug_dnslib_edns("Parsed payload: %u\n", opt_rr->payload);
+
+	pos += 2;
+	opt_rr->ext_rcode = *(pos++);
+	opt_rr->version = *(pos++);
+	opt_rr->flags = dnslib_wire_read_u16(pos);
+	pos += 2;
+
+	parsed = DNSLIB_EDNS_MIN_SIZE;
+
+	// ignore RDATA, but move pos behind them
+	uint16_t rdlength = dnslib_wire_read_u16(pos);
+
+	if (max_size - parsed < rdlength) {
+		debug_dnslib_response("Not enough data to parse ENDS.\n");
+		return -3;
+	}
+
+	pos += 2 + rdlength;
+	parsed += rdlength;
+
+	return parsed;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -65,6 +127,28 @@ void dnslib_edns_set_version(dnslib_opt_rr_t *opt_rr,
 
 /*----------------------------------------------------------------------------*/
 
+uint16_t dnslib_edns_get_flags(const dnslib_opt_rr_t *opt_rr)
+{
+	return opt_rr->flags;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int dnslib_edns_do(const dnslib_opt_rr_t *opt_rr)
+{
+	debug_dnslib_edns("Flags: %u\n", opt_rr->flags);
+	return (opt_rr->flags & DNSLIB_EDNS_DO_MASK);
+}
+
+/*----------------------------------------------------------------------------*/
+
+void dnslib_edns_set_do(dnslib_opt_rr_t *opt_rr)
+{
+	opt_rr->flags |= DNSLIB_EDNS_DO_MASK;
+}
+
+/*----------------------------------------------------------------------------*/
+
 short dnslib_edns_to_wire(const dnslib_opt_rr_t *opt_rr, uint8_t *wire,
                           short max_size)
 {
@@ -81,7 +165,7 @@ short dnslib_edns_to_wire(const dnslib_opt_rr_t *opt_rr, uint8_t *wire,
 	pos += 2;
 	*(pos++) = opt_rr->ext_rcode;
 	*(pos++) = opt_rr->version;
-	dnslib_wire_write_u16(pos, 0);
+	dnslib_wire_write_u16(pos, opt_rr->flags);
 	pos += 2;
 	dnslib_wire_write_u16(pos, 0);
 
