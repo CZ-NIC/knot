b31694bf1bd9fc4323fd85cf451db9e568be9a98
Marek Vavrusa <marek@vavrusa.com>
2011-02-14 17:55:45 +0100

    Designing new log system APIs.
    refs #463, #464

--- src/conf/logconf.c
+++ src/conf/logconf.c
@@ -9,50 +9,30 @@
 #include "conf.h"
 #include "lists.h"
 
-/* Extern symbols from log subsystem.
- * This is required to silently extend log.c facilities.
- */
-extern size_t LOG_LUT_SIZE;
-extern uint8_t *LOG_LUT;
-
 int log_load_conf()
 {
-	// Initialize LUT
+	// Initialize logsystem
 	const config_t *conf = config_get();
-	uint8_t *lp = 0;
-	LOG_LUT_SIZE = conf->logs_count << 3;
-	LOG_LUT = malloc(LOG_LUT_SIZE);
-	memset(LOG_LUT, 0, LOG_LUT_SIZE);
+	log_setup(conf->logs_count);
 
 	// Setup logs
-	int fileno = 0;
 	node *n = 0;
 	WALK_LIST(n, conf->logs) {
 
-		conf_log_t* log = (conf_log_t*)n;
-
 		// Calculate offset
-		int offset = log->type;
-		if (offset == LOGT_FILE) {
-			offset += fileno++;
+		conf_log_t* log = (conf_log_t*)n;
+		int facility = log->type;
+		if (facility == LOGT_FILE) {
+			facility = log_open_file(log->file);
 		}
-		offset <<= 3;
-		lp = LOG_LUT + offset;
 
 		// Setup sources mapping
 		node *m = 0;
 		WALK_LIST(m, log->map) {
-			conf_log_map_t *map = (conf_log_map_t*)m;
 
-			// Assign level if not multimask
-			if (map->source != LOG_ANY) {
-				*(lp + map->source) |= map->levels;
-			} else {
-				// Any == add to all sources
-				for (int i = 0; i < LOG_ANY; ++i) {
-					*(lp + i) |= map->levels;
-				}
-			}
+			// Assign mapped level
+			conf_log_map_t *map = (conf_log_map_t*)m;
+			log_levels_add(facility, map->source, map->levels);
 		}
 	}
 
