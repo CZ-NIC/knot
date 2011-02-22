65d62cf2c2b4e19b35ed546f0f9790fe7f49195e
Lubos Slovak <lubos.slovak@nic.cz>
2011-02-22 16:02:10 +0100

    Added nsec3params structure + nsec3 hash function
    - Parsing NSEC3 parameters from NSEC3PARAM RR.
    - SHA-1 hashing function.
    
    refs #425

---
+++ src/dnslib/nsec3.c
@@ -0,0 +1,129 @@
+#include <stdint.h>
+#include <assert.h>
+#include <stdlib.h>
+
+#include <gnutls/gnutls.h>
+#include <gnutls/crypto.h>
+
+#include "nsec3.h"
+#include "common.h"
+#include "descriptor.h"
+#include "utils.h"
+
+/*----------------------------------------------------------------------------*/
+
+int dnslib_nsec3_params_from_wire(dnslib_nsec3_params_t *params,
+                                  const dnslib_rrset_t *nsec3param)
+{
+	assert(dnslib_rrset_type(nsec3param) == DNSLIB_RRTYPE_NSEC3PARAM);
+	const dnslib_rdata_t *rdata = dnslib_rrset_rdata(nsec3param);
+
+	assert(rdata->count == 4);
+
+	params->algorithm = *dnslib_rdata_item(rdata, 0)->raw_data;
+	params->flags = *dnslib_rdata_item(rdata, 1)->raw_data;
+	params->iterations = dnslib_wire_read_u16((uint8_t *)
+	                          dnslib_rdata_item(rdata, 2)->raw_data);
+	params->salt_length = *dnslib_rdata_item(rdata, 3)->raw_data;
+	params->salt = (uint8_t *)malloc(params->salt_length);
+
+	CHECK_ALLOC_LOG(params->salt, -1);
+
+	memcpy(params->salt, dnslib_rdata_item(rdata, 3)->raw_data + 1,
+	       params->salt_length);
+
+	return 0;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int dnslib_nsec3_sha1(const dnslib_nsec3_params_t *params, const uint8_t *data,
+                      size_t size, uint8_t **digest, size_t *digest_size)
+{
+	assert(digest != NULL);
+	assert(digest_size != NULL);
+
+	if (data == NULL) {
+		return -3;
+	}
+
+	uint8_t *salt = params->salt;
+	uint8_t salt_length = params->salt_length;
+	uint16_t iterations = params->iterations;
+
+	int dig_size = gnutls_hash_get_len(GNUTLS_DIG_SHA1);
+
+	void *digest_old = malloc(dig_size);
+	CHECK_ALLOC_LOG(digest_old, -1);
+
+	size_t to_hash_size = size + salt_length;
+
+	void *to_hash = malloc(to_hash_size);
+	if (to_hash == NULL) {
+		ERR_ALLOC_FAILED;
+		free(digest_old);
+		return -1;
+	}
+
+	memcpy(to_hash, data, size);
+	memcpy(to_hash + size, salt, salt_length);
+
+	// first iteration
+	int res = gnutls_hash_fast(GNUTLS_DIG_SHA1, to_hash, to_hash_size,
+	                           digest_old);
+
+	if (res != GNUTLS_E_SUCCESS) {
+		log_error("Error calculating SHA-1 hash.\n");
+		free(digest_old);
+		free(to_hash);
+		return -2;
+	}
+
+	void *digest_new = malloc(dig_size);
+	if (digest_new == NULL) {
+		ERR_ALLOC_FAILED;
+		free(digest_old);
+		return -1;
+	}
+
+	to_hash_size = dig_size + salt_length;
+	free(to_hash);
+	to_hash = malloc(to_hash_size);
+	if (to_hash == NULL) {
+		ERR_ALLOC_FAILED;
+		free(digest_old);
+		free(digest_new);
+		return -1;
+	}
+
+	// other iterations
+	for (int i = 0; i < iterations; ++i) {
+		memcpy(to_hash, digest_old, dig_size);
+		memcpy(to_hash + dig_size, salt, salt_length);
+
+		res = gnutls_hash_fast(GNUTLS_DIG_SHA1, to_hash, to_hash_size,
+		                       digest_new);
+
+		if (res != GNUTLS_E_SUCCESS) {
+			log_error("Error calculating SHA-1 hash.\n");
+			free(digest_old);
+			free(digest_new);
+			free(to_hash);
+			return -2;
+		}
+
+		// copy the new digest to the old one and repeat
+		memcpy(digest_old, digest_new, dig_size);
+	}
+
+	free(digest_new);
+	free(to_hash);
+
+	*digest = digest_old;
+	*digest_size = dig_size;
+
+//	gnutls_hash_deinit(context, NULL);
+
+	return 0;
+}
+
