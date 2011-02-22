a5bfdabfc113ebec8841b847b449368d37013aa9
Ondřej Surý <ondrej@sury.org>
2011-02-22 16:58:03 +0100

    Reformat to linux coding style

--- src/base32/base32.h
+++ src/base32/base32.h
@@ -1,6 +1,6 @@
 /* base32.h -- Encode binary data using printable characters.
-   Copyright (C) 2004, 2005, 2006 Free Software Foundation, Inc.
-   Written by Simon Josefsson.
+   Copyright (C) 2004, 2005, 2006, 2010 Free Software Foundation, Inc.
+   Written by Ondřej Surý & Simon Josefsson.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
