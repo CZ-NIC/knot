7f8f5cb4a1a010c88487d02625da6d815b4912fc
Lubos Slovak <lubos.slovak@nic.cz>
2011-03-31 15:51:09 +0200

    tolower documented.
    refs #646

--- src/dnslib/tolower.h
+++ src/dnslib/tolower.h
@@ -14,14 +14,24 @@
 
 #include <stdint.h>
 
+/*! \brief Size of the character conversion table. */
 #define KNOT_CHAR_TABLE_SIZE 256
 
 enum {
+	/*! \brief Size of the character conversion table. */
 	CHAR_TABLE_SIZE = KNOT_CHAR_TABLE_SIZE
 };
 
+/*! \brief Character table mapping uppercase letters to lowercase. */
 const uint8_t char_table[CHAR_TABLE_SIZE];
 
+/*!
+ * \brief Converts ASCII character to lowercase.
+ * 
+ * \param c ASCII character code.
+ *
+ * \return \a c converted to lowercase (or \a c if not applicable).
+ */
 static inline uint8_t dnslib_tolower(uint8_t c) {
 #if KNOT_CHAR_TABLE_SIZE < 256
 	assert(c < CHAR_TABLE_SIZE);
