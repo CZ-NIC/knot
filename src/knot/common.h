8af9cae65ad9ddace6efa8371df432838fbccaf9
Marek Vavrusa <marek@vavrusa.com>
2011-04-06 16:42:30 +0200

    Updated common.h comments.

--- src/knot/common.h
+++ src/knot/common.h
@@ -16,68 +16,88 @@
 #include <stdint.h>
 #include "config.h"
 
-/* Common types and constants.
+/*
+ * Common types and constants.
  */
 
-#define PROJECT_NAME PACKAGE // Project name
-#define PROJECT_VER  0x000100  // 0xMMIIRR (MAJOR,MINOR,REVISION)
-#define PROJECT_EXEC "knotd" // Project executable
-#define ZONEPARSER_EXEC "knot-zcompile" // Zoneparser executable
-#define PID_FILE "knot.pid"
+typedef unsigned int uint; /*!< \brief Unsigned. */
 
-/* Server. */
-#define CPU_ESTIMATE_MAGIC 2   // Extra threads above the number of processors
-#define DEFAULT_THR_COUNT 2    // Default thread count for socket manager
-#define DEFAULT_PORT 53531     // Default port
+#define PROJECT_NAME PACKAGE /*!< \brief Project name. */
+#define PROJECT_VER  0x000100  /*!< \brief  0xMMIIRR (MAJOR,MINOR,REVISION). */
+#define PROJECT_EXEC "knotd" /*!< \brief  Project executable. */
+#define ZONEPARSER_EXEC "knot-zcompile" /*!< \brief  Zoneparser executable. */
+#define PID_FILE "knot.pid" /*!< \brief Server PID file name. */
 
-/* Sockets. */
-#define TCP_BACKLOG_SIZE 5     // TCP listen backlog size
+/*
+ * Server.
+ */
 
-/* Memory allocator. */
-//#define MEM_SLAB_CAP 3   // Cap slab_cache empty slab count (undefined = inf)
-#define MEM_COLORING       // Slab cache coloring
+#define CPU_ESTIMATE_MAGIC 2 /*!< \brief Extra threads to the number of cores.*/
+#define DEFAULT_THR_COUNT 2  /*!< \brief Default thread count. */
+#define DEFAULT_PORT 53531   /*!< \brief Default interface port. */
+#define TCP_BACKLOG_SIZE 5   /*!< \brief TCP listen backlog size. */
 
 #define USE_HASH_TABLE
 //#define COMPRESSION_PEDANTIC
 //#define TEST_WITH_LDNS
+//#define STAT_COMPILE
+#ifdef HAVE_LIBLDNS
+#define TEST_WITH_LDNS
+#endif
 
-/* Common includes.
+/*
+ * Common includes.
  */
 
 #include "common/latency.h"
 #include "common/print.h"
 #include "knot/other/log.h"
 #include "knot/other/debug.h"
-#include "dnslib/dnslib-common.h"
 
-
-/* Eliminate compiler warning with unused parameters. */
-#ifndef UNUSED
+/*! \brief Eliminate compiler warning with unused parameters. */
 #define UNUSED(param) (void)(param)
-#endif
 
-/* Type-safe minimum and maximum macros. */
-#ifndef MIN
+/*! \brief Type-safe minimum macro. */
 #define MIN(a, b) \
 	({ typeof (a) _a = (a); typeof (b) _b = (b); _a < _b ? _a : _b; })
-#endif
 
-#ifndef MAX
+/*! \brief Type-safe maximum macro. */
 #define MAX(a, b) \
 	({ typeof (a) _a = (a); typeof (b) _b = (b); _a > _b ? _a : _b; })
-#endif
 
 /* Optimisation macros. */
 #ifndef likely
+/*! \brief Optimize for x to be true value. */
 #define likely(x)       __builtin_expect((x),1)
 #endif
 #ifndef unlikely
+/*! \brief Optimize for x to be false value. */
 #define unlikely(x)     __builtin_expect((x),0)
 #endif
 
-//#define STAT_COMPILE
-#ifdef HAVE_LIBLDNS
-#define TEST_WITH_LDNS
+/*! \todo Refactor theese. We should have an allocator function handling this.*/
+#ifndef ERR_ALLOC_FAILED
+#define ERR_ALLOC_FAILED fprintf(stderr, "Allocation failed at %s:%d (%s ver.%x)\n", \
+				 __FILE__, __LINE__, PROJECT_NAME, PROJECT_VER)
+#endif
+
+#ifndef CHECK_ALLOC_LOG
+#define CHECK_ALLOC_LOG(var, ret) \
+	do { \
+		if ((var) == NULL) { \
+			ERR_ALLOC_FAILED; \
+			return (ret); \
+		} \
+	} while (0)
+#endif
+
+#ifndef CHECK_ALLOC
+#define CHECK_ALLOC(var, ret) \
+	do { \
+		if ((var) == NULL) { \
+			return (ret); \
+		} \
+	} while (0)
 #endif
 
 #endif /* _KNOT_COMMON_H_ */
