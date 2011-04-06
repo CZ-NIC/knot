4e8ec566ba2210358617a0cdd3a3eb98dc58d845
Lubos Slovak <lubos.slovak@nic.cz>
2011-04-06 16:31:33 +0200

    Commented some macros in common.h

--- src/knot/common.h
+++ src/knot/common.h
@@ -37,8 +37,17 @@ typedef unsigned int uint; /*!< \brief Unsigned. */
 #define DEFAULT_PORT 53531   /*!< \brief Default interface port. */
 #define TCP_BACKLOG_SIZE 5   /*!< \brief TCP listen backlog size. */
 
+/*! \brief If defined, zone structures will use hash table for lookup. */
 #define USE_HASH_TABLE
+
+///*! \brief If defined, zone structures will use hash table for lookup. */
 //#define COMPRESSION_PEDANTIC
+
+///*!
+// * \brief If defined, tests will use ldns library to parse sample data.
+// *
+// * If not defined, some tests will be disabled.
+// */
 //#define TEST_WITH_LDNS
 //#define STAT_COMPILE
 #ifdef HAVE_LIBLDNS
