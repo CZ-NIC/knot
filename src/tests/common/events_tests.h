53a2fdd5bf5217e14c6d2731682db433d027d590
Marek Vavrusa <marek@vavrusa.com>
2011-05-02 10:06:14 +0200

    Adding forgotten tests.
    Commit refs #799.

---
+++ src/tests/common/events_tests.h
@@ -0,0 +1,9 @@
+#ifndef _KNOT_EVENTS_TESTS_H_
+#define _KNOT_EVENTS_TESTS_H_
+
+#include "common/libtap/tap_unit.h"
+
+/* Unit API. */
+unit_api events_tests_api;
+
+#endif /* _KNOT_EVENTS_TESTS_H_ */
