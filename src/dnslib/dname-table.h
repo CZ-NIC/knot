b28595a891cfb472cdbda981fcf9c2612d35be1f
Jan Kadlec <jan.kadlec@nic.cz>
2011-05-03 17:14:57 +0200

    Documented dname table API. Refs #821 @35m

--- src/dnslib/dname-table.h
+++ src/dnslib/dname-table.h
@@ -1,5 +1,17 @@
-#ifndef DNAMETABLE_H
-#define DNAMETABLE_H
+/*!
+ * \file dname-table.h
+ *
+ * \author Jan Kadlec <jan.kadlec.@nic.cz>
+ *
+ * \brief Structures representing dname table and functions for
+ *        manipulating these structures.
+ *
+ * \addtogroup dnslib
+ * @{
+ */
+
+#ifndef _KNOT_DNSLIB_DNAME_TABLE_H_
+#define _KNOT_DNSLIB_DNAME_TABLE_H_
 
 #include <config.h>
 
@@ -9,30 +21,81 @@
 #include "dnslib/dnslib-common.h"
 
 
+/*!
+ * \brief Structure encapsulating
+ */
 struct dname_table_node {
-	dnslib_dname_t *dname;
-	TREE_ENTRY(dname_table_node) avl;
+	dnslib_dname_t *dname; /*!< Dname stored in node. */
+	TREE_ENTRY(dname_table_node) avl; /*!< Tree variables. */
 };
 
+/*!
+ * \brief Tree-related functions.
+ */
 typedef TREE_HEAD(avl, dname_table_node) table_tree_t;
 
-/*!< \note contains only tree now, but might change in the future. */
+/*!
+ * \brief Structure holding tree together with dname ID counter.
+ */
 struct dnslib_dname_table {
-	unsigned int id_counter;
-	table_tree_t *tree;
+	unsigned int id_counter; /*!< ID counter (starts from 1) */
+	table_tree_t *tree;  /*!< AVL tree */
 };
 
 typedef struct dnslib_dname_table dnslib_dname_table_t;
 
+/*!
+ * \brief Creates new empty domain name table.
+ *
+ * \retval Created table on success.
+ * \retval NULL on memory error.
+ */
 dnslib_dname_table_t *dnslib_dname_table_new();
 
+/*!
+ * \brief Finds name in the domain name table.
+ *
+ * \param table Domain name table to be searched.
+ * \param dname Dname to be searched.
+ *
+ * \retval Pointer to found dname when dname is present in the table.
+ * \retval NULL when dname is not present.
+ */
 dnslib_dname_t *dnslib_dname_table_find_dname(const dnslib_dname_table_t *table,
 					      dnslib_dname_t *dname);
 
+/*!
+ * \brief Adds domain name to domain name table.
+ *
+ * \param table Domain name table to be added to.
+ * \param dname Domain name to be added.
+ *
+ * \warning Function does not check for duplicates!
+ *
+ * \note This function encapsulates dname in a structure and saves it to a tree.
+ *
+ * \retval DNSLIB_EOK on success.
+ * \retval DNSLIB_ENOMEM when memory runs out.
+ */
 int dnslib_dname_table_add_dname(dnslib_dname_table_t *table,
 				 dnslib_dname_t *dname);
 
+/*!
+ * \brief Frees dname table without its nodes. Sets pointer to NULL.
+ *
+ * \param table Table to be freed.
+ */
 void dnslib_dname_table_free(dnslib_dname_table_t **table);
+
+/*!
+ * \brief Frees dname table and all its nodes (including dnames in the nodes)
+ *        Sets pointer to NULL.
+ *
+ * \param table Table to be freed.
+ */
 void dnslib_dname_table_deep_free(dnslib_dname_table_t **table);
 
-#endif // DNAMETABLE_H
+#endif // _KNOT_DNSLIB_DNAME_TABLE_H_
+
+/*! @} */
+
