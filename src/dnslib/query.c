506ee900218fd386905af2e17b78c3b331e0fd7d
Lubos Slovak <lubos.slovak@nic.cz>
2011-05-09 15:42:00 +0200

    Added query API
    - query_init() and query_set_question()
    
    refs #816 @10m

--- src/dnslib/query.c
+++ src/dnslib/query.c
@@ -1,5 +1,8 @@
 #include "dnslib/query.h"
 
+#include "dnslib/error.h"
+#include "dnslib/wire.h"
+
 /*----------------------------------------------------------------------------*/
 
 int dnslib_query_dnssec_requested(const dnslib_packet_t *query)
@@ -12,4 +15,33 @@ int dnslib_query_dnssec_requested(const dnslib_packet_t *query)
 int dnslib_query_nsid_requested(const dnslib_packet_t *query)
 {
 	return dnslib_edns_has_option(&query->opt_rr, EDNS_OPTION_NSID);
+}
+
+/*----------------------------------------------------------------------------*/
+
+int dnslib_query_init(dnslib_packet_t *query)
+{
+	// set the qr bit to 0
+	dnslib_wire_flags_clear_qr(&query->header.flags1);
+
+	uint8_t *pos = query->wireformat;
+	dnslib_packet_header_to_wire(&query->header, &pos, &query->size);
+
+	return DNSLIB_EOK;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int dnslib_query_set_question(dnslib_packet_t *query,
+                              const dnslib_question_t *question)
+{
+	if (query == NULL || question == NULL) {
+		return DNSLIB_EBADARG;
+	}
+
+	query->question.qname = question->qname;
+	query->question.qclass = question->qclass;
+	query->question.qtype = question->qtype;
+
+	return DNSLIB_EOK;
 }
