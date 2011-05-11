f6d69eca61065dc922fc2b279557df83d44cada6
Lubos Slovak <lubos.slovak@nic.cz>
2011-05-11 16:45:28 +0200

    Comment to axfrin_transfer_needed()

--- src/knot/server/axfr-in.h
+++ src/knot/server/axfr-in.h
@@ -22,8 +22,20 @@
 int axfrin_create_soa_query(const dnslib_dname_t *zone_name, uint8_t *buffer,
                             size_t *size);
 
+/*!
+ * \brief Checks if a zone transfer is required by comparing the zone's SOA with
+ *        the one received from master server.
+ *
+ * \param zone Zone to check.
+ * \param soa_response Response to SOA query received from master server.
+ *
+ * \retval < 0 if an error occured.
+ * \retval 1 if the transfer is needed.
+ * \retval 0 if the transfer is not needed.
+ */
 int axfrin_transfer_needed(const dnslib_zone_t *zone,
                            dnslib_packet_t *soa_response);
+
 
 int axfrin_create_axfr_query(const dnslib_dname_t *zone_name, uint8_t *buffer,
                              size_t *size);
