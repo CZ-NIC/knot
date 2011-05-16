90d267d7cd9abc18375a658c5df654511212b7cf
Lubos Slovak <lubos.slovak@nic.cz>
2011-05-16 13:53:17 +0200

    Fixed one warning
    (by an explicit cast - not very nice)

--- src/dnslib/packet.c
+++ src/dnslib/packet.c
@@ -655,7 +655,7 @@ int dnslib_packet_parse_from_wire(dnslib_packet_t *packet,
 	// save the wireformat in the packet
 	// TODO: can we just save the pointer, or we have to copy the data??
 	assert(packet->wireformat == NULL);
-	packet->wireformat = wireformat;
+	packet->wireformat = (uint8_t*)wireformat;
 	packet->size = size;
 	packet->free_wireformat = 0;
 
