d815ebf7b7fbd4d87b9ac2ad878dfe2e338ed999
Lubos Slovak <lubos.slovak@nic.cz>
2011-06-21 18:33:51 +0200

    Added function for adding dname if not present.
    refs #920 @20m

--- src/dnslib/dname-table.c
+++ src/dnslib/dname-table.c
@@ -104,6 +104,36 @@ int dnslib_dname_table_add_dname(dnslib_dname_table_t *table,
 	return DNSLIB_EOK;
 }
 
+int dnslib_dname_table_add_dname2(dnslib_dname_table_t *table,
+                                  dnslib_dname_t **dname)
+{
+	dnslib_dname_t *found_dname = NULL;
+	int ret;
+
+	char *name = dnslib_dname_to_str(*dname);
+	printf("Inserting name %s to dname table.\n", name);
+	free(name);
+
+	if ((!(found_dname =
+	      dnslib_dname_table_find_dname(table ,*dname))) &&
+	      (ret = dnslib_dname_table_add_dname(table, *dname))
+	      != DNSLIB_EOK) {
+		printf("Error!\n");
+		return ret;
+	} else if (found_dname != NULL && found_dname != *dname) {
+		/*! \todo Remove the check for equality. */
+		name = dnslib_dname_to_str(found_dname);
+		printf("Already there: %s (%p)\n", name, found_dname);
+		free(name);
+		dnslib_dname_free(dname);
+		*dname = found_dname;
+	}
+
+	printf("Done.\n");
+
+	return DNSLIB_EOK;
+}
+
 void dnslib_dname_table_free(dnslib_dname_table_t **table)
 {
 	if (table == NULL || *table == NULL) {
