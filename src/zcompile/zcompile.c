9bfb95192c57de9fb884ab478d60501fade54e8c
Jan Kadlec <jan.kadlec@nic.cz>
2011-06-24 13:19:59 +0200

    Changed handling od RRSIGs in zparser.

--- src/zcompile/zcompile.c
+++ src/zcompile/zcompile.c
@@ -1503,8 +1503,8 @@ int find_rrset_for_rrsig_in_node(dnslib_zone_t *zone,
 	/*!< \todo inserting RRSIGs to zone!!! */
 
 	if (tmp_rrset->rrsigs != NULL) {
-		dnslib_zone_add_rrset(zone, rrsig, &node,
-		                      DNSLIB_RRSET_DUPL_MERGE);
+		dnslib_zone_add_rrsigs(zone, rrsig, tmp_rrset,
+		                       DNSLIB_RRSET_DUPL_MERGE);
 //		dnslib_rrset_merge((void *)&tmp_rrset->rrsigs, (void *)&rrsig);
 		dnslib_rrset_free(&rrsig);
 	} else {
@@ -1855,10 +1855,10 @@ int zone_read(const char *name, const char *zonefile, const char *outfile,
 	/* This is *almost* unnecessary */
 //	dnslib_zone_deep_free(&(parser->current_zone), 0);
 
-//	dnslib_zone_t *some_zone= dnslib_zload_load(dnslib_zload_open(outfile));
+	dnslib_zone_t *some_zone= dnslib_zload_load(dnslib_zload_open(outfile));
 
-//	printf("apex of new zone: %s\n",
-//	       dnslib_dname_to_str(some_zone->apex->owner));
+	printf("apex of new zone: %s\n",
+	       dnslib_dname_to_str(some_zone->apex->owner));
 
 	fclose(yyin);
 
