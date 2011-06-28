743ea884ddc5186d5e06735ef8ad94487ccdaafd
Marek Vavrusa <marek.vavrusa@nic.cz>
2011-06-28 14:12:24 +0200

    Documented journal, use of standard error codes, updated tests.
    Commit refs #927, #928.

--- src/knot/server/journal.c
+++ src/knot/server/journal.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "knot/other/error.h"
 #include "knot/other/debug.h"
 #include "journal.h"
 
@@ -21,30 +22,31 @@ int journal_create(const char *fn, uint16_t max_nodes)
 	FILE *fp = fopen(fn, "w");
 	if (!fp) {
 		debug_journal("journal: failed to create file '%s'\n", fn);
-		return -1;
+		return KNOT_EINVAL;
 	}
 
 	/* Disable buffering. */
 	setvbuf(fp, (char *)0, _IONBF, 0);
 
-	/*! \todo Unlink created file on error? */
-
 	/* Create journal header. */
 	debug_journal("journal: creating header\n");
 	if (!sfwrite(&max_nodes, sizeof(uint16_t), fp)) {
 		fclose(fp);
-		return -1;
+		unlink(fn);
+		return KNOT_ERROR;
 	}
 
 	/* Create empty queue head + tail. */
 	uint16_t zval = 0;
 	if (!sfwrite(&zval, sizeof(uint16_t), fp)) {
 		fclose(fp);
-		return -1;
+		unlink(fn);
+		return KNOT_ERROR;
 	}
 	if (!sfwrite(&zval, sizeof(uint16_t), fp)) {
 		fclose(fp);
-		return -1;
+		unlink(fn);
+		return KNOT_ERROR;
 	}
 
 	debug_journal("journal: creating free segment descriptor\n");
@@ -57,7 +59,8 @@ int journal_create(const char *fn, uint16_t max_nodes)
 	jn.len = 0;
 	if (!sfwrite(&jn, sizeof(journal_node_t), fp)) {
 		fclose(fp);
-		return -1;
+		unlink(fn);
+		return KNOT_ERROR;
 	}
 
 	/* Create nodes. */
@@ -66,13 +69,14 @@ int journal_create(const char *fn, uint16_t max_nodes)
 	for(uint16_t i = 0; i < max_nodes; ++i) {
 		if (!sfwrite(&jn, sizeof(journal_node_t), fp)) {
 			fclose(fp);
-			return -1;
+			unlink(fn);
+			return KNOT_ERROR;
 		}
 	}
 
 	/* Journal file created. */
 	debug_journal("journal: file '%s' initialized\n", fn);
-	return 0;
+	return KNOT_EOK;
 }
 
 journal_t* journal_open(const char *fn)
@@ -152,11 +156,11 @@ int journal_fetch(journal_t *journal, int id, const journal_node_t** dst)
 
 		if (journal->nodes[i].id == id) {
 			*dst = journal->nodes + i;
-			return 0;
+			return KNOT_EOK;
 		}
 	}
 
-	return -1;
+	return KNOT_ENOENT;
 }
 
 int journal_read(journal_t *journal, int id, char *dst)
@@ -165,13 +169,13 @@ int journal_read(journal_t *journal, int id, char *dst)
 	if(journal_fetch(journal, id, &n) != 0) {
 		debug_journal("journal: failed to fetch node with id=%d\n",
 			      id);
-		return -1;
+		return KNOT_ENOENT;
 	}
 
 	/* Check valid flag. */
 	if (n->flags != JOURNAL_VALID) {
 		debug_journal("journal: node with id=%d is invalid\n", id);
-		return -1;
+		return KNOT_EINVAL;
 	}
 
 	debug_journal("journal: reading node with id=%d, data=<%u, %u>\n",
@@ -181,7 +185,12 @@ int journal_read(journal_t *journal, int id, char *dst)
 	fseek(journal->fp, n->pos, SEEK_SET);
 
 	/* Read journal node content. */
-	return fread(dst, n->len, 1, journal->fp);
+	int ret = fread(dst, n->len, 1, journal->fp);
+	if (ret != 1) {
+		return KNOT_ERROR;
+	}
+
+	return KNOT_EOK;
 }
 
 int journal_write(journal_t *journal, int id, const char *src, size_t size)
@@ -208,7 +217,7 @@ int journal_write(journal_t *journal, int id, const char *src, size_t size)
 		}
 	}
 
-	/*! \todo Free nodes if necessary. */
+	/* Evict occupied nodes if necessary. */
 	while (journal->free.len < size) {
 
 		/* Evict least recent node if not empty. */
@@ -218,7 +227,7 @@ int journal_write(journal_t *journal, int id, const char *src, size_t size)
 		/* Write back evicted node. */
 		fseek(journal->fp, JOURNAL_HSIZE + (journal->qhead + 1) * node_len, SEEK_SET);
 		if (!sfwrite(head, node_len, journal->fp)) {
-			return -1;
+			return KNOT_ERROR;
 		}
 
 		debug_journal("journal: evicted node=%u, growing by +%u\n",
@@ -229,7 +238,7 @@ int journal_write(journal_t *journal, int id, const char *src, size_t size)
 		uint16_t qstate[2] = {journal->qhead, journal->qtail};
 		fseek(journal->fp, JOURNAL_HSIZE - 2 * sizeof(uint16_t), SEEK_SET);
 		if (!sfwrite(qstate, 2 * sizeof(uint16_t), journal->fp)) {
-			return -1;
+			return KNOT_ERROR;
 		}
 
 		/* Increase free segment. */
@@ -247,20 +256,20 @@ int journal_write(journal_t *journal, int id, const char *src, size_t size)
 	if (!sfwrite(n, node_len, journal->fp)) {
 		debug_journal("journal: failed to writeback node=%d to %u\n",
 			      n->id, jn_fpos);
-		return -1;
+		return KNOT_ERROR;
 	}
 
 	/* Write data to permanent storage. */
 	fseek(journal->fp, n->pos, SEEK_SET);
 	if (!sfwrite(src, size, journal->fp)) {
-		return -1;
+		return KNOT_ERROR;
 	}
 
 	/* Mark node as valid and write back. */
 	n->flags = JOURNAL_VALID;
 	fseek(journal->fp, jn_fpos, SEEK_SET);
 	if (!sfwrite(n, node_len, journal->fp)) {
-		return -1;
+		return KNOT_ERROR;
 	}
 
 	debug_journal("journal: written node=%u, data=<%u, %u>\n",
@@ -290,7 +299,7 @@ int journal_write(journal_t *journal, int id, const char *src, size_t size)
 		/*! \todo Node is marked valid and failed to shrink free space,
 			  node will be overwritten on the next open - this may be
 			  a problem, how to solve it properly? */
-		return -1;
+		return KNOT_ERROR;
 	}
 
 	/* Write back query state, not essential as it may be recovered.
@@ -300,7 +309,7 @@ int journal_write(journal_t *journal, int id, const char *src, size_t size)
 	uint16_t qstate[2] = {journal->qhead, journal->qtail};
 	fseek(journal->fp, JOURNAL_HSIZE - 2 * sizeof(uint16_t), SEEK_SET);
 	if (!sfwrite(qstate, 2 * sizeof(uint16_t), journal->fp)) {
-		return -1;
+		return KNOT_ERROR;
 	}
 
 	/*! \todo Delayed write-back? */
@@ -308,7 +317,7 @@ int journal_write(journal_t *journal, int id, const char *src, size_t size)
 	debug_journal("journal: write finished, nqueue=<%u, %u>\n",
 		      journal->qhead, journal->qtail);
 
-	return size;
+	return KNOT_EOK;
 }
 
 int journal_close(journal_t *journal)
@@ -320,5 +329,6 @@ int journal_close(journal_t *journal)
 
 	/* Free allocated resources. */
 	free(journal);
-	return 0;
+
+	return KNOT_EOK;
 }
--- src/knot/server/journal.h
+++ src/knot/server/journal.h
@@ -5,6 +5,20 @@
  *
  * \brief Journal for storing transactions on permanent storage.
  *
+ * Journal stores entries on a permanent storage.
+ * Each written entry is guaranteed to persist until
+ * the maximum file size or node count is reached.
+ * Entries are removed from the least recent.
+ *
+ * Journal file structure
+ * <pre>
+ *  uint16_t node_count
+ *  uint16_t node_queue_head
+ *  uint16_t node_queue_tail
+ *  journal_entry_t free_segment
+ *  node_count *journal_entry_t
+ *  ...data...
+ * </pre>
  * \addtogroup utils
  * @{
  */
@@ -14,21 +28,22 @@
 
 #include <stdint.h>
 
-/* File structure
- * uint16_t max_nodes
- * uint16_t qhead
- * uint16_t qtail
- * <max_nodes + 1> * journal_entry_t
- * <data>
+/*!
+ * \brief Journal entry flags.
  */
-
 typedef enum journal_flag_t {
 	JOURNAL_NULL  = 0 << 0, /*!< Invalid journal entry. */
 	JOURNAL_FREE  = 1 << 0, /*!< Free journal entry. */
-	JOURNAL_VALID = 1 << 1  /*!< Valid journal entry. */
+	JOURNAL_VALID = 1 << 1, /*!< Valid journal entry. */
+	JOURNAL_DIRTY = 1 << 2  /*!< Journal entry cannot be evicted. */
 } journal_flag_t;
 
-/*! 12 bytes. */
+/*!
+ * \brief Journal node structure.
+ *
+ * Each node represents journal entry and points
+ * to position of the data in the permanent storage.
+ */
 typedef struct journal_node_t
 {
 	uint16_t id;    /*!< Node ID. */
@@ -37,32 +52,100 @@ typedef struct journal_node_t
 	uint32_t len;   /*!< Entry data length. */
 } journal_node_t;
 
-/*! \todo Review nodes data storage type. */
-
+/*!
+ * \brief Journal structure.
+ *
+ * Journal organizes entries as nodes.
+ * Nodes are stored in-memory for fast lookup and also
+ * backed by a permanent storage.
+ * Each journal has a fixed number of nodes.
+ *
+ * \todo Organize nodes in an advanced structure, like
+ *       btree or hash table to improve lookup time.
+ */
 typedef struct journal_t
 {
 	FILE *fp;
-	uint16_t max_nodes; /*!< Number of nodes. */
-	uint16_t qhead;     /*!< Node queue head. */
-	uint16_t qtail;     /*!< Node queue tail. */
-	journal_node_t free; /*!< Free segment. */
+	uint16_t max_nodes;     /*!< Number of nodes. */
+	uint16_t qhead;         /*!< Node queue head. */
+	uint16_t qtail;         /*!< Node queue tail. */
+	journal_node_t free;    /*!< Free segment. */
 	journal_node_t nodes[]; /*!< Array of nodes. */
 } journal_t;
 
+/*
+ * Journal defaults and constants.
+ */
 #define JOURNAL_NCOUNT 512 /*!< Default node count. */
+#define JOURNAL_HSIZE (sizeof(uint16_t) * 3) /*!< max_entries, qhead, qtail */
 
-/*!  max_entries, qhead, qtail */
-#define JOURNAL_HSIZE (sizeof(uint16_t)*3)
-
-/*! \todo Document functions. */
-
+/*!
+ * \brief Create new journal.
+ *
+ * \param fn Journal file name, will be created if not exist.
+ * \param max_nodes Maximum number of nodes in journal.
+ *
+ * \retval KNOT_EOK if successful.
+ * \retval KNOT_EINVAL if the file with given name cannot be created.
+ * \retval KNOT_ERROR on I/O error.
+ */
 int journal_create(const char *fn, uint16_t max_nodes);
+
+/*!
+ * \brief Open journal file for read/write.
+ *
+ * \param fn Journal file name.
+ *
+ * \retval new journal instance if successful.
+ * \retval NULL on error.
+ */
 journal_t* journal_open(const char *fn);
+
+/*!
+ * \brief Fetch entry node for given identifier.
+ *
+ * \param journal Associated journal.
+ * \param id Entry identifier.
+ * \param dst Destination for journal entry.
+ *
+ * \retval KNOT_EOK if successful.
+ * \retval KNOT_ENOENT if not found.
+ */
 int journal_fetch(journal_t *journal, int id, const journal_node_t** dst);
+
+/*!
+ * \brief Read journal entry data.
+ *
+ * \param journal Associated journal.
+ * \param id Entry identifier.
+ * \param dst Pointer to destination memory.
+ *
+ * \retval KNOT_EOK if successful.
+ * \retval KNOT_ENOENT if the entry cannot be found.
+ * \retval KNOT_EINVAL if the entry is invalid.
+ * \retval KNOT_ERROR on I/O error.
+ */
 int journal_read(journal_t *journal, int id, char *dst);
+
+/*!
+ * \brief Write journal entry data.
+ *
+ * \param journal Associated journal.
+ * \param id Entry identifier.
+ * \param src Pointer to source data.
+ *
+ * \retval KNOT_EOK if successful.
+ * \retval KNOT_ERROR on I/O error.
+ */
 int journal_write(journal_t *journal, int id, const char *src, size_t size);
+
+/*!
+ * \brief Close journal file.
+ *
+ * \param journal Associated journal.
+ *
+ * \retval KNOT_EOK
+ */
 int journal_close(journal_t *journal);
-
-
 
 #endif /* _KNOT_JOURNAL_H_ */
