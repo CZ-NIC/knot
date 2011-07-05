b8ff19739563a9480924969fc9ebe992030bbd60
Marek Vavrusa <marek.vavrusa@nic.cz>
2011-07-05 22:30:54 +0200

    IXFR changesets serialization API proposition.
    Commit refs #975.

--- src/knot/server/xfr-in.h
+++ src/knot/server/xfr-in.h
@@ -20,6 +20,11 @@
 #include "dnslib/packet.h"
 #include "knot/server/name-server.h"
 
+/*! \todo Changeset must be serializable/deserializable, so
+ *        all data and pointers have to be changeset-exclusive,
+ *        or more advanced structure serialization scheme has to be
+ *        implemented.
+ */
 typedef struct {
 	dnslib_rrset_t *soa_from;
 	dnslib_rrset_t **remove;
@@ -125,6 +130,42 @@ int xfrin_process_axfr_packet(const uint8_t *pkt, size_t size,
 
 int xfrin_process_ixfr_packet(const uint8_t *pkt, size_t size,
                               xfrin_changesets_t **changesets);
+
+/*!
+ * \brief Store changesets in journal.
+ *
+ * Changesets will be stored on a permanent storage.
+ * Journal may be compacted, resulting in flattening changeset history.
+ *
+ * \param zone Zone associated with the changeset.
+ * \param src Changesets.
+ *
+ * \retval KNOT_EOK on success.
+ * \retval KNOT_EINVAL on invalid parameters.
+ * \retval KNOT_EAGAIN if journal needs to be synced with zonefile first.
+ */
+int xfrin_store_changesets(dnslib_zone_t *zone, const xfrin_changesets_t *src);
+
+/*!
+ * \brief Load changesets from journal.
+ *
+ * Changesets will be stored on a permanent storage.
+ * Journal may be compacted, resulting in flattening changeset history.
+ *
+ * In case of KNOT_ERANGE error, whole zone content should be sent instead,
+ * as the changeset history cannot be recovered.
+ *
+ * \param zone Zone containing a changeset journal.
+ * \param dst Container to be loaded.
+ * \param from Starting SOA serial (oldest).
+ * \param to Ending SOA serial (newest).
+ *
+ * \retval KNOT_EOK on success.
+ * \retval KNOT_EINVAL on invalid parameters.
+ * \retval KNOT_ERANGE when changeset history cannot be reconstructed.
+ */
+int xfr_load_changesets(dnslib_zone_t *zone, xfrin_changesets_t *dst,
+			int64_t from, int64_t to);
 
 #endif /* _KNOT_XFR_IN_H_ */
 
