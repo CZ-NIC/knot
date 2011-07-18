de55f22530a15f8a9cc43d626b28dcf14826265a
Jan Kadlec <jan.kadlec@nic.cz>
2011-07-18 13:13:36 +0200

    Added new tests from branch tests-realdata. Refs #998

---
+++ src/dnslib/tests/realdata/dnslib/zonedb_tests_realdata.c
@@ -0,0 +1,28 @@
+#include "dnslib/tests/realdata/dnslib/zonedb_tests_realdata.h"
+
+
+static int zonedb_tests_count(int argc, char *argv[]);
+static int zonedb_tests_run(int argc, char *argv[]);
+
+/*! Exported unit API.
+ */
+unit_api zonedb_tests_api = {
+	"Zone database",      //! Unit name
+	&zonedb_tests_count,  //! Count scheduled tests
+	&zonedb_tests_run     //! Run scheduled tests
+};
+
+/*! This helper routine should report number of
+ *  scheduled tests for given parameters.
+ */
+static int zonedb_tests_count(int argc, char *argv[])
+{
+	return 0;
+}
+
+/*! Run all scheduled tests for given parameters.
+ */
+static int zonedb_tests_run(int argc, char *argv[])
+{
+	return 0;
+}
