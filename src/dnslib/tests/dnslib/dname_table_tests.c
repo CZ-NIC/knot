36a26f6577bc67a78b1a21d807f95eee2ff37ab5
Jan Kadlec <jan.kadlec@nic.cz>
2011-07-19 15:08:40 +0200

    Added test for dname_table_new() refs #1001 @10m

--- src/dnslib/tests/dnslib/dname_table_tests.c
+++ src/dnslib/tests/dnslib/dname_table_tests.c
@@ -1,4 +1,7 @@
 #include "dname_table_tests.h"
+#include "dnslib/dname-table.h"
+/* *test_t structures */
+#include "dnslib/tests/realdata/dnslib_tests_loader_realdata.h"
 
 static int dnslib_dname_table_tests_count(int argc, char *argv[]);
 static int dnslib_dname_table_tests_run(int argc, char *argv[]);
@@ -10,6 +13,17 @@ unit_api dname_table_tests_api = {
 	&dnslib_dname_table_tests_count,  //! Count scheduled tests
 	&dnslib_dname_table_tests_run     //! Run scheduled tests
 };
+
+static int test_dname_table_new()
+{
+	dnslib_dname_table_t *table = dnslib_dname_table_new();
+	if (table == NULL) {
+		return 0;
+	}
+
+	dnslib_dname_table_free(&table);
+	return 1;
+}
 
 static const int DNSLIB_DNAME_TABLE_TEST_COUNT = 7;
 
@@ -25,4 +39,15 @@ static int dnslib_dname_table_tests_count(int argc, char *argv[])
  */
 static int dnslib_dname_table_tests_run(int argc, char *argv[])
 {
+	int final_res = 1;
+	int res = 0;
+
+	ok((res = test_dname_table_new()), "dname table: new");
+	final_res *= res;
+
+	skip(!res, 6);
+
+	endskip;
+
+	return final_res;
 }
