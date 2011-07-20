751b2bbeee14c0e14e80eae29d6039cadaa29f4e
Jan Kadlec <jan.kadlec@nic.cz>
2011-07-20 20:54:19 +0200

    Added API for packet realdata tests. Refs #1027 @10m

---
+++ src/dnslib/tests/realdata/dnslib/packet_tests_realdata.c
@@ -0,0 +1,31 @@
+/* blame: jan.kadlec@nic.cz */
+
+#include <assert.h>
+
+#include "packet_tests_realdata.h"
+#include "dnslib/error.h"
+#include "dnslib/packet.h"
+/* *test_t structures */
+#include "dnslib/tests/realdata/dnslib_tests_loader_realdata.h"
+
+static int packet_tests_count(int argc, char *argv[]);
+static int packet_tests_run(int argc, char *argv[]);
+
+/*! Exported unit API.
+ */
+unit_api packet_tests_api = {
+	"Packet",     //! Unit name
+	&packet_tests_count,  //! Count scheduled tests
+	&packet_tests_run     //! Run scheduled tests
+};
+static const uint DNSLIB_PACKET_TEST_COUNT = 14;
+
+static int packet_tests_count(int argc, char *argv[])
+{
+	return DNSLIB_PACKET_TEST_COUNT;
+}
+
+static int packet_tests_run(int argc, char *argv[])
+{
+}
+
