73d9ce000154df66cc4a8795399765636b7b92d3
Jan Kadlec <jan.kadlec@nic.cz>
2011-07-27 20:29:35 +0200

    Implemented NSEC3 tests - error in SHA1! ldns has different result. Closes #1032 @4h

--- src/dnslib/tests/dnslib/nsec3_tests.c
+++ src/dnslib/tests/dnslib/nsec3_tests.c
@@ -6,7 +6,12 @@
 #include "dnslib/error.h"
 #include "dnslib/nsec3.h"
 #include "dnslib/utils.h"
+#include "common/base32hex.h"
 #include "nsec3_tests.h"
+
+#ifdef TEST_WITH_LDNS
+#include "ldns/ldns.h"
+#endif
 
 static int dnslib_nsec3_tests_count(int argc, char *argv[]);
 static int dnslib_nsec3_tests_run(int argc, char *argv[]);
@@ -21,6 +26,8 @@ unit_api nsec3_tests_api = {
 
 extern int compare_wires_simple(uint8_t *w1, uint8_t *w2, uint count);
 
+static dnslib_nsec3_params_t nsec3_test_params;
+
 static int test_nsec3_params_from_wire()
 {
 	/* Create sample NSEC3PARAM rdata */
@@ -29,12 +36,10 @@ static int test_nsec3_params_from_wire()
 	rdata->items = items;
 	rdata->count = 4;
 	dnslib_rdata_item_set_raw_data(rdata, 0, (uint16_t *)"\x1\x0\x1");
-	dnslib_rdata_item_set_raw_data(rdata, 1, (uint16_t *)"\x1\x0\x5");
-	dnslib_rdata_item_set_raw_data(rdata, 2, (uint16_t *)"\x2\x0\x0\xF");
-//	raw_data = (((uint8_t *)raw_data) + 3;
-//	raw_data = "12345678";
+	dnslib_rdata_item_set_raw_data(rdata, 1, (uint16_t *)"\x1\x0\x0");
+	dnslib_rdata_item_set_raw_data(rdata, 2, (uint16_t *)"\x2\x0\x0\x64");
 	dnslib_rdata_item_set_raw_data(rdata, 3,
-	                    (uint16_t *)"\x9\x0\x8\xF\xF\xF\xF\xF\xF\xF\xF\xF");
+	                    (uint16_t *)"\xF\x0\xE20110331084524");
 
 	dnslib_rrset_t *rrset =
 		dnslib_rrset_new(dnslib_dname_new_from_str("cz.",
@@ -45,7 +50,7 @@ static int test_nsec3_params_from_wire()
 	assert(rrset);
 	assert(dnslib_rrset_add_rdata(rrset, rdata) == DNSLIB_EOK);
 
-	dnslib_nsec3_params_t params;
+	dnslib_nsec3_params_t nsec3_tests_params;
 
 	int errors = 0;
 	int lived = 0;
@@ -57,7 +62,7 @@ static int test_nsec3_params_from_wire()
 		lived = 1;
 
 		lived = 0;
-		if (dnslib_nsec3_params_from_wire(&params, NULL) !=
+		if (dnslib_nsec3_params_from_wire(&nsec3_test_params, NULL) !=
 		    DNSLIB_EBADARG) {
 			errors++;
 		}
@@ -73,32 +78,33 @@ static int test_nsec3_params_from_wire()
 	}, "nsec3 params from wire NULL tests");
 	errors += lived != 1;
 
-	if (dnslib_nsec3_params_from_wire(&params, rrset) != DNSLIB_EOK) {
+	if (dnslib_nsec3_params_from_wire(&nsec3_test_params, rrset) != DNSLIB_EOK) {
 		diag("Could not convert nsec3 params to wire!");
 		return 0;
 	}
 
-	if (params.algorithm != 1) {
+	if (nsec3_test_params.algorithm != 1) {
 		diag("Algorithm error");
 		errors++;
 	}
 
-	if (params.flags != 5) {
+	if (nsec3_test_params.flags != 2) {
 		diag("Flags error");
 		errors++;
 	}
 
-	if (params.iterations != 15) {
+	if (nsec3_test_params.iterations != 15) {
 		diag("Iterations error");
 		errors++;
 	}
 
-	if (params.salt_length != 8) {
+	if (nsec3_test_params.salt_length != 8) {
 		diag("Salt length error");
 		return 0;
 	}
 
-	if (compare_wires_simple(params.salt, "\xF\xF\xF\xF\xF\xF\xF\xF",
+	if (compare_wires_simple((uint8_t *)nsec3_test_params.salt,
+	                         (uint8_t *)"\xF\xF\xF\xF\xF\xF\xF\xF",
 	                         8) != 0) {
 		diag("Salt wire error");
 		errors++;
@@ -108,7 +114,113 @@ static int test_nsec3_params_from_wire()
 	return (errors == 0);
 }
 
-static const int DNSLIB_NSEC3_TESTS_COUNT = 1;
+static int test_nsec3_sha1()
+{
+	int errors = 0;
+	int lived = 0;
+	lives_ok({
+		if (dnslib_nsec3_sha1(NULL, NULL, 1, NULL, NULL) !=
+	            DNSLIB_EBADARG) {
+			errors++;
+		}
+		lived = 1;
+		lived = 0;
+		if (dnslib_nsec3_sha1(&nsec3_test_params,
+	                              NULL, 1, NULL, NULL) !=
+	            DNSLIB_EBADARG) {
+			errors++;
+		}
+		uint8_t data[20];
+		lived = 1;
+		lived = 0;
+		if (dnslib_nsec3_sha1(&nsec3_test_params,
+	                              data, 20, NULL, NULL) !=
+	            DNSLIB_EBADARG) {
+			errors++;
+		}
+		uint8_t *digest = NULL;
+		lived = 1;
+		lived = 0;
+		if (dnslib_nsec3_sha1(&nsec3_test_params,
+	                              data, 20, &digest, NULL) !=
+	            DNSLIB_EBADARG) {
+			errors++;
+		}
+		size_t size = 0;
+		lived = 1;
+		lived = 0;
+		if (dnslib_nsec3_sha1(&nsec3_test_params,
+	                              data, 20, &digest, &size) !=
+	            DNSLIB_EBADARG) {
+			errors++;
+		}
+		size = NULL;
+		digest = 0xaaaaaa;
+		lived = 1;
+		lived = 0;
+		if (dnslib_nsec3_sha1(&nsec3_test_params,
+	                              data, 20, &digest, &size) !=
+	            DNSLIB_EBADARG) {
+			errors++;
+		}
+		lived = 1;
+	}, "NSEC3: nsec3 sha1 NULL tests");
+	if (errors) {
+		diag("Does not return DNSLIB_EBADARG after "
+		     "execution with wrong arguments!");
+	}
+
+	errors += lived != 1;
+
+	uint8_t *digest = NULL;
+	size_t digest_size = 0;
+	if (dnslib_nsec3_sha1(&nsec3_test_params, "\2ns\3nic\2cz",
+	                      strlen("\2ns\3nic\2cz"), &digest,
+	                      &digest_size) != DNSLIB_EOK) {
+		diag("Could not hash name!");
+		return 0;
+	}
+
+#ifdef TEST_WITH_LDNS
+	ldns_rdf *name = ldns_dname_new_frm_str("ns.nic.cz.");
+	assert(name);
+	ldns_rdf *hashed_name = ldns_nsec3_hash_name(name,
+	                                          nsec3_test_params.algorithm,
+	                                          nsec3_test_params.iterations,
+	                                          nsec3_test_params.salt_length,
+	                                          nsec3_test_params.salt);
+	assert(hashed_name);
+	dnslib_dname_t *dname_from_ldns =
+		dnslib_dname_new_from_wire(ldns_rdf_data(hashed_name),
+	                                   ldns_rdf_size(hashed_name),
+	                                   NULL);
+
+	char *name_b32 = NULL;
+	size_t size_b32 = base32hex_encode_alloc((char *)digest, digest_size,
+	                                     &name_b32);
+
+//	hex_print(name_b32, size_b32);
+//	hex_print(ldns_rdf_data(hashed_name), ldns_rdf_size(hashed_name));
+	if (ldns_rdf_size(hashed_name) != size_b32) {
+		diag("Wrong hashed name length! Should be: %d is: %d",
+		     ldns_rdf_size(hashed_name), size_b32);
+		return 0;
+	}
+
+	if (compare_wires_simple(ldns_rdf_data(hashed_name),
+	                         (uint8_t *)name_b32, size_b32) != 0) {
+		diag("Wrong hashed name wire!");
+		errors++;
+	}
+#endif
+
+#ifndef TEST_WITH_LDNS
+	diag("Warning: without ldns this test is only partial!");
+#endif
+	return (errors == 0);
+}
+
+static const int DNSLIB_NSEC3_TESTS_COUNT = 2;
 
 /*! This helper routine should report number of
  *  scheduled tests for given parameters.
@@ -123,5 +235,6 @@ static int dnslib_nsec3_tests_count(int argc, char *argv[])
 static int dnslib_nsec3_tests_run(int argc, char *argv[])
 {
 	ok(test_nsec3_params_from_wire(), "nsec3: params from wire");
+	ok(test_nsec3_sha1(), "nsec3: sha1");
 	return 1;
 }
