24d886e3ca99118ab19d2b69f319e15e9fafbdc5
Lubos Slovak <lubos.slovak@nic.cz>
2011-08-03 22:06:30 +0200

    Added license text
    - Also added NSD's BSD license text to descriptor.c/.h

--- libknot/hash/hash-functions.h
+++ libknot/hash/hash-functions.h
@@ -12,6 +12,21 @@
  * \addtogroup hashing
  * @{
  */
+/*  Copyright (C) 2011 CZ.NIC Labs
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef _KNOT_HASH_FUNCTIONS_H_
 #define _KNOT_HASH_FUNCTIONS_H_
@@ -34,12 +49,10 @@ typedef  unsigned     char  u1;   /* unsigned 1-byte type */
  */
 unsigned long int fnv_hash(const char *data, int size, int bits);
 
-//unsigned long int fnv_hash2(char *data, int size, int bits);
-
 /*!
  * \brief Jenkins hash function.
  *
- * Downloaded from ???
+ * Downloaded from http://burtleburtle.net/bob/hash/evahash.html
  *
  * \param k Data to hash
  * \param length Size of the data in bytes.
@@ -50,51 +63,6 @@ unsigned long int fnv_hash(const char *data, int size, int bits);
  * \todo Add source.
  */
 u4 jhash(register u1 *k, u4 length, u4 initval);
-
-/*!
- * \brief SDBM hash function.
- *
- * Downloaded from ??? (a very similar version is here:
- * http://www.partow.net/programming/hashfunctions/).
- *
- * \param key Key to hash.
- * \param size Size of the key in bytes.
- *
- * \return Hash of the data.
- *
- * \todo Add source.
- */
-unsigned long sdbm_hash(const unsigned char *key, int size);
-
-/*!
- * \brief DJB Hash (by Daniel J. Bernstein).
- *
- * Downloaded from ???
- *
- * \param key Key to hash.
- * \param size Size of the key in bytes.
- *
- * \return Hash of the data.
- *
- * \todo Add source.
- */
-unsigned long djb_hash(const unsigned char *key, int size);
-
-//unsigned long jsw_hash( const unsigned char *key, int size );
-
-/*!
- * \brief ELF hash function.
- *
- * Downloaded from ???
- *
- * \param key Key to hash.
- * \param size Size of the key in bytes.
- *
- * \return Hash of the data.
- *
- * \todo Add source.
- */
-unsigned long elf_hash(const unsigned char *key, int size);
 
 #endif /* _KNOTDHASH_FUNCTIONS_H_ */
 
