b56947b5b92c4c8530ada3de67256fa0f312c4ca
Marek Vavrusa <marek.vavrusa@nic.cz>
2011-08-10 19:41:20 +0200

    Thread-local PRNG state.
    refs #631

--- src/common/WELL1024a.h
+++ src/common/WELL1024a.h
@@ -9,3 +9,18 @@
 rngstate_t* InitWELLRNG1024a (unsigned *init);
 double WELLRNG1024a (rngstate_t* s);
 
+/*!
+ * \brief Get pseudorandom number from PRNG initialized in thread-local storage.
+ *
+ * No need for initialization, TLS will take care of it.
+ *
+ * \retval Pseudorandom number.
+ */
+double tls_rand();
+
+/*!
+ * \brief Set PRNG seed in thread-local storage to requested value.
+ *
+ * Seed is 4 * 32 bytes wide.
+ */
+void tls_seed_set(unsigned init[32]);
