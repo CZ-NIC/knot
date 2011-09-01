222cab24845a79b20b6141a346535868599c2d0a
Lubos Slovak <lubos.slovak@nic.cz>
2011-09-01 09:42:10 +0200

    Disabled all debug output

--- libknot/util/debug.h
+++ libknot/util/debug.h
@@ -93,8 +93,8 @@ void knot_zone_contents_dump(knot_zone_contents_t *zone, char loaded_zone);
 //#define KNOT_ZONEDB_DEBUG
 //#define KNOT_DNAME_DEBUG
 //#define KNOT_NODE_DEBUG
-#define KNOT_RESPONSE_DEBUG
-#define KNOT_PACKET_DEBUG
+//#define KNOT_RESPONSE_DEBUG
+//#define KNOT_PACKET_DEBUG
 //#define KNOT_EDNS_DEBUG
 //#define KNOT_RRSET_DEBUG
 //#define KNOT_NSEC3_DEBUG
