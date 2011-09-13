ed771df17e2c131c75e159d294369c98122540f7
Marek Vavrusa <marek.vavrusa@nic.cz>
2011-09-13 16:42:10 +0200

    Automatic polling mechanism selection from config.h
    Commit refs #983

--- src/common/os_fdset.c
+++ src/common/os_fdset.c
@@ -1,9 +1,20 @@
 #include "os_fdset.h"
+#include "config.h"
 
-#define HAVE_EPOLL
-
-#ifdef HAVE_EPOLL
-#include "os_fdset_epoll.c"
+/* Attempt to use epoll_wait(). */
+#ifdef HAVE_EPOLL_WAIT
+  #include "os_fdset_epoll.c"
 #else
-#include "os_fdset_poll.c"
-#endif
+  /* Attempt to use kqueue(). */
+  #ifdef HAVE_KQUEUE
+    #warning "os_fdset: kqueue backend N/A, fallback to poll()"
+    #include "os_fdset_poll.c"
+  #else
+    /* poll() API */
+    #ifdef HAVE_POLL
+      #include "os_fdset_poll.c"
+    #else
+      #error "os_fdset: no socket polling API found"
+    #endif /* HAVE_POLL */
+  #endif /* HAVE_KQUEUE */
+#endif /* HAVE_EPOLL_WAIT */
