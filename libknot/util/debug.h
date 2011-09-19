b56fc661f79d0844fe0cff3fdcbd9751ba5734c1
Lubos Slovak <lubos.slovak@nic.cz>
2011-09-19 13:49:16 +0200

    Debug output modified

--- libknot/util/debug.h
+++ libknot/util/debug.h
@@ -99,7 +99,7 @@ void knot_zone_contents_dump(knot_zone_contents_t *zone, char loaded_zone);
 //#define KNOT_NSEC3_DEBUG
 //#define CUCKOO_DEBUG
 //#define CUCKOO_DEBUG_HASH
-//#define KNOT_NS_DEBUG
+#define KNOT_NS_DEBUG
 //#define KNOT_XFR_DEBUG
 //#define KNOT_DDNS_DEBUG
 
