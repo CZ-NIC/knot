a059464ff33a2a177937789e99e5e1f64da7c5ca
Marek Vavrusa <marek.vavrusa@nic.cz>
2011-09-21 21:06:12 +0200

    Properly implemented backend selection for fdset.
    refs #1289

--- src/common/fdset.c
+++ src/common/fdset.c
@@ -13,32 +13,22 @@ struct fdset_backend_t _fdset_backend = {
 
 /*! \brief Set backend implementation. */
 static void fdset_set_backend(struct fdset_backend_t *backend) {
-	_fdset_backend.fdset_new = backend->fdset_new;
-	_fdset_backend.fdset_destroy = backend->fdset_destroy;
-	_fdset_backend.fdset_add = backend->fdset_add;
-	_fdset_backend.fdset_remove = backend->fdset_remove;
-	_fdset_backend.fdset_wait = backend->fdset_wait;
-	_fdset_backend.fdset_begin = backend->fdset_begin;
-	_fdset_backend.fdset_end = backend->fdset_end;
-	_fdset_backend.fdset_next = backend->fdset_next;
-	_fdset_backend.fdset_method = backend->fdset_method;
+	memcpy(&_fdset_backend, backend, sizeof(struct fdset_backend_t));
 }
 
 /* Linux epoll API. */
 #ifdef HAVE_EPOLL_WAIT
-//  /*! \todo Implement correctly. */
-//  #include "common/fdset_epoll.c"
+  #include "common/fdset_epoll.h"
 #endif /* HAVE_EPOLL_WAIT */
 
 /* BSD kqueue API */
 #ifdef HAVE_KQUEUE
-  #warning "fixme: missing kqueue backend"
-  //#include "common/fdset_kqueue.h"
+  #include "common/fdset_kqueue.h"
 #endif /* HAVE_KQUEUE */
 
 /* POSIX poll API */
 #ifdef HAVE_POLL
-  #include "common/fdset_poll.c"
+  #include "common/fdset_poll.h"
 #endif /* HAVE_POLL */
 
 /*! \brief Bootstrap polling subsystem (it is called automatically). */
@@ -46,24 +36,24 @@ void __attribute__ ((constructor)) fdset_init()
 {
 	/* Preference: epoll */
 #ifdef HAVE_EPOLL_WAIT
-//	if (dlsym(RTLD_DEFAULT, "epoll_wait") != 0) {
-//		fdset_set_backend(&_fdset_epoll);
-//		return;
-//	}
+	if (dlsym(RTLD_DEFAULT, "epoll_wait") != 0) {
+		fdset_set_backend(&FDSET_EPOLL);
+		return;
+	}
 #endif
 
 	/* Preference: kqueue */
 #ifdef HAVE_KQUEUE
-//	if (dlsym(RTLD_DEFAULT, "kqueue") != 0) {
-//		fdset_set_backend(&_fdset_kqueue);
-//		return;
-//	}
+	if (dlsym(RTLD_DEFAULT, "kqueue") != 0) {
+		fdset_set_backend(&FDSET_KQUEUE);
+		return;
+	}
 #endif
 
-	/* Preference: poll */
+	/* Fallback: poll */
 #ifdef HAVE_POLL
 	if (dlsym(RTLD_DEFAULT, "poll") != 0) {
-		fdset_set_backend(&_fdset_poll);
+		fdset_set_backend(&FDSET_POLL);
 		return;
 	}
 #endif
