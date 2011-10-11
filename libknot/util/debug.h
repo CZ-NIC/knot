28c71a5b78340dbbd375483ab856e3dd4c1112e8
Lubos Slovak <lubos.slovak@nic.cz>
2011-10-11 17:42:13 +0200

    Enabled debug output + added debug message.

--- libknot/util/debug.h
+++ libknot/util/debug.h
@@ -50,13 +50,13 @@
 //#define KNOT_ZONEDB_DEBUG
 //#define KNOT_DNAME_DEBUG
 //#define KNOT_NODE_DEBUG
-//#define KNOT_PACKET_DEBUG
+#define KNOT_PACKET_DEBUG
 //#define KNOT_EDNS_DEBUG
 //#define KNOT_RRSET_DEBUG
 //#define KNOT_NSEC3_DEBUG
 //#define CUCKOO_DEBUG
 //#define CUCKOO_DEBUG_HASH
-//#define KNOT_NS_DEBUG
+#define KNOT_NS_DEBUG
 //#define KNOT_XFRIN_DEBUG
 //#define KNOT_DDNS_DEBUG
 
