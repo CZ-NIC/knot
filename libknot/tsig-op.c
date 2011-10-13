b9af19d90cc74b56a8f0ef15798cf0622a4a3933
Lubos Slovak <lubos.slovak@nic.cz>
2011-10-13 17:04:02 +0200

    Added empty implementations of TSIG-op API

--- libknot/tsig-op.c
+++ libknot/tsig-op.c
@@ -1,3 +1,49 @@
 #include "tsig-op.h"
+#include "util/error.h"
 
+/*----------------------------------------------------------------------------*/
 
+int knot_tsig_sign(const uint8_t *msg, size_t msg_len, 
+                   const uint8_t *request_mac, size_t request_mac_len,
+                   const knot_rrset_t *tsig_rr,
+                   uint8_t *mac, size_t size)
+{
+	return KNOT_ENOTSUP;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int knot_tsig_sign_next(const uint8_t *msg, size_t msg_len, 
+                        const uint8_t *prev_digest, size_t prev_digest_len,
+                        const knot_rrset_t *tsig_rr,
+                        uint8_t *mac, size_t size)
+{
+	return KNOT_ENOTSUP;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int knot_tsig_server_check(const knot_rrset_t *tsig_rr,
+                           const uint8_t *wire, size_t size)
+{
+	return KNOT_ENOTSUP;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int knot_tsig_client_check(const knot_rrset_t *tsig_rr,
+                           const uint8_t *wire, size_t size,
+                           const uint8_t *request_mac, size_t request_mac_len)
+{
+	return KNOT_ENOTSUP;
+}
+
+/*----------------------------------------------------------------------------*/
+
+int knot_tsig_client_check_next(const knot_rrset_t *tsig_rr,
+                                const uint8_t *wire, size_t size,
+                                const uint8_t *prev_digest, 
+                                size_t prev_digest_len)
+{
+	return KNOT_ENOTSUP;
+}
