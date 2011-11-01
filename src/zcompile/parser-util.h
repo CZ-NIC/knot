6606f35dc9624fcb923be22211a6ac9645354c1e
Ondřej Surý <ondrej@sury.org>
2011-11-01 14:24:42 +0100

    Add copyright headers to all CZ.NIC owned content

--- src/zcompile/parser-util.h
+++ src/zcompile/parser-util.h
@@ -4,11 +4,27 @@
  * \author NLnet Labs
  *         Copyright (c) 2001-2006, NLnet Labs. All rights reserved.
  *         See LICENSE for the license.
+ *         Modification by CZ.NIC, z.s.p.o.
  *
  * \brief Zone compiler utility functions.
  *
  * \addtogroup zoneparser
  * @{
+ */
+/*  Copyright (C) 2011 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _KNOTD_PARSER_UTIL_H_
