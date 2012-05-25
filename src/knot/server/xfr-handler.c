d294d54eebc6e82616c7e17075500b9995e83219
Marek Vavrusa <marek.vavrusa@nic.cz>
2012-05-25 14:25:48 +0200

    Fixed missing RCU unlock.

--- src/knot/server/xfr-handler.c
+++ src/knot/server/xfr-handler.c
@@ -1018,6 +1018,7 @@ static int xfr_update_msgpref(knot_ns_xfr_t *req, const char *keytag)
 		zonedata_t *zd = (zonedata_t *)knot_zone_data(req->zone);
 		if (zd == NULL) {
 			free(r_key);
+			conf_read_unlock();
 			return KNOTD_EINVAL;
 		} else {
 			zname = zd->conf->name;
