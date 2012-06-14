91e0cf9ce7f026c2a03ad4363eb27b6af78adc1c
Lubos Slovak <lubos.slovak@nic.cz>
2012-06-14 13:57:27 +0200

    Fixed some return value and parameter checking.

--- src/knot/server/xfr-handler.c
+++ src/knot/server/xfr-handler.c
@@ -1277,6 +1277,10 @@ int xfr_answer(knot_nameserver_t *ns, knot_ns_xfr_t *xfr)
 	}
 	
 	int ret = knot_ns_init_xfr(ns, xfr);
+	if (ret != KNOT_EOK) {
+		return ret;
+	}
+
 	int xfr_failed = (ret != KNOT_EOK);
 	const char * errstr = knot_strerror(ret);
 	
