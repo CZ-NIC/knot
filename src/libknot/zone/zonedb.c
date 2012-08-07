682b85f955c8ef47115541197635be7cff7d371f
Marek Vavrusa <marek.vavrusa@nic.cz>
2012-08-07 16:16:43 +0200

    Implemented reference counting to knot_zone_t.
    refs #1976

--- src/libknot/zone/zonedb.c
+++ src/libknot/zone/zonedb.c
@@ -280,7 +280,8 @@ static void delete_zone_from_db(void *node, void *data)
 	knot_zone_t *zone = (knot_zone_t *)node;
 	assert(zone);
 	synchronize_rcu();
-	knot_zone_deep_free(&zone, 0);
+	knot_zone_release(zone);
+	zone = NULL;
 }
 
 void knot_zonedb_deep_free(knot_zonedb_t **db)
