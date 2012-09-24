5fe4eacf241e40a70a74482952e99b8fc82d3515
Daniel Salzman <daniel.salzman@nic.cz>
2012-09-24 14:37:44 +0200

    Added some tests + small fixes
    refs #2041 @2h

--- src/common/descriptor_new.c
+++ src/common/descriptor_new.c
@@ -51,7 +51,7 @@ static const rdata_descriptor_t rdata_descriptors[] = {
 	[KNOT_RRTYPE_KEY]        = { { 4, KNOT_RDATA_WF_REMAINDER,
 				       KNOT_RDATA_WF_END } },
 	[KNOT_RRTYPE_AAAA]       = { { 16, KNOT_RDATA_WF_END } },
-	[KNOT_RRTYPE_LOC]        = { { KNOT_RDATA_WF_REMAINDER,
+	[KNOT_RRTYPE_LOC]        = { { 16,
 				       KNOT_RDATA_WF_END } },
 	[KNOT_RRTYPE_SRV]        = { { 6, KNOT_RDATA_WF_UNCOMPRESSED_DNAME,
 				       KNOT_RDATA_WF_END } },
--- src/zscanner/test/run_tests.sh
+++ src/zscanner/test/run_tests.sh
@@ -7,6 +7,7 @@ echo $separation
 for file in `/usr/bin/find ./tests/ -name "*.in" | /usr/bin/sort`; do
 	fileout=`echo "$file" | /bin/sed 's/.in/.out/'`
 	../../unittests-zscanner -m 2 . $file > /tmp/_zscanner_test
+	/bin/sed --in-place 's/Zone processing has stopped.*//' /tmp/_zscanner_test
 #	../../unittests-zscanner -m 2 . $file > /tmp/$fileout
 	echo $fileout
 	diff /tmp/_zscanner_test $fileout
