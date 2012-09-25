8880924785c4827e933b5bb359bed973368e9832
Daniel Salzman <daniel.salzman@nic.cz>
2012-09-25 15:12:23 +0200

    Added tests on ORIGIN directive
    refs #2041 @3h

--- src/zscanner/test/run_tests.sh
+++ src/zscanner/test/run_tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+mkdir /tmp/tests
+
 separation="========================================================="
 
 echo $separation
 
 for file in `/usr/bin/find ./tests/ -name "*.in" | /usr/bin/sort`; do
 	fileout=`echo "$file" | /bin/sed 's/.in/.out/'`
-	../../unittests-zscanner -m 2 . $file > /tmp/_zscanner_test
-	/bin/sed --in-place 's/Zone processing has stopped.*//' /tmp/_zscanner_test
-#	../../unittests-zscanner -m 2 . $file > /tmp/$fileout
+	../../unittests-zscanner -m 2 . $file > /tmp/$fileout
+	/bin/sed --in-place 's/Zone processing has stopped.*//' /tmp/$fileout
 	echo $fileout
-	diff /tmp/_zscanner_test $fileout
+	diff /tmp/$fileout $fileout
 	echo $separation
 done
