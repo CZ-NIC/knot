b3bdc165fa9d33f48c46b88303f6a17a3c204557
Daniel Salzman <daniel.salzman@nic.cz>
2013-02-19 15:47:18 +0100

    Fix strptime warning under Linux

--- src/zscanner/test/tests.c
+++ src/zscanner/test/tests.c
@@ -14,6 +14,9 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// strptime under Linux
+#define _XOPEN_SOURCE
+
 #include "zscanner/test/tests.h"
 
 #include <inttypes.h>			// PRIu64
