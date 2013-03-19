63ec3f987e66d9af87cc3d125a631b765806ffcf
Marek Vavrusa <marek.vavrusa@nic.cz>
2013-03-19 12:52:58 +0100

    Implemented proper locking and lookup using <cls,imputed(qname),netblock>.
    Updated documentation and corrected default settings.

--- src/knot/server/rrl.c
+++ src/knot/server/rrl.c
@@ -43,21 +43,6 @@
 /* Enable RRL logging. */
 #define RRL_ENABLE_LOG
 
-/* RRL granular locking. */
-static int rrl_lock_mx(rrl_table_t *t, int lk_id)
-{
-	assert(lk_id > -1);
-	dbg_rrl_verb("%s: locking id '%d'\n", __func__, lk_id);
-	return pthread_mutex_lock(&t->lk[lk_id].mx);
-}
-
-static int rrl_unlock_mx(rrl_table_t *t, int lk_id)
-{
-	assert(lk_id > -1);
-	dbg_rrl_verb("%s: unlocking id '%d'\n", __func__, lk_id);
-	return pthread_mutex_unlock(&t->lk[lk_id].mx);
-}
-
 /* Classification */
 enum {
 	CLS_NULL     = 0 << 0, /* Empty bucket. */
@@ -201,8 +186,11 @@ static int rrl_classify(char *dst, size_t maxlen, const sockaddr_t *a,
 	blklen += sizeof(nb);
 
 	/* Name */
+	uint16_t *nlen = (uint16_t*)(dst + blklen);
+	blklen += sizeof(uint16_t);
 	int len = rrl_clsname(dst + blklen, maxlen - blklen, cls, p, z);
 	if (len < 0) return len;
+	*nlen = len;
 	blklen += len;
 	
 	/* Seed. */
@@ -218,12 +206,14 @@ static int bucket_free(rrl_item_t *b, uint32_t now) {
 	return b->cls == CLS_NULL || (b->time + 1 < now);
 }
 
-static int bucket_match(rrl_item_t *b, uint8_t cls, uint64_t addr)
+static int bucket_match(rrl_item_t *b, rrl_item_t *m)
 {
-	return b->cls == cls && b->pref == addr;
+	return b->cls    == m->cls &&
+	       b->netblk == m->netblk &&
+	       b->qname  == m->qname;
 }
 
-static unsigned find_free(rrl_table_t *t, unsigned i, uint8_t cls, uint64_t addr, uint32_t now)
+static int find_free(rrl_table_t *t, unsigned i, uint32_t now)
 {
 	rrl_item_t *np = t->arr + t->size;
 	rrl_item_t *b = NULL;
@@ -244,6 +234,51 @@ static unsigned find_free(rrl_table_t *t, unsigned i, uint8_t cls, uint64_t addr
 	return i;
 }
 
+static inline unsigned find_match(rrl_table_t *t, uint32_t id, rrl_item_t *m)
+{
+	unsigned f = 0;
+	unsigned d = 0;
+	unsigned match = t->arr[id].hop;
+	while (match != 0) {
+		d = __builtin_ctz(match);
+		f = (id + d) % t->size;
+		if (bucket_match(t->arr + f, m)) {
+			return d;
+		} else {
+			match &= ~(1<<d); /* clear potential match */
+		}
+	}
+
+	return HOP_LEN + 1;
+}
+
+static inline unsigned reduce_dist(rrl_table_t *t, unsigned id, unsigned d, unsigned *f)
+{
+	unsigned rd = HOP_LEN - 1;
+	while (rd > 0) {
+		unsigned s = (t->size + *f - rd) % t->size; /* bucket to be vacated */
+		unsigned o = __builtin_ctz(t->arr[s].hop); /* offset of first valid bucket */
+		if (t->arr[s].hop != 0 && o < rd) {        /* only offsets in <s, f> are interesting */
+			unsigned e = (s + o) % t->size;    /* this item will be displaced to [f] */
+			unsigned keep_hop = t->arr[*f].hop; /* unpredictable padding */
+			memcpy(t->arr + *f, t->arr + e, sizeof(rrl_item_t));
+			t->arr[*f].hop = keep_hop;
+			t->arr[e].cls = CLS_NULL;
+			t->arr[s].hop &= ~(1<<o);
+			t->arr[s].hop |= 1<<rd;
+			*f = e;
+			return d - (rd - o);
+		}
+		--rd;
+	}
+	
+	assert(rd == 0); /* this happens with p=1/fact(HOP_LEN) */
+	*f = id;
+	d = 0; /* force vacate initial element */
+	dbg_rrl("%s: no potential relocation, freeing bucket %u\n", __func__, id);
+	return d;
+}
+
 static void rrl_log_state(const sockaddr_t *a, uint16_t flags, uint8_t cls)
 {
 #ifdef RRL_ENABLE_LOG
@@ -260,18 +295,14 @@ static void rrl_log_state(const sockaddr_t *a, uint16_t flags, uint8_t cls)
 #endif
 }
 
-
-
 rrl_table_t *rrl_create(size_t size)
 {
 	const size_t tbl_len = sizeof(rrl_table_t) + size * sizeof(rrl_item_t);
 	rrl_table_t *t = malloc(tbl_len);
 	if (!t) return NULL;
-	
-	memset(t, 0, tbl_len);
-	t->rate = 0;
-	t->seed = (uint32_t)(tls_rand() * (double)UINT32_MAX);
+	memset(t, 0, sizeof(rrl_table_t));
 	t->size = size;
+	rrl_reseed(t);
 	dbg_rrl("%s: created table size '%zu'\n", __func__, t->size);
 	return t;
 }
@@ -290,25 +321,31 @@ uint32_t rrl_rate(rrl_table_t *rrl)
 	return rrl->rate;
 }
 
-int rrl_setlocks(rrl_table_t *rrl, size_t granularity)
+int rrl_setlocks(rrl_table_t *rrl, unsigned granularity)
 {
 	if (!rrl) return KNOT_EINVAL;
 	assert(!rrl->lk); /* Cannot change while locks are used. */
+	assert(granularity <= rrl->size / 10); /* Due to int. division err. */
+	
+	if (pthread_mutex_init(&rrl->ll, NULL) < 0) {
+		return KNOT_ENOMEM;
+	}
 	
 	/* Alloc new locks. */
-	rrl->lk = malloc(granularity * sizeof(rrl_lock_t));
+	rrl->lk = malloc(granularity * sizeof(pthread_mutex_t));
 	if (!rrl->lk) return KNOT_ENOMEM;
-	memset(rrl->lk, 0, granularity * sizeof(rrl_lock_t));
+	memset(rrl->lk, 0, granularity * sizeof(pthread_mutex_t));
 	
 	/* Initialize. */
 	for (size_t i = 0; i < granularity; ++i) {
-		if (pthread_mutex_init(&rrl->lk[i].mx, NULL) < 0) break;
+		if (pthread_mutex_init(rrl->lk + i, NULL) < 0) break;
 		++rrl->lk_count;
 	}
+
 	/* Incomplete initialization */
 	if (rrl->lk_count != granularity) {
 		for (size_t i = 0; i < rrl->lk_count; ++i) {
-			pthread_mutex_destroy(&rrl->lk[i].mx);
+			pthread_mutex_destroy(rrl->lk + i);
 		}
 		free(rrl->lk);
 		rrl->lk_count = 0;
@@ -321,7 +358,7 @@ int rrl_setlocks(rrl_table_t *rrl, size_t granularity)
 }
 
 rrl_item_t* rrl_hash(rrl_table_t *t, const sockaddr_t *a, rrl_req_t *p,
-                     const knot_zone_t *zone, uint32_t stamp, int *lk)
+                     const knot_zone_t *zone, uint32_t stamp, int *lock)
 {
 	char buf[RRL_CLSBLK_MAXLEN];
 	int len = rrl_classify(buf, sizeof(buf), a, p, zone, t->seed);
@@ -331,83 +368,54 @@ rrl_item_t* rrl_hash(rrl_table_t *t, const sockaddr_t *a, rrl_req_t *p,
 	
 	uint32_t id = hash(buf, len) % t->size;
 	
-	/*! \todo locking is wrong now, need to map to regions and check */
-	*lk = -1;
-	if (t->lk_count > 0) {
-		*lk = id % t->lk_count;
-		rrl_lock_mx(t, *lk);
+	/* Lock for lookup. */
+	pthread_mutex_lock(&t->ll);
+
+	/* Find an exact match in <id, id + HOP_LEN). */
+	uint16_t *qname = (uint16_t*)(buf + sizeof(uint8_t) + sizeof(uint64_t));
+	rrl_item_t match = {
+	        0, *((uint64_t*)(buf + 1)), t->rate,    /* hop, netblk, ntok */
+	        buf[0], RRL_BF_NULL,                    /* cls, flags */
+	        hash((char*)(qname + 1), *qname), stamp /* qname, time*/
+	};
+
+	unsigned d = find_match(t, id, &match);
+	if (d > HOP_LEN) { /* not an exact match, find free element [f] */
+		d = find_free(t, id, stamp);
 	}
 	
-	/* Find an exact match in <id, id+K). */
-	unsigned f = 0;
-	unsigned d = 0;
-	uint64_t nprefix = *((uint64_t*)(buf + sizeof(uint8_t)));
-	unsigned match = t->arr[id].hop;
-	while (match != 0) {
-		d = __builtin_ctz(match);
-		f = (id + d) % t->size;
-		if (bucket_match(t->arr + f, buf[0], nprefix)) {
-			break;
-		} else {
-			match &= ~(1<<d); /* clear potential match */
-		}
-	}
-
-	if (match == 0) { /* not an exact match, find free element [f] */
-		d = find_free(t, id, buf[0], nprefix, stamp); 
-		f = (id + d) % t->size;
-	}
+	/* Reduce distance to fit <id, id + HOP_LEN) */
+	unsigned f = (id + d) % t->size;
 	while (d >= HOP_LEN) {
-		unsigned rd = HOP_LEN - 1;
-		while (rd > 0) {
-			unsigned s = (t->size + f - rd) % t->size; /* bucket to be vacated */
-			unsigned o = __builtin_ctz(t->arr[s].hop); /* offset of first valid bucket */
-			if (t->arr[s].hop != 0 && o < rd) {        /* only offsets in <s, f> are interesting */
-				unsigned e = (s + o) % t->size;    /* this item will be displaced to [f] */
-				unsigned keep_hop = t->arr[f].hop; /* unpredictable padding */
-				memcpy(t->arr + f, t->arr + e, sizeof(rrl_item_t));
-				t->arr[f].hop = keep_hop;
-				t->arr[e].cls = CLS_NULL;
-				t->arr[s].hop &= ~(1<<o);
-				t->arr[s].hop |= 1<<rd;
-				f = e;
-				d -= (rd - o);
-				break;
-			}
-			--rd;
-		}
-		if (rd == 0) { /* this happens with p=1/fact(HOP_LEN) */
-			f = id;
-			d = 0; /* force vacate initial element */
-			dbg_rrl("%s: no potential relocation, freeing bucket %u\n", __func__, id);
-		}
+		d = reduce_dist(t, id, d, &f);
 	}
+	
+	/* Assign granular lock and unlock lookup. */
+	*lock = f % t->lk_count;
+	rrl_lock(t, *lock);
+	pthread_mutex_unlock(&t->ll);
+
 	/* found free elm 'k' which is in <id, id + HOP_LEN) */
 	t->arr[id].hop |= (1 << d);
 	rrl_item_t* b = t->arr + f;
 	assert(f == (id+d) % t->size);
-	dbg_rrl("%s: classified pkt as %u '%u+%u' bucket=%p \n", __func__, f, id, d, b);
+	dbg_rrl("%s: classified pkt as %4x '%u+%u' bucket=%p \n", __func__, f, id, d, b);
 
 	/* Inspect bucket state. */
+	unsigned hop = b->hop;
 	if (b->cls == CLS_NULL) {
-		b->cls = *buf; /* Stored as a first byte in clsblock. */
-		b->flags = RRL_BF_NULL;
-		b->ntok = t->rate;
-		b->time = stamp;
-		b->pref = nprefix; /* Invalidate */
+		memcpy(b, &match, sizeof(rrl_item_t));
+		b->hop = hop;
 	}
 	/* Check for collisions. */
-	/* \todo <prefix, imputed(qname), cls> */
-	if (!bucket_match(b, buf[0], nprefix)) {
-		dbg_rrl("%s: collision in bucket '0x%4x'\n", __func__, id);
+	if (!bucket_match(b, &match)) {
+		dbg_rrl("%s: collision in bucket '%4x'\n", __func__, id);
 		if (!(b->flags & RRL_BF_SSTART)) {
-			b->pref = nprefix;
-			b->cls = *buf;
-			b->flags = RRL_BF_NULL; /* Reset flags. */
-			b->time = stamp; /* Reset time */
+			memcpy(b, &match, sizeof(rrl_item_t));
+			b->hop = hop;
 			b->ntok = t->rate + t->rate / RRL_SSTART;
 			b->flags |= RRL_BF_SSTART;
-			dbg_rrl("%s: bucket '0x%4x' slow-start\n", __func__, id);
+			dbg_rrl("%s: bucket '%4x' slow-start\n", __func__, id);
 		}
 	}
 	
@@ -425,8 +433,8 @@ int rrl_query(rrl_table_t *rrl, const sockaddr_t *a, rrl_req_t *req,
 	uint32_t now = time(NULL);
 	rrl_item_t *b = rrl_hash(rrl, a, req, zone, now, &lock);
 	if (!b) {
-		assert(lock < 0);
 		dbg_rrl("%s: failed to compute bucket from packet\n", __func__);
+		if (lock > -1) rrl_unlock(rrl, lock);
 		return KNOT_ERROR;
 	}
 	
@@ -450,7 +458,6 @@ int rrl_query(rrl_table_t *rrl, const sockaddr_t *a, rrl_req_t *req,
 		/* Add new tokens. */
 		uint32_t dn = rrl->rate * dt;
 		if (b->flags & RRL_BF_SSTART) { /* Bucket in slow-start. */
-			dn /= RRL_SSTART;
 			b->flags &= ~RRL_BF_SSTART;
 			dbg_rrl("%s: bucket '0x%x' slow-start finished\n",
 			        __func__, (unsigned)(b - rrl->arr));
@@ -473,12 +480,8 @@ int rrl_query(rrl_table_t *rrl, const sockaddr_t *a, rrl_req_t *req,
 	} else if (b->ntok == 0) {
 		ret = KNOT_ELIMIT;
 	}
-
-	/* Unlock bucket. */
-	if (lock > -1) {
-		rrl_unlock_mx(rrl, lock);
-	}
 	
+	if (lock > -1) rrl_unlock(rrl, lock);
 	return ret;
 }
 
@@ -486,12 +489,57 @@ int rrl_destroy(rrl_table_t *rrl)
 {
 	if (rrl) {
 		dbg_rrl("%s: freeing table %p\n", __func__, rrl);
+		if (rrl->lk_count > 0) pthread_mutex_destroy(&rrl->ll);
 		for (size_t i = 0; i < rrl->lk_count; ++i) {
-			pthread_mutex_destroy(&rrl->lk[i].mx);
+			pthread_mutex_destroy(rrl->lk + i);
 		}
 		free(rrl->lk);
 	}
 	
 	free(rrl);
+	return KNOT_EOK;
+}
+
+int rrl_reseed(rrl_table_t *rrl)
+{
+	/* Lock entire table. */
+	if (rrl->lk_count > 0) {
+		pthread_mutex_lock(&rrl->ll);
+		for (unsigned i = 0; i < rrl->lk_count; ++i) {
+			rrl_lock(rrl, i);
+		}
+	}
+	
+	memset(rrl->arr, 0, rrl->size * sizeof(rrl_item_t));
+	rrl->seed = (uint32_t)(tls_rand() * (double)UINT32_MAX);
+	dbg_rrl("%s: reseed to '%u'\n", __func__, rrl->seed);
+	
+	if (rrl->lk_count > 0) {
+		for (unsigned i = 0; i < rrl->lk_count; ++i) {
+			rrl_unlock(rrl, i);
+		}
+		pthread_mutex_unlock(&rrl->ll);
+	}
+	
+	return KNOT_EOK;
+}
+
+int rrl_lock(rrl_table_t *t, int lk_id)
+{
+	assert(lk_id > -1);
+	dbg_rrl_verb("%s: locking id '%d'\n", __func__, lk_id);
+	if (pthread_mutex_lock(t->lk + lk_id) != 0) {
+		return KNOT_ERROR;
+	}
+	return KNOT_EOK;
+}
+
+int rrl_unlock(rrl_table_t *t, int lk_id)
+{
+	assert(lk_id > -1);
+	dbg_rrl_verb("%s: unlocking id '%d'\n", __func__, lk_id);
+	if (pthread_mutex_unlock(t->lk + lk_id)!= 0) {
+		return KNOT_ERROR;
+	}
 	return KNOT_EOK;
 }
