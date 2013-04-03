e52e2815dfe7ff18cd026da191fafa43f57738c0
Daniel Salzman <daniel.salzman@nic.cz>
2013-04-03 16:56:10 +0200

    unittests: add zscanner unittest

--- src/zscanner/test/run_tests.sh
+++ src/zscanner/test/run_tests.sh
@@ -2,19 +2,43 @@
 
 TESTS_DIR="./tests"
 OUTS_DIR="/tmp/zscanner_tests"
-TEST_BIN="../../unittests-zscanner -m 2"
+TEST_BIN="../../zscanner-tool -m 2"
 
-SEPARATION="========================================================="
+# If verbose (default - no parameter) mode.
+if [ $# -eq 0 ]; then
+	echo "Launching zscanner tests"
+fi
 
+# Create output directory and copy include zone files.
 mkdir -p ${OUTS_DIR}/${TESTS_DIR}
 cp -r ${TESTS_DIR}/includes ${OUTS_DIR}
 
-echo ${SEPARATION}
-
+# Run zscanner on all test zone files.
 for file in `find ${TESTS_DIR} -name "*.in" | sort`; do
 	fileout=`echo "${file}" | sed 's/.in/.out/'`
+
+	# Run zscanner.
 	${TEST_BIN} . ${file} > ${OUTS_DIR}/${fileout}
-	echo ${fileout}
-	diff ${OUTS_DIR}/${fileout} ${fileout}
-	echo ${SEPARATION}
+
+	# Compare result with a reference one.
+	cmp ${OUTS_DIR}/${fileout} ${fileout} > /dev/null 2>&1
+
+	# Check for differences.
+	if [ $? -ne 0 ]; then
+		# If verbose print diff.
+		if [ $# -eq 0 ]; then
+			echo "\n=== ${fileout} DIFF ======================"
+			diff ${OUTS_DIR}/${fileout} ${fileout}
+		# Return error and exit.
+		else
+			return 1
+		fi
+	fi
 done
+
+if [ $# -eq 0 ]; then
+	echo "\nFinished"
+else
+	return 0
+fi
+
