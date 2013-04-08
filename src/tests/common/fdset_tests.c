3db4ae7286e37c78373b9f8229ae951ed8d20c9d
Daniel Salzman <daniel.salzman@nic.cz>
2013-04-08 14:23:48 +0200

    fdset-test: improve unused warning fix (Clang)

--- src/tests/common/fdset_tests.c
+++ src/tests/common/fdset_tests.c
@@ -83,9 +83,9 @@ void* thr_action(void *arg)
 
 	/* Write pattern. */
 	char pattern = WRITE_PATTERN;
-	/* If removes compile warning. */
-	if (write(*fd, &pattern, WRITE_PATTERN_LEN))
-		;
+	if (write(*fd, &pattern, WRITE_PATTERN_LEN) == -1) {
+		// Error.
+	}
 
 	return NULL;
 }
