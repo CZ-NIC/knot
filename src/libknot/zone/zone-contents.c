c6b8b4ec8f01eaa9fb06384946cdf816d376fac0
Jan Kadlec <jan.kadlec@nic.cz>
2013-04-09 17:26:02 +0200

    Added check to zone adjusting.
    - DS RRSets w\o NS RRSet will trigger error.
    - Refs #2391

--- src/libknot/zone/zone-contents.c
+++ src/libknot/zone/zone-contents.c
@@ -353,6 +353,11 @@ static int knot_zone_contents_adjust_rrsets(knot_node_t *node,
 				dbg_zone("DS RDATA check failed: %s\n", knot_strerror(ret));
 				return KNOT_EMALF;
 			}
+			/* Check that this node contains an NS RR as well. */
+			if (knot_node_rrset(node, KNOT_RRTYPE_NS) == NULL) {
+				dbg_zone("DS RR without NS record.\n");
+				return KNOT_EMALF;
+			}
 		}
 	}
 
