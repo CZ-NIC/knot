bbef208f1e09fded138f1eb4e164ad397cf93398
Marek Vavrusa <marek.vavrusa@nic.cz>
2013-04-24 15:21:01 +0200

    Imported semi-lockless circular queue.
    Author: Rusty Russel <rusty@rustcorp.com.au>
    Licensed under MIT-BSD.
    
    Extended with consumer sleep/wakeup for our use,
    where we want the consumers to sleep instead of
    racing for new packets.
    
    Change-Id: I37b57befcd98e1da6a485aaf48433dc06c68fa5e

---
+++ src/common/queue.c
@@ -0,0 +1,260 @@
+/*
+ * Circular semi-lockless queue from Rusty Russel <rusty@rustcorp.com.au>
+ * https://github.com/rustyrussell/ccan/tree/antithread
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "queue.h"
+#include "config.h"
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+
+#define HAVE_GCC_ATOMICS 1
+#if HAVE_GCC_ATOMICS
+/* Even these will go away with stdatomic.h */
+static unsigned int read_once(unsigned int *ptr, int memmodel)
+{
+	return __atomic_load_n(ptr, memmodel);
+}
+
+static void *read_ptr(void **ptr, int memmodel)
+{
+	return __atomic_load_n(ptr, memmodel);
+}
+
+static void store_once(unsigned int *ptr, unsigned int val, int memmodel)
+{
+	__atomic_store_n(ptr, val, memmodel);
+}
+
+static void store_ptr(void **ptr, void *val, int memmodel)
+{
+	__atomic_store_n(ptr, val, memmodel);
+}
+
+static void atomic_inc(unsigned int *val, int memmodel)
+{
+	__atomic_add_fetch(val, 1, memmodel);
+}
+
+static void atomic_dec(unsigned int *val, int memmodel)
+{
+	__atomic_sub_fetch(val, 1, memmodel);
+}
+
+static bool compare_and_swap(unsigned int *ptr,
+			     unsigned int old, unsigned int new, int memmodel)
+{
+	return __atomic_compare_exchange_n(ptr, &old, new, false,
+					   memmodel, memmodel);
+}
+#else
+#undef __ATOMIC_SEQ_CST
+#undef __ATOMIC_RELAXED
+#undef __ATOMIC_ACQUIRE
+#undef __ATOMIC_RELEASE
+
+/* Overkill, but all-or-nothing keeps it simple. */
+#define __ATOMIC_SEQ_CST 1
+#define __ATOMIC_RELAXED 1
+#define __ATOMIC_ACQUIRE 1
+#define __ATOMIC_RELEASE 0
+
+#ifdef __i386__
+static inline void mb(void)
+{
+	asm volatile ("mfence" : : : "memory");
+}
+#else
+#error implement mb
+#endif
+
+static unsigned int read_once(unsigned int *ptr, bool barrier)
+{
+	if (barrier)
+		mb();
+	return *(volatile unsigned int *)ptr;
+}
+
+static void *read_ptr(void **ptr, bool barrier)
+{
+	if (barrier)
+		mb();
+	return *(void * volatile *)ptr;
+}
+
+static void store_once(unsigned int *ptr, unsigned int val, bool barrier)
+{
+	*(volatile unsigned int *)ptr = val;
+	if (barrier)
+		mb();
+}
+
+static void store_ptr(void **ptr, void *val, bool barrier)
+{
+	*(void * volatile *)ptr = val;
+	if (barrier)
+		mb();
+}
+
+static void atomic_inc(unsigned int *val, bool barrier)
+{
+#ifdef __i386__
+	asm volatile ("lock addl $1, (%0)" : : "r"(val) : "memory");
+#else
+#error implement atomic_inc
+#endif
+}
+
+static void atomic_dec(unsigned int *val, bool barrier)
+{
+#ifdef __i386__
+	asm volatile ("lock subl $1, (%0)" : : "r"(val) : "memory");
+#else
+#error implement atomic_dec
+#endif
+}
+
+static bool compare_and_swap(unsigned int *ptr,
+			     unsigned int old, unsigned int new,
+			     int memmodel)
+{
+#ifdef __i386__
+	unsigned int prev;
+
+	asm volatile ("lock cmpxchgl %1, (%2)"
+		      : "=a"(prev) : "r"(new), "r"(ptr), "a"(old) : "memory");
+	return prev == old;
+#else
+#error implement compare_and_swap
+#endif
+}
+#endif /* ! GCC 4.7 or above */
+
+static void wait_for_change(unsigned int *ptr, unsigned int val)
+{
+	while (read_once(ptr, __ATOMIC_RELAXED) == val);
+}
+
+static void sleep_consumer(struct queue *q)
+{
+	pthread_mutex_lock(&q->mx);
+	pthread_cond_wait(&q->cond, &q->mx);
+	pthread_mutex_unlock(&q->mx);
+}
+
+static void sleep_producer(struct queue *q)
+{
+	/* sleep_consumer(q); */
+}
+
+static void wake_consumer(struct queue *q)
+{
+	pthread_mutex_lock(&q->mx);
+	pthread_cond_signal(&q->cond);
+	pthread_mutex_unlock(&q->mx);
+}
+
+static void wake_producer(struct queue *q)
+{
+	/* wake_consumer(q); */
+}
+
+void queue_init(struct queue *q)
+{
+	memset(q->elems, 0xFF, sizeof(q->elems));
+	q->prod_waiting = q->prod_lock = 0;
+	q->tail = q->cons_waiting = 0;
+	/* We need at least one barrier here. */
+	store_once(&q->head, 0, __ATOMIC_SEQ_CST);
+	pthread_mutex_init(&q->mx, NULL);
+	pthread_cond_init(&q->cond, NULL);
+}
+
+void queue_deinit(struct queue *q)
+{
+	pthread_mutex_destroy(&q->mx);
+	pthread_cond_destroy(&q->cond);
+}
+
+void queue_insert(struct queue *q, void *elem)
+{
+	unsigned int t, h;
+
+again:
+	/* Bottom bit means someone is updating now. */
+	while ((h = read_once(&q->head, __ATOMIC_RELAXED)) & 1) {
+		atomic_inc(&q->prod_waiting, __ATOMIC_SEQ_CST);
+		wait_for_change(&q->head, h);
+		atomic_dec(&q->prod_waiting, __ATOMIC_RELAXED);
+	}
+	t = read_once(&q->tail, __ATOMIC_RELAXED);
+
+	if (h == t + QUEUE_ELEMS * 2) {
+		/* Full.  Wait. */
+		atomic_inc(&q->prod_waiting, __ATOMIC_SEQ_CST);
+		sleep_producer(q);
+		wait_for_change(&q->tail, t);
+		atomic_dec(&q->prod_waiting, __ATOMIC_RELAXED);
+		goto again;
+	}
+
+	/* This tells everyone we're updating. */
+	if (!compare_and_swap(&q->head, h, h+1, __ATOMIC_ACQUIRE))
+		goto again;
+
+	store_ptr(&q->elems[(h/2) % QUEUE_ELEMS], elem, __ATOMIC_RELAXED);
+	assert(read_once(&q->head, __ATOMIC_RELAXED) == h + 1);
+	store_once(&q->head, h+2, __ATOMIC_RELEASE);
+
+	if (read_once(&q->cons_waiting, __ATOMIC_SEQ_CST))
+		wake_consumer(q);
+	return;
+}
+
+void *queue_remove(struct queue *q)
+{
+	unsigned int h, t;
+	void *elem;
+
+	do {
+		for (;;) {
+			/* Read tail before head (reverse how they change) */
+			t = read_once(&q->tail, __ATOMIC_SEQ_CST);
+			h = read_once(&q->head, __ATOMIC_SEQ_CST);
+			if ((h & ~1) != t)
+				break;
+			/* Empty... */
+			atomic_inc(&q->cons_waiting, __ATOMIC_SEQ_CST);
+			sleep_consumer(q);
+			wait_for_change(&q->head, h);
+			atomic_dec(&q->cons_waiting, __ATOMIC_RELAXED);
+		}
+		assert(t < h);
+		elem = read_ptr(&q->elems[(t/2) % QUEUE_ELEMS],
+				__ATOMIC_SEQ_CST);
+	} while (!compare_and_swap(&q->tail, t, t+2, __ATOMIC_SEQ_CST));
+
+	if (q->prod_waiting)
+		wake_producer(q);
+
+	return elem;
+}
