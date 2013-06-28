e3d745cbb0c9d2c75897c07fa6784689ff9afdd8
Jan Vcelak <jan.vcelak@nic.cz>
2013-06-28 14:02:18 +0200

    common lib: add hex string to bytes conversion

---
+++ src/common/hex.h
@@ -0,0 +1,46 @@
+/*  Copyright (C) 2011 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*!
+ * \file hex.h
+ *
+ * \brief Coversion between HEX strings and bytes.
+ *
+ * \author Jan Vcelak <jan.vcelak@nic.cz>
+ *
+ * \addtogroup common_lib
+ * @{
+ */
+
+#ifndef _KNOT_COMMON_HEX_H_
+#define _KNOT_COMMON_HEX_H_
+
+#include <stdint.h>
+#include <stdlib.h>
+
+/*!
+ * \brief Convert string encoded in hex to bytes.
+ *
+ * \param input        Hex encoded input string.
+ * \param output       Decoded bytes.
+ * \param output_size  Size of the output.
+ *
+ * \return Error code, KNOT_EOK if successful.
+ */
+int hex_decode(const char *input, uint8_t **output, size_t *output_size);
+
+#endif // _KNOT_COMMON_HEX_H_
+/*! @} */
