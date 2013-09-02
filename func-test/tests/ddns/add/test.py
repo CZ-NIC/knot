45120880df9f3c4bed7f42ba0e1e92103229158d
Daniel Salzman <daniel.salzman@nic.cz>
2013-09-02 16:03:58 +0200

    func-test: partial ddns add test

--- func-test/tests/ddns/add/test.py
+++ func-test/tests/ddns/add/test.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 
-''' '''
+'''Test for DDNS - add record'''
 
 import dnstest
 
 t = dnstest.DnsTest()
 
-master = t.server("knot")
+srv = t.server("bind")
 zone = t.zone("example.com.", "example.com.zone")
 
-t.link(zone, master, ddns=True)
+t.link(zone, srv, ddns=True)
 
 t.start()
 
-update = master.update(zone)
-update.add("t1.example.com.", 1234, "A", "1.2.3.4")
-update.add("t2.example.com.", 1234, "A", "1.2.3.4")
-update.add("t3.example.com.", 1234, "A", "1.2.3.4")
+update = srv.update(zone)
+
+# Add one record.
+update.add("test1.example.com.", 1234, "A", "1.2.3.4")
 update.send()
-update.delete("t2.example.com.")
+resp = srv.dig("test1.example.com.", "A")
+resp.check("1.2.3.4", 1234)
+
+# Add more records including glue record.
+update.add("test2.example.com.", 1234, "NS", "test2.sub.example.com.")
+update.add("test2.sub.example.com.", 2222, "A", "1.2.3.4")
 update.send()
+resp_a = srv.dig("test2.sub.example.com.", "A")
+resp_a.check("1.2.3.4", 2222)
 
 t.stop()
