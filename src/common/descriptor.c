a8f7bc25eb15823880170ea5add03f35b5751f60
Daniel Salzman <daniel.salzman@nic.cz>
2013-10-10 15:37:24 +0200

    descriptor: reformat function

--- src/common/descriptor.c
+++ src/common/descriptor.c
@@ -320,9 +320,9 @@ int knot_rrtype_is_metatype(const uint16_t type)
 
 int knot_rrtype_is_ddns_forbidden(const uint16_t type)
 {
-	return type == KNOT_RRTYPE_RRSIG  ||
-	type == KNOT_RRTYPE_DNSKEY  ||
-	type == KNOT_RRTYPE_NSEC3PARAM ||
-	type == KNOT_RRTYPE_NSEC ||
-	type == KNOT_RRTYPE_NSEC3;
+	return type == KNOT_RRTYPE_RRSIG      ||
+	       type == KNOT_RRTYPE_DNSKEY     ||
+	       type == KNOT_RRTYPE_NSEC3PARAM ||
+	       type == KNOT_RRTYPE_NSEC       ||
+	       type == KNOT_RRTYPE_NSEC3;
 }
