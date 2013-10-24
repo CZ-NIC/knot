d26a57b6e60d4f649aa0e2d857e9c81890d0ecf8
Marek Vavrusa <marek.vavrusa@nic.cz>
2013-10-24 10:19:09 +0200

    Added test with mangled DNS packets (packet size < header + minimal qst size)

---
+++ tests-extra/functional/tests/security/dnsheader/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+'''DNS packet header parsing tests. '''
+
+import socket
+import dnstest
+
+t = dnstest.DnsTest()
+knot = t.server("knot")
+zone = t.zone("example.com.")
+t.link(zone, knot)
+
+# Update configuration
+knot.ratelimit = 5 # Check for crashes also in rate-limit code
+
+t.start()
+
+# Packet lengths shorter than DNS header
+data = '\x00'
+max_len = (12 + 5) # Header + minimal question size
+udp_socket = knot.create_sock(socket.SOCK_DGRAM)
+for i in range(1, max_len):
+	knot.send_raw(data * i, udp_socket)
+udp_socket.close()
+
+# Check if the server is still alive 
+resp = knot.dig("example.com", "SOA", timeout=5, tries=1)
+resp.check(rcode="NOERROR")
+
+t.end()
