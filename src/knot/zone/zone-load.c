CZ-NIC/knot

commit 427cc820a6559205f7c1b8c37bdfb9e59af4bede

zone-load: fix memory leak if out-of-zone

src/knot/zone/zone-load.c
@@ -163,6 +163,7 @@ static knot_node_t *create_node(knot_zone_contents_t *zone,
 	if (ret != KNOT_EOK) {
 		log_zone_warning("Node could not be added (%s).\n",
 		                 knot_strerror(ret));
+		knot_node_free(&node);
 		return NULL;
 	}
 
@@ -517,6 +518,7 @@ static void process_rr(const scanner_t *scanner)
 			                 zone_name, name);
 			free(name);
 			free(zone_name);
+			knot_rrset_deep_free(&current_rrset, 1, 1);
 			return;
 		}
 	}

commit 3754b84b822396b8b74f56efc99a0991c9df9c28

Temporarily fixed memory errors introduced in 427cc820a6559205f7c1b8c37bdfb9e59af4bede
This is already solved properly in 1.4 branch and it should NOT be
ported to 1.4 branch.

src/knot/zone/zone-load.c
@@ -518,6 +518,8 @@ static void process_rr(const scanner_t *scanner)
 			                 zone_name, name);
 			free(name);
 			free(zone_name);
+			/* This is already deprecated in 1.4, won't create a new API. */
+			hattrie_del(parser->lookup_tree, current_owner->name, current_owner->size);
 			knot_rrset_deep_free(&current_rrset, 1, 1);
 			return;
 		}