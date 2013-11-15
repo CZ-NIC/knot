cf023f4a9075730cb24acd29a3ab0c00583bb436
Jan Vcelak <jan.vcelak@nic.cz>
2013-11-15 18:25:06 +0100

    DNSKEYs signing, add todo about sorting
    ref #103

--- src/libknot/dnssec/zone-sign.c
+++ src/libknot/dnssec/zone-sign.c
@@ -798,6 +798,7 @@ static int add_missing_dnskeys(const knot_rrset_t *soa,
 	}
 
 	if (to_add != NULL && result == KNOT_EOK) {
+		//! \todo Sorting should be handled by changesets application.
 		result = knot_rrset_sort_rdata(to_add);
 		if (result != KNOT_EOK) {
 			knot_rrset_deep_free(&to_add, 1);
