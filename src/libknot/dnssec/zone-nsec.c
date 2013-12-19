53fa4ba4dc14af48b466bdc4c5ca2ed7cc1eec68
Lubos Slovak <lubos.slovak@nic.cz>
2013-12-19 16:26:40 +0100

    There may be no NSEC in a node
    refs #125

--- src/libknot/dnssec/zone-nsec.c
+++ src/libknot/dnssec/zone-nsec.c
@@ -627,6 +627,9 @@ static int remove_nsec_from_node(const knot_node_t *node,
 	assert(chgset);
 
 	const knot_rrset_t *nsec = knot_node_rrset(node, KNOT_RRTYPE_NSEC);
+	if (nsec == NULL) {
+		return KNOT_EOK;
+	}
 	assert(knot_rrset_rdata_rr_count(nsec) <= 1);
 
 	return changeset_remove_nsec(nsec, chgset);
