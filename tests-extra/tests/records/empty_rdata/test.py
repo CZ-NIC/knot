abe59bdf0f469e414f4e30baa1657ecc5c9c1a07
Daniel Salzman <daniel.salzman@nic.cz>
2014-01-07 16:16:21 +0100

    func-tests: add test for empty rdata

---
+++ tests-extra/tests/records/empty_rdata/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''Test for empty rdata loading.'''
+
+from dnstest.test import Test
+
+t = Test()
+
+master = t.server("knot")
+slave = t.server("knot")
+zone = t.zone("empty.", local=True)
+
+t.link(zone, master, slave)
+
+t.start()
+
+master.zones_wait(zone)
+slave.zones_wait(zone)
+
+slave.flush()
+t.sleep(1)
+
+t.end()
