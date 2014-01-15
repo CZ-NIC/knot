44e53a15413ca4face4ef241b9ae1413f155cb0e
Marek Vavrusa <marek.vavrusa@nic.cz>
2014-01-15 14:17:51 +0100

    Fixed PROTOS test using TSIG randomly (not supported).

--- tests-extra/tests/security/protos/test.py
+++ tests-extra/tests/security/protos/test.py
@@ -8,13 +8,19 @@ from dnstest.test import Test
 from dnstest.params import get_binary
 
 # Find PROTOS binaries
+protos_bin = [ "c09-dns-query-r1.jar", "c09-dns-zonetransfer-r1.jar" ]
 protos_java_bin = get_binary("PROTOS_JAVA_BIN", "java")
-protos_query_bin = get_binary("PROTOS_QUERY_BIN", "c09-dns-query-r1.jar")
-protos_zonetransfer_bin = get_binary("PROTOS_ZONETRANSFER_BIN", "c09-dns-zonetransfer-r1.jar")
+protos_query_bin = get_binary("PROTOS_QUERY_BIN", protos_bin[0])
+protos_zonetransfer_bin = get_binary("PROTOS_ZONETRANSFER_BIN", protos_bin[1])
 if not protos_java_bin:
     raise Exception("Java not found, skipping test.")
+if not protos_query_bin:
+    raise Exception("'%s' PROTOS binary not found" % protos_bin[0])
+if not protos_zonetransfer_bin:
+    raise Exception("'%s' PROTOS binary not found" % protos_bin[1])
 
-t = Test(ip = 4) # PROTOS works on IPv4
+
+t = Test(ip=4, tsig=False) # PROTOS works on IPv4, no TSIG
 master = t.server("dummy")
 slave = t.server("knot")
 zone = t.zone("protos.invalid.", exists=False)
