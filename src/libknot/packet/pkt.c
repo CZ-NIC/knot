eea5aa909946885bca7a7d4a02f8f7313bd4c741
Marek Vavrusa <marek.vavrusa@nic.cz>
2014-01-29 14:06:14 +0100

    Don't ignore packets with bad question/header.
    If at least DNS header comes, treat it as a DNS packet; else ignore.
    If query doesn't carry a question, reply with FORMERR.
    If query carries more questions, reply with FORMERR.
    If packet parsing fails, reply with FORMERR.
    If there is a TC/RA/AA flag in query, accept it as soft error.

--- src/libknot/packet/pkt.c
+++ src/libknot/packet/pkt.c
@@ -228,6 +228,7 @@ int knot_pkt_init_response(knot_pkt_t *pkt, const knot_pkt_t *query)
 	knot_wire_clear_tc(pkt->wire);
 	knot_wire_clear_ad(pkt->wire);
 	knot_wire_clear_ra(pkt->wire);
+	knot_wire_clear_aa(pkt->wire);
 
 	/* Clear payload. */
 	pkt_clear_payload(pkt);
@@ -588,9 +589,12 @@ int knot_pkt_parse_question(knot_pkt_t *pkt)
 	
 	/* Check at least header size. */
 	if (pkt->size < KNOT_WIRE_HEADER_SIZE) {
-		dbg_packet("%s: smaller than DNS header, FORMERR\n", __func__);
+		dbg_packet("%s: smaller than DNS header, NOREPLY\n", __func__);
 		return KNOT_EMALF;
 	}
+
+	/* We have at least some DNS header. */
+	pkt->parsed = KNOT_WIRE_HEADER_SIZE;
 
 	/* Check QD count. */
 	uint16_t qd = knot_wire_get_qdcount(pkt->wire);
@@ -598,8 +602,6 @@ int knot_pkt_parse_question(knot_pkt_t *pkt)
 		dbg_packet("%s: QD(%u) > 1, FORMERR\n", __func__, qd);
 		return KNOT_EMALF;
 	}
-
-	pkt->parsed = KNOT_WIRE_HEADER_SIZE;
 
 	/* No question. */
 	if (qd == 0) {
