9c870427984b20116fde230e4cfad2e4949a4c65
Lubos Slovak <lubos.slovak@nic.cz>
2014-02-04 15:31:17 +0100

    Not counting empty RRSets into RRSet count in node
    Was causing problems when RRSet was removed but its RRSIG remained
    in the zone.

--- src/knot/zone/node.c
+++ src/knot/zone/node.c
@@ -111,7 +111,9 @@ int knot_node_add_rrset_no_merge(knot_node_t *node, knot_rrset_t *rrset)
 	}
 	node->rrset_tree = p;
 	node->rrset_tree[node->rrset_count] = rrset;
-	++node->rrset_count;
+	if (knot_rrset_rdata_rr_count(rrset) > 0) {
+		++node->rrset_count;
+	}
 	return KNOT_EOK;
 }
 
