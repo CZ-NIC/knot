4d254c6de4552c349cdbb38938fac26ee5a36df0
Jan Vcelak <jan.vcelak@nic.cz>
2014-03-02 16:39:50 +0100

    [dnssec] legacy API, rename parameters

--- dnssec/src/keys/legacy.c
+++ dnssec/src/keys/legacy.c
@@ -2,14 +2,15 @@
 #include "../error.h"
 #include "../key.h"
 #include "legacy.h"
+#include <gnutls/x509.h>
 
 int dnssec_key_from_rsa_params(dnssec_key_t *key,
 			       dnssec_key_algorithm_t algorithm,
 			       const dnssec_binary_t *modulus,
 			       const dnssec_binary_t *public_exponent,
 			       const dnssec_binary_t *private_exponent,
-			       const dnssec_binary_t *first_prime,
-			       const dnssec_binary_t *second_prime,
+			       const dnssec_binary_t *prime_one,
+			       const dnssec_binary_t *prime_two,
 			       const dnssec_binary_t *coefficient)
 {
 //	int result;
@@ -20,11 +21,11 @@ int dnssec_key_from_rsa_params(dnssec_key_t *key,
 
 int dnssec_key_from_dsa_params(dnssec_key_t *key,
 			       dnssec_key_algorithm_t algorithm,
-			       const dnssec_binary_t *p,
-			       const dnssec_binary_t *q,
-			       const dnssec_binary_t *g,
-			       const dnssec_binary_t *y,
-			       const dnssec_binary_t *x)
+			       const dnssec_binary_t *prime,
+			       const dnssec_binary_t *subprime,
+			       const dnssec_binary_t *base,
+			       const dnssec_binary_t *private_x,
+			       const dnssec_binary_t *private_y)
 {
 //	gnutls_x509_privkey_import_dsa_raw()
 	return DNSSEC_ERROR;
--- dnssec/src/keys/legacy.h
+++ dnssec/src/keys/legacy.h
@@ -8,17 +8,17 @@ int dnssec_key_from_rsa_params(dnssec_key_t *key,
 			       const dnssec_binary_t *modulus,
 			       const dnssec_binary_t *public_exponent,
 			       const dnssec_binary_t *private_exponent,
-			       const dnssec_binary_t *first_prime,
-			       const dnssec_binary_t *second_prime,
+			       const dnssec_binary_t *prime_one,
+			       const dnssec_binary_t *prime_two,
 			       const dnssec_binary_t *coefficient);
 
 int dnssec_key_from_dsa_params(dnssec_key_t *key,
 			       dnssec_key_algorithm_t algorithm,
-			       const dnssec_binary_t *p,
-			       const dnssec_binary_t *q,
-			       const dnssec_binary_t *g,
-			       const dnssec_binary_t *y,
-			       const dnssec_binary_t *x);
+			       const dnssec_binary_t *prime,
+			       const dnssec_binary_t *subprime,
+			       const dnssec_binary_t *base,
+			       const dnssec_binary_t *private_x,
+			       const dnssec_binary_t *private_y);
 
 int dnssec_key_from_ecdsa_params(dnssec_key_t *key,
                                  dnssec_key_algorithm_t algorithm,
