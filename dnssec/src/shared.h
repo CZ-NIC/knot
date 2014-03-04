43f33e3594f691100e104b3d24c1bf0104e84290
Jan Vcelak <jan.vcelak@nic.cz>
2014-03-04 20:44:17 +0100

    [dnssec] improve automatic cleanup functions

--- dnssec/src/shared.h
+++ dnssec/src/shared.h
@@ -1,18 +1,33 @@
 #pragma once
 
 #include <stdio.h>
-
-static inline void fclose_ptr(FILE **handle)
-{
-	fclose(*handle);
-}
-
-#define _unused_ __attribute__((unused))
-#define _cleanup_(var) __attribute__((cleanup(var)))
-#define _destructor_ __attribute__((destructor))
-
-#define _cleanup_free_ _cleanup_(free)
-#define _cleanup_fclose_ _cleanup_(fclose_ptr)
+#include <stdlib.h>
+#include <unistd.h>
 
 #define _public_ __attribute__((visibility("default")))
 #define _hidden_ __attribute__((visibility("hidden")))
+
+#define _unused_ __attribute__((unused))
+#define _cleanup_(var) __attribute__((cleanup(var)))
+
+static inline void close_ptr(int *ptr)
+{
+	if (*ptr != -1) {
+		close(*ptr);
+	}
+}
+static inline void fclose_ptr(FILE **ptr)
+{
+	if (*ptr) {
+		fclose(*ptr);
+	}
+}
+
+static inline void free_ptr(void *ptr)
+{
+	free(*(void **)ptr);
+}
+
+#define _cleanup_free_ _cleanup_(free_ptr)
+#define _cleanup_fclose_ _cleanup_(fclose_ptr)
+#define _cleanup_fclose_ _cleanup_(fclose_ptr)
