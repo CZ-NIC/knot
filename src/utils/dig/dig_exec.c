c948b3141a374fb610ea192b3eb8d9dbf153cd29
Robert S. Edmonds <edmonds@debian.org>
2014-03-24 09:43:16 +0100

    Various spelling and typo fixes

--- src/utils/dig/dig_exec.c
+++ src/utils/dig/dig_exec.c
@@ -409,7 +409,7 @@ static void process_query(const query_t *query)
 	WALK_LIST(server, query->servers) {
 		srv_info_t *remote = (srv_info_t *)server;
 
-		DBG("Quering for owner(%s), class(%u), type(%u), server(%s), "
+		DBG("Querying for owner(%s), class(%u), type(%u), server(%s), "
 		    "port(%s), protocol(%s)\n", query->owner, query->class_num,
 		    query->type_num, remote->name, remote->service,
 		    get_sockname(socktype));
@@ -652,7 +652,7 @@ static void process_query_xfr(const query_t *query)
 	// Use the first nameserver from the list.
 	srv_info_t *remote = HEAD(query->servers);
 
-	DBG("Quering for owner(%s), class(%u), type(%u), server(%s), "
+	DBG("Querying for owner(%s), class(%u), type(%u), server(%s), "
 	    "port(%s), protocol(%s)\n", query->owner, query->class_num,
 	    query->type_num, remote->name, remote->service,
 	    get_sockname(socktype));
