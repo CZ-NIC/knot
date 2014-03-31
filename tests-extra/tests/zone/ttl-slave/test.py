41b79e9ed937259f5b53a4f530402b85325b712d
Daniel Salzman <daniel.salzman@nic.cz>
2014-03-31 13:24:48 +0200

    tests-extra: improve ttl-slave test

--- tests-extra/tests/zone/ttl-slave/test.py
+++ tests-extra/tests/zone/ttl-slave/test.py
@@ -2,6 +2,9 @@
 
 '''Test for mismatched TTLs handling on slave zone load.'''
 
+'''NOTE: dnspython can't keep different TTLs in one rrset. So we can't check
+         the slave server properly.'''
+
 from dnstest.test import Test
 
 t = Test()
@@ -9,7 +12,7 @@ t = Test()
 master = t.server("dummy")
 slave = t.server("knot")
 
-zone = t.zone("ttl-mismatch.", storage=".")
+zone = t.zone("ttl-mismatch.", storage=".", exists=False)
 
 t.link(zone, master, slave)
 
@@ -18,6 +21,8 @@ slave.update_zonefile(zone, version=1)
 
 t.start()
 
-t.sleep(1)
+# Check if the zone was loaded.
+resp = slave.dig("ttl.ttl-mismatch.", "A")
+resp.check(rcode="NOERROR", flags="QR AA", noflags="TC AD RA")
 
 t.end()
