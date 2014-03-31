ceda7d071a1965b2dcc83f6cb909a961675a4c63
Daniel Salzman <daniel.salzman@nic.cz>
2014-03-31 16:24:49 +0200

    tests-extra: add ixfr many zones test

---
+++ tests-extra/tests/ixfr/many_zones/test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+'''Test for IXFR of many zones from Bind to Knot'''
+
+from dnstest.test import Test
+
+t = Test()
+
+master = t.server("bind")
+slave = t.server("knot")
+zones = t.zone_rnd(400, records=10, dnssec=False)
+
+t.link(zones, master, slave, ixfr=True)
+
+t.start()
+
+# Wait for AXFR to slave server.
+serials_init = master.zones_wait(zones)
+slave.zones_wait(zones)
+
+# Update zone files on master.
+for zone in zones:
+    master.update_zonefile(zone, random=True)
+
+master.reload()
+
+t.sleep(20)
+
+# Wait for IXFR to slave.
+master.zones_wait(zones, serials_init)
+slave.zones_wait(zones, serials_init)
+
+# Compare IXFR between servers.
+t.xfr_diff(master, slave, zones, serials_init)
+
+t.end()
