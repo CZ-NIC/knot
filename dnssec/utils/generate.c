037680835173c06d58e84a64e57cac4448d9e31e
Jan Vcelak <jan.vcelak@nic.cz>
2014-04-08 16:27:33 +0200

    [dnssec] sample utility for key generating

---
+++ dnssec/utils/generate.c
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include <dnssec/crypto.h>
+#include <dnssec/error.h>
+#include <dnssec/key.h>
+#include <dnssec/keystore.h>
+
+static void usage(void)
+{
+	fprintf(stderr, "usage: generate <storage-dir>\n");
+}
+
+int main(int argc, char *argv[])
+{
+	int exit_status = 1;
+	int r = 0;
+
+	if (argc != 2) {
+		usage();
+		return 1;
+	}
+
+	const char *storage_dir = argv[1];
+
+	dnssec_crypto_init();
+
+	dnssec_keystore_t *keystore = NULL;
+	r = dnssec_keystore_create_pkcs8_dir(&keystore, storage_dir);
+	if (r != DNSSEC_EOK) {
+		fprintf(stderr, "Cannot open key store (%s).\n", dnssec_strerror(r));
+		goto fail;
+	}
+
+	dnssec_key_id_t key_id = { 0 };
+	r = dnssec_keystore_generate_key(keystore, DNSSEC_KEY_ALGORITHM_RSA_SHA256, 2048, key_id);
+	if (r != DNSSEC_EOK) {
+		fprintf(stderr, "Cannot generate key (%s).\n", dnssec_strerror(r));
+		goto fail;
+	}
+
+	char *key_id_str = NULL;
+	r = dnssec_key_id_to_string(key_id, &key_id_str);
+	if (r != DNSSEC_EOK) {
+		fprintf(stderr, "Cannot get key ID (%s).\n", dnssec_strerror(r));
+		goto fail;
+	}
+	fprintf(stderr, "Generated key id: %s\n", key_id_str);
+	free(key_id_str);
+
+	exit_status = 0;
+
+fail:
+	dnssec_keystore_close(keystore);
+	dnssec_crypto_cleanup();
+
+	return exit_status;
+}
