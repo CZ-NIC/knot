e60335fbba1830f55ee7105482e8a0957d63ca1b
Daniel Salzman <daniel.salzman@nic.cz>
2014-04-24 15:41:55 +0200

    tests-extra: extend ixfr from differences test

---
+++ tests-extra/tests/ixfr/from_differences/06_add_rm_delegation/step.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+'''Add and remove zone delegation.'''
+
+def run(i):
+    i.test.start()
+
+    i.check()
+    i.check_rec("deleg", "NS", rcode="NXDOMAIN")
+    i.check_rec("n.deleg", "A", rcode="NXDOMAIN")
+
+    i.check(1)
+    i.check_rec("deleg", "NS", rcode="NOERROR", nordata="n.deleg.example.com.")
+    i.check_rec("n.deleg", "A", rcode="NOERROR", nordata="1.2.3.4")
+
+    i.check(2)
+    i.check_rec("deleg", "NS", rcode="NXDOMAIN")
+    i.check_rec("n.deleg", "A", rcode="NXDOMAIN")
+
+    i.test.end()
---
+++ tests-extra/tests/ixfr/from_differences/08_none_nsec3_nsec_none/step.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''Unsigned -> signed(NSEC3) -> signed(NSEC) -> unsigned transitions.'''
+
+def run(i):
+    i.test.start()
+
+    i.check()
+
+    i.check(1)
+
+    i.check(2)
+
+    i.check(3)
+
+    i.test.end()
