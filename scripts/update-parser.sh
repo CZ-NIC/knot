c8d7fcc25aff615896b0e20109895257273b6f05
Daniel Salzman <daniel.salzman@nic.cz>
2014-05-05 15:02:10 +0200

    zscanner: remove unused constants from scanner.t0 to cure clang 3.4 warnings

--- scripts/update-parser.sh
+++ scripts/update-parser.sh
@@ -9,6 +9,10 @@ ragel -T0 -s -o $OUT $IN
 sed '/#line/d' $OUT > $OUT.t0
 rm $OUT
 
+# Remove unused constants because of clang 3.4 warnings
+sed -e '/static\ const\ int\ zone_scanner_en_/d' -e '/zone_scanner_first_final/d' $OUT.t0 > ./tmp
+mv -f ./tmp $OUT.t0
+
 ragel -G2 -s -o $OUT $IN
 sed '/#line/d' $OUT > $OUT.g2
 rm $OUT
