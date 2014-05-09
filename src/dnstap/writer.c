88586ee41e8f850d5068ab8cf756acb84bf6ff06
Marek Vavruša <marek.vavrusa@nic.cz>
2014-05-09 17:15:36 +0200

    dnstap: uncommented writer open

--- src/dnstap/writer.c
+++ src/dnstap/writer.c
@@ -59,12 +59,11 @@ dt_writer_t* dt_writer_create(const char *file_path, const char *version)
 	if (writer->fw == NULL) {
 		goto fail;
 	}
-	/*
+
 	res = fstrm_writer_open(writer->fw);
 	if (res != fstrm_res_success) {
 		goto fail;
 	}
-	*/
 
 	return writer;
 fail:
