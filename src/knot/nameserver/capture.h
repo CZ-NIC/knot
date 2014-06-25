de2e61ce3059597a1e5610e1e1ae77348398b4f3
Marek Vavruša <marek.vavrusa@nic.cz>
2014-06-25 17:32:20 +0200

    nameserver: packet capture processor

---
+++ src/knot/nameserver/capture.h
@@ -0,0 +1,44 @@
+/*!
+ * \file capture.h
+ *
+ * \author Marek Vavrusa <marek.vavrusa@nic.cz>
+ *
+ * \brief Simple packet capture processor.
+ *
+ * \addtogroup answer_processing
+ * @{
+ */
+/*  Copyright (C) 2014 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "libknot/processing/process.h"
+#include "libknot/packet/pkt.h"
+
+/* Processing module implementation. */
+const knot_process_module_t *proc_capture_get_module(void);
+#define NS_PROC_CAPTURE proc_capture_get_module()
+#define NS_PROC_CAPTURE_ID 3
+
+/*!
+ * \brief Processing module parameters.
+ */
+struct process_capture_param {
+	knot_pkt_t *sink;        /*!< Container for captured response. */
+};
+
+/*! @} */
