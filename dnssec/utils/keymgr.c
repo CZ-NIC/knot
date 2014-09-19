4126191e1f1a2ae14d9013962b1e50026fdf7b0d
Jan Vcelak <jan.vcelak@nic.cz>
2014-09-19 12:20:26 +0200

    [dnssec] keymgr: new zone initialization
    draft, leaks memory

--- dnssec/utils/keymgr.c
+++ dnssec/utils/keymgr.c
@@ -26,11 +26,15 @@
 
 #include "utils.h"
 
+/* -- global options ------------------------------------------------------- */
+
 struct options {
 	char *kasp_dir;
 };
 
 typedef struct options options_t;
+
+/* -- subcommands processing ----------------------------------------------- */
 
 struct command {
 	const char *name;
@@ -39,12 +43,38 @@ struct command {
 
 typedef struct command command_t;
 
+static int subcommand(const command_t *subcommands, options_t *options,
+			   int argc, char *argv[])
+{
+	assert(subcommands);
+	assert(options);
+	assert(argv);
+
+	if (argc < 1) {
+		error("No command specified");
+		return 1;
+	}
+
+	char *command = argv[0];
+	for (const command_t *cmd = subcommands; cmd->name != NULL; cmd++) {
+		if (strcmp(command, cmd->name) == 0) {
+			fprintf(stderr, "[debug] command '%s'\n", cmd->name);
+			return cmd->callback(options, argc, argv);
+		}
+	}
+
+	error("Invalid command");
+	return 1;
+}
+
+/* -- actions implementation ----------------------------------------------- */
+
 /*
  * keymgr init
  */
-static int main_init(options_t *options, int argc, char *argv[])
+static int cmd_init(options_t *options, int argc, char *argv[])
 {
-	if (argc != 0) {
+	if (argc != 1) {
 		error("Extra parameters supplied.\n");
 		return 1;
 	}
@@ -63,80 +93,118 @@ static int main_init(options_t *options, int argc, char *argv[])
 	return (r == DNSSEC_EOK ? 0 : 1);
 }
 
-static int main_zone(options_t *options, int argc, char *argv[])
+/*
+ * keymgr zone add [--policy <policy>] <name>
+ */
+static int cmd_zone_add(options_t *options, int argc, char *argv[])
 {
-	error("Not implemented.");
-	return 1;
-}
+	char *policy_name = NULL;
 
-static int main_policy(options_t *options, int argc, char *argv[])
-{
-	error("Not implemented.");
-	return 1;
-}
+	static const struct option opts[] = {
+		{ "policy", required_argument, NULL, 'p' },
+		{ NULL }
+	};
 
-static int main_keystore(options_t *options, int argc, char *argv[])
-{
-	error("Not implemented.");
-	return 1;
-}
-
-static int main_key(options_t *options, int argc, char *argv[])
-{
-	error("Not implemented.");
-	return 1;
-}
-
-static int subcommand(const command_t *subcommands, options_t *options,
-			   int argc, char *argv[])
-{
-	assert(subcommands);
-	assert(options);
-	assert(argv);
-
-	if (argc < 1) {
-		error("No command specified");
-		return 1;
-	}
-
-	char *command = argv[0];
-	for (const command_t *cmd = subcommands; cmd->name != NULL; cmd++) {
-		if (strcmp(command, cmd->name) == 0) {
-			return cmd->callback(options, argc - 1, argv + 1);
+	int c = 0;
+	optind = 0;
+	while (c = getopt_long(argc, argv, "+p:", opts, NULL), c != -1) {
+		switch (c) {
+		case 'p':
+			policy_name = optarg;
+			break;
+		case '?':
+			error("Invalid option");
+			return 1;
+		default:
+			assert(0);
 		}
 	}
 
-	error("Invalid command");
+	if (argc != optind + 1) {
+		error("Invalid number of positional arguments.\n");
+		return 1;
+	}
+
+	char *zone_name = argv[optind];
+
+	// create zone
+
+	dnssec_kasp_t *kasp = NULL;
+	dnssec_kasp_init_dir(&kasp);
+
+	int r = dnssec_kasp_open(kasp, options->kasp_dir);
+	if (r != DNSSEC_EOK) {
+		error("dnssec_kasp_open: %s\n", dnssec_strerror(r));
+		return 1;
+	}
+
+	dnssec_kasp_zone_t *zone = dnssec_kasp_zone_new(zone_name);
+	if (!zone) {
+		error("dnssec_kasp_zone_new: %s\n", dnssec_strerror(r));
+		return 1;
+	}
+
+	r = dnssec_kasp_save_zone(kasp, zone);
+	if (r != DNSSEC_EOK) {
+		error("dnssec_kasp_save_zone: %s\n", dnssec_strerror(r));
+		return 1;
+	}
+
+	dnssec_kasp_deinit(kasp);
+
+	return 0;
+}
+
+/*
+ * keymgr zone
+ */
+static int cmd_zone(options_t *options, int argc, char *argv[])
+{
+	static const command_t commands[] = {
+		{ "add", cmd_zone_add },
+		{ NULL }
+	};
+
+	return subcommand(commands, options, argc - 1, argv + 1);
+}
+
+static int cmd_policy(options_t *options, int argc, char *argv[])
+{
+	error("Not implemented.");
 	return 1;
 }
 
-static const command_t main_commands[] = {
-	{ "init",     main_init },
-	{ "zone",     main_zone },
-	{ "policy",   main_policy },
-	{ "keystore", main_keystore },
-	{ "key",      main_key },
-	{ NULL }
-};
+static int cmd_keystore(options_t *options, int argc, char *argv[])
+{
+	error("Not implemented.");
+	return 1;
+}
+
+static int cmd_key(options_t *options, int argc, char *argv[])
+{
+	error("Not implemented.");
+	return 1;
+}
 
 int main(int argc, char *argv[])
 {
 	int exit_code = 1;
 
 	// global configuration
+
 	options_t options = { 0 };
 	options.kasp_dir = getcwd(NULL, 0);
 	assert(options.kasp_dir);
 
 	// global options
-	static struct option opts[] = {
+
+	static const struct option opts[] = {
 		{ "dir", required_argument, NULL, 'd' },
 		{ NULL }
 	};
 
 	int c = 0;
-	int opt_index = 0;
-	while (c = getopt_long(argc, argv, "+", opts, &opt_index), c != -1) {
+	while (c = getopt_long(argc, argv, "+", opts, NULL), c != -1) {
 		switch (c) {
 		case 'd':
 			free(options.kasp_dir);
@@ -149,7 +217,18 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	exit_code = subcommand(main_commands, &options, argc - optind, argv + optind);
+	// subcommands
+
+	static const command_t commands[] = {
+		{ "init",     cmd_init },
+		{ "zone",     cmd_zone },
+		{ "policy",   cmd_policy },
+		{ "keystore", cmd_keystore },
+		{ "key",      cmd_key },
+		{ NULL }
+	};
+
+	exit_code = subcommand(commands, &options, argc - optind, argv + optind);
 
 failed:
 	free(options.kasp_dir);
