f4837af85f2297e9d7af1c0fc07baf9ae8f5e6d0
Jan Vcelak <jan.vcelak@nic.cz>
2014-10-10 13:36:49 +0200

    DNSSEC tests: new test for case sensitivity of domain names

---
+++ tests-extra/tests/dnssec/case_sensitivity/test.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+'''Test for no resigning if the zone is properly signed.'''
+
+from dnstest.utils import set_err
+from dnstest.test import Test
+import subprocess
+
+def patch_zone(t, server, zone, script):
+    """
+    Update zone file on a master server.
+    """
+    zone = zone[0]
+    zonefile = "%s/master/%s" % (server.dir, zone.file_name)
+    modify_script = "%s/modify.sh" % t.data_dir
+    patch_script = "%s/%s" % (t.data_dir, script)
+    subprocess.check_call([modify_script, zonefile, patch_script])
+
+t = Test()
+
+server = t.server("knot")
+zone = t.zone("example.", storage=".")
+
+server.dnssec_enable = True
+server.gen_key(zone, ksk=True)
+server.gen_key(zone)
+
+t.link(zone, server)
+
+t.start()
+
+serial = server.zone_wait(zone)
+
+scripts = [
+    ("insensitive RRs", "modify-insensitive.awk", False),
+    ("NSEC RR", "modify-nsec.awk", True),
+    ("LP RR", "modify-lp.awk", True),
+]
+
+for name, script, resign in scripts:
+    server.flush()
+    server.stop()
+    patch_zone(t, server, zone, script)
+    server.start()
+
+    new_serial = server.zone_wait(zone)
+    signed = new_serial != serial
+
+    if signed != resign:
+        set_err("Invalid state after %s change" % name)
+        break
+
+    serial = new_serial
+
+t.stop()
