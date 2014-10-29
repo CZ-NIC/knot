58b7f6ccc8b70a4ca5f3eeb1d2b0f3784e08e9b1
Marek Vavruša <marek.vavrusa@nic.cz>
2014-10-29 17:16:38 +0100

    rosedb: tool supports file import

--- src/knot/modules/rosedb_tool.c
+++ src/knot/modules/rosedb_tool.c
@@ -4,25 +4,29 @@
 #include "knot/modules/rosedb.c"
 #include "zscanner/scanner.h"
 #include "common/mem.h"
+#include "common/getline.h"
 
-static int rosedb_add(struct cache *cache, int argc, char *argv[]);
-static int rosedb_del(struct cache *cache, int argc, char *argv[]);
-static int rosedb_get(struct cache *cache, int argc, char *argv[]);
-static int rosedb_list(struct cache *cache, int argc, char *argv[]);
+static int rosedb_add(struct cache *cache, MDB_txn *txn, int argc, char *argv[]);
+static int rosedb_del(struct cache *cache, MDB_txn *txn, int argc, char *argv[]);
+static int rosedb_get(struct cache *cache, MDB_txn *txn, int argc, char *argv[]);
+static int rosedb_list(struct cache *cache, MDB_txn *txn, int argc, char *argv[]);
+static int rosedb_import(struct cache *cache, MDB_txn *txn, int argc, char *argv[]);
 
 struct tool_action {
 	const char *name;
-	int (*func)(struct cache *, int, char *[]);
+	int (*func)(struct cache *, MDB_txn *, int, char *[]);
 	int min_args;
 	const char *info;
 };
 
-#define TOOL_ACTION_COUNT 4
+#define TOOL_ACTION_MAXARG 7
+#define TOOL_ACTION_COUNT 5
 static struct tool_action TOOL_ACTION[TOOL_ACTION_COUNT] = {
-{ "add",  rosedb_add, 6, "<zone> <rrtype> <ttl> <rdata> <threat_code> <syslog_ip>" },
-{ "del",  rosedb_del, 1, "<zone> [rrtype]" },
-{ "get",  rosedb_get, 1, "<zone> [rrtype]" },
-{ "list", rosedb_list, 0, "" },
+{ "add",    rosedb_add,    6, "<zone> <rrtype> <ttl> <rdata> <threat_code> <syslog_ip>" },
+{ "del",    rosedb_del,    1, "<zone> [rrtype]" },
+{ "get",    rosedb_get,    1, "<zone> [rrtype]" },
+{ "import", rosedb_import, 1, "<file>" },
+{ "list",   rosedb_list,   0, "" }
 };
 
 static int help(void)
@@ -66,11 +70,20 @@ int main(int argc, char *argv[])
 			if (argc < ta->min_args) {
 				return help();
 			}
+			
+			MDB_txn *txn = NULL;
+			int ret = mdb_txn_begin(cache->env, NULL, 0, &txn);
+			if (ret != 0) {
+				return ret;
+			}			
 
 			found = true;
-			ret = ta->func(cache, argc, argv);
+			ret = ta->func(cache, txn, argc, argv);
 			if (ret != 0) {
 				fprintf(stderr, "FAILED\n");
+				mdb_txn_abort(txn);
+			} else {
+				mdb_txn_commit(txn);
 			}
 
 			break;
@@ -121,7 +134,7 @@ static int parse_rdata(struct entry *entry, const char *owner, const char *rrtyp
 	return ret;
 }
 
-static int rosedb_add(struct cache *cache, int argc, char *argv[])
+static int rosedb_add(struct cache *cache, MDB_txn *txn, int argc, char *argv[])
 {
 	printf("ADD %s\t%s\t%s\t%s\t%s\t%s\n", argv[0], argv[1], argv[2], argv[3], argv[4], argv[5]);
 
@@ -134,55 +147,44 @@ static int rosedb_add(struct cache *cache, int argc, char *argv[])
 	entry.threat_code = argv[4];
 	entry.syslog_ip   = argv[5];
 	if (ret != 0) {
-		return ret;
-	}
-
-	MDB_txn *txn = NULL;
-	ret = mdb_txn_begin(cache->env, NULL, 0, &txn);
-	if (ret != 0) {
+		fprintf(stderr, "PARSE: %s\n", knot_strerror(ret));
 		return ret;
 	}
 
 	ret = cache_insert(txn, cache->dbi, key, &entry);
-
-	mdb_txn_commit(txn);
-
+	if (ret != 0) {
+		fprintf(stderr, "%s\n", mdb_strerror(ret));
+	}
+	
 	return ret;
 }
 
-static int rosedb_del(struct cache *cache, int argc, char *argv[])
+static int rosedb_del(struct cache *cache, MDB_txn *txn, int argc, char *argv[])
 {
 	printf("DEL %s\n", argv[0]);
 
-	MDB_txn *txn = NULL;
-	int ret = mdb_txn_begin(cache->env, NULL, 0, &txn);
-	if (ret != 0) {
-		return ret;
-	}
-
 	knot_dname_t key[KNOT_DNAME_MAXLEN] = { '\0' };
 	knot_dname_from_str(key, argv[0], sizeof(key));
-	ret = cache_remove(txn, cache->dbi, key);
-
-	mdb_txn_commit(txn);
-
+	knot_dname_to_lower(key);
+	
+	int ret = cache_remove(txn, cache->dbi, key);
+	if (ret != 0) {
+		fprintf(stderr, "%s\n", mdb_strerror(ret));
+	}
+	
 	return ret;
 }
 
-static int rosedb_get(struct cache *cache, int argc, char *argv[])
+static int rosedb_get(struct cache *cache, MDB_txn *txn, int argc, char *argv[])
 {
-	MDB_txn *txn = NULL;
-	int ret = mdb_txn_begin(cache->env, NULL, MDB_RDONLY, &txn);
-	if (ret != 0) {
-		return ret;
-	}
-
 	knot_dname_t key[KNOT_DNAME_MAXLEN] = { '\0' };
 	knot_dname_from_str(key, argv[0], sizeof(key));
+	knot_dname_to_lower(key);
+	
 	char type_str[16] = { '\0' };
 
 	struct iter it;
-	ret = cache_query_fetch(txn, cache->dbi, &it, key);
+	int ret = cache_query_fetch(txn, cache->dbi, &it, key);
 	while (ret == 0) {
 		struct entry entry;
 		cache_iter_val(&it, &entry);
@@ -196,25 +198,18 @@ static int rosedb_get(struct cache *cache, int argc, char *argv[])
 	}
 
 	cache_iter_free(&it);
-	mdb_txn_abort(txn);
 
 	return ret;
 }
 
-static int rosedb_list(struct cache *cache, int argc, char *argv[])
+static int rosedb_list(struct cache *cache, MDB_txn *txn, int argc, char *argv[])
 {
-	MDB_txn *txn = NULL;
-	int ret = mdb_txn_begin(cache->env, NULL, MDB_RDONLY, &txn);
-	if (ret != 0) {
-		return ret;
-	}
-
 	MDB_cursor *cursor = cursor_acquire(txn, cache->dbi);
 	MDB_val key, data;
 	char dname_str[KNOT_DNAME_MAXLEN] = {'\0'};
 	char type_str[16] = { '\0' };
 
-	ret = mdb_cursor_get(cursor, &key, &data, MDB_FIRST);
+	int ret = mdb_cursor_get(cursor, &key, &data, MDB_FIRST);
 	while (ret == 0) {
 		struct entry entry;
 		unpack_entry(&data, &entry);
@@ -227,7 +222,90 @@ static int rosedb_list(struct cache *cache, int argc, char *argv[])
 	}
 
 	cursor_release(cursor);
-	mdb_txn_abort(txn);
 
 	return KNOT_EOK;
+}
+
+static char *trim(char *line)
+{
+	int last = strlen(line) - 1;
+	if (line[last] == '\n') {
+		line[last] = '\0';
+		last -= 1;
+	}
+	if (*line == '"') {
+		line[last] = '\0';
+		line += 1;
+	}
+	
+	return line;
+}
+
+static int rosedb_import_line(struct cache *cache, MDB_txn *txn, char *line)
+{
+	int ret = 0;
+	int argc = 0;
+	char *argv[TOOL_ACTION_MAXARG];
+	
+	/* Tokenize */
+	char *saveptr = line;
+	char *token = NULL;
+	while ((token = strtok_r(saveptr, " ", &saveptr)) != NULL) {
+		token = trim(token);
+		if (*token == '\0') {
+			continue;
+		}
+		if (argc <= TOOL_ACTION_MAXARG) {
+			argv[argc] = token;
+			argc += 1;
+		} else {
+			fprintf(stderr, "PARSE: '%s', too much parameters.\n", line);
+			return KNOT_EPARSEFAIL;
+		}
+	}
+	
+	if (argc < 1) {
+		return KNOT_EOK; /* Ignore NOOP */
+	}
+
+	/* Execute action. */
+	for (unsigned i = 0; i < TOOL_ACTION_COUNT; ++i) {
+		struct tool_action *ta = &TOOL_ACTION[i];
+		if (strcmp(ta->name, argv[0]) == 0) {
+			if (argc < ta->min_args) {
+				return help();
+			}
+			ret = ta->func(cache, txn, argc - 1, argv + 1);
+			break;
+		}
+	}
+
+	return ret;
+}
+
+static int rosedb_import(struct cache *cache, MDB_txn *txn, int argc, char *argv[])
+{
+	printf("IMPORT %s\n", argv[0]);
+	
+	int ret = 0;
+	char *line = NULL;
+	size_t line_len = 0;
+	ssize_t rb = 0;
+	FILE *fp = fopen(argv[0], "r");
+	if (fp == NULL) {
+		return KNOT_ENOENT;
+	}
+	
+	while ((rb = knot_getline(&line, &line_len, fp)) != -1) {
+		ret = rosedb_import_line(cache, txn, line);
+		if (ret != 0) {
+			assert(0);
+			break;
+		}
+	}
+
+	free(line);
+	fclose(fp);
+	
+	return ret;
 }
