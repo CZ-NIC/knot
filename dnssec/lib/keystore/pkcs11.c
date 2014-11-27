4a6de6cdce0e5081ef1ea599e01033e3f59fba12
Jan Vcelak <jan.vcelak@nic.cz>
2014-11-27 20:05:05 +0100

    [dnssec] simplify PKCS 11 placeholder non-implementation

--- dnssec/lib/keystore/pkcs11.c
+++ dnssec/lib/keystore/pkcs11.c
@@ -21,65 +21,15 @@
 #include "keystore/internal.h"
 #include "shared.h"
 
-/*!
- * PKCS #11 key store context.
- */
-typedef struct pkcs11_ctx {
-	int tmp;
-} pkcs11_ctx_t;
-
 /* -- internal API --------------------------------------------------------- */
 
 static int pkcs11_ctx_new(void **ctx_ptr, _unused_ void *data)
-{
-	pkcs11_ctx_t *ctx = calloc(1, sizeof(*ctx));
-	if (!ctx) {
-		return DNSSEC_ENOMEM;
-	}
-
-	*ctx_ptr = ctx;
-	return DNSSEC_EOK;
-}
-
-static void pkcs11_ctx_free(void *ctx)
-{
-	free(ctx);
-}
-
-static int pkcs11_open(void *_ctx, const char *config)
-{
-	return DNSSEC_NOT_IMPLEMENTED_ERROR;
-}
-
-static int pkcs11_close(void *_ctx)
-{
-	return DNSSEC_NOT_IMPLEMENTED_ERROR;
-}
-
-static int pkcs11_list_keys(void *ctx, void *list)
-{
-	return DNSSEC_NOT_IMPLEMENTED_ERROR;
-}
-
-static int pkcs11_generate_key(void *_ctx, gnutls_pk_algorithm_t algorithm,
-			       unsigned bits, char **id_ptr)
-{
-	return DNSSEC_NOT_IMPLEMENTED_ERROR;
-}
-
-static int pkcs11_delete_key(void *_ctx, const char *id)
 {
 	return DNSSEC_NOT_IMPLEMENTED_ERROR;
 }
 
 const keystore_functions_t PKCS11_FUNCTIONS = {
 	.ctx_new = pkcs11_ctx_new,
-	.ctx_free = pkcs11_ctx_free,
-	.open = pkcs11_open,
-	.close = pkcs11_close,
-	.list_keys = pkcs11_list_keys,
-	.generate_key = pkcs11_generate_key,
-	.delete_key = pkcs11_delete_key,
 };
 
 /* -- public API ----------------------------------------------------------- */
