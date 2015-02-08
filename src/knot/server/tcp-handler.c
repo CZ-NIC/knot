2573412a8c9ea288538f9f3e3d4be1a643a3865e
Jan Vcelak <jan.vcelak@nic.cz>
2015-02-08 20:50:08 +0100

    TCP: minor log message updates

--- src/knot/server/tcp-handler.c
+++ src/knot/server/tcp-handler.c
@@ -77,7 +77,7 @@ static enum fdset_sweep_state tcp_sweep(fdset_t *set, int i, void *data)
 	if (getpeername(fd, (struct sockaddr*)&ss, &len) == 0) {
 		char addr_str[SOCKADDR_STRLEN] = {0};
 		sockaddr_tostr(&ss, addr_str, sizeof(addr_str));
-		log_notice("connection terminated due to inactivity, address '%s'", addr_str);
+		log_notice("TCP, terminated inactive client, address '%s'", addr_str);
 	}
 
 	close(fd);
@@ -121,9 +121,8 @@ static int tcp_handle(tcp_context_t *tcp, int fd,
 			rcu_read_lock();
 			char addr_str[SOCKADDR_STRLEN] = {0};
 			sockaddr_tostr(&ss, addr_str, sizeof(addr_str));
-			log_warning("connection timed out, address '%s', "
-			            "timeout %d seconds",
-			            addr_str, conf()->max_conn_idle);
+			log_warning("TCP, connection timed out, address '%s'",
+			            addr_str);
 			rcu_read_unlock();
 		}
 		return KNOT_ECONNREFUSED;
@@ -180,8 +179,8 @@ int tcp_accept(int fd)
 		rcu_read_unlock();
 		tv.tv_usec = 0;
 		if (setsockopt(incoming, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
-			log_warning("cannot set up TCP connection watchdog "
-			            "timer, fd %d", incoming);
+			log_warning("TCP, failed to set up watchdog timer"
+			            ", fd %d", incoming);
 		}
 #endif
 	}
