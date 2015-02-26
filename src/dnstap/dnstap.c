37a963f32f162a0d250196113409e01a8aa83d74
Daniel Salzman <daniel.salzman@nic.cz>
2015-02-26 11:05:36 +0100

    dnstap: fix variable initialization warning

--- src/dnstap/dnstap.c
+++ src/dnstap/dnstap.c
@@ -23,7 +23,7 @@
 
 uint8_t* dt_pack(const Dnstap__Dnstap *d, uint8_t **buf, size_t *sz)
 {
-	ProtobufCBufferSimple sbuf = {0};
+	ProtobufCBufferSimple sbuf = { { NULL } };
 
 	sbuf.base.append = protobuf_c_buffer_simple_append;
 	sbuf.len = 0;
