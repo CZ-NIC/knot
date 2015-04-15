e6930e77d8fd28b57d30b59b9d0367ff8b5a1ec4
Jan Vcelak <jan.vcelak@nic.cz>
2015-04-15 22:39:12 +0200

    update-man.sh: remove bold from definition lists

--- scripts/update-man.sh
+++ scripts/update-man.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# The script
+# 1. Generates manual pages from Sphinx.
+# 2. Removes bold from definition lists (bug in Sphinx).
+#    https://github.com/sphinx-doc/sphinx/issues/1498
+# 3. Replaces version placeholders
+#
 
 pushd ../doc
 
@@ -7,7 +14,9 @@ make man
 for f in ./_build/man/*; do
   file=`basename $f`
   echo "Processing '${file}' file..."
-  sed -e "s/__VERSION__/@VERSION@/g; s/__DATE__/@RELEASE_DATE@/g" ${f} > ../man/${file}.in
+  sed -e '/^.TP$/{n;s/^.B //}' \
+      -e "s/__VERSION__/@VERSION@/g; s/__DATE__/@RELEASE_DATE@/g" \
+      "${f}" > "../man/${file}.in"
 done
 
 popd
