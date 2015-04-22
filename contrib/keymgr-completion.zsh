7d7b353dc1f6e48e97344ed70b4a55e92614ff00
Dominik Taborsky <dominik.taborsky@nic.cz>
2015-04-22 15:44:05 +0200

    contrib: ZSH compl. script cleanup.

--- contrib/keymgr-completion.zsh
+++ contrib/keymgr-completion.zsh
@@ -10,28 +10,26 @@ zones=( \( $matchany ":zones:zones:$zone_list" \) )
 
 local -a keystore_cmds
 _regex_words keystore-commands "keystore commands" \
-    'l*ist:list all keys'
+	'l*ist:list all keys'
 keystore_cmds=("$reply[@]")
-
 
 local -a policy_cmds
 _regex_words policy-commands "policy commands" \
-    'a*dd:add a policy' \
-    'l*ist:list all added policies' \
-    'r*emove:remove a policy:$policies' \
-    'se*t:set policy attributes:$policies' \
-    'sh*ow:show policy attributes::$policies'
+	'a*dd:add a policy' \
+	'l*ist:list all added policies' \
+	'r*emove:remove a policy:$policies' \
+	'se*t:set policy attributes:$policies' \
+	'sh*ow:show policy attributes::$policies'
 policy_cmds=("$reply[@]")
-
 
 local -a zone_cmds
 _regex_words zone-commands "zone commands" \
-    'a*dd:add a zone' \
-    'k*ey:manipulate zone keys:$zones' \
-    'l*ist:list all added zones' \
-    'r*emove:remove a zone:$zones' \
-    'se*t:set zone attributes:$zones' \
-    'sh*ow:show zone attributes::$zones'
+	'a*dd:add a zone' \
+	'k*ey:manipulate zone keys:$zones' \
+	'l*ist:list all added zones' \
+	'r*emove:remove a zone:$zones' \
+	'se*t:set zone attributes:$zones' \
+	'sh*ow:show zone attributes::$zones'
 zone_cmds=("$reply[@]")
 
 # Arguments to _regex_arguments, built up in array $args.
@@ -40,16 +38,12 @@ local -a args reply
 args=( $matchany )
 
 _regex_words commands "keymgr command" \
-  'i*nit:initialize KASP directory and default keystore' \
-  'k*eystore:manipulate keystore:$keystore_cmds' \
-  'p*olicy:manipulate policies:$policy_cmds' \
-  'z*one:manipule zones:$zone_cmds'
+	'i*nit:initialize KASP directory and default keystore' \
+	'k*eystore:manipulate keystore:$keystore_cmds' \
+	'p*olicy:manipulate policies:$policy_cmds' \
+	'z*one:manipule zones:$zone_cmds'
 args+=("$reply[@]")
 
 _regex_arguments _keymgr "${args[@]}"
 
 _keymgr "$@"
-
-# Local Variables: 
-# mode:shell-script
-# End:             
