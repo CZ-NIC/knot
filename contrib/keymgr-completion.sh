f761000c46014379315679d00863d491e76cadf8
Dominik Taborsky <dominik.taborsky@nic.cz>
2015-04-22 10:46:45 +0200

    contrib: bash_completion fixed indentation.

--- contrib/keymgr-completion.sh
+++ contrib/keymgr-completion.sh
@@ -2,86 +2,86 @@
 
 _keymgr() 
 {
-    local cur prev words cword
-    _init_completion || return
+	local cur prev words cword
+	_init_completion || return
 
-    
-    case $prev in
-        -V|-version)
-            return 0
-            ;;
-        -h|--help)
-            return 0
-            ;;
-        -d|--dir)
-            _filedir
-            return 0
-            ;;
-    esac
 
-    local subcword cmd subcmd
-    for (( subcword=1; subcword < ${#words[@]}-1; subcword++ )); do
-        [[ ${words[subcword]} == @(-d|--dir) ]] && return 0
-        [[ -n $cmd ]] && subcmd=${words[subcword]} && break
-        [[ ${words[subcword]} != -* ]] && \
-            cmd=${words[subcword]}
-    done
+	case $prev in
+		-V|-version)
+			return 0
+			;;
+		-h|--help)
+			return 0
+			;;
+		-d|--dir)
+			_filedir
+			return 0
+			;;
+	esac
 
-    if [[ -z $cmd ]]; then
-        case $cur in
-            -*)
-                local c="--version --help --dir"
-                COMPREPLY=( $( compgen -W "$c" -- "$cur" ) )
-                return 0
-                ;;
-            *)
+	local subcword cmd subcmd
+	for (( subcword=1; subcword < ${#words[@]}-1; subcword++ )); do
+		[[ ${words[subcword]} == @(-d|--dir) ]] && return 0
+		[[ -n $cmd ]] && subcmd=${words[subcword]} && break
+		[[ ${words[subcword]} != -* ]] && \
+			cmd=${words[subcword]}
+	done
+
+	if [[ -z $cmd ]]; then
+		case $cur in
+			-*)
+				local c="--version --help --dir"
+				COMPREPLY=( $( compgen -W "$c" -- "$cur" ) )
+				return 0
+				;;
+			*)
 				local c="init zone policy keystore"
-                COMPREPLY=( $( compgen -W "$c" -- "$cur" ) )
-                return 0
-                ;;
-        esac
-    fi
+				COMPREPLY=( $( compgen -W "$c" -- "$cur" ) )
+				return 0
+				;;
+		esac
+	fi
 
-    [[ $subcmd == help ]] && return 0
+	[[ $subcmd == help ]] && return 0
 
-    case $cmd in
-        init)
-            ;;
-        keystore)
-            case $subcmd in
-                list)
+	case $cmd in
+		init)
+			;;
+		keystore)
+			case $subcmd in
+				list)
 					;;
 				*)
 					[[ $cword -eq $subcword ]] && \
-                        COMPREPLY=( $( compgen -W 'list' \
-                        -- "$cur" ) )
-                    ;;
-            esac
-            ;;
-        policy)
-            case $subcmd in
-                add|list|remove|set|show)
-                    # TODO
-                    ;;
-                *)
-                    [[ $cword -eq $subcword ]] && \
-                        COMPREPLY=( $( compgen -W 'add list remove set show' \
-                        -- "$cur" ) )
-                    ;;
-            esac
-            ;;
-        zone)
-            case $subcmd in
-                add|key|list|remove|set|show)
-                    # TODO
-                    ;;
-                *)
-                    [[ $cword -eq $subcword ]] && \
-                        COMPREPLY=( $( compgen -W 'add key list remove set show' \
-                        -- "$cur" ) )
-                    ;;
-            esac
-            ;;
-    esac
+						COMPREPLY=( $( compgen -W 'list' \
+						-- "$cur" ) )
+					;;
+			esac
+			;;
+		policy)
+			case $subcmd in
+				add|list|remove|set|show)
+					# TODO
+					;;
+				*)
+					[[ $cword -eq $subcword ]] && \
+						COMPREPLY=( $( compgen -W 'add list remove set show' \
+						-- "$cur" ) )
+					;;
+			esac
+			;;
+		zone)
+			case $subcmd in
+				add|key|list|remove|set|show)
+					# TODO
+					;;
+				*)
+					[[ $cword -eq $subcword ]] && \
+						COMPREPLY=( $( compgen -W 'add key list remove set show' \
+						-- "$cur" ) )
+					;;
+			esac
+			;;
+	esac
 }
 complete -F _keymgr keymgr
