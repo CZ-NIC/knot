006a8f08024a8f3c5a77069ff6198c04888b3b1b
Jan Vcelak <jan.vcelak@nic.cz>
2015-04-24 12:55:27 +0200

    DNSSEC: fix compilation with LibreSSL
    Closes #335

--- src/libknot/dnssec/crypto.c
+++ src/libknot/dnssec/crypto.c
@@ -122,13 +122,22 @@ static unsigned long openssl_threadid_cb(void)
 
 /*- pluggable engines -------------------------------------------------------*/
 
-#if KNOT_ENABLE_GOST
+#if KNOT_ENABLE_GOST && !defined(LIBRESSL_VERSION_NUMBER)
+  #define KNOT_LOAD_GOST 1
+#else
+  #undef KNOT_LOAD_GOST
+#endif
+
+
+#if KNOT_LOAD_GOST
 
 static ENGINE *gost_engine = NULL;
 
 static void init_gost_engine(void)
 {
 	assert(gost_engine == NULL);
+
+	ENGINE_load_builtin_engines();
 
 #ifndef OPENSSL_NO_STATIC_ENGINE
 	ENGINE_load_gost();
@@ -206,16 +215,19 @@ void knot_crypto_cleanup_threads(void)
 
 void knot_crypto_load_engines(void)
 {
-#if KNOT_ENABLE_GOST
+#if KNOT_LOAD_GOST
 	if (!gost_engine) {
 		init_gost_engine();
 	}
+#endif
+#if KNOT_ENABLE_GOST
+	OpenSSL_add_all_algorithms();
 #endif
 }
 
 void knot_crypto_unload_engines(void)
 {
-#if KNOT_ENABLE_GOST
+#if KNOT_LOAD_GOST
 	if (gost_engine) {
 		deinit_gost_engine();
 	}
