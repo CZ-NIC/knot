c6bab3c9690c88456d62b30ff0d87e04692bf39a
Jan Vcelak <jan.vcelak@nic.cz>
2015-06-24 13:35:58 +0200

    online signing: NSEC synthesis for NXDOMAIN

--- src/knot/modules/online_sign/module.c
+++ src/knot/modules/online_sign/module.c
@@ -20,6 +20,7 @@
 #include "knot/conf/base.h"
 
 #include "knot/modules/online_sign/module.h"
+#include "knot/modules/online_sign/nsec_next.h"
 #include "knot/nameserver/process_query.h"
 #include "knot/nameserver/internet.h"
 
@@ -27,6 +28,10 @@
 #include "dnssec/error.h"
 #include "dnssec/kasp.h"
 #include "dnssec/sign.h"
+#include "dnssec/nsec.h"
+
+#define NSEC_RR_TTL 1200
+
 const yp_item_t scheme_mod_online_sign[] = {
 	{ C_ID, YP_TSTR, YP_VNONE },
 	{ NULL }
@@ -38,6 +43,112 @@ struct online_sign_ctx {
 };
 
 typedef struct online_sign_ctx online_sign_ctx_t;
+
+static bool want_dnssec(struct query_data *qdata)
+{
+	return knot_pkt_has_dnssec(qdata->query);
+}
+
+static dnssec_nsec_bitmap_t *nxdomain_bitmap(void)
+{
+	dnssec_nsec_bitmap_t *bitmap = dnssec_nsec_bitmap_new();
+	dnssec_nsec_bitmap_add(bitmap, KNOT_RRTYPE_NSEC);
+	dnssec_nsec_bitmap_add(bitmap, KNOT_RRTYPE_RRSIG);
+	return bitmap;
+}
+
+static knot_rrset_t *nxdomain_nsec(const knot_dname_t *qname,
+                                   const knot_dname_t *apex,
+                                   mm_ctx_t *mm)
+{
+	knot_rrset_t *nsec = knot_rrset_new(qname, KNOT_RRTYPE_NSEC, KNOT_CLASS_IN, mm);
+	if (!nsec) {
+		return NULL;
+	}
+
+	knot_dname_t *next = online_nsec_next(qname, apex);
+	if (!next) {
+		knot_rrset_free(&nsec, mm);
+		return NULL;
+	}
+
+	dnssec_nsec_bitmap_t *bitmap = nxdomain_bitmap();
+	if (!bitmap) {
+		free(next);
+		knot_rrset_free(&nsec, mm);
+	}
+
+	size_t size = knot_dname_size(next) + dnssec_nsec_bitmap_size(bitmap);
+	uint8_t rdata[size];
+
+	int written = knot_dname_to_wire(rdata, next, size);
+	dnssec_nsec_bitmap_write(bitmap, rdata + written);
+
+	knot_dname_free(&next, NULL);
+	dnssec_nsec_bitmap_free(bitmap);
+
+	if (knot_rrset_add_rdata(nsec, rdata, size, NSEC_RR_TTL, mm) != KNOT_EOK) {
+		knot_rrset_free(&nsec, mm);
+		return NULL;
+	}
+
+	return nsec;
+}
+
+static int solve_nxdomain(int state, knot_pkt_t *pkt, struct query_data *qdata, void *_ctx)
+{
+	log_zone_debug(qdata->zone->name, "current state %d", state);
+
+	//! TODO: is this correct approach?
+	const knot_pktsection_t *ans = knot_pkt_section(pkt, KNOT_ANSWER);
+	if (ans->count > 0) {
+		return state;
+	}
+
+	// synthesise NSEC
+
+	if (want_dnssec(qdata)) {
+		knot_rrset_t *nsec = nxdomain_nsec(qdata->name, qdata->zone->name, &pkt->mm);
+		int r = knot_pkt_put(pkt, KNOT_COMPR_HINT_QNAME, nsec, KNOT_PF_FREE);
+		if (r != DNSSEC_EOK) {
+			knot_rrset_free(&nsec, &pkt->mm);
+			return ERROR;
+		}
+	}
+
+	// promote NXDOMAIN to NODATA
+
+	return NODATA;
+}
+
+static bool is_apex_query(struct query_data *qdata)
+{
+	return knot_dname_is_equal(qdata->name, qdata->zone->name);
+}
+
+static int synth_answer(int state, knot_pkt_t *pkt, struct query_data *qdata, void *_ctx)
+{
+	// disallow ANY for now
+	if (knot_pkt_qtype(pkt) == KNOT_RRTYPE_ANY) {
+		qdata->rcode = KNOT_RCODE_REFUSED;
+		return ERROR;
+	}
+
+	if (knot_pkt_qtype(pkt) == KNOT_RRTYPE_DNSKEY && is_apex_query(qdata)) {
+		return ERROR;
+	}
+
+	if (knot_pkt_qtype(pkt) == KNOT_RRTYPE_NSEC) {
+		return ERROR;
+	}
+
+	if (knot_pkt_qtype(pkt) == KNOT_RRTYPE_RRSIG) {
+		qdata->rcode = KNOT_RCODE_REFUSED;
+		return ERROR;
+	}
+
+	return solve_nxdomain(state, pkt, qdata, NULL);
+}
 
 static int get_dnssec_key(dnssec_key_t **key_ptr,
                           const knot_dname_t *zone_name,
@@ -189,6 +300,8 @@ int online_sign_load(struct query_plan *plan, struct query_module *module,
 		log_zone_error(zone, "online signing, failed to load signing key (%s)", dnssec_strerror(r));
 		return KNOT_ERROR;
 	}
+
+	query_plan_step(plan, QPLAN_ANSWER, synth_answer, ctx);
 
 	module->ctx = ctx;
 
