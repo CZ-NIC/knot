340ab89c3732325122ce6a3415084b85a0bb258d
Jan Vcelak <jan.vcelak@nic.cz>
2015-07-13 12:48:04 +0200

    tests-extra: add tests for CNAME following

---
+++ tests-extra/tests/basic/cname-follow/test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+
+Basic checks for CNAME following.
+
+- Query for CNAME, NSEC, RRSIG is not followed.
+- Query for ANY meta type is not followed.
+- Query for any other type is followed.
+
+"""
+
+from dnstest.test import Test
+
+t = Test()
+
+knot = t.server("knot")
+zone = t.zone("follow", storage=".")
+t.link(zone, knot)
+
+t.start()
+
+# follow CNAME (type exists)
+
+resp = knot.dig("test.follow", "AAAA")
+resp.check(rcode="NOERROR", flags="AA")
+resp.check_rr("answer", "test.follow", "CNAME")
+resp.check_rr("answer", "follow", "AAAA")
+resp.check_empty("authority")
+
+# follow CNAME (type doesn't exist)
+
+resp = knot.dig("test.follow", "TXT")
+resp.check(rcode="NOERROR", flags="AA")
+resp.check_rr("answer", "test.follow", "CNAME")
+resp.check_no_rr("answer", "test")
+resp.check_rr("authority", "follow", "SOA")
+
+# query for CNAME
+
+resp = knot.dig("test.follow", "CNAME")
+resp.check(rcode="NOERROR", flags="AA")
+resp.check_rr("answer", "test.follow", "CNAME")
+resp.check_no_rr("answer", "test")
+resp.check_empty("authority")
+
+# query for RRSIG
+
+resp = knot.dig("test.follow", "RRSIG")
+resp.check(rcode="NOERROR", flags="AA")
+resp.check_rr("answer", "test.follow", "RRSIG")
+resp.check_no_rr("answer", "test")
+resp.check_empty("authority")
+
+# query for NSEC
+
+resp = knot.dig("test.follow", "NSEC")
+resp.check(rcode="NOERROR", flags="AA")
+resp.check_rr("answer", "test.follow", "NSEC")
+resp.check_no_rr("answer", "test")
+resp.check_empty("authority")
+
+# query for ANY
+
+resp = knot.dig("test.follow", "ANY")
+resp.check(rcode="NOERROR", flags="AA")
+resp.check_rr("answer", "test.follow", "CNAME")
+resp.check_rr("answer", "test.follow", "NSEC")
+resp.check_rr("answer", "test.follow", "RRSIG")
+resp.check_no_rr("answer", "test")
+resp.check_empty("authority")
+
+t.end()
