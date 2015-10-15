512b203045ad14bb14e09b519b3aa2377c6e5b68
Jan Vcelak <jan.vcelak@nic.cz>
2015-10-15 19:21:07 +0200

    dnssec: pkcs11 placeholder API

--- src/dnssec/lib/keystore/pkcs11.c
+++ src/dnssec/lib/keystore/pkcs11.c
@@ -28,14 +28,69 @@ static int pkcs11_ctx_new(void **ctx_ptr, _unused_ void *data)
 	return DNSSEC_NOT_IMPLEMENTED_ERROR;
 }
 
-const keystore_functions_t PKCS11_FUNCTIONS = {
-	.ctx_new = pkcs11_ctx_new,
-};
+static int pkcs11_ctx_free(void *ctx)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
+
+static int pkcs11_init(void *ctx, const char *config)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
+
+static int pkcs11_open(void *_ctx, const char *config)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
+
+static int pkcs11_close(void *ctx)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
+
+static int pkcs11_list_keys(void *ctx, dnssec_list_t **list)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
+
+static int pkcs11_generate_key(void *ctx, gnutls_pk_algorithm_t algorithm,
+			       unsigned bits, char **id_ptr)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
+
+static int pkcs11_import_key(void *ctx, const dnssec_binary_t *pem, char **id_ptr)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
+
+static int pkcs11_remove_key(void *ctx, const char *id)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
+
+static int pkcs11_get_private(void *ctx, const char *id, gnutls_privkey_t *key_ptr)
+{
+	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+}
 
 /* -- public API ----------------------------------------------------------- */
 
 _public_
-int dnssec_keystore_create_pkcs11(dnssec_keystore_t **store_ptr)
+int dnssec_keystore_init_pkcs11(dnssec_keystore_t **store_ptr)
 {
-	return keystore_create(store_ptr, &PKCS11_FUNCTIONS, NULL);
+	static const keystore_functions_t IMPLEMENTATION = {
+		.ctx_new      = pkcs11_ctx_new,
+		.ctx_free     = pkcs11_ctx_free,
+		.init         = pkcs11_init,
+		.open         = pkcs11_open,
+		.close        = pkcs11_close,
+		.list_keys    = pkcs11_list_keys,
+		.generate_key = pkcs11_generate_key,
+		.import_key   = pkcs11_import_key,
+		.remove_key   = pkcs11_remove_key,
+		.get_private  = pkcs11_get_private,
+	};
+
+	return keystore_create(store_ptr, &IMPLEMENTATION, NULL);
 }
