abc390f9a3fb1231bd218d59036a5f9e919ff653
Jan Vcelak <jan.vcelak@nic.cz>
2015-10-19 09:51:27 +0200

    dnssec: PKCS11 generate key with fixed CKA_ID

--- src/dnssec/lib/keystore/pkcs11.c
+++ src/dnssec/lib/keystore/pkcs11.c
@@ -15,22 +15,51 @@
 */
 
 #include <gnutls/gnutls.h>
+#include <pthread.h>
 
 #include "error.h"
+#include "hex_gnutls.h"
 #include "keystore.h"
 #include "keystore/internal.h"
 #include "shared.h"
 
+struct pkcs11_ctx {
+	char *url;
+	char *pin;
+};
+
+typedef struct pkcs11_ctx pkcs11_ctx_t;
+
 /* -- internal API --------------------------------------------------------- */
+
+static void disable_pkcs11_callbacks(void)
+{
+	gnutls_pkcs11_set_pin_function(NULL, NULL);
+	gnutls_pkcs11_set_token_function(NULL, NULL);
+}
 
 static int pkcs11_ctx_new(void **ctx_ptr, _unused_ void *data)
 {
-	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+	static pthread_once_t once = PTHREAD_ONCE_INIT;
+	pthread_once(&once, disable_pkcs11_callbacks);
+
+	pkcs11_ctx_t *ctx = calloc(1, sizeof(*ctx));
+	if (!ctx) {
+		return DNSSEC_ENOMEM;
+	}
+
+	*ctx_ptr = ctx;
+
+	return DNSSEC_EOK;
 }
 
 static int pkcs11_ctx_free(void *ctx)
 {
-	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+	if (ctx) {
+		free(ctx);
+	}
+
+	return DNSSEC_EOK;
 }
 
 static int pkcs11_init(void *ctx, const char *config)
@@ -38,9 +67,55 @@ static int pkcs11_init(void *ctx, const char *config)
 	return DNSSEC_NOT_IMPLEMENTED_ERROR;
 }
 
+/**
+ * Parse configuration string. Accepted format: "<pkcs11-uri>[ <module-path>]"
+ */
+static int parse_config(const char *config, char **uri_ptr, char **module_ptr)
+{
+	const char *space = strchr(config, ' ');
+
+	char *uri = NULL;
+	char *module = NULL;
+
+	if (space != NULL) {
+		uri = strndup(config, space - config);
+		module = strdup(space + 1);
+	} else {
+		uri = strdup(config);
+		module = NULL;
+	}
+
+	if (!uri || (space && !module)) {
+		free(uri);
+		free(module);
+		return DNSSEC_ENOMEM;
+	}
+
+	*uri_ptr = uri;
+	*module_ptr = module;
+
+	return DNSSEC_EOK;
+}
+
 static int pkcs11_open(void *_ctx, const char *config)
 {
-	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+	pkcs11_ctx_t *ctx = _ctx;
+
+	char *module = NULL;
+	int r = parse_config(config, &ctx->url, &module);
+	if (r != DNSSEC_EOK) {
+		return r;
+	}
+
+	if (module) {
+		r = gnutls_pkcs11_add_provider(module, NULL);
+		free(module);
+		if (r != GNUTLS_E_SUCCESS) {
+			return DNSSEC_PKCS11_FAILED_TO_LOAD;
+		}
+	}
+
+	return DNSSEC_EOK;
 }
 
 static int pkcs11_close(void *ctx)
@@ -53,10 +128,44 @@ static int pkcs11_list_keys(void *ctx, dnssec_list_t **list)
 	return DNSSEC_NOT_IMPLEMENTED_ERROR;
 }
 
-static int pkcs11_generate_key(void *ctx, gnutls_pk_algorithm_t algorithm,
+static int pkcs11_generate_key(void *_ctx, gnutls_pk_algorithm_t algorithm,
 			       unsigned bits, char **id_ptr)
 {
-	return DNSSEC_NOT_IMPLEMENTED_ERROR;
+	pkcs11_ctx_t *ctx = _ctx;
+
+	// generate the key in the token
+
+	int flags = GNUTLS_PKCS11_OBJ_FLAG_MARK_PRIVATE |
+		    GNUTLS_PKCS11_OBJ_FLAG_MARK_SENSITIVE |
+		    GNUTLS_PKCS11_OBJ_FLAG_LOGIN;
+
+	gnutls_datum_t ckaid = { .data = (uint8_t *)"\x42", .size = 1 };
+	_cleanup_datum_ gnutls_datum_t der = { 0 };
+	int r = gnutls_pkcs11_privkey_generate3(ctx->url, algorithm, bits, NULL, &ckaid, GNUTLS_X509_FMT_DER, &der, 0, flags);
+	if (r != GNUTLS_E_SUCCESS) {
+		return DNSSEC_KEY_GENERATE_ERROR;
+	}
+
+	// get the key ID
+
+	_cleanup_pubkey_ gnutls_pubkey_t pubkey = NULL;
+	r = gnutls_pubkey_init(&pubkey);
+	if (r != GNUTLS_E_SUCCESS) {
+		return DNSSEC_ENOMEM;
+	}
+
+	r = gnutls_pubkey_import(pubkey, &der, GNUTLS_X509_FMT_DER);
+	if (r != GNUTLS_E_SUCCESS) {
+		return DNSSEC_ENOMEM;
+	}
+
+	char *id = gnutls_pubkey_hex_key_id(pubkey);
+	if (!id) {
+		return DNSSEC_ENOMEM;
+	}
+
+	*id_ptr = id;
+	return DNSSEC_EOK;
 }
 
 static int pkcs11_import_key(void *ctx, const dnssec_binary_t *pem, char **id_ptr)
