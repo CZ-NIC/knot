c46dfcac3de87a7bff772724f70b0700e91a3c12
Jan Vcelak <jan.vcelak@nic.cz>
2015-12-04 17:18:23 +0100

    dnssec: fix typo in tests

--- src/dnssec/tests/keystore_pkcs11.c
+++ src/dnssec/tests/keystore_pkcs11.c
@@ -257,7 +257,7 @@ int main(int argc, char *argv[])
 	// key manipulation
 
 	r = dnssec_keystore_init(store, config);
-	ok(r == DNSSEC_NOT_IMPLEMENTED_ERROR, "dnssec_keystore_init(), not implmeneted");
+	ok(r == DNSSEC_NOT_IMPLEMENTED_ERROR, "dnssec_keystore_init(), not implemented");
 
 	r = dnssec_keystore_open(store, config);
 	ok(r == DNSSEC_EOK, "dnssec_keystore_open()");
