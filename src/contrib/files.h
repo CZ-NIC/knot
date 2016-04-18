68f6d0fbc89bfae01e791af45bb9a382febee74f
Dominik Taborsky <dominik.taborsky@nic.cz>
2016-04-18 13:05:14 +0200

    contrib: add file&directory removal function, use safer readdir

--- src/contrib/files.h
+++ src/contrib/files.h
@@ -19,26 +19,8 @@
 #include <stdbool.h>
 
 /*!
- * \brief Create a temporary file.
- *
- * If TMPDIR environment variable is set, the file is created within
- * that directory. If the variable is not set, the file is created
- * within /tmp.
- */
-char *test_mktemp(void);
-
-/*!
- * \brief Create a temporary directory.
- *
- * If TMPDIR environment variable is set, the directory is created within
- * that directory. If the variable is not set, the directory is created
- * within /tmp.
- */
-char *test_mkdtemp(void);
-
-/*!
  * \brief Delete file or directory (recursive).
  *
  * \return true on success, false when one or more files failed to be removed.
  */
-bool test_rm_rf(const char *path);
+bool remove_path(const char *path);
