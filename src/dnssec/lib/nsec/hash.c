c523ed9f12c3f2a8912885367825277b64db07ed
Jan Vcelak <jan.vcelak@nic.cz>
2016-06-30 15:45:51 +0200

    dnssec: move NSEC3PARAM parsing into separate soure file

--- src/dnssec/lib/nsec/hash.c
+++ src/dnssec/lib/nsec/hash.c
@@ -92,63 +92,6 @@ static gnutls_digest_algorithm_t algorithm_d2g(dnssec_nsec3_algorithm_t dnssec)
 /* -- public API ----------------------------------------------------------- */
 
 /*!
- * Free NSEC3 parameters.
- */
-_public_
-void dnssec_nsec3_params_free(dnssec_nsec3_params_t *params)
-{
-	if (!params) {
-		return;
-	}
-
-	dnssec_binary_free(&params->salt);
-	clear_struct(&params);
-}
-
-/*!
- * Parse NSEC3 parameters from NSEC3PARAM RDATA.
- *
- * \see RFC 5155 (section 4.2)
- */
-_public_
-int dnssec_nsec3_params_from_rdata(dnssec_nsec3_params_t *params,
-				   const dnssec_binary_t *rdata)
-{
-	if (!params || !rdata || !rdata->data) {
-		return DNSSEC_EINVAL;
-	}
-
-	dnssec_nsec3_params_t new_params = { 0 };
-
-	wire_ctx_t wire = wire_init_binary(rdata);
-
-	if (wire_available(&wire) < 5) {
-		return DNSSEC_MALFORMED_DATA;
-	}
-
-	new_params.algorithm  = wire_read_u8(&wire);
-	new_params.flags      = wire_read_u8(&wire);
-	new_params.iterations = wire_read_u16(&wire);
-	new_params.salt.size  = wire_read_u8(&wire);
-
-	if (wire_available(&wire) != new_params.salt.size) {
-		return DNSSEC_MALFORMED_DATA;
-	}
-
-	new_params.salt.data = malloc(new_params.salt.size);
-	if (new_params.salt.data == NULL) {
-		return DNSSEC_ENOMEM;
-	}
-
-	wire_read_binary(&wire, &new_params.salt);
-	assert(wire_tell(&wire) == rdata->size);
-
-	*params = new_params;
-
-	return DNSSEC_EOK;
-}
-
-/*!
  * Compute NSEC3 hash for given data.
  */
 _public_
