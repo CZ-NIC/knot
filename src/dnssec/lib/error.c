7f2e3b61f26c16dbd9978897b0fe425c7606b5e7
Jan Vcelak <jan.vcelak@nic.cz>
2016-07-15 13:55:33 +0200

    dnssec: remove unused error code

--- src/dnssec/lib/error.c
+++ src/dnssec/lib/error.c
@@ -69,7 +69,6 @@ static const error_message_t ERROR_MESSAGES[] = {
 	{ DNSSEC_KEYSTORE_INVALID_BACKEND, "invalid KASP keystore backend" },
 	{ DNSSEC_KEYSTORE_INVALID_CONFIG,  "invalid KASP keystore configuration" },
 
-	{ DNSSEC_P11_INVALID_CONFIG,        "invalid PKCS #11 configuration" },
 	{ DNSSEC_P11_FAILED_TO_LOAD_MODULE, "failed to load PKCS #11 module" },
 	{ DNSSEC_P11_TOO_MANY_MODULES,      "too many PKCS #11 modules loaded" },
 	{ DNSSEC_P11_TOKEN_NOT_AVAILABLE,   "PKCS #11 token not available" },
