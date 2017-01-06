f38e7f55a80340f5e5facbad6968cf1ccdb85b84
Daniel Salzman <daniel.salzman@nic.cz>
2017-01-06 16:29:19 +0100

    doc: increase Python recursion limit to 1500

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os, time
 
+sys.setrecursionlimit(1500)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
