c60dc0db08b56feeecd38be06182c899e4e3cb5a
Libor Peltan <libor.peltan@nic.cz>
2017-01-16 18:04:32 +0100

    tests: dnssec: keytag conflict testcase added

---
+++ tests-extra/tests/dnssec/keytag_conflict/test.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+"""
+Check if keytag conflict is correctly handled by Knot.
+"""
+
+import collections
+import os
+import shutil
+import datetime
+import subprocess
+
+from dnstest.utils import *
+from dnstest.keys import Keymgr
+from dnstest.test import Test
+
+def key_set(server, zone, key_id, **new_values):
+    cmd = ["zone", "key", "set", zone, key_id]
+    for option, value in new_values.items():
+        cmd += [option, value]
+    Keymgr.run_check(server.keydir, *cmd)
+
+# check zone if keys are present and used for signing
+def check_zone4(server, min_dnskeys, min_rrsigs, msg):
+    dnskeys = server.dig("example.com", "DNSKEY")
+    found_dnskeys = dnskeys.count("DNSKEY")
+
+    soa = server.dig("mail.example.com", "A", dnssec=True)
+    found_rrsigs = soa.count("RRSIG")
+
+    check_log("RRSIGs: %d (expected min %d)" % (found_rrsigs, min_rrsigs));
+    check_log("DNSKEYs: %d (expected min %d)" % (found_dnskeys, min_dnskeys));
+
+    if found_rrsigs < min_rrsigs:
+        set_err("BAD RRSIG COUNT: " + msg)
+        detail_log("!RRSIGs not published and activated as expected: " + msg)
+
+    if found_dnskeys < min_dnskeys:
+        set_err("BAD DNSKEY COUNT: " + msg)
+        detail_log("!DNSKEYs not published and activated as expected: " + msg)
+
+    detail_log(SEP)
+
+t = Test()
+
+knot = t.server("knot")
+zone = t.zone("example.com.")
+t.link(zone, knot)
+knot.dnssec(zone).enable = True
+knot.dnssec(zone).manual = True
+knot.dnssec(zone).rrsig_lifetime = 5
+knot.dnssec(zone).rrsig_refresh = 2
+knot.zonefile_sync = "0"
+
+# install keys (one always enabled, one for testing)
+shutil.copytree(os.path.join(t.data_dir, "keys"), knot.keydir)
+
+# parameters
+ZONE = "example.com"
+KSK = "7a3500c7feac3fd99f09a208a83b97f7455fa3e0"
+ZSK1 = "712d0d0d57fa0aa006b5e20cd84e23941e5f3ab2"
+ZSK2 = "301d3fc5392e83ea02312dc5bdc1a9f0b7937ddf"
+ZSK3 = "6abddc73bcb46c4e6078cf764290ac315fff03f0"
+
+key_set(knot, ZONE, KSK, publish="-2y", active="-1y", retire="+1y", remove="+2y")
+key_set(knot, ZONE, ZSK1, publish="-20", active="-10", retire="+15", remove="+20")
+key_set(knot, ZONE, ZSK2, publish="+8", active="+14", retire="+31", remove="+36")
+key_set(knot, ZONE, ZSK3, publish="+24", active="+30", retire="+1y", remove="+2y") 
+
+t.start()
+t.sleep(4)
+
+check_zone4(knot, 2, 1, "initial keys")
+
+t.sleep(15)
+
+check_zone4(knot, 2, 1, "standard rollover")
+
+t.sleep(13)
+
+for x in range(1, 8):
+	check_zone4(knot, 2, 1, "conflicting rollover %i" % x)
+	t.sleep(2)
+
+t.end()
