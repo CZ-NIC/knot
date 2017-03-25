7eae9f55775220f79894806f6e5ba8382e1623ec
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2017-03-25 13:15:24 -0500

    added surmised C indentation rules for emacs users

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;; emacs local configuration settings for knot source
+;; surmised by dkg on 2017-03-25 12:15:55-0500
+
+((c-mode
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (c-basic-offset . 8)
+  (c-file-style . "linux"))
+ )
