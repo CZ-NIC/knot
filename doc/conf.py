7d71d5b4a5901daed6cd5bc5523a583ce09dbd0a
Daniel Salzman <daniel.salzman@nic.cz>
2017-05-08 19:52:14 +0200

    doc: fix warning about obsolete paper size setting

--- doc/conf.py
+++ doc/conf.py
@@ -172,7 +172,7 @@ htmlhelp_basename = 'KnotDNSdoc'
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
 #'pointsize': '10pt',
