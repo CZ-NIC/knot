6af4199fd49f4447adbc71210540dc033d205cf9
Libor Peltan <libor.peltan@nic.cz>
2017-05-22 10:51:25 +0200

    journal: fix not decrementing zone count if zone missing

--- src/knot/journal/journal.c
+++ src/knot/journal/journal.c
@@ -1609,19 +1609,16 @@ int journal_scrape(journal_t *j)
 		WALK_LIST(del_one, to_del) {
 			txn->ret = j->db->db_api->del(txn->txn, (knot_db_val_t *)del_one->d);
 		}
-		md_update_journal_count(txn, -1);
+		if (!EMPTY_LIST(to_del)) {
+			md_update_journal_count(txn, -1);
+		}
 
 		md_del_last_inserter_zone(txn, j->zone);
 
 		txn->ret = j->db->db_api->txn_commit(txn->txn);
 	}
 scrape_end:
-
-	WALK_LIST(del_one, to_del) {
-		free(del_one->d);
-	}
-
-	ptrlist_free(&to_del, NULL);
+	ptrlist_deep_free(&to_del, NULL);
 
 	return txn->ret;
 }
