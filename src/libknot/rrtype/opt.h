0c14e4d07233faeee4d7ee3c4172da41419727b3
Daniel Salzman <daniel.salzman@nic.cz>
2018-02-01 11:52:52 +0100

    libknot/opt: remove useless operations

--- src/libknot/rrtype/opt.h
+++ src/libknot/rrtype/opt.h
@@ -270,33 +270,6 @@ void knot_edns_set_do(knot_rrset_t *opt_rr)
 }
 
 /*!
- * \brief Removes all EDNS options with given \a code.
- *
- * \param[in] opt_rr  OPT RR structure to remove the options from.
- * \param[in] code    Option code.
- *
- * \return Error code, KNOT_EOK if successful (even if nothing removed).
- */
-int knot_edns_remove_options(knot_rrset_t *opt_rr, uint16_t code);
-
-/*!
- * \brief Adds EDNS option into the package with empty (zeroed) content.
- *
- * \note All other occurrences of the option type will be removed.
- *
- * \param[in]  opt_rr    OPT RR structure to reserve the option in.
- * \param[in]  code      Option code.
- * \param[in]  size      Desired option size.
- * \param[out] wire_ptr  Pointer to reserved option data (can be NULL).
- * \param[in]  mm        Memory context.
- *
- * \return Error code, KNOT_EOK if successful.
- */
-int knot_edns_reserve_unique_option(knot_rrset_t *opt_rr, uint16_t code,
-                                    uint16_t size, uint8_t **wire_ptr,
-                                    knot_mm_t *mm);
-
-/*!
  * \brief Add EDNS option into the package with empty (zeroed) content.
  *
  * \param[in]  opt_rr    OPT RR structure to reserve the option in.
