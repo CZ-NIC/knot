5b71169b1edc7f4f7224d3abc20a23a7420b2574
Daniel Salzman <daniel.salzman@nic.cz>
2018-03-20 09:11:21 +0100

    keymgr: add missing '#pragma once' (lgtm.com)

--- src/utils/keymgr/functions.h
+++ src/utils/keymgr/functions.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2017 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2018 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -13,6 +13,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#pragma once
 
 #include "knot/dnssec/context.h"
 
