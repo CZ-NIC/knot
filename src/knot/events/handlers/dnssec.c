37959a2b2d838b8b972215dbe6f0020c5b0c92f6
Daniel Salzman <daniel.salzman@nic.cz>
2018-06-28 13:03:48 +0200

    events: remove omitted urcu.h include

--- src/knot/events/handlers/dnssec.c
+++ src/knot/events/handlers/dnssec.c
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2017 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2018 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
  */
 
 #include <assert.h>
-#include <urcu.h>
 
 #include "knot/common/log.h"
 #include "knot/conf/conf.h"
