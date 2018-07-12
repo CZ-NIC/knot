673944337c3beb215e888e3449390f49fb2f9b42
Mark Karpilovskij <mark.karpilovskij@nic.cz>
2018-07-12 18:11:16 +0200

    geoip: finalize config format, support for geoname_id

--- src/knot/modules/geoip/geodb.h
+++ src/knot/modules/geoip/geodb.h
@@ -14,91 +14,44 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#pragma once
+
+#include<libknot/libknot.h>
 #if HAVE_MAXMINDDB
 #include <maxminddb.h>
 #endif
 
-enum geodb_key {
-	CONTINENT,
-	COUNTRY,
-	CITY,
-	ISP
-};
-
 // MaxMind DB related constants.
-#define MAX_PATH_LEN 4
-#define GEODB_KEYS 4
+#define GEODB_MAX_PATH_LEN 8
+#define GEODB_MAX_DEPTH 8
+
+typedef enum {
+	GEODB_KEY_ID,
+	GEODB_KEY_TXT
+} geodb_key_type_t;
+
+static const knot_lookup_t geodb_key_types[] =  {
+	{ GEODB_KEY_ID, "id" },
+	{ GEODB_KEY_TXT, "" }
+};
 
 typedef struct {
-	const char *path[MAX_PATH_LEN];
-}mmdb_path_t;
+	geodb_key_type_t type;
+	char *path[GEODB_MAX_PATH_LEN];
+}geodb_path_t;
 
-mmdb_path_t paths[] = {
-	{{"continent", "code", NULL}},
-	{{"country", "iso_code", NULL}},
-	{{"city", "names", "en", NULL}},
-	{{"isp", NULL}}
-};
+int parse_geodb_path(geodb_path_t *path, char *input);
 
-void *geodb_open(const char *filename)
-{
-#if HAVE_MAXMINDDB
-	MMDB_s *db = calloc(1, sizeof(MMDB_s));
-	if (db == NULL) {
-		return NULL;
-	}
-	int mmdb_error = MMDB_open(filename, MMDB_MODE_MMAP, db);
-	if (mmdb_error != MMDB_SUCCESS) {
-		return NULL;
-	}
-	return (void *)db;
-#endif
-	return NULL;
-}
+int parse_geodata(char *input, void **geodata, uint32_t *geodata_len, uint8_t *geodepth,
+                  geodb_path_t *path, uint16_t path_cnt);
 
-void geodb_close(void *geodb)
-{
-#if HAVE_MAXMINDDB
-	MMDB_s *db = (MMDB_s *)geodb;
-	MMDB_close(db);
-#endif
-}
+void *geodb_open(const char *filename);
 
-int geodb_query(void *geodb, struct sockaddr *remote,
-                enum geodb_key *keys, uint16_t keyc,
-                char **geodata, uint32_t *geodata_len, uint16_t *netmask)
-{
-#if HAVE_MAXMINDDB
-	MMDB_s *db = (MMDB_s *)geodb;
-	int mmdb_error = 0;
-	MMDB_lookup_result_s res;
-	res = MMDB_lookup_sockaddr(db, remote, &mmdb_error);
-	if (mmdb_error != MMDB_SUCCESS || !res.found_entry) {
-		return -1;
-	}
+void *geodb_alloc_entries(uint16_t count);
 
-	// Save netmask.
-	*netmask = res.netmask;
+void geodb_close(void *geodb);
 
-	MMDB_entry_data_s entry;
-	// Set the remote's geo information.
-	for (uint16_t i = 0; i < keyc; i++) {
-		enum geodb_key key = keys[i];
-		geodata[key] = NULL;
-		mmdb_error = MMDB_aget_value(&res.entry, &entry, paths[key].path);
-		if (mmdb_error != MMDB_SUCCESS &&
-			mmdb_error != MMDB_LOOKUP_PATH_DOES_NOT_MATCH_DATA_ERROR) {
-			return -1;
-		}
-		if (mmdb_error == MMDB_LOOKUP_PATH_DOES_NOT_MATCH_DATA_ERROR ||
-			!entry.has_data || entry.type != MMDB_DATA_TYPE_UTF8_STRING) {
-			continue;
-		}
-		geodata[key] = (char *)entry.utf8_string;
-		geodata_len[key] = entry.data_size;
-	}
+int geodb_query(void *geodb, void *entries, struct sockaddr *remote,
+                geodb_path_t *paths, uint16_t path_cnt, uint16_t *netmask);
 
-	return 0;
-#endif
-	return -1;
-}
+bool remote_in_geo(void **geodata, uint32_t *geodata_len, uint16_t geodepth, void *entries);
