93a112a5ff5a6794eb2bb18cb836f39590a0fb0b
Libor Peltan <libcha.p@gmail.com>
2018-09-16 08:20:48 +0200

    dnssec: separated function for determining cdnskeys

--- src/knot/dnssec/zone-sign.h
+++ src/knot/dnssec/zone-sign.h
@@ -46,6 +46,17 @@ int knot_zone_sign_update_dnskeys(zone_update_t *update,
 bool knot_zone_sign_use_key(const zone_key_t *key, const knot_rrset_t *covered);
 
 /*!
+ * \brief Return those keys for whose the CDNSKEY/CDS records shall be created.
+ *
+ * \param ctx        DNSSEC context.
+ * \param zone_keys  Zone keyset, includeing ZSKs.
+ *
+ * \return Dynarray containing pointers on some KSKs in keyset.
+ */
+keyptr_dynarray_t knot_zone_sign_get_cdnskeys(const kdnssec_ctx_t *ctx,
+					      zone_keyset_t *zone_keys);
+
+/*!
  * \brief Update zone signatures and store performed changes in update.
  *
  * Updates RRSIGs, NSEC(3)s, and DNSKEYs.
