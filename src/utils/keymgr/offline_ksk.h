306a29be00831201b35ee56a5e98bbef2554810b
Libor Peltan <libcha.p@gmail.com>
2018-10-08 14:11:53 +0200

    offline KSK: improved deleting offline records

--- src/utils/keymgr/offline_ksk.h
+++ src/utils/keymgr/offline_ksk.h
@@ -22,7 +22,7 @@ int keymgr_pregenerate_zsks(kdnssec_ctx_t *ctx, knot_time_t upto);
 
 int keymgr_print_rrsig(kdnssec_ctx_t *ctx, knot_time_t when);
 
-int keymgr_delete_rrsig(kdnssec_ctx_t *ctx, knot_time_t until);
+int keymgr_delete_rrsig(kdnssec_ctx_t *ctx, knot_time_t from, knot_time_t to);
 
 int keymgr_del_all_old(kdnssec_ctx_t *ctx);
 
