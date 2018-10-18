66776355ef4fdfd3c6c67d008b5722c52b845998
Libor Peltan <libor.peltan@nic.cz>
2018-10-18 18:49:32 +0200

    tests: attempt to fix dnssec/nsec_update

--- tests-extra/tests/dnssec/nsec_update/test.py
+++ tests-extra/tests/dnssec/nsec_update/test.py
@@ -10,7 +10,7 @@ t = Test()
 
 master = t.server("knot")
 slave = t.server("knot")
-zones = t.zone_rnd(5, dnssec=False, records=50) + t.zone("records.")
+zones = t.zone_rnd(5, dnssec=False, records=30) + t.zone("records.")
 
 t.link(zones, master, slave)
 
@@ -24,8 +24,9 @@ for zone in zones:
     master.dnssec(zone).nsec3_salt_len = 8
 
 t.start()
+t.sleep(4)
+slave.ctl("zone-refresh")
 
-master.zones_wait(zones)
 slave.zones_wait(zones)
 
 # initial convenience check
@@ -38,13 +39,17 @@ for zone in zones:
     master.random_ddns(zone)
 t.sleep(4) # zones_wait fails if an empty update is generated
 
+after_update = master.zones_wait(zones)
+
 # sync slave with current master's state
 slave.ctl("zone-refresh")
 t.sleep(5)
 
+slave.zones_wait(zones, after_update, equal=True, greater=False)
+
 # re-sign master and check that the re-sign made nothing
 master.ctl("zone-sign")
-master.zones_wait(zones)
+master.zones_wait(zones, after_update, equal=False, greater=True)
 
 t.xfr_diff(master, slave, zones, no_rrsig_rdata=True)
 
