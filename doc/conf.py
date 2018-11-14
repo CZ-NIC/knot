fb40276dbfcc0f13b6961904e8149fe39fad8c94
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2018-11-14 04:37:23 -0500

    docs: manually set copyright year in doc/conf.py
    To make builds bit-by-bit reproducible, the source code should not
    change what is built based on the current time.
    
    Currently, the documentation sets its build year by querying the
    system clock, which means that the produced documentation changes when
    you build it in a different year.
    
    This approach costs one manual commit per year of development, but
    always builds the same thing, and claims no additional copyright years
    if no changes have been made to the source.
    
    It provides a nice warning so that (hopefully) someone building the
    package which needs the manual year update should notice and can make
    the update themselves if the source code is actually active (as i
    expect it to be in 2020, but who knows what will happen in 2035?)
    
    Compare with the use of SOURCE_DATE_EPOCH:
    https://gitlab.labs.nic.cz/knot/knot-dns/merge_requests/951

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os, time
+import sys, os, time, logging
 
 sys.setrecursionlimit(1500)
 
@@ -43,8 +43,13 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Knot DNS'
-copyright = u'Copyright 2010–%d, CZ.NIC, z.s.p.o.' % time.localtime().tm_year
-author = 'CZ.NIC Labs <http://www.knot-dns.cz>'
+copyright_year = 2018
+current_year = time.localtime().tm_year
+if current_year > copyright_year:
+    logging.warning('Copyright year is %d, but current year is %d.'%(copyright_year, current_year))
+    logging.warning('Maybe you should update copyright_year in doc/conf.py?')
+copyright = u'Copyright 2010–%d, CZ.NIC, z.s.p.o.' % copyright_year
+author = 'CZ.NIC Labs <https://www.knot-dns.cz>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
