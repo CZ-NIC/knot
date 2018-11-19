48665d2272902c17358fc0b87ca001b3bd618b09
Libor Peltan <libor.peltan@nic.cz>
2018-11-19 14:06:52 +0100

    zone: moved node adjusting to extra functions

---
+++ src/knot/zone/adjust.h
@@ -0,0 +1,28 @@
+/*  Copyright (C) 2018 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "knot/zone/contents.h"
+
+int zone_adjust_node_pointers(zone_node_t *node, const zone_contents_t *zone);
+
+int zone_adjust_nsec3_pointers(zone_node_t *node, const zone_contents_t *zone);
+
+int zone_adjust_nsec3_chain(zone_node_t *node, const zone_contents_t *zone);
+
+int zone_adjust_additionals(zone_node_t *node, const zone_contents_t *zone);
+
