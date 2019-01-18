8999ac5615b3b3ed342cda55eb1a8e6e5c2e35ac
Libor Peltan <libor.peltan@nic.cz>
2019-01-18 14:02:39 +0100

    tests: added test if signing is consistent

---
+++ tests-extra/tests/dnssec/duplicit_rrsig/test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+'''Test for no re-signing if the zone is properly signed.'''
+
+from dnstest.utils import *
+from dnstest.test import Test
+
+t = Test()
+
+master = t.server("knot")
+
+zone = t.zone("example.com.", storage=".")
+
+t.link(zone, master, ixfr=True, journal_content="all")
+
+master.dnssec(zone).enable = True
+
+t.start()
+
+serial = master.zone_wait(zone)
+
+master.random_ddns(zone)
+
+serial = master.zone_wait(zone, serial)
+
+master.stop()
+t.sleep(1)
+master.start()
+
+new_serial = master.zone_wait(zone)
+
+if new_serial != serial:
+    set_err("zone got re-signed")
+
+t.stop()
