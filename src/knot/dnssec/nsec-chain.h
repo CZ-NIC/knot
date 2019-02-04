59f7bb15ba16b7f7c1795a503c2751101c6f3bc2
Libor Peltan <libor.peltan@nic.cz>
2019-02-04 16:41:20 +0100

    general: determine if node is apex from flags not parent ptr

--- src/knot/dnssec/nsec-chain.h
+++ src/knot/dnssec/nsec-chain.h
@@ -54,7 +54,7 @@ inline static void bitmap_add_node_rrsets(dnssec_nsec_bitmap_t *bitmap,
                                           const zone_node_t *node)
 {
 	bool deleg = node->flags & NODE_FLAGS_DELEG;
-	bool apex = node->parent == NULL;
+	bool apex = node->flags & NODE_FLAGS_APEX;
 	for (int i = 0; i < node->rrset_count; i++) {
 		knot_rrset_t rr = node_rrset_at(node, i);
 		if (deleg && (rr.type != KNOT_RRTYPE_NS && rr.type != KNOT_RRTYPE_DS)) {
