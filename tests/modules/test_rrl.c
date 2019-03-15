bffa4eb4db2efe03981bd7081d8f05abc16f57dc
Mark Karpilovskij <mark.karpilovskij@nic.cz>
2019-03-15 16:07:44 +0100

    mod-rrl: remove unnecessary qname length from hash

--- tests/modules/test_rrl.c
+++ tests/modules/test_rrl.c
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2018 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2019 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -146,10 +146,10 @@ int main(int argc, char *argv[])
 	/* 3. Endian-independent hash input buffer. */
 	uint8_t buf[RRL_CLSBLK_MAXLEN];
 	// CLS_LARGE + remote + dname wire length + dname wire.
-	uint8_t expectedv4[] = "\x10\x01\x02\x03\x00\x00\x00\x00\x00\x06\x00\x04""beef";
+	uint8_t expectedv4[] = "\x10\x01\x02\x03\x00\x00\x00\x00\x00\x04""beef";
 	rrl_classify(buf, sizeof(buf), &addr, &rq, qname);
 	is_int(0, memcmp(buf, expectedv4, sizeof(expectedv4)), "rrl: IPv4 hash input buffer");
-	uint8_t expectedv6[] = "\x10\x11\x22\x33\x44\x55\x66\x77\x00\x06\x00\x04""beef";
+	uint8_t expectedv6[] = "\x10\x11\x22\x33\x44\x55\x66\x77\x00\x04""beef";
 	rrl_classify(buf, sizeof(buf), &addr6, &rq, qname);
 	is_int(0, memcmp(buf, expectedv6, sizeof(expectedv6)), "rrl: IPv6 hash input buffer");
 
