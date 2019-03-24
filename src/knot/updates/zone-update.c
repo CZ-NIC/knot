2da1828ffc2452856d702cafab8b315ceecbc885
Daniel Salzman <daniel.salzman@nic.cz>
2019-03-24 21:28:15 +0100

    zone-update: fix memory leak in zone_update_clear if full update
    Can be reproduced by server shutdown after failed initial zone-commit.

--- src/knot/updates/zone-update.c
+++ src/knot/updates/zone-update.c
@@ -333,6 +333,7 @@ void zone_update_clear(zone_update_t *update)
 		changeset_clear(&update->change);
 	} else if (update->flags & UPDATE_FULL) {
 		assert(update->new_cont_deep_copy);
+		update_cleanup(update->a_ctx);
 		zone_contents_deep_free(update->new_cont);
 	}
 	free(update->a_ctx);
