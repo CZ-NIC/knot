00b6db410a544456d37b94a8ddb19854e81e97d0
Libor Peltan <libor.peltan@nic.cz>
2019-04-01 18:22:42 +0200

    apply: COW mutex -> semaphore, cause freed by RCU in other thread

--- src/knot/zone/zone.h
+++ src/knot/zone/zone.h
@@ -16,6 +16,8 @@
 
 #pragma once
 
+#include <semaphore.h>
+
 #include "knot/conf/conf.h"
 #include "knot/conf/confio.h"
 #include "knot/journal/journal_basic.h"
@@ -72,7 +74,7 @@ typedef struct zone
 	struct zone_update *control_update;
 
 	/*! \brief Ensue one COW tramsaction on zone's trees at a time. */
-	pthread_mutex_t cow_lock;
+	sem_t cow_lock;
 
 	/*! \brief Ptr to journal DB (in struct server) */
 	knot_lmdb_db_t *journaldb;
