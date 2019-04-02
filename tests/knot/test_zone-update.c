e0e89351af0fb1f753acb90ffe0281a6432564ed
Libor Peltan <libor.peltan@nic.cz>
2019-04-02 17:46:37 +0200

    semaphore: custom implementation to somehow work on OpenBSD and OSX

--- tests/knot/test_zone-update.c
+++ tests/knot/test_zone-update.c
@@ -108,9 +108,9 @@ static int test_node_unified(zone_node_t *n1, void *v)
 
 static void test_zone_unified(zone_t *z)
 {
-	sem_wait(&z->cow_lock);
+	knot_sem_wait(&z->cow_lock);
 	zone_tree_apply(z->contents->nodes, test_node_unified, NULL);
-	sem_post(&z->cow_lock);
+	knot_sem_post(&z->cow_lock);
 }
 
 void test_full(zone_t *zone, zs_scanner_t *sc)
