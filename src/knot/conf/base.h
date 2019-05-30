061f52ea4029eca15ef1a5b25ee7b6156269dd73
Jan Hák <jan.hak@nic.cz>
2019-05-30 14:14:49 +0200

    configuration: added option for setup tcp query timeout

--- src/knot/conf/base.h
+++ src/knot/conf/base.h
@@ -109,6 +109,7 @@ typedef struct {
 		int16_t srv_max_ipv4_udp_payload;
 		int16_t srv_max_ipv6_udp_payload;
 		int32_t srv_tcp_idle_timeout;
+		int32_t srv_tcp_query_timeout;
 		int32_t srv_tcp_reply_timeout;
 		int32_t srv_max_tcp_clients;
 		int32_t ctl_timeout;
