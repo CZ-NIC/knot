7aab6873410ce0cac9af2601ba69492484451ef5
Jan Hák <jan.hak@nic.cz>
2019-06-11 12:49:42 +0200

    blocking tasks: Proof of concept (WiP)

--- src/knot/events/events.c
+++ src/knot/events/events.c
@@ -226,6 +226,12 @@ static void event_wrap(task_t *task)
 	if (ret == KNOT_EOK) {
 		/* Execute the event callback. */
 		ret = info->callback(conf, zone);
+		
+		if (events->blocking_cv) {
+			pthread_cond_broadcast(events->blocking_cv[type]);
+			events->blocking_cv[type] = NULL;
+		}
+		
 		conf_free(conf);
 	}
 
@@ -343,17 +349,6 @@ void _zone_events_schedule_at(zone_t *zone, ...)
 	va_end(args);
 }
 
-void zone_events_schedule_blocking(zone_t *zone, zone_event_type_t  type) {
-
-	zone_events_schedule_now(zone, type);
-
-	time_t run_time = zone_events_get_time(zone, type);
-	while (zone->events.running || (run_time > 0 && run_time <= time(NULL))) {
-		usleep(10000);
-		run_time = zone_events_get_time(zone, type);
-	}
-}
-
 void zone_events_schedule_user(zone_t *zone, zone_event_type_t type)
 {
 	if (!zone || !valid_event(type)) {
@@ -369,6 +364,33 @@ void zone_events_schedule_user(zone_t *zone, zone_event_type_t type)
 
 	// reschedule because get_next_event result changed outside of _zone_events_schedule_at
 	reschedule(events);
+}
+
+void zone_events_schedule_blocking(zone_t *zone, zone_event_type_t type, bool user)
+{
+	if (!zone || !valid_event(type)) {
+		return;
+	}
+
+	pthread_mutex_t mutex_blocker = PTHREAD_MUTEX_INITIALIZER;
+	pthread_cond_t  cv = PTHREAD_COND_INITIALIZER;
+	pthread_mutex_lock(&mutex_blocker);
+	
+	while (zone->events.blocking_cv[type]) { //TODO blokujici event bezi.. vymyslet (domyslet), jak se zachovat...
+		pthread_cond_wait(zone->events.blocking_cv[type], &mutex_blocker);
+	}
+	zone->events.blocking_cv[type] = &cv;
+
+	if (user) {
+		zone_events_schedule_user(zone, type);
+	} else {
+		zone_events_schedule_now(zone, type);
+	}
+
+	while (zone->events.blocking_cv[type]) { // 'spurious wakeup' condition
+		pthread_cond_wait(&cv , &mutex_blocker); 
+	}
+	pthread_mutex_unlock(&mutex_blocker);
 }
 
 void zone_events_enqueue(zone_t *zone, zone_event_type_t type)
