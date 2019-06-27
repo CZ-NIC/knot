b9d46d594214602fcefe7dfbdd58072bcc617f10
Tomas Krizek <tomas.krizek@nic.cz>
2019-06-27 10:14:21 +0200

    distro/tests: support more OBS repos in test-distro.sh

--- distro/tests/test-distro.sh
+++ distro/tests/test-distro.sh
@@ -1,30 +1,16 @@
 #!/bin/bash -x
 
-# ./test-distro.sh {devel|latest} {distro}
-# Example usage: ./test-distro.sh devel debian9
+# ./test-distro.sh {obs_repo} {distro}
+# Example usage: ./test-distro.sh knot-dns-devel debian9
 
 pkgtestdir="$(dirname ${0})"
 repofile="$pkgtestdir/repos.yaml"
 
-distro=$2
 repo=$1
+distro=$2
 
 # Select repos
-case "$repo" in
-	devel)
-		echo -e 'repos:\n  - knot-dns-devel' > $repofile
-		;;
-	testing)
-		echo -e 'repos:\n  - knot-dns-testing' > $repofile
-        ;;
-	latest)
-		echo -e 'repos:\n  - knot-dns-latest' > $repofile
-		;;
-	*)
-		echo "Unknown repo, choose devel|latest|testing"
-		exit 1
-		;;
-esac
+echo -e "repos:\n  - $repo" > $repofile
 
 pushd "$pkgtestdir/$distro"
 vagrant destroy -f &>/dev/null
