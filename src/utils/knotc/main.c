e06698ffbcc8795a72336d574a71262c28cef7fa
David Vašek <david.vasek@nic.cz>
2019-07-01 13:06:14 +0200

    doc: knotd/knotc - improve the -m parameter description

--- src/utils/knotc/main.c
+++ src/utils/knotc/main.c
@@ -37,7 +37,7 @@ static void print_help(void)
 	       "                          "SPACE" (default %s)\n"
 	       " -C, --confdb <dir>       "SPACE"Use a binary configuration database directory.\n"
 	       "                          "SPACE" (default %s)\n"
-	       " -m, --max-conf-size <MiB>"SPACE"Set maximum configuration size (max 10000 MiB).\n"
+	       " -m, --max-conf-size <MiB>"SPACE"Set maximum size of the configuration database (max 10000 MiB).\n"
 	       "                          "SPACE" (default %d MiB)\n"
 	       " -s, --socket <path>      "SPACE"Use a control UNIX socket path.\n"
 	       "                          "SPACE" (default %s)\n"
