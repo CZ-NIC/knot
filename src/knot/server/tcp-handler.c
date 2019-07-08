4c96ce7b1c3d714e8612195384fe4e63440e0350
Daniel Salzman <daniel.salzman@nic.cz>
2019-07-08 15:30:33 +0200

    --fuxup=edae959b65936932462c24c917dfbb28b466c6e5

--- src/knot/server/tcp-handler.c
+++ src/knot/server/tcp-handler.c
@@ -270,10 +270,7 @@ int tcp_master(dthread_t *thread)
 	knot_layer_init(&tcp.layer, &mm, process_query_layer());
 
 	/* Prepare structures for bound sockets. */
-	rcu_read_lock();
-	conf_val_t val = conf_get(conf(), C_SRV, C_LISTEN);
-	fdset_init(&tcp.set, conf_val_count(&val));
-	rcu_read_unlock();
+	fdset_init(&tcp.set, FDSET_INIT_SIZE);
 
 	/* Create iovec abstraction. */
 	for (unsigned i = 0; i < 2; ++i) {
