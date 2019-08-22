d7b5b10836099cff44d092aa9b7bc5424a4e1cbb
Jan Hák <jan.hak@nic.cz>
2019-08-22 11:12:35 +0200

    edns_cookies: refactoring

--- src/libknot/cookies.c
+++ src/libknot/cookies.c
@@ -47,6 +47,7 @@ int knot_edns_cookie_client_generate(knot_edns_cookie_t *out,
 	SipHash24_Update(&ctx, addr, addr_len);
 
 	uint64_t hash = SipHash24_End(&ctx);
+	//hash = htobe64(hash);
 	memcpy(out->data, &hash, sizeof(hash));
 	out->len = sizeof(hash);
 
@@ -66,7 +67,7 @@ int knot_edns_cookie_client_check(const knot_edns_cookie_t *cc,
 	if (ret != KNOT_EOK) {
 		return ret;
 	}
-	assert(ref.len == KNOT_EDNS_COOKIE_CLNT_MIN_SIZE);
+	assert(ref.len >= KNOT_EDNS_COOKIE_CLNT_MIN_SIZE);
 
 	ret = const_time_memcmp(cc->data, ref.data, KNOT_EDNS_COOKIE_CLNT_MIN_SIZE);
 	if (ret != 0) {
@@ -92,7 +93,7 @@ int knot_edns_cookie_server_generate(knot_edns_cookie_t *out,
 	out->data[3] = 0; /* reserved */
 	out->len = 4;
 
-	uint32_t now = htobe32((uint32_t)time(NULL));
+	uint32_t now = htonl((uint32_t)time(NULL));
 	memcpy(&out->data[out->len], &now, sizeof(uint32_t));
 	out->len += sizeof(uint32_t);
 
@@ -135,15 +136,18 @@ int knot_edns_cookie_server_check(const knot_edns_cookie_t *sc,
 		return KNOT_EINVAL;
 	}
 
+	/* UNUSED */
 	uint32_t cookie_now;
 	memcpy(&cookie_now, &sc->data[4], sizeof(uint32_t));
-	cookie_now = htobe32(cookie_now);
+	cookie_now = ntohl(cookie_now);
+	/* END UNUSED */
 
 	size_t addr_len = 0;
 	void *addr = sockaddr_raw(params->client_addr, &addr_len);
 
 	uint64_t cookie_hash;
 	memcpy(&cookie_hash, &sc->data[8], sizeof(uint64_t));
+	
 	SIPHASH_CTX ctx;
 	assert(sizeof(params->secret) == sizeof(SIPHASH_KEY));
 	SipHash24_Init(&ctx, (const SIPHASH_KEY *)params->secret);
