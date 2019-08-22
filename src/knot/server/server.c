07e8d3921d496f52d63e0898f8c4da7a585f71b7
David Vašek <david.vasek@nic.cz>
2019-08-22 16:38:44 +0200

    no dynamic configuration: remove reference counting

--- src/knot/server/server.c
+++ src/knot/server/server.c
@@ -338,8 +338,6 @@ static int configure_sockets(conf_t *conf, server_t *s)
 	/* Prepare helper lists. */
 	int bound = 0;
 	ifacelist_t *newlist = malloc(sizeof(ifacelist_t));
-	ref_init(&newlist->ref, &remove_ifacelist);
-	ref_retain(&newlist->ref);
 	init_list(&newlist->u);
 	init_list(&newlist->l);
 
@@ -378,7 +376,6 @@ static int configure_sockets(conf_t *conf, server_t *s)
 	for (unsigned proto = IO_UDP; proto <= IO_TCP; ++proto) {
 		dt_unit_t *tu = s->handlers[proto].handler.unit;
 		for (unsigned i = 0; i < tu->size; ++i) {
-			ref_retain((ref_t *)newlist);
 			s->handlers[proto].handler.thread_state[i] |= ServerReload;
 			s->handlers[proto].handler.thread_id[i] = thread_count++;
 		}
@@ -922,7 +919,7 @@ void server_update_zones(conf_t *conf, server_t *server)
 	}
 }
 
-ref_t *server_set_ifaces(server_t *server, fdset_t *fds, int index, int thread_id)
+ifacelist_t *server_set_ifaces(server_t *server, fdset_t *fds, int index, int thread_id)
 {
 	if (server == NULL || server->ifaces == NULL || fds == NULL) {
 		return NULL;
@@ -951,5 +948,5 @@ ref_t *server_set_ifaces(server_t *server, fdset_t *fds, int index, int thread_i
 	}
 	rcu_read_unlock();
 
-	return &server->ifaces->ref;
+	return server->ifaces;
 }
