a43f73b7f5c62ca432ca320c734d983d58cbe748
David Vašek <david.vasek@nic.cz>
2019-09-24 17:45:56 +0200

    tcp-handler: implement TCP throttle logging strategy with/without reuseport

--- src/knot/server/server.c
+++ src/knot/server/server.c
@@ -47,11 +47,37 @@ enum {
 	TCP_MIN_SNDSIZE = sizeof(uint16_t) + UINT16_MAX
 };
 
-/*! \brief Timer and its lock for TCP throttle warnings. */
-locked_timeout_t tcp_throttle_log = {
-	.timer_end.tv_sec = 0,
-	.timer_end.tv_nsec = 0
-};
+/*! \brief Throttle records, a timer, and its lock for TCP throttle warnings. */
+/* Warning: the initialization here is neccessary because of macOS compiler! */
+throttle_log_t tcp_throttle_log = { 0 };
+
+/*! \brief Initialize TCP throttle logger. */
+static int throttle_log_init(conf_t *conf)
+{
+	tcp_throttle_log.timer_end.tv_sec = 0;
+	tcp_throttle_log.timer_end.tv_nsec = 0;
+	tcp_throttle_log.threads = conf->cache.srv_tcp_threads;
+
+	tcp_throttle_log.was_throttled =
+		calloc(tcp_throttle_log.threads, sizeof(*tcp_throttle_log.was_throttled));
+	if (tcp_throttle_log.was_throttled == NULL) {
+		return KNOT_ENOMEM;
+	}
+
+	/* Initialize the spinlock for TCP threshold logging. */
+	knot_spin_init(&tcp_throttle_log.lock);
+
+	return KNOT_EOK;
+}
+
+/*! \brief Destroy and free TCP throttle logger. */
+static void throttle_log_deinit(void)
+{
+	/* Destroy the TCP throttle log timer spinlock. */
+	knot_spin_destroy(&tcp_throttle_log.lock);
+
+	free(tcp_throttle_log.was_throttled);
+}
 
 /*! \brief Unbind interface and clear the structure. */
 static void server_deinit_iface(iface_t *iface)
@@ -464,8 +490,8 @@ void server_deinit(server_t *server)
 	/* Free threads and event handlers. */
 	worker_pool_destroy(server->workers);
 
-	/* Destroy the TCP throttle log timer spinlock. */
-	knot_spin_destroy(&tcp_throttle_log.lock);
+	/* Destroy and free TCP throttle logger. */
+	throttle_log_deinit();
 
 	/* Free zone database. */
 	knot_zonedb_deep_free(&server->zone_db);
@@ -821,8 +847,11 @@ static int configure_threads(conf_t *conf, server_t *server)
 		return ret;
 	}
 
-	/* Initialize the spinlock for TCP threshold logging. */
-	knot_spin_init(&tcp_throttle_log.lock);
+	/* Initialize TCP throttle logger. */
+	ret = throttle_log_init(conf);
+	if (ret != KNOT_EOK) {
+		return ret;
+	}
 
 	return set_handler(server, IO_TCP, conf->cache.srv_tcp_threads, tcp_master);
 }
