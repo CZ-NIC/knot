2334e2e63b1c412e625de7abb8df5a47a810bb53
Jan Hák <jan.hak@nic.cz>
2019-09-30 15:27:08 +0200

    copr: makefile for COPR 'make srpm' integration (WiP)

--- scripts/copr-spec.sh
+++ scripts/copr-spec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 version=`ls knot*.tar.xz | sed "s/knot-\(.*\).tar.xz/\1/" | sort -r | head -n 1`
-sed -e "s/__VERSION__/${version}/g" distro/rpm/knot.spec > ~/rpmbuild/SPECS/$1
+sed -e "s/__VERSION__/${version}/g" distro/rpm/knot.spec > ~/rpmbuild/SPECS/knot.spec
 find distro/rpm/ ! -name knot.spec -exec cp -t ~/rpmbuild/SOURCES/ {} + 2> /dev/null 
 ln -sr knot-${version}.tar.xz ~/rpmbuild/SOURCES/knot_${version}.orig.tar.xz
 exit 0
