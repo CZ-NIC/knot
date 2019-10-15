c92c602058908a85b2557a2698b6949da06f8bfc
Libor Peltan <libor.peltan@nic.cz>
2019-10-15 14:50:46 +0200

    journal: warning instead of infloop on changeset X->X
    ...such changeset must not appear in journal, but well it might...

--- src/knot/journal/journal_read.c
+++ src/knot/journal/journal_read.c
@@ -52,6 +52,10 @@ static bool go_next_changeset(journal_read_t *ctx, bool go_zone, const knot_dnam
 	if (!knot_lmdb_find_prefix(&ctx->txn, &ctx->key_prefix)) {
 		return false;
 	}
+	if (ctx->next == journal_next_serial(&ctx->txn.cur_val)) {
+		ctx->txn.ret = KNOT_ELOOP;
+		return false;
+	}
 	ctx->next = journal_next_serial(&ctx->txn.cur_val);
 	update_ctx_wire(ctx);
 	return true;
