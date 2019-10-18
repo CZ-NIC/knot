bb01a06c0f789c9b22cf8fad11b1cd82078f4bca
Daniel Salzman <daniel.salzman@nic.cz>
2019-10-18 20:12:29 +0200

    ds-push: mute log 'debug: config, conf_id_get_txn (invalid parameter)'

--- src/knot/events/handlers/ds_push.c
+++ src/knot/events/handlers/ds_push.c
@@ -225,6 +225,7 @@ int event_ds_push(conf_t *conf, zone_t *zone)
 	int timeout = conf->cache.srv_tcp_reply_timeout * 1000;
 
 	conf_val_t policy_id = conf_zone_get(conf, C_DNSSEC_POLICY, zone->name);
+	conf_id_fix_default(&policy_id);
 	conf_val_t ds_push = conf_id_get(conf, C_POLICY, C_DS_PUSH, &policy_id);
 	while (ds_push.code == KNOT_EOK) {
 		conf_val_t addr = conf_id_get(conf, C_RMT, C_ADDR, &ds_push);
