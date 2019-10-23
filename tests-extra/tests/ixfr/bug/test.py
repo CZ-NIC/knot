042ba2de28889c63a951b3ca7e0dedaeb5776a66
Daniel Salzman <daniel.salzman@nic.cz>
2019-10-23 21:01:39 +0200

    simplified

--- tests-extra/tests/ixfr/bug/test.py
+++ tests-extra/tests/ixfr/bug/test.py
@@ -10,39 +10,29 @@ t = Test()
 
 master = t.server("knot")
 slave = t.server("knot")
-zones = t.zone(".")
+zone = t.zone(".")
 
-t.link(zones, master, slave, ixfr=True)
+t.link(zone, master, slave, ixfr=True)
 ctl = libknot.control.KnotCtl()
 
 t.start()
 
-# Wait for AXFR to slave server.
-serials_init = master.zones_wait(zones)
-slave.zones_wait(zones)
+slave.zones_wait(zone)
 
 ctl.connect(os.path.join(slave.dir, "knot.sock"))
 
-serials_prev = serials_init
-for i in range(4):
-#    input("%s" % i)
-    # Update zone files on master.
-    for zone in zones:
-        master.update_zonefile(zone, random=True)
+master.update_zonefile(zone, random=True)
 
+ctl.send_block(cmd="zone-begin")
+resp = ctl.receive_block()
+
+master.reload()
+t.sleep(2)
+
+for j in range(2):
+    ctl.send_block(cmd="zone-abort")
+    resp = ctl.receive_block()
     ctl.send_block(cmd="zone-begin")
-
-    master.reload()
-
-    for j in range(2):
-        ctl.send_block(cmd="zone-abort")
-        resp = ctl.receive_block()
-        ctl.send_block(cmd="zone-begin")
-        resp = ctl.receive_block()
-
-    # Wait for IXFR to slave.
-    serials = master.zones_wait(zones, serials_prev)
-    slave.zones_wait(zones, serials_prev)
-    serials_prev = serials
+    resp = ctl.receive_block()
 
 t.end()
