1a62a2c7db75f9f46e3c69bcfba82d746f35f726
Vladimír Čunát <vladimir.cunat@nic.cz>
2019-11-07 16:18:23 +0100

    knot_rdataset_subtract(): fix an old bug

--- src/libknot/rdataset.c
+++ src/libknot/rdataset.c
@@ -264,7 +264,6 @@ int knot_rdataset_subtract(knot_rdataset_t *from, const knot_rdataset_t *what,
 
 	if (from->rdata == what->rdata) {
 		knot_rdataset_clear(from, mm);
-		knot_rdataset_init((knot_rdataset_t *) what);
 		return KNOT_EOK;
 	}
 
