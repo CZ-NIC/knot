6e6fc2a7f7db0ba2a2368bec730c0fc843b307b9
Libor Peltan <libor.peltan@nic.cz>
2019-12-23 15:36:19 +0100

    xdp: listen only on configured port number

--- src/libknot/xdp/bpf-user.c
+++ src/libknot/xdp/bpf-user.c
@@ -1,4 +1,20 @@
+/*  Copyright (C) 2019 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "contrib/bpf/bpf_endian.h"
 #include "libknot/xdp/bpf-user.h"
 
 #include <errno.h>
@@ -122,14 +138,15 @@ static void unget_bpf_maps(struct kxsk_iface *iface)
 	iface->qidconf_map_fd = iface->xsks_map_fd = -1;
 }
 
-int kxsk_socket_start(const struct kxsk_iface *iface, int queue_id, struct xsk_socket *xsk)
+int kxsk_socket_start(const struct kxsk_iface *iface, int queue_id,
+                      uint16_t listen_port, struct xsk_socket *xsk)
 {
 	int fd = xsk_socket__fd(xsk);
 	int err = bpf_map_update_elem(iface->xsks_map_fd, &queue_id, &fd, 0);
 	if (err)
 		return err;
 
-	int qid = true;
+	int qid = bpf_htons(listen_port);
 	err = bpf_map_update_elem(iface->qidconf_map_fd, &queue_id, &qid, 0);
 	if (err)
 		bpf_map_delete_elem(iface->xsks_map_fd, &queue_id);
--- src/libknot/xdp/bpf-user.h
+++ src/libknot/xdp/bpf-user.h
@@ -1,3 +1,19 @@
+/*  Copyright (C) 2019 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include  <stdint.h>
@@ -79,7 +95,8 @@ struct kxsk_iface * kxsk_iface_new(const char *ifname, const char *prog_fname);
 int kxsk_iface_free(struct kxsk_iface *iface, bool unload_bpf);
 
 /** Activate this AF_XDP socket through the BPF maps. */
-int kxsk_socket_start(const struct kxsk_iface *iface, int queue_id, struct xsk_socket *xsk);
+int kxsk_socket_start(const struct kxsk_iface *iface, int queue_id,
+                      uint16_t listen_port, struct xsk_socket *xsk);
 
 /** Deactivate this AF_XDP socket through the BPF maps. */
 int kxsk_socket_stop(const struct kxsk_iface *iface, int queue_id);
