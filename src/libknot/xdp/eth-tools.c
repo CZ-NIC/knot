cdef499dd7ec258c3f94ebad4723c3a2601517e0
Libor Peltan <libor.peltan@nic.cz>
2020-01-17 13:27:01 +0100

    xdp: get number of threads from RX queue count

---
+++ src/libknot/xdp/eth-tools.c
@@ -0,0 +1,60 @@
+/*  Copyright (C) 2020 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/ethtool.h>
+#include <linux/if.h>
+#include <linux/sockios.h>
+
+#include "contrib/openbsd/strlcpy.h"
+#include "libknot/attribute.h"
+#include "libknot/errcode.h"
+
+_public_
+int knot_eth_get_rx_queues(const char *devname)
+{
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd < 0) {
+		return knot_map_errno();
+	}
+
+	struct ethtool_channels ch = { 0 };
+	ch.cmd = ETHTOOL_GCHANNELS;
+	struct ifreq ifr = { 0 };
+	ifr.ifr_data = &ch;
+	strlcpy(ifr.ifr_name, devname, IFNAMSIZ);
+
+	int ret = ioctl(fd, SIOCETHTOOL, &ifr);
+	if (ret) {
+		if (errno == EOPNOTSUPP) {
+			ret = 1;
+		} else {
+			ret = knot_map_errno();
+		}
+	} else {
+		if (ch.combined_count == 0) {
+			ret = 1;
+		} else {
+			ret = ch.combined_count;
+		}
+	}
+
+	close(fd);
+	return ret;
+}
