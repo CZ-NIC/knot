bb1453f5ed06b1acd7f8a58c4e5a1da354d28e69
Daniel Salzman <daniel.salzman@nic.cz>
2020-02-05 09:58:12 +0100

    conf: hide ed25519 and ed448 algorithm values if not supported

--- src/knot/conf/schema.c
+++ src/knot/conf/schema.c
@@ -63,8 +63,12 @@ static const knot_lookup_t dnssec_key_algs[] = {
 	{ DNSSEC_KEY_ALGORITHM_RSA_SHA512,        "rsasha512" },
 	{ DNSSEC_KEY_ALGORITHM_ECDSA_P256_SHA256, "ecdsap256sha256" },
 	{ DNSSEC_KEY_ALGORITHM_ECDSA_P384_SHA384, "ecdsap384sha384" },
+#ifdef HAVE_ED25519
 	{ DNSSEC_KEY_ALGORITHM_ED25519,           "ed25519" },
+#endif
+#ifdef HAVE_ED448
 	{ DNSSEC_KEY_ALGORITHM_ED448,             "ed448" },
+#endif
 	{ 0, NULL }
 };
 
