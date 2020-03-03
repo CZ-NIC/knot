b5e07bf778cc19481fb44e821718d3d6103439d9
Libor Peltan <libor.peltan@nic.cz>
2020-03-03 17:21:33 +0100

    nsec3 incremental fix: detect changed NSEC3PARAM by update

--- src/knot/dnssec/nsec3-chain.c
+++ src/knot/dnssec/nsec3-chain.c
@@ -772,7 +772,7 @@ int knot_nsec3_fix_chain(zone_update_t *update,
 	assert(params);
 
 	// ensure that the salt has not changed
-	if (!knot_nsec3param_uptodate(update->zone->contents, params)) {
+	if (!knot_nsec3param_uptodate(update->new_cont, params)) {
 		int ret = knot_nsec3param_update(update, params);
 		if (ret != KNOT_EOK) {
 			return ret;
