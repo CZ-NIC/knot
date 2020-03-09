4154d664a82f2c4dfb838bc85df9040c180fa9da
Daniel Salzman <daniel.salzman@nic.cz>
2020-03-09 18:25:58 +0100

    xdp: decrease FRAME_SIZE to 2048

--- src/libknot/xdp/af_xdp.c
+++ src/libknot/xdp/af_xdp.c
@@ -35,7 +35,7 @@
 
 #include "contrib/macros.h"
 
-#define FRAME_SIZE 4096
+#define FRAME_SIZE 2048
 #define UMEM_FRAME_COUNT_RX 4096
 #define UMEM_FRAME_COUNT_TX UMEM_FRAME_COUNT_RX /* no reason to differ so far */
 #define UMEM_RING_LEN_RX (UMEM_FRAME_COUNT_RX * 2)
