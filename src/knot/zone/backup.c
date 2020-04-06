7a5ca244de8b17c4577a31dafddab8fcbf370f0e
David Vašek <david.vasek@nic.cz>
2020-04-06 15:09:13 +0200

    zone: modified file_overwrite()
    Made to match already existing zonefile_write(). First create the file,
    then move it to its place.

--- src/knot/zone/backup.c
+++ src/knot/zone/backup.c
@@ -22,6 +22,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include "contrib/files.h"
 #include "knot/zone/backup.h"
 #include "knot/common/log.h"
 #include "knot/dnssec/kasp/kasp_zone.h"
@@ -106,26 +107,6 @@ static char *dir_file(const char *dir_name, const char *file_name)
 	return sprintf_alloc("%s/%s", dir_name, basename);
 }
 
-// TODO rewrite this better!
-static int file_overwrite(const char *what, const char *with)
-{
-	errno = 0;
-	FILE *fr = fopen(with, "r"), *fw = fopen(what, "w");
-	int c, ret = 0;
-	if (fr != NULL && fw != NULL) {
-		while ((c = getc(fr)) != EOF && (ret = putc(c, fw)) != EOF) ;
-	}
-	if (fr == NULL || fw == NULL || ret == EOF) {
-		ret = knot_map_errno();
-	} else {
-		ret = KNOT_EOK;
-	}
-	if (fr != NULL) {
-		fclose(fr);
-	}
-	return fw && fclose(fw) == 0 ? ret : (ret == KNOT_EOK ? knot_map_errno() : ret);
-}
-
 static int backup_key(key_params_t *parm, dnssec_keystore_t *from, dnssec_keystore_t *to)
 {
 	dnssec_key_t *key = NULL;
@@ -205,7 +186,7 @@ int zone_backup(conf_t *conf, zone_t *zone)
 		if (ctx->restore_mode) {
 			char *local_zf = conf_zonefile(conf, zone->name);
 			char *backup_zf = dir_file(ctx->backup_dir, local_zf);
-			ret = file_overwrite(local_zf, backup_zf);
+			ret = copy_file(local_zf, backup_zf);
 			free(backup_zf);
 			free(local_zf);
 		} else {
