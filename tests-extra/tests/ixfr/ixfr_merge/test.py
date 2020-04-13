6ddf7c6dc90db51aa570457b3de77ed5518c5433
David Vašek <david.vasek@nic.cz>
2020-04-13 13:13:49 +0200

    tests-extra: ixfr/ixfr_merge -- raise some TCP timeouts to make the test stable under Valgrind

--- tests-extra/tests/ixfr/ixfr_merge/test.py
+++ tests-extra/tests/ixfr/ixfr_merge/test.py
@@ -15,8 +15,16 @@ zones = t.zone_rnd(5)
 t.link(zones, s1, s2, ixfr=True)
 t.link(zones, s2, s3, ixfr=True)
 
-s2.tcp_remote_io_timeout = 8000
-s3.tcp_remote_io_timeout = 8000
+if not s1.valgrind:
+    s2.tcp_remote_io_timeout = 8000
+    s3.tcp_remote_io_timeout = 8000
+else:
+    s1.tcp_remote_io_timeout = 45000
+    s2.tcp_remote_io_timeout = 45000
+    s3.tcp_remote_io_timeout = 45000
+    s1.ctl_params_add = ["-b", "-t", "45"]
+    s2.ctl_params_add = ["-b", "-t", "45"]
+    s3.ctl_params_add = ["-b", "-t", "45"]
 
 for zone in zones:
     s1.dnssec(zone).enable = True
