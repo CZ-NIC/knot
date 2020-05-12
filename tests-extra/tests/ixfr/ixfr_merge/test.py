b25990cf87fc02f590f1be492d955c90ac4dbb2a
David Vašek <david.vasek@nic.cz>
2020-05-12 16:47:48 +0200

    tests-extra: ixfr/ixfr_merge -- always control Knot in blocking mode

--- tests-extra/tests/ixfr/ixfr_merge/test.py
+++ tests-extra/tests/ixfr/ixfr_merge/test.py
@@ -15,6 +15,10 @@ zones = t.zone_rnd(5)
 t.link(zones, s1, s2, ixfr=True)
 t.link(zones, s2, s3, ixfr=True)
 
+s1.ctl_params_append = ["-b"]
+s2.ctl_params_append = ["-b"]
+s3.ctl_params_append = ["-b"]
+
 if not s1.valgrind:
     s2.tcp_remote_io_timeout = 8000
     s3.tcp_remote_io_timeout = 8000
@@ -22,9 +26,9 @@ else:
     s1.tcp_remote_io_timeout = 45000
     s2.tcp_remote_io_timeout = 45000
     s3.tcp_remote_io_timeout = 45000
-    s1.ctl_params_append = ["-b", "-t", "45"]
-    s2.ctl_params_append = ["-b", "-t", "45"]
-    s3.ctl_params_append = ["-b", "-t", "45"]
+    s1.ctl_params_append += ["-t", "45"]
+    s2.ctl_params_append += ["-t", "45"]
+    s3.ctl_params_append += ["-t", "45"]
 
 for zone in zones:
     s1.dnssec(zone).enable = True
@@ -34,12 +38,9 @@ t.start()
 serials_init = s3.zones_wait(zones)
 
 s2.ctl("zone-freeze")
-t.sleep(1)
 
 s1.ctl("zone-sign")
-t.sleep(2)
 s1.ctl("zone-sign")
-t.sleep(2)
 
 s2.ctl("zone-thaw")
 
