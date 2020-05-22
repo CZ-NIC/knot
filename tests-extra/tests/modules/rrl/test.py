dd9775256b471e96329ffd7e500b05e3aac98c0a
David Vašek <david.vasek@nic.cz>
2020-05-22 23:51:26 +0200

    tests-extra: modules/rrl -- make the test more stable under Valgrind
    By allowing the test to send more requests under Valgrind, there is a better
    statistical chance for obtaining more accurate test results and avoiding false
    test failure (it should prevent false RRL ERRORs).
    
    Under Valgrind, it is also safer to provide the server more time to respond
    (it should prevent false RRL STATS ERRORs).

--- tests-extra/tests/modules/rrl/test.py
+++ tests-extra/tests/modules/rrl/test.py
@@ -23,10 +23,15 @@ knot = t.server("knot")
 zones = t.zone_rnd(2, dnssec=False, records=1)
 t.link(zones, knot)
 
-def send_queries(server, name, run_time=1.0, query_time=0.05):
+def send_queries(server, name, run_time=None, query_time=None):
     """
     Send UDP queries to the server for certain time and get replies statistics.
     """
+    if run_time is None:
+        run_time = 1.0 if not server.valgrind else 3.0
+    if query_time is None:
+        query_time = 0.05 if not server.valgrind else 0.15
+
     replied, slipped, dropped = 0, 0, 0
     start = time.time()
     while time.time() < start + run_time:
