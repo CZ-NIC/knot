029124e537cb127051a78b2e57fce6522af71323
Libor Peltan <libor.peltan@nic.cz>
2020-06-05 15:04:15 +0200

    catalog: only allow normal queries to catalog zone over TCP

--- src/knot/nameserver/process_query.c
+++ src/knot/nameserver/process_query.c
@@ -423,9 +423,11 @@ static int prepare_answer(knot_pkt_t *query, knot_pkt_t *resp, knot_layer_t *ctx
 		qdata->extra->contents = qdata->extra->zone->contents;
 	}
 
-	if (query_type(query) == KNOTD_QUERY_TYPE_NORMAL &&
-	    qdata->extra->zone != NULL && (qdata->extra->zone->flags & ZONE_IS_CATALOG)) {
-		if (!process_query_acl_check(conf(), ACL_ACTION_TRANSFER, qdata)) {
+	/* Allow normal queries to catalog only over TCP and if allowed by ACL. */
+	if (qdata->extra->zone != NULL && (qdata->extra->zone->flags & ZONE_IS_CATALOG) &&
+	    query_type(query) == KNOTD_QUERY_TYPE_NORMAL) {
+		if ((qdata->params->flags & KNOTD_QUERY_FLAG_LIMIT_SIZE) ||
+		    !process_query_acl_check(conf(), ACL_ACTION_TRANSFER, qdata)) {
 			qdata->extra->zone = NULL;
 			qdata->extra->contents = NULL;
 		}
