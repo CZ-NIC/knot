2a533a7760986020bd098a610c8ba17d3afa8a01
Jan Hák <jan.hak@nic.cz>
2020-06-23 20:40:13 +0200

    libknot: add DNS probe API

---
+++ src/libknot/probe/data.h
@@ -0,0 +1,132 @@
+/*  Copyright (C) 2021 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*!
+ * \file
+ *
+ * \brief A DNS traffic probe data structure.
+ *
+ * \addtogroup probe
+ * @{
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+#include "libknot/consts.h"
+#include "libknot/packet/pkt.h"
+
+/*! EDE absence indication. */
+#define KNOT_PROBE_DATA_EDE_NONE	0xFFFF
+
+/*! Data transport protocol types. */
+typedef enum {
+	KNOT_PROBE_PROTO_UDP = 0,
+	KNOT_PROBE_PROTO_TCP,
+	KNOT_PROBE_PROTO_TLS,
+	KNOT_PROBE_PROTO_HTTPS,
+	KNOT_PROBE_PROTO_QUIC,
+} knot_probe_proto_t;
+
+/*! DNS message header in wire format (network byte order!). */
+typedef struct {
+	uint16_t id;
+	uint8_t byte3; /*!< QR, OPCODE, AA, TC, RD. */
+	uint8_t byte4; /*!< RA, Z, AD, CD, RCODE. */
+	uint16_t questions;
+	uint16_t answers;
+	uint16_t authorities;
+	uint16_t additionals;
+} knot_probe_data_wire_hdr_t;
+
+/*! Probe data unit. */
+typedef struct {
+	uint8_t ip;    /*!< IP protocol: 4 or 6. */
+	uint8_t proto; /*!< Transport protocol \ref knot_probe_proto_t. */
+
+	struct {
+		uint8_t addr[16]; /*!< Query destination address. */
+		uint16_t port;    /*!< Query destination port. */
+	} local;
+
+	struct {
+		uint8_t addr[16]; /*!< Query source address. */
+		uint16_t port;    /*!< Query source port. */
+	} remote;
+
+	struct {
+		knot_probe_data_wire_hdr_t hdr; /*!< DNS reply header. */
+		uint16_t size;  /*!< DNS reply size (0 if no reply). */
+		uint16_t rcode; /*!< Final RCODE (header + EDNS + TSIG). */
+		uint16_t ede;   /*!< EDE code if present. */
+	} reply;
+
+	uint32_t tcp_rtt; /*!< Average TCP RTT in microseconds. */
+
+	struct {
+		uint32_t options; /*!< EDNS options bit map (e.g. NSID ~ 1 << 3). */
+		uint16_t payload; /*!< EDNS payload size. */
+		uint8_t version;  /*!< EDNS version. */
+		uint8_t present  : 1; /*!< EDNS presence indication. */
+		uint8_t flag_do  : 1; /*!< DO flag indication. */
+		uint8_t reserved : 6; /*!< Unused. */
+	} query_edns;
+
+	struct {
+		knot_probe_data_wire_hdr_t hdr; /*!< DNS query header. */
+		uint16_t size;     /*!< DNS query size. */
+		uint16_t qclass;   /*!< QCLASS. */
+		uint16_t qtype;    /*!< QTYPE. */
+		uint8_t qname_len; /*!< QNAME length. */
+		uint8_t qname[KNOT_DNAME_MAXLEN]; /*!< QNAME. */
+	} query;
+} knot_probe_data_t;
+
+/*!
+ * \brief Initializes a probe data unit.
+ *
+ * \note 'reply.ede' and 'tcp.rtt' are zeroed only and require further setting.
+ *
+ * \param data         Output probe data unit.
+ * \param proto        Transport protocol \ref knot_probe_proto_t.
+ * \param local_addr   Query destination address (optional).
+ * \param remote_addr  Query source address.
+ * \param query        Query packet.
+ * \param reply        Reply packet (optional).
+ * \param rcode        Extended rcode (combination of RCODE, EDNS, TSIG).
+ *
+ * \retval KNOT_EOK  Success.
+ * \return KNOT_E*   If error.
+ */
+int knot_probe_data_set(knot_probe_data_t *data, knot_probe_proto_t proto,
+                        const struct sockaddr_storage *local_addr,
+                        const struct sockaddr_storage *remote_addr,
+                        const knot_pkt_t *query, const knot_pkt_t *reply,
+                        uint16_t rcode);
+
+/*!
+ * \brief Gets averate TCP RRT for a given socket descriptor.
+ *
+ * \note Implemented on Linux only!
+ *
+ * \param sockfd  Socket descriptor of a TCP connection.
+ *
+ * \return Average TCP RTT.
+ */
+uint32_t knot_probe_tcp_rtt(int sockfd);
+
+/*! @} */
---
+++ src/libknot/probe/probe.c
@@ -0,0 +1,209 @@
+/*  Copyright (C) 2021 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <fcntl.h>
+#include <poll.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include "libknot/attribute.h"
+#include "libknot/errcode.h"
+#include "libknot/probe/probe.h"
+#include "contrib/time.h"
+
+struct knot_probe {
+	struct sockaddr_un path;
+	uint32_t last_unconn_time;
+	bool consumer;
+	int fd;
+};
+
+_public_
+knot_probe_t *knot_probe_alloc(void)
+{
+	knot_probe_t *probe = calloc(1, sizeof(*probe));
+	if (probe == NULL) {
+		return NULL;
+	}
+
+	probe->fd = -1;
+
+	return probe;
+}
+
+_public_
+void knot_probe_free(knot_probe_t *probe)
+{
+	if (probe == NULL) {
+		return;
+	}
+
+	close(probe->fd);
+	if (probe->consumer) {
+		(void)unlink(probe->path.sun_path);
+	}
+	free(probe);
+}
+
+static int probe_connect(knot_probe_t *probe)
+{
+	return connect(probe->fd, (const struct sockaddr *)(&probe->path),
+	               sizeof(probe->path));
+}
+
+static int probe_init(knot_probe_t *probe, const char *dir, uint16_t idx)
+{
+	if (probe == NULL || dir == NULL || idx == 0) {
+		return KNOT_EINVAL;
+	}
+
+	probe->path.sun_family = AF_UNIX;
+	int ret = snprintf(probe->path.sun_path, sizeof(probe->path.sun_path),
+	                   "%s/probe%02u.sock", dir, idx);
+	if (ret < 0 || ret >= sizeof(probe->path.sun_path)) {
+		return KNOT_ERANGE;
+	}
+
+	probe->fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+	if (probe->fd < 0) {
+		return knot_map_errno();
+	}
+
+	if (fcntl(probe->fd, F_SETFL, O_NONBLOCK) == -1) {
+		close(probe->fd);
+		probe->fd = -1;
+		return knot_map_errno();
+	}
+
+	return KNOT_EOK;
+}
+
+_public_
+int knot_probe_set_producer(knot_probe_t *probe, const char *dir, uint16_t idx)
+{
+	int ret = probe_init(probe, dir, idx);
+	if (ret != KNOT_EOK) {
+		return ret;
+	}
+
+	ret = probe_connect(probe);
+	if (ret != 0) {
+		return KNOT_ECONN;
+	}
+
+	return KNOT_EOK;
+}
+
+_public_
+int knot_probe_set_consumer(knot_probe_t *probe, const char *dir, uint16_t idx)
+{
+	int ret = probe_init(probe, dir, idx);
+	if (ret != KNOT_EOK) {
+		return ret;
+	}
+
+	probe->consumer = true;
+
+	(void)unlink(probe->path.sun_path);
+
+	ret = bind(probe->fd, (const struct sockaddr *)(&probe->path),
+	           sizeof(probe->path));
+	if (ret != 0) {
+		return knot_map_errno();
+	}
+
+	if (fchmod(probe->fd, S_IRWXU | S_IRWXG | S_IRWXO) != 0) {
+		close(probe->fd);
+		return knot_map_errno();
+	}
+
+	return KNOT_EOK;
+}
+
+_public_
+int knot_probe_fd(knot_probe_t *probe)
+{
+	if (probe == NULL) {
+		return -1;
+	}
+
+	return probe->fd;
+}
+
+_public_
+int knot_probe_produce(knot_probe_t *probe, const knot_probe_data_t *data, uint8_t count)
+{
+	if (probe == NULL || data == NULL || count != 1) {
+		return KNOT_EINVAL;
+	}
+
+	uint8_t diff = KNOT_DNAME_MAXLEN - data->query.qname_len;
+	if (send(probe->fd, data, sizeof(*data) - diff, 0) == -1) {
+		struct timespec now = time_now();
+		if (now.tv_sec - probe->last_unconn_time > 2) {
+			probe->last_unconn_time = now.tv_sec;
+			if ((errno == ENOTCONN || errno == ECONNREFUSED) &&
+			    probe_connect(probe) == 0 &&
+			    send(probe->fd, data, sizeof(*data), 0) > 0) {
+				return KNOT_EOK;
+			}
+		}
+		return knot_map_errno();
+	}
+
+	return KNOT_EOK;
+}
+
+_public_
+int knot_probe_consume(knot_probe_t *probe, knot_probe_data_t *data, uint8_t count,
+                       int timeout_ms)
+{
+	if (probe == NULL || data == NULL) {
+		return KNOT_EINVAL;
+	}
+
+	struct mmsghdr msgs[count];
+	struct iovec iovecs[count];
+
+	memset(msgs, 0, sizeof(msgs));
+	for (int i = 0; i < count; i++) {
+		iovecs[i].iov_base         = &(data[i]);
+		iovecs[i].iov_len          = sizeof(*data);
+		msgs[i].msg_hdr.msg_iov    = &iovecs[i];
+		msgs[i].msg_hdr.msg_iovlen = 1;
+	}
+
+	struct pollfd pfd = { .fd = probe->fd, .events = POLLIN };
+	int ret = poll(&pfd, 1, timeout_ms);
+	if (ret == -1) {
+		return knot_map_errno();
+	} else if ((pfd.revents & POLLIN) == 0) {
+		return KNOT_ENOENT;
+	}
+
+	ret = recvmmsg(probe->fd, msgs, count, 0, NULL);
+	if (ret == -1) {
+		return knot_map_errno();
+	}
+
+	return ret;
+}
