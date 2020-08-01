a8758a217d82ac52b4a61b084992974e03301905
David Vašek <david.vasek@nic.cz>
2020-08-01 23:24:08 +0200

    WIP scripts: kinquire.sh - improved network devices information

--- scripts/kinquire.sh
+++ scripts/kinquire.sh
@@ -130,7 +130,11 @@ ku_net_devs_info() {
 		return
 	fi
 
+	local DEV_START="############################"
+	local DEVS_END="#########################################################################"
+
 	for DEV in $DEVICES; do
+		ku_execute echo "$DEV_START  " $DEV "  $DEV_START"
 		ku_execute $ETHTOOL $DEV
 		ku_execute $ETHTOOL -i $DEV
 		ku_execute $ETHTOOL -l $DEV
@@ -140,7 +144,10 @@ ku_net_devs_info() {
 		ku_execute $ETHTOOL -c $DEV
 		ku_execute $ETHTOOL -n $DEV rx-flow-hash udp4
 		ku_execute $ETHTOOL -n $DEV rx-flow-hash udp6
+		ku_execute $ETHTOOL -S $DEV
+		[ $DEV = bond* ] && ku_execute $CAT /proc/net/bonding/$DEV
 	done
+	ku_execute echo "$DEVS_END"
 }
 
 ku_knotd_binary_info() {
