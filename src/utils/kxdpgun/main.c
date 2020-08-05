6bb1a2b94d718195645bb82289543ecc90d97abb
Libor Peltan <libor.peltan@nic.cz>
2020-08-05 13:18:39 +0200

    kxdpgun: display response data rate

--- src/utils/kxdpgun/main.c
+++ src/utils/kxdpgun/main.c
@@ -676,7 +676,8 @@ int main(int argc, char *argv[])
 	if (global_pkts_sent > 0 && ctx.listen_port != KNOT_XDP_LISTEN_PORT_DROP) {
 		printf("total replies: %lu (%lu pps) (%lu%%)\n", global_pkts_recv,
 		       global_pkts_recv * 1000 / (ctx.duration / 1000), global_pkts_recv * 100 / global_pkts_sent);
-		printf("average reply size: %lu B\n", global_pkts_recv > 0 ? global_size_recv / global_pkts_recv : 0);
+		printf("average reply size: %lu B (%lu Bps)\n", global_pkts_recv > 0 ? global_size_recv / global_pkts_recv : 0,
+		       global_size_recv * 1000 / (ctx.duration / 1000));
 		for (int i = 0; i < KNOWN_RCODE_MAX; i++) {
 			uint64_t rcode_count = 0;
 			for (size_t j = 0; j < ctx.n_threads; j++) {
