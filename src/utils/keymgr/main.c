8d8d149637ff808de7c5ed9351246a7b73ae9c19
Daniel Salzman <daniel.salzman@nic.cz>
2020-08-07 21:03:17 +0200

    keymgr: update blank configuration to new schema

--- src/utils/keymgr/main.c
+++ src/utils/keymgr/main.c
@@ -315,10 +315,9 @@ static bool init_confile(const char *confile)
 
 static bool init_conf_blank(const char *kasp_dir)
 {
-	char *confstr = sprintf_alloc("template:\n"""
-	                              "  - id: default\n"
-	                              "    storage: .\n"
-	                              "    kasp-db: \"%s\"\n", kasp_dir);
+	char *confstr = sprintf_alloc("database:\n"
+	                              "  storage: .\n"
+	                              "  kasp-db: \"%s\"\n", kasp_dir);
 	int ret = conf_import(conf(), confstr, false, false);
 	free(confstr);
 	if (ret != KNOT_EOK) {
