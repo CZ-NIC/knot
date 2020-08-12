fdc846334262f78906ccb8991073009213a1c5b4
Daniel Salzman <daniel.salzman@nic.cz>
2020-08-12 15:24:36 +0200

    1

--- src/knot/dnssec/kasp/kasp_zone.h
+++ src/knot/dnssec/kasp/kasp_zone.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2019 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2020 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,7 +18,6 @@
 
 #include "knot/dnssec/kasp/kasp_db.h"
 #include "knot/zone/contents.h"
-
 #include "libdnssec/keystore.h"
 
 typedef struct {
