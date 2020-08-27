cf1164862518cb85662b101de40d4df3d3f2463b
David Vašek <david.vasek@nic.cz>
2020-08-27 15:46:48 +0200

    WIP: contrib/sockaddr: abandon __POSIX_VISIBLE define

--- src/contrib/sockaddr.h
+++ src/contrib/sockaddr.h
@@ -16,13 +16,13 @@
 
 #pragma once
 
-/* BSD IPv6 */
-#ifndef __POSIX_VISIBLE
- #define __POSIX_VISIBLE 200112
- #ifdef __OpenBSD__
-  #define _BSD_SOURCE	/* If __POSIX_VISIBLE is defined, OpenBSD needs this. */
- #endif
-#endif
+// /* BSD IPv6 */
+// #ifndef __POSIX_VISIBLE
+//  #define __POSIX_VISIBLE 200112
+//  #ifdef __OpenBSD__
+//   #define _BSD_SOURCE	/* If __POSIX_VISIBLE is defined, OpenBSD needs this. */
+//  #endif
+// #endif
 
 #include <stdbool.h>
 #include <sys/socket.h>
