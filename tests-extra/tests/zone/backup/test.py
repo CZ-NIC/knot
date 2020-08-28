51b90d6781afeac9fef1b95c7892dd721e80da60
David Vašek <david.vasek@nic.cz>
2020-08-28 11:38:08 +0200

    tests-extra: zone/backup -- allow enough time to proceed even under Valgrind

--- tests-extra/tests/zone/backup/test.py
+++ tests-extra/tests/zone/backup/test.py
@@ -66,9 +66,9 @@ if dnskey1_2 == dnskey1_1 or dnskey2_2 == dnskey2_1:
 
 test_added(master, zones, [ "NXDOMAIN", "NXDOMAIN" ])
 
-master.ctl("zone-restore +backupdir %s %s" % (backup_dir, zones[0].name))
+master.ctl("zone-restore +backupdir %s %s" % (backup_dir, zones[0].name), wait=True)
 
-t.sleep(6)
+t.sleep(5)
 
 (dnskey1_3, dnskey2_3) = get_dnskeys(master, zones)
 if dnskey1_3 != dnskey1_1:
@@ -108,6 +108,9 @@ for i in range(start_time + 45 - int(t.uptime())):
     if resp.rcode() != "NOERROR":
         break
 # the zone should expire in 45 seconds (45 = SOA) according to restored timers
+
+if slave.valgrind:    # Compensate for time dilation under Valgrind.
+    t.sleep(1)
 
 resp = slave.dig(zones[0].name, "SOA")
 resp.check(rcode="SERVFAIL")
