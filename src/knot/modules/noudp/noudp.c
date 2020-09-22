7c6463c18d13bedf8e56ce791c0958446c38287e
Jan Hak <jan.hak@nic.cz>
2020-09-22 15:58:10 +0200

    mod-noudp: added deny rate (wip)

--- src/knot/modules/noudp/noudp.c
+++ src/knot/modules/noudp/noudp.c
@@ -16,16 +16,23 @@
 
 #include "knot/include/module.h"
 
-#define MOD_UDP_ALLOW_RATE	"\x0e""udp-allow-rate"
+#define MOD_UDP_ALLOW_RATE		"\x0e""udp-allow-rate"
+#define MOD_UDP_TRUNCATE_RATE	"\x11""udp-truncate-rate"
 
 const yp_item_t noudp_conf[] = {
 	{ MOD_UDP_ALLOW_RATE, YP_TINT, YP_VINT = { 0, UINT32_MAX, 0 } },
+	{ MOD_UDP_TRUNCATE_RATE, YP_TINT, YP_VINT = { 0, UINT32_MAX, 0 } },
 	{ NULL }
 };
 
 typedef struct {
 	uint32_t rate;
 	uint32_t *counters;
+	enum {
+		NONE,
+		ALLOW,
+		DENY
+	} mode;
 } noudp_ctx_t;
 
 static bool is_udp(knotd_qdata_t *qdata)
@@ -38,12 +45,23 @@ static knotd_state_t noudp_begin(knotd_state_t state, knot_pkt_t *pkt,
 {
 	if (is_udp(qdata)) {
 		noudp_ctx_t *ctx = knotd_mod_ctx(mod);
-		if (ctx->rate > 0 && ++ctx->counters[qdata->params->thread_id] >= ctx->rate) {
-			ctx->counters[qdata->params->thread_id] = 0;
-			return state;
+		if (ctx->rate > 0) {
+			if (ctx->mode == ALLOW) {
+				if (++ctx->counters[qdata->params->thread_id] >= ctx->rate) {
+					ctx->counters[qdata->params->thread_id] = 0;
+					return state;
+				}
+				knot_wire_set_tc(pkt->wire);
+				return KNOTD_STATE_DONE;
+			} else if (ctx->mode == DENY) {
+				if (++ctx->counters[qdata->params->thread_id] >= ctx->rate) {
+					ctx->counters[qdata->params->thread_id] = 0;
+					knot_wire_set_tc(pkt->wire);
+					return KNOTD_STATE_DONE;
+				}
+				return state;
+			}
 		}
-		knot_wire_set_tc(pkt->wire);
-		return KNOTD_STATE_DONE;
 	}
 
 	return state;
@@ -57,13 +75,33 @@ int noudp_load(knotd_mod_t *mod)
 	}
 
 	knotd_conf_t conf = knotd_conf_mod(mod, MOD_UDP_ALLOW_RATE);
-	ctx->rate = conf.single.integer;
-	if (ctx->rate > 0) {
+	int rate = conf.single.integer;
+	if (rate > 0) {
 		ctx->counters = calloc(knotd_mod_threads(mod), sizeof(uint32_t));
 		if (ctx->counters == NULL) {
 			free(ctx);
 			return KNOT_ENOMEM;
 		}
+		ctx->rate = rate;
+		ctx->mode = ALLOW;
+	}
+
+	conf = knotd_conf_mod(mod, MOD_UDP_TRUNCATE_RATE);
+	rate = conf.single.integer;
+	if (rate > 0) {
+		if (ctx->mode) {
+			//TODO Error - setup both, allow-rate and truncate-rate
+			free(ctx->counters);
+			free(ctx);
+			return KNOT_ENOTSUP;
+		}
+		ctx->counters = calloc(knotd_mod_threads(mod), sizeof(uint32_t));
+		if (ctx->counters == NULL) {
+			free(ctx);
+			return KNOT_ENOMEM;
+		}
+		ctx->rate = rate;
+		ctx->mode = DENY;
 	}
 
 	knotd_mod_ctx_set(mod, ctx);
