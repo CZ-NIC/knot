359a9ab8e1a8a9cb546800f7bd1cf541044f1f0c
Jan Hak <jan.hak@nic.cz>
2020-09-23 14:46:38 +0200

    mod-noudp: added tests-extra (wip)

---
+++ tests-extra/tests/modules/noudp/test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+''' Check 'dnstap' query module functionality. '''
+
+import os
+import re
+import dns.flags
+from dnstest.test import Test
+from dnstest.module import ModNoudp
+from dnstest.utils import *
+
+t = Test()
+ModNoudp.check()
+
+# Initialize server configuration
+knot = t.server("knot")
+zone = t.zone("example.")
+t.link(zone, knot)
+
+# Configure 'noudp' module for all queries (default).
+knot.add_module(None, ModNoudp(deny_mode=False, rate=2))
+#knot.add_module(zone, ModNoudp(deny_mode=True, rate=0))
+
+
+t.start()
+for _ in range(0, 2):
+    resp = knot.dig("ns1.a.example.", "A")
+    resp.check(noflags="TC")
+
+    resp = knot.dig("ns1.a.example.", "A")
+    resp.check(flags="TC")
+
+
+knot.stop()
+t.end()
