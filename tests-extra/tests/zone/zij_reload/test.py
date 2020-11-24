9cca5b06e8ef88c184d8d6b10540cece47ffb034
Libor Peltan <libor.peltan@nic.cz>
2020-11-24 18:46:32 +0100

    load: proper check of zone-in-journal non-existence before storing it
    see for reference:
    https://gitlab.nic.cz/knot/knot-dns/-/commit/5d8d399d39ad8840b55b768b1a8625b91ce35837
    https://gitlab.nic.cz/knot/knot-dns/-/commit/75632433db952613e505e8a0f031c225c839fb97

---
+++ tests-extra/tests/zone/zij_reload/test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+Test of zone-in-journal dynamic configuration.
+"""
+
+from dnstest.utils import *
+from dnstest.test import Test
+
+t = Test()
+
+knot = t.server("knot")
+zone = t.zone("example.")
+t.link(zone, knot)
+
+t.start()
+knot.zone_wait(zone)
+
+knot.ctl("conf-begin")
+knot.ctl("conf-set zone[%s].journal-content all" % zone[0].name)
+knot.ctl("conf-commit")
+
+knot.stop()
+
+knot.zones[zone[0].name].journal_content = "all"
+knot.zonefile_load = "none"
+knot.gen_confile()
+
+knot.zones[zone[0].name].zfile.remove() # just to make sure
+
+knot.start()
+knot.zone_wait(zone)
+
+t.end()
