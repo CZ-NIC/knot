cf3fa8bc103b36f2b231e37f9fbcd4a1b58e9f24
Libor Peltan <libor.peltan@nic.cz>
2020-11-27 12:06:51 +0100

    test/doc: zone-reload -f doesnt reload with -b

--- tests-extra/tests/zone/reload/test.py
+++ tests-extra/tests/zone/reload/test.py
@@ -20,30 +20,34 @@ t.start()
 # Load zones
 serial = master.zone_wait(zone)
 
-def reload_zone(version, exp_serial, exp_version):
+def reload_zone(version, exp_serial, exp_version, msg):
     master.update_zonefile(zone, version)
 
-    if random.random() < 0.5:
+    opt = random.choice([1, 2, 3])
+    if opt == 1:
         master.reload()
-    else:
-        master.ctl(random.choice(["-f", " "]) + " zone-reload " + zone[0].name, wait=True)
+    if opt == 2:
+        master.ctl("zone-reload " + zone[0].name, wait=True)
+    if opt == 3:
+        master.ctl("-f zone-reload " + zone[0].name, wait=True)
+        t.sleep(1) # forced zone-reload does *only schedule* LOAD event
 
     new_serial = master.zone_wait(zone)
     if new_serial != exp_serial:
-        set_err("SOA MISMATCH")
+        set_err("SOA MISMATCH %s (%d)" % (msg, opt))
         detail_log("!Zone '%s' SOA serial %s != %s" % (zone[0].name, new_serial, exp_serial))
         return
     resp = master.dig("new-record%d.%s" % (exp_version, zone[0].name), "A")
     resp.check(rcode="NOERROR")
 
 # Zone changes, serial increases (create changeset)
-reload_zone(1, serial + 1, 1)
+reload_zone(1, serial + 1, 1, "create changeset")
 
 # Zone changes, serial doesn't change (create changeset, increment serial automatically)
-reload_zone(2, serial + 2, 2)
+reload_zone(2, serial + 2, 2, "auto incremented")
 
 # Zone changes, serial jumps out-of-range (journal is not applicable)
-reload_zone(3, serial + 2, 2)
+reload_zone(3, serial + 2, 2, "journal not applied")
 
 # Stop master.
 master.stop()
