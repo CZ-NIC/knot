db9efeafbec8236702f9848977e2e580dc4c6e8a
Daniel Salzman <daniel.salzman@nic.cz>
2021-03-09 17:28:23 +0100

    server: fix broken TFO detection

--- src/knot/server/server.c
+++ src/knot/server/server.c
@@ -17,6 +17,8 @@
 #define __APPLE_USE_RFC_3542
 
 #include <assert.h>
+#include <sys/types.h>   // OpenBSD
+#include <netinet/tcp.h> // TCP_FASTOPEN
 #include <sys/resource.h>
 
 #include "libknot/libknot.h"
