c4c4e510192a23153336530af9f112d1252f4b48
Libor Peltan <libor.peltan@nic.cz>
2021-03-18 17:27:14 +0100

    dnssec: check matching algorithm before sharing KSK

--- tests-extra/tools/dnstest/server.py
+++ tests-extra/tools/dnstest/server.py
@@ -39,6 +39,7 @@ class ZoneDnssec(object):
     def __init__(self):
         self.enable = None
         self.validate = None
+        self.disable = None # create the policy in config, but set dnssec-signing: off
         self.manual = None
         self.single_type_signing = None
         self.alg = None
@@ -1401,7 +1402,7 @@ class Knot(Server):
 
             # this is weird but for the sake of testing, the cataloged zones inherit dnssec policy from catalog zone
             if z.dnssec.enable:
-                s.item_str("dnssec-signing", "on")
+                s.item_str("dnssec-signing", "off" if z.dnssec.disable else "on")
                 s.item_str("dnssec-policy", z.name)
             for module in z.modules:
                 if module.conf_name == "mod-onlinesign":
@@ -1480,7 +1481,7 @@ class Knot(Server):
                 s.item_str("zonefile-load", "difference")
 
             if z.dnssec.enable:
-                s.item_str("dnssec-signing", "on")
+                s.item_str("dnssec-signing", "off" if z.dnssec.disable else "on")
                 s.item_str("dnssec-policy", z.dnssec.shared_policy_with or z.name)
 
             if z.catalog:
