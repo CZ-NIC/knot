abe50bc9e329b76511eb0d2426b6abcd0096e517
Daniel Salzman <daniel.salzman@nic.cz>
2021-05-09 19:53:50 +0200

    notify: remove unused constant NOTIFY_TIMEOUT

--- src/knot/nameserver/notify.h
+++ src/knot/nameserver/notify.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2017 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2021 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,8 +18,6 @@
 
 #include "libknot/packet/pkt.h"
 #include "knot/nameserver/process_query.h"
-
-#define NOTIFY_TIMEOUT 3 /*!< Interval between NOTIFY retries. */
 
 /*!
  * \brief Answer IN class zone NOTIFY message (RFC1996).
