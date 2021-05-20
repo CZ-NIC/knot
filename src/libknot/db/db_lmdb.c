ec2c4ae3a08d802c2a62f7db05e9645b4fc23397
Vladimír Čunát <vladimir.cunat@nic.cz>
2021-05-20 18:35:28 +0200

    libknot/lmdb: tiny fix in lmdb del()
    Passing the data means to only delete the data that matches,
    which is not what we wanted to do here.  The only luck has been
    that LMDB ignores this parameter when MDB_DUPSORT isn't in use,
    but for example the mdbx for does not.

--- src/libknot/db/db_lmdb.c
+++ src/libknot/db/db_lmdb.c
@@ -497,9 +497,8 @@ static int del(knot_db_txn_t *txn, knot_db_val_t *key)
 {
 	struct lmdb_env *env = txn->db;
 	MDB_val db_key = { key->len, key->data };
-	MDB_val data = { 0, NULL };
 
-	int ret = mdb_del(txn->txn, env->dbi, &db_key, &data);
+	int ret = mdb_del(txn->txn, env->dbi, &db_key, NULL);
 	if (ret != MDB_SUCCESS) {
 		return lmdb_error_to_knot(ret);
 	}
