f6d8dfdfc64e0b60dfbab96bbbaf620f8562d50a
Daniel Salzman <daniel.salzman@nic.cz>
2021-05-26 13:18:17 +0200

    tests-extra: update to refactored libknot Python wrapper

--- tests-extra/tools/dnstest/libknot.py
+++ tests-extra/tools/dnstest/libknot.py
@@ -7,7 +7,9 @@ import dnstest.params as params
 
 try:
     sys.path.append(params.repo_binary("python"))
+    import libknot
     import libknot.control
-    libknot.control.load_lib(params.libknot_lib)
+    import libknot.probe
+    libknot.Knot(params.libknot_lib)
 except:
     raise Skip("libknot not available or set KNOT_TEST_LIBKNOT to another libknot without ASAN")
