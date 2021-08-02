9d3f307b19066363fc80a52577757afd244962e8
Libor Peltan <libor.peltan@nic.cz>
2021-08-02 17:54:37 +0200

    utilities/keymgr: move configuration-handling routines to general source file...
    ...while fixing few little bugs

---
+++ src/utils/common/util_conf.c
@@ -0,0 +1,129 @@
+/*  Copyright (C) 2021 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "utils/common/util_conf.h"
+
+#include "contrib/string.h"
+#include "knot/conf/conf.h"
+#include "libknot/attribute.h"
+#include "utils/common/msg.h"
+
+bool util_conf_initialized(void)
+{
+	return (conf() != NULL);
+}
+
+int util_conf_init_confdb(const char *confdb)
+{
+	if (util_conf_initialized()) {
+		ERR("Configuration already initialized.\n");
+		return KNOT_ESEMCHECK;
+	}
+
+	size_t max_conf_size = (size_t)CONF_MAPSIZE * 1024 * 1024;
+
+	conf_flag_t flags = CONF_FNOHOSTNAME | CONF_FOPTMODULES;
+	if (confdb != NULL) {
+		flags |= CONF_FREADONLY;
+	}
+
+	conf_t *new_conf = NULL;
+	int ret = conf_new(&new_conf, conf_schema, confdb, max_conf_size, flags);
+	if (ret != KNOT_EOK) {
+		ERR("failed opening configuration database %s (%s)\n",
+		    (confdb == NULL ? "" : confdb), knot_strerror(ret));
+	} else {
+		conf_update(new_conf, CONF_UPD_FNONE);
+	}
+	return ret;
+}
+
+int util_conf_init_file(const char *conffile)
+{
+	int ret = util_conf_init_confdb(NULL);
+	if (ret != KNOT_EOK) {
+		return ret;
+	}
+
+	ret = conf_import(conf(), conffile, true, false);
+	if (ret != KNOT_EOK) {
+		ERR("failed opening configuration file %s (%s)\n",
+		    conffile, knot_strerror(ret));
+	}
+	return ret;
+}
+
+int util_conf_init_justdb(const char *db_type, const char *db_path)
+{
+	int ret = util_conf_init_confdb(NULL);
+	if (ret != KNOT_EOK) {
+		return ret;
+	}
+
+	char *conf_str = sprintf_alloc("database:\n"
+	                               "  storage: .\n"
+	                               "  %s: \"%s\"\n", db_type, db_path);
+	if (conf_str == NULL) {
+		return KNOT_ENOMEM;
+	}
+
+	ret = conf_import(conf(), conf_str, false, false);
+	free(conf_str);
+	if (ret != KNOT_EOK) {
+		ERR("failed creating temporary configuration (%s)\n", knot_strerror(ret));
+	}
+	return ret;
+}
+
+int util_conf_init_default(void)
+{
+	struct stat st;
+	if (util_conf_initialized()) {
+		return KNOT_EOK;
+	} else if (conf_db_exists(CONF_DEFAULT_DBDIR)) {
+		return util_conf_init_confdb(CONF_DEFAULT_DBDIR);
+	} else if (stat(CONF_DEFAULT_FILE, &st) == 0) {
+		return util_conf_init_file(CONF_DEFAULT_FILE);
+	} else {
+		ERR("couldn't initialize configuration, please provide -c, -C, or -d option\n");
+		return KNOT_EINVAL;
+	}
+}
+
+void util_update_privileges(void)
+{
+	int uid, gid;
+	if (conf_user(conf(), &uid, &gid) != KNOT_EOK) {
+		return;
+	}
+
+	// Just try to alter process privileges if different from configured.
+	_unused_ int unused;
+	if ((gid_t)gid != getgid()) {
+		unused = setregid(gid, gid);
+	}
+	if ((uid_t)uid != getuid()) {
+		unused = setreuid(uid, uid);
+	}
+}
+
+void util_conf_deinit(void)
+{
+	conf_free(conf());
+}
--- src/utils/keymgr/main.c
+++ src/utils/keymgr/main.c
@@ -17,17 +17,12 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 
-#include "contrib/string.h"
 #include "contrib/strtonum.h"
-#include "knot/conf/conf.h"
 #include "knot/dnssec/zone-keys.h"
-#include "libknot/attribute.h"
 #include "libknot/libknot.h"
 #include "utils/common/params.h"
+#include "utils/common/util_conf.h"
 #include "utils/keymgr/functions.h"
 #include "utils/keymgr/offline_ksk.h"
 
@@ -293,77 +288,6 @@ main_end:
 	return ret;
 }
 
-static bool init_conf(const char *confdb)
-{
-	size_t max_conf_size = (size_t)CONF_MAPSIZE * 1024 * 1024;
-
-	conf_flag_t flags = CONF_FNOHOSTNAME | CONF_FOPTMODULES;
-	if (confdb != NULL) {
-		flags |= CONF_FREADONLY;
-	}
-
-	conf_t *new_conf = NULL;
-	int ret = conf_new(&new_conf, conf_schema, confdb, max_conf_size, flags);
-	if (ret != KNOT_EOK) {
-		ERROR("failed opening configuration database %s (%s)\n",
-		       (confdb == NULL ? "" : confdb), knot_strerror(ret));
-		return false;
-	}
-	conf_update(new_conf, CONF_UPD_FNONE);
-	return true;
-}
-
-static bool init_confile(const char *confile)
-{
-	int ret = conf_import(conf(), confile, true, false);
-	if (ret != KNOT_EOK) {
-		ERROR("failed opening configuration file %s (%s)\n",
-		       confile, knot_strerror(ret));
-		return false;
-	}
-	return true;
-}
-
-static bool init_conf_blank(const char *kasp_dir)
-{
-	char *confstr = sprintf_alloc("database:\n"
-	                              "  storage: .\n"
-	                              "  kasp-db: \"%s\"\n", kasp_dir);
-	int ret = conf_import(conf(), confstr, false, false);
-	free(confstr);
-	if (ret != KNOT_EOK) {
-		ERROR("failed creating fake configuration (%s)\n",
-		       knot_strerror(ret));
-		return false;
-	}
-	return true;
-}
-
-static void update_privileges(void)
-{
-	int uid, gid;
-	if (conf_user(conf(), &uid, &gid) != KNOT_EOK) {
-		return;
-	}
-
-	// Just try to alter process privileges if different from configured.
-	_unused_ int unused;
-	if ((gid_t)gid != getgid()) {
-		unused = setregid(gid, gid);
-	}
-	if ((uid_t)uid != getuid()) {
-		unused = setreuid(uid, uid);
-	}
-}
-
-static bool conf_initialized = false; // This is a singleton as well as conf() is.
-
-#define CHECK_CONF_UNINIT \
-	if ((conf_initialized = !conf_initialized) == false) { \
-		ERROR("multiple arguments attempting configuration initializatioin\n"); \
-		return EXIT_FAILURE; \
-	}
-
 int main(int argc, char *argv[])
 {
 	struct option opts[] = {
@@ -398,20 +322,17 @@ int main(int argc, char *argv[])
 			print_version(PROGRAM_NAME);
 			return EXIT_SUCCESS;
 		case 'd':
-			CHECK_CONF_UNINIT
-			if (!init_conf(NULL) || !init_conf_blank(optarg)) {
+			if (util_conf_init_justdb("kasp-db", optarg) != KNOT_EOK) {
 				return EXIT_FAILURE;
 			}
 			break;
 		case 'c':
-			CHECK_CONF_UNINIT
-			if (!init_conf(NULL) || !init_confile(optarg)) {
+			if (util_conf_init_file(optarg) != KNOT_EOK) {
 				return EXIT_FAILURE;
 			}
 			break;
 		case 'C':
-			CHECK_CONF_UNINIT
-			if (!init_conf(argv[2])) {
+			if (util_conf_init_confdb(optarg) != KNOT_EOK) {
 				return EXIT_FAILURE;
 			}
 			break;
@@ -442,20 +363,11 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (!conf_initialized) {
-		struct stat st;
-		if (conf_db_exists(CONF_DEFAULT_DBDIR) && init_conf(CONF_DEFAULT_DBDIR)) {
-			// initialized conf from default DB location
-		} else if (stat(CONF_DEFAULT_FILE, &st) == 0 &&
-			   init_conf(NULL) && init_confile(CONF_DEFAULT_FILE)) {
-			// initialized conf from default confile
-		} else {
-			ERROR("couldn't initialize configuration, please provide -c, -C, or -d option\n");
-			return EXIT_FAILURE;
-		}
+	if (util_conf_init_default() != KNOT_EOK) {
+		return EXIT_FAILURE;
 	}
 
-	update_privileges();
+	util_update_privileges();
 
 	knot_lmdb_db_t kaspdb = { 0 };
 	conf_val_t mapsize = conf_db_param(conf(), C_KASP_DB_MAX_SIZE);
@@ -470,7 +382,7 @@ int main(int argc, char *argv[])
 	}
 
 	knot_lmdb_deinit(&kaspdb);
-	conf_free(conf());
+	util_conf_deinit();
 
 	return (ret == KNOT_EOK ? EXIT_SUCCESS : EXIT_FAILURE);
 }
