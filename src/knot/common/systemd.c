3f6f9b0aea5958357851cce59904e45126acf590
Daniel Salzman <daniel.salzman@nic.cz>
2021-08-06 15:18:17 +0200

    systemd: add KNOT_ prefix to ZONE_LOAD_TIMEOUT_SEC

--- src/knot/common/systemd.c
+++ src/knot/common/systemd.c
@@ -29,7 +29,7 @@ static int zone_load_timeout_s;
 
 static int systemd_zone_load_timeout(void)
 {
-	const char *timeout = getenv("ZONE_LOAD_TIMEOUT_SEC");
+	const char *timeout = getenv("KNOT_ZONE_LOAD_TIMEOUT_SEC");
 
 	int out;
 	if (timeout != NULL && timeout[0] != '\0' &&
