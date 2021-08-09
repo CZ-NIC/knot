a73165912cb4df95e787c30dda8a37c74f1fa2a0
Jan Hak <jan.hak@nic.cz>
2021-08-09 15:23:40 +0200

    sphinx: change configuration to disable convert double-dashes into single unicode character

--- doc/conf.py
+++ doc/conf.py
@@ -133,9 +133,9 @@ html_logo = 'logo.svg'
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
 
-# If true, SmartyPants will be used to convert quotes and dashes to
+# If true, smartquotes will be used to convert quotes and dashes to
 # typographically correct entities.
-#html_use_smartypants = False
+smartquotes = False
 
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
