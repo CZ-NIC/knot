873ee762e346389841ab06d2be09373da6898259
Libor Peltan <libor.peltan@nic.cz>
2021-12-02 15:28:27 +0100

    unreachables: also consider local 'via' address

--- tests/knot/test_unreachable.c
+++ tests/knot/test_unreachable.c
@@ -14,12 +14,15 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <tap/basic.h>
+
 #include "knot/common/unreachable.h"
 
-#include <tap/basic.h>
+#include "contrib/sockaddr.h"
 
 #define UR_TEST_ADDRS 32
 struct sockaddr_storage ur_test_addrs[UR_TEST_ADDRS] = { 0 };
+struct sockaddr_storage ur_test_via[2] = { 0 };
 
 int main(int argc, char *argv[])
 {
@@ -28,33 +31,29 @@ int main(int argc, char *argv[])
 	global_unreachables = knot_unreachables_init(1000);
 	ok(global_unreachables != NULL, "unreachables: init");
 
+	// ur_test_via[0] left empty - AF_UNSPEC
+	sockaddr_set(&ur_test_via[1], AF_INET6, "::1", 0);
+
 	for (int i = 0; i < UR_TEST_ADDRS; i++) {
 		struct sockaddr_storage *s = &ur_test_addrs[i];
-		struct sockaddr_in6 *s6 = (struct sockaddr_in6 *)s;
-		s6->sin6_family = AF_INET6;
-		s6->sin6_port = i + 1;
+		sockaddr_set(s, AF_INET6, "::2", i + 1);
+		struct sockaddr_storage *via = &ur_test_via[i % 2];
+		struct sockaddr_storage *not_via = &ur_test_via[1 - i % 2];
 
-		ok(!knot_unreachable_is(global_unreachables, s), "unreachables: pre[%d]", i);
-		knot_unreachable_add(global_unreachables, s);
-		ok(knot_unreachable_is(global_unreachables, s), "unreachables: post[%d]", i);
+		ok(!knot_unreachable_is(global_unreachables, s, via), "unreachables: pre[%d]", i);
+		knot_unreachable_add(global_unreachables, s, via);
+		ok(knot_unreachable_is(global_unreachables, s, via), "unreachables: post[%d]", i);
+		ok(!knot_unreachable_is(global_unreachables, s, not_via), "unreachables: via[%d]", i);
 
 		usleep(100);
 		if (i >= 10) {
-			ok(!knot_unreachable_is(global_unreachables, &ur_test_addrs[i - 10]), "unreachables: expired[%d]", i - 10);
-		}
-	}
-	usleep(1000);
-
-	for (int i = 0; i < UR_TEST_ADDRS; i++) {
-		knot_unreachable_add(global_unreachables, &ur_test_addrs[i]);
-
-		usleep(10);
-		if (i >= KNOT_UNREACHABLE_COUNT) {
-			ok(!knot_unreachable_is(global_unreachables, &ur_test_addrs[i - KNOT_UNREACHABLE_COUNT]), "unreachables: overfill[%d]", i - 10);
+			ok(!knot_unreachable_is(global_unreachables, &ur_test_addrs[i - 10], via),
+			   "unreachables: expired[%d]", i - 10);
 		}
 	}
 
 	knot_unreachables_deinit(&global_unreachables);
 	ok(global_unreachables == NULL, "unreachables: deinit");
+
 	return 0;
 }
