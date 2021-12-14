7152223f0096d41d65de74caf99ae0e98a39aea0
Daniel Salzman <daniel.salzman@nic.cz>
2021-12-14 12:16:25 +0100

    knot: change server.remote-retry-delay units from seconds to milliseconds

--- src/knot/common/unreachable.h
+++ src/knot/common/unreachable.h
@@ -27,12 +27,12 @@ typedef struct {
 	node_t n;
 	struct sockaddr_storage addr;
 	struct sockaddr_storage via;
-	uint32_t time;
+	uint32_t time_ms;
 } knot_unreachable_t;
 
 typedef struct {
 	pthread_mutex_t mutex;
-	uint32_t ttl;
+	uint32_t ttl_ms;
 	list_t urs;
 } knot_unreachables_t;
 
@@ -41,16 +41,26 @@ extern knot_unreachables_t *global_unreachables;
 /*!
  * \brief Allocate Unreachables structure.
  *
- * \param ttl   TTL for unreachable in usecs.
+ * \param ttl   TTL for unreachable in milliseconds.
  *
  * \return Allocated structure, or NULL.
  */
-knot_unreachables_t *knot_unreachables_init(uint32_t ttl);
+knot_unreachables_t *knot_unreachables_init(uint32_t ttl_ms);
 
 /*!
  * \brief Free Unreachables structure.
  */
 void knot_unreachables_deinit(knot_unreachables_t **urs);
+
+/*!
+ * \brief Get and/or set the TTL.
+ *
+ * \param urs          Unreachables structure.
+ * \param new_ttl_ms   New TTL value in milliseconds.
+ *
+ * \return Previous value of TTL.
+ */
+uint32_t knot_unreachables_ttl(knot_unreachables_t *urs, uint32_t new_ttl_ms);
 
 /*!
  * \brief Determine if given address is unreachable.
