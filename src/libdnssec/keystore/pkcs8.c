d978024dfe7b4bbb26ba9709aec0e098a1214132
Daniel Salzman <daniel.salzman@nic.cz>
2021-12-15 16:14:36 +0100

    libdnssec: fix memleak when pkcs8_import_key() fails

--- src/libdnssec/keystore/pkcs8.c
+++ src/libdnssec/keystore/pkcs8.c
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2020 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2021 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -358,6 +358,7 @@ static int pkcs8_import_key(void *ctx, const dnssec_binary_t *pem, char **id_ptr
 			*id_ptr = id;
 			return DNSSEC_EOK;
 		}
+		free(id);
 		return r;
 	}
 
