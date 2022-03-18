5b1e03878e110bfe87df7994eb29375db41e730c
Daniel Salzman <daniel.salzman@nic.cz>
2022-03-18 10:41:53 +0100

    tests-extra: rework test journal/serial_arithmetic

--- tests-extra/tests/journal/serial_arithmetic/test.py
+++ tests-extra/tests/journal/serial_arithmetic/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-'''Test for serial number arithmetics'''
+'''Test for serial number arithmetics when loading a zone file and storing its difference'''
 
 import os
 from dnstest.test import Test
@@ -22,12 +22,14 @@ dns2 A     192.0.2.2
 mail A     192.0.2.3
      AAAA  2001:DB8::3
 
-new  A     1.1.1.%d"""
+new  A     1.1.1.%d
+"""
 
 SERIALS = {
-    "z1.": [0, 1, 2147483648, 4294967295, 0],
-    "z2.": [0, 1, 2147483648, 4294967295, 1],
-    "z3.": [0, 1, 2147483648, 4294967295, 2]
+                           # max incr, max value,  next uniq,  prev invalid
+    "z1.": [0,          1, 2147483648, 4294967295, 3,          2],
+                                                               # next invalid
+    "z2.": [4294967295, 0,          1, 2147483648, 4294967294, 1 + (4294967294 + 2147483647) % 4294967296]
 }
 
 t = Test()
@@ -52,7 +54,7 @@ master.zones_wait(zones)
 refer.zones_wait(zones)
 t.xfr_diff(master, refer, zones)
 
-for i in range(1, 5):
+for i in range(1, 6):
     # Update zone files.
     for zone in zones:
         master.update_zonefile(zone, version=i, storage=t.zones_dir)
@@ -60,15 +62,19 @@ for i in range(1, 5):
     master.reload()
     refer.reload()
 
+    fix = 0
+    if i == 5: # Invalid serial change - zone unchanged
+        fix = 1;
+        t.sleep(1)
+
     previous = dict()
 
     for zone in zones:
-        master.zone_wait(zone, SERIALS[zone.name][i], equal=True, greater=False)
-        refer.zone_wait(zone, SERIALS[zone.name][i], equal=True, greater=False)
-        previous[zone.name] = SERIALS[zone.name][i - 1]
+        master.zone_wait(zone, SERIALS[zone.name][i - fix], equal=True, greater=False)
+        refer.zone_wait(zone, SERIALS[zone.name][i - fix], equal=True, greater=False)
+        previous[zone.name] = SERIALS[zone.name][i - 1 - fix]
 
     t.xfr_diff(master, refer, zones)
-    if i < 4: # Dnspython fails for i = 4.
-        # Compare last IXFR changeset.
-        t.xfr_diff(master, refer, zones, serials=previous)
+    t.xfr_diff(master, refer, zones, serials=previous)
+
 t.end()
