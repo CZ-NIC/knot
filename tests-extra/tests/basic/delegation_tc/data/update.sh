577ef42a5ee5bb339882499c97c2aa92ecb3d36b
Daniel Salzman <daniel.salzman@nic.cz>
2022-04-20 11:21:26 +0200

    tests-extra: update and fix (reserved DS algorithm) test basic/delegation_tc on Ubuntu 22.04

--- tests-extra/tests/basic/delegation_tc/data/update.sh
+++ tests-extra/tests/basic/delegation_tc/data/update.sh
@@ -3,7 +3,7 @@
 WORKDIR=$(mktemp -d)
 trap 'cd /; rm -rf "$WORKDIR"' EXIT
 
-( cd $WORKDIR && dnssec-keygen -r/dev/urandom -a 13 tc.test; )
+( cd $WORKDIR && dnssec-keygen -a 13 tc.test; )
 
-dnssec-signzone -S -P -r/dev/urandom -d "$WORKDIR" -K "$WORKDIR" \
+dnssec-signzone -S -P -d "$WORKDIR" -K "$WORKDIR" \
 	-o tc.test -f tc.test.zone tc.test.zone.unsigned
