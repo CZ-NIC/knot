CZ-NIC/knot

commit 925ad42d43365081ad6add1708f5de46a22c3900

heap: increase, decrease, replace operations

src/knot/common/evsched.c
@@ -139,15 +139,15 @@ int evsched_schedule(event_t *ev, uint32_t dt)
 	evsched_t *sched = ev->sched;
 	pthread_mutex_lock(&sched->heap_lock);
 
+	ev->tv = new_time;
+
 	/* Make sure it's not already enqueued. */
 	int found = 0;
 	if ((found = heap_find(&sched->heap, ev))) {
-		heap_delete(&sched->heap, found);
+		heap_replace(&sched->heap, found, ev);
+	} else {
+		heap_insert(&sched->heap, ev);
 	}
-
-	ev->tv = new_time;
-
-	heap_insert(&sched->heap, ev);
 
 	/* Unlock calendar. */
 	pthread_cond_broadcast(&sched->notify);

src/libknot/internal/heap.c
@@ -90,6 +90,27 @@ static inline void _heap_bubble_up(struct heap *h, int e)
 
 }
 
+static void heap_increase(struct heap *h, int pos, void *e)
+{
+	*HELEMENT(h, pos) = e;
+	_heap_bubble_down(h, pos);
+}
+
+static void heap_decrease(struct heap *h, int pos, void *e)
+{
+	*HELEMENT(h, pos) = e;
+	_heap_bubble_up(h, pos);
+}
+
+void heap_replace(struct heap *h, int pos, void *e)
+{
+	if (h->cmp(*HELEMENT(h, pos),e) < 0) {
+		heap_increase(h, pos, e);
+	} else {
+		heap_decrease(h, pos, e);
+	}
+}
+
 void heap_delmin(struct heap *h)
 {
 	if(h->num == 0) return;

src/libknot/internal/heap.h
@@ -48,5 +48,7 @@ void heap_delmin(struct heap *);
 int heap_insert(struct heap *, void *);
 int heap_find(struct heap *, void *);
 void heap_delete(struct heap *, int);
+void heap_replace(struct heap *h, int pos, void *e);
+
 
 /*! @} */

commit 1769fdef8c93b740d95499438a71512bb67e0614

heap: in heap_replace(), replace properly even an identical element
Current code as used by evsched_schedule() has led to a corrupted heap
(with its elements out of order), resulting in unpredictable event scheduling
behaviour, including events being processed out of order or already scheduled
events being postponed, sometimes for several days, weeks, or maybe longer.

Solving it by modifying the heap.c library a little -- the new code properly
handles a situation when the original element and the element it's being replaced by
are identical (that is, the same memory object), but already containing a modified
value than what originally was in the heap. Instead of comparing the new element
with the one it replaces, we now start comparing it with its parent.

This commit fixes an old bug introduced in:
https://gitlab.nic.cz/knot/knot-dns/-/commit/925ad42d43365081ad6add1708f5de46a22c3900

src/contrib/ucw/heap.c
@@ -108,7 +108,7 @@ static void heap_decrease(struct heap *h, int pos, heap_val_t *e)
 
 void heap_replace(struct heap *h, int pos, heap_val_t *e)
 {
-	if (h->cmp(*HELEMENT(h, pos),e) < 0) {
+	if (pos == 1 || h->cmp(*HELEMENT(h, pos / 2), e) < 0) {
 		heap_increase(h, pos, e);
 	} else {
 		heap_decrease(h, pos, e);

src/knot/common/evsched.c
@@ -195,6 +195,7 @@ int evsched_schedule(event_t *ev, uint32_t dt)
 	/* Make sure it's not already enqueued. */
 	int found = heap_find(&sched->heap, (heap_val_t *)ev);
 	if (found > 0) {
+		/* "Replacing" with itself -- just repositioning it. */
 		heap_replace(&sched->heap, found, (heap_val_t *)ev);
 	} else {
 		heap_insert(&sched->heap, (heap_val_t *)ev);