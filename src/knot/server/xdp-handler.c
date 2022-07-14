9bdcbd6e62454aff49e8ca5925b24e6d7aec0512
Daniel Salzman <daniel.salzman@nic.cz>
2022-07-14 13:45:10 +0200

    fixup! xdp-quic: share quic_creds among threads

--- src/knot/server/xdp-handler.c
+++ src/knot/server/xdp-handler.c
@@ -170,14 +170,9 @@ xdp_handle_ctx_t *xdp_handle_init(struct server *server, knot_xdp_socket_t *xdp_
 	if (ctx->quic_port > 0) {
 #ifdef ENABLE_QUIC
 		conf_t *pconf = conf();
-		char *tls_cert = conf_tls(pconf, C_TLS_CERT);
-		char *tls_key = conf_tls(pconf, C_TLS_KEY);
-
 		size_t udp_pl = MIN(pconf->cache.srv_udp_max_payload_ipv4, pconf->cache.srv_udp_max_payload_ipv6);
 		ctx->quic_table = knot_xquic_table_new(true, ctx->tcp_max_conns, ctx->tcp_max_inbufs,
 		                                       ctx->tcp_max_obufs, udp_pl, server->quic_creds);
-		free(tls_cert);
-		free(tls_key);
 		if (ctx->quic_table == NULL) {
 			xdp_handle_free(ctx);
 			return NULL;
