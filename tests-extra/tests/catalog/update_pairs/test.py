668fccfa8b0112635a5ce4177c79d433efdb9518
David Vašek <david.vasek@nic.cz>
2022-10-17 14:55:49 +0200

    WIP tests-extra: catalog/update_pairs -- improve the speed of updating, add fixed catalog zones, and load th bg workers

--- tests-extra/tests/catalog/update_pairs/test.py
+++ tests-extra/tests/catalog/update_pairs/test.py
@@ -13,52 +13,99 @@ import threading
 import shutil
 
 ROUNDS = 20
-BATCH = 30
+BATCH_MAX = 300
+FIXED_ZONES = 2000
 
-t = Test(stress=False)
+#t = Test(stress=False)
+t = Test()
 
 knot = t.server("knot")
 
-if knot.valgrind:
-    knot.semantic_check = False
+#if knot.valgrind:
+#    knot.semantic_check = False
+knot.semantic_check = False
 
 catz = t.zone("catalog1.", storage=".")
+rzone = t.zone(".")
+nuzone = t.zone("nu.", storage=".")
 
 t.link(catz, knot)
+t.link(rzone, knot)
+t.link(nuzone, knot)
 knot.cat_interpret(catz)
+catalog_dir = os.path.join(knot.dir, "catalog")
+os.mkdir(catalog_dir)
 
-os.mkdir(knot.dir + "/catalog")
+knot.dnssec(rzone).enable = True
+knot.dnssec(nuzone).enable = True
+
+knot.zonefile_load = "difference"
+knot.zones["catalog1."].journal_content = "all"
+knot.gen_confile()
 
 t.start()
+
+zonefile_src = os.path.join(t.data_dir, "generic.zone")
+
+fixed_counter = 0
+
+while fixed_counter < FIXED_ZONES:
+    fixed_remains = FIXED_ZONES - fixed_counter
+    up = knot.update(catz)
+    for j in range(fixed_remains if (fixed_remains < BATCH_MAX) else BATCH_MAX):
+        member_zone = "fixed_member%d." % fixed_counter
+        fixed_counter += 1
+        unique_id = hashlib.md5(member_zone.encode()).hexdigest()
+        name = unique_id + ".zones"
+##
+        owner = name + "." + catz[0].name
+        zonefile = os.path.join(catalog_dir, member_zone + "zone")
+        shutil.copyfile(zonefile_src, zonefile)
+
+        up.add(name, 0, "PTR", member_zone)
+    up.try_send()
+
+
+knot.ctl("zone-sign . nu.")
+
+
+member_list = list()
+zonefile_src = os.path.join(t.data_dir, "generic.zone")
+
+# Prepare detailed list of possible members in advance.
+for j in range(BATCH_MAX):
+    member_zone = "member%d." % j
+    unique_id = hashlib.md5(member_zone.encode()).hexdigest()
+    name = unique_id + ".zones"
+    owner = name + "." + catz[0].name
+    zonefile = os.path.join(catalog_dir, member_zone + "zone")
+    member_list += [(member_zone, name, owner, zonefile)]
 
 knot.zone_wait(catz, udp=False, tsig=True)
 
 for i in range(ROUNDS):
-    up = knot.update(catz)
-    for j in range(BATCH):
-        zone_add = "member%d." % j
-        name_hash = hashlib.md5(zone_add.encode()).hexdigest()
+    batch = random.randrange(1, BATCH_MAX)
+#    batch = BATCH_MAX
 
-        shutil.copyfile(t.data_dir + "generic.zone" , knot.dir + "/catalog/" + zone_add + "zone")
-        up.add(name_hash + ".zones", 0, "PTR", zone_add)
+    knot.ctl("zone-sign . nu.")
+
+    up = knot.update(catz)
+    for j in range(batch):
+##
+        shutil.copyfile(zonefile_src, member_list[j][3])
+        up.add(member_list[j][1], 0, "PTR", member_list[j][0])
     up.try_send()
 
     up = knot.update(catz)
-    for j in range(BATCH):
-        zone_add = "member%d." % j
-        name_hash = hashlib.md5(zone_add.encode()).hexdigest()
-
-        up.delete(name_hash + ".zones", "PTR", zone_add)
+    for j in range(batch):
+        up.delete(member_list[j][1], "PTR", member_list[j][0])
     up.try_send()
 
-    for j in range(BATCH):
-        zone_add = "member%d." % j
-        name_hash = hashlib.md5(zone_add.encode()).hexdigest()
-
-        resp = knot.dig(name_hash + ".zones." + catz[0].name, "PTR", udp=False, tsig=True)
+    for j in range(batch):
+        resp = knot.dig(member_list[j][2], "PTR", udp=False, tsig=True)
         try:
             resp.check(rcode="NXDOMAIN", nordata="PTR")
         except:
-            set_err("MEMBER NOT DELETED FROM CATZ")
+            set_err("MEMBER NOT DELETED FROM CATZ: %s" % member_list[j][0])
 
 t.end()
