83d2692cf9c740481b8ba2c971e4324ecf539e5c
Jan Hák <jan.hak@nic.cz>
2022-10-20 13:14:45 +0200

    kdig: send message with PROXYv2 header

--- src/contrib/proxyv2/proxyv2.h
+++ src/contrib/proxyv2/proxyv2.h
@@ -19,6 +19,11 @@
 #include <stddef.h>
 #include <sys/socket.h>
 
+extern const size_t PROXYV2_HEADER_MAXLEN;
+
 int proxyv2_header_offset(void *base, size_t len_base);
 
 int proxyv2_addr_store(void *base, size_t len_base, struct sockaddr_storage *ss);
+
+int proxyv2_write_header(char *buf, size_t buflen, int socktype, const struct sockaddr *src,
+                         const struct sockaddr *dst);
