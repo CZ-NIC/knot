48954cdffd85d7e1ac6242024ea31472d82df8aa
Daniel Salzman <daniel.salzman@nic.cz>
2023-01-16 21:27:41 +0100

    tests-extra: merge ixfr tests knot_knot with knot_knot_unix

--- tests-extra/tests/ixfr/knot_knot/test.py
+++ tests-extra/tests/ixfr/knot_knot/test.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-'''Test for IXFR from Knot to Knot'''
+'''Test for IXFR from Knot to Knot (also over a UNIX socket)'''
 
+import os
+import random
 from dnstest.test import Test
 
 t = Test()
 
-master = t.server("knot")
-slave = t.server("knot")
+use_unix = random.choice([True, False])
+master_address = os.path.join(t.out_dir, "master.sock") if use_unix else None
+slave_address = os.path.join(t.out_dir, "slave.sock") if use_unix else None
+
+master = t.server("knot", address=master_address)
+slave = t.server("knot", address=slave_address)
 zones = t.zone_rnd(5, records=50) + t.zone("records.")
 
 t.link(zones, master, slave, ixfr=True)
@@ -23,8 +29,8 @@ slave.tcp_remote_io_timeout = 8000
 t.start()
 
 # Wait for AXFR to slave server.
-serials_init = master.zones_wait(zones)
-slave.zones_wait(zones)
+serials_init = master.zones_wait(zones, use_ctl=use_unix)
+slave.zones_wait(zones, use_ctl=use_unix)
 
 serials_prev = serials_init
 for i in range(4):
@@ -34,11 +40,12 @@ for i in range(4):
     master.reload()
 
     # Wait for IXFR to slave.
-    serials = master.zones_wait(zones, serials_prev)
-    slave.zones_wait(zones, serials_prev)
+    serials = master.zones_wait(zones, serials_prev, use_ctl=use_unix)
+    slave.zones_wait(zones, serials_prev, use_ctl=use_unix)
     serials_prev = serials
 
     # Compare IXFR between servers.
-    t.xfr_diff(master, slave, zones, serials_init)
+    if not use_unix:
+        t.xfr_diff(master, slave, zones, serials_init)
 
 t.end()
