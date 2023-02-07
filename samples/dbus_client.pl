fbb36325fbc943538dd762bc4f9011003b48f107
David Vašek <david.vasek@nic.cz>
2023-02-07 16:24:23 +0100

    samples: dbus_client.pl -- follow the same output style as .sh and .py samples

--- samples/dbus_client.pl
+++ samples/dbus_client.pl
@@ -6,15 +6,46 @@ use Net::DBus::Reactor;
 my $bus = Net::DBus->system;
 
 # Get a handle to the 'knotd' service
-my $knotd = $bus->get_service("cz.nic.knotd");
+my $knotd;
+while (true) {
+    eval {
+        $knotd = $bus->get_service("cz.nic.knotd");
+    };
+    if (!$@) {
+        last;
+    }
+    sleep(1);
+}
 
 # Get the device manager
 my $knotd_interface = $knotd->get_object("/cz/nic/knotd", "cz.nic.knotd.events");
 
+$knotd_interface->connect_to_signal('started', sub
+{
+    print "Server started\n";
+});
+
+$knotd_interface->connect_to_signal('stopped', sub
+{
+    print "Server stopped\n";
+});
+
 $knotd_interface->connect_to_signal('zone_updated', sub
 {
     my ($zone, $serial) = @_;
-    print "Zone $zone updated, SOA serial $serial\n";
+    print "Updated zone=$zone to serial=$serial\n";
+});
+
+$knotd_interface->connect_to_signal('zone_ksk_submission', sub
+{
+    my ($zone, $key_tag, $kasp_id) = @_;
+    print "Ready KSK for zone=$zone keytag=$key_tag keyid=$kasp_id\n";
+});
+
+$knotd_interface->connect_to_signal('zone_dnssec_invalid', sub
+{
+    my ($zone) = @_;
+    print "Invalid DNSSEC for zone=$zone\n";
 });
 
 # Main loop
