3536295efc4104d07f842a526a71dcd23c0033c2
Daniel Salzman <daniel.salzman@nic.cz>
2023-02-23 14:03:36 +0100

    quic: unify and reduce ALPN for QUIC to just 'doq'

--- src/utils/common/quic.h
+++ src/utils/common/quic.h
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2022 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2023 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -96,7 +96,7 @@ typedef struct {
 	uint64_t idle_ts;
 } quic_ctx_t;
 
-extern const gnutls_datum_t doq_alpn[];
+extern const gnutls_datum_t doq_alpn;
 
 uint64_t quic_timestamp(void);
 
