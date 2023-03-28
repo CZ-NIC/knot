0fb6482d9b560f60698c728949214ee8625daf90
秦龙飞 <qinlongfei.1@bytedance.com>
2023-03-28 13:47:59 +0800

    Invoke rcu_register_thread() before its first call to rcu_read_lock()

--- src/knot/common/stats.c
+++ src/knot/common/stats.c
@@ -251,6 +251,7 @@ static void dump_stats(server_t *server)
 
 static void *dumper(void *data)
 {
+	rcu_register_thread();
 	while (true) {
 		assert(stats.timer > 0);
 		sleep(stats.timer);
@@ -261,7 +262,7 @@ static void *dumper(void *data)
 		rcu_read_unlock();
 		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
 	}
-
+	rcu_unregister_thread();
 	return NULL;
 }
 
