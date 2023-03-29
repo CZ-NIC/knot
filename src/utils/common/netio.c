a12592812baf08df846d3d5de3f649db693a5573
Daniel Salzman <daniel.salzman@nic.cz>
2023-03-29 08:56:07 +0200

    Revert "kdig: testing of enable TCP_QUICKACK socket option to improve XFR-over-TCP performance with XDP server"
    This reverts commit bc136d0946b3565d4affa162f87ff17b8137a95c.

--- src/utils/common/netio.c
+++ src/utils/common/netio.c
@@ -444,11 +444,6 @@ int net_connect(net_t *net)
 			return KNOT_NET_ESOCKET;
 		}
 #endif
-#ifdef TCP_QUICKACK
-		if (setsockopt(sockfd, IPPROTO_TCP, TCP_QUICKACK, &cs, sizeof(cs))) {
-			return KNOT_NET_ESOCKET;
-		}
-#endif
 
 		// Establish a connection.
 		if (net->tls.params == NULL || !fastopen) {
