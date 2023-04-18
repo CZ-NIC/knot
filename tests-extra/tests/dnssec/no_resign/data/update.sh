4342661909179a19e6493357b06668288ba84458
Libor Peltan <libor.peltan@nic.cz>
2023-04-18 13:58:53 +0200

    tests/no_resign: fix messy keys/keys/keys directory

--- tests-extra/tests/dnssec/no_resign/data/update.sh
+++ tests-extra/tests/dnssec/no_resign/data/update.sh
@@ -4,6 +4,11 @@ set -e
 
 KEYMGR=${KEYMGR:-keymgr}
 
+if basename $(pwd) | grep -q '^keys$'; then
+    echo "Error: please cd to outside of 'keys' directory."
+    exit 1
+fi
+
 export BASEDIR=`mktemp -d "/tmp/zone_sign-XXX"`
 ../../../../tools/zone_sign.sh example. ../../../../data/example.zone nsec
 mv ../../../../data/example.zone.signed ./example.zone
