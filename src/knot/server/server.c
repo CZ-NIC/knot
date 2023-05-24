5ef301fdeca99add56e26d0efae608e4b69f9c2a
David Vašek <david.vasek@nic.cz>
2023-05-24 12:36:50 +0200

    quic: create the key directory as executable by user and group

--- src/knot/server/server.c
+++ src/knot/server/server.c
@@ -540,7 +540,7 @@ static int init_creds(server_t *server, conf_t *conf)
 	if (cert_file == NULL) {
 		assert(key_file == NULL);
 		char *kasp_dir = conf_db(conf, C_KASP_DB);
-		int ret = make_dir(kasp_dir, 0660, true);
+		int ret = make_dir(kasp_dir, S_IRWXU | S_IRWXG, true);
 		if (ret != KNOT_EOK) {
 			log_error("QUIC, failed to create directory '%s'", kasp_dir);
 			free(kasp_dir);
