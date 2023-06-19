d261c3d5748682068b582d61823383529a4bb85d
Daniel Salzman <daniel.salzman@nic.cz>
2023-06-19 11:13:43 +0200

    python: add dname from bytes initialization (in cooperation with Jan Hák)

--- python/libknot/dname.py
+++ python/libknot/dname.py
@@ -14,13 +14,14 @@ class KnotDname(object):
     DnameTxtStorage = ctypes.c_char * CAPACITY_TXT
 
     SIZE = None
+    CHECK = None
     TO_STR = None
     FROM_STR = None
 
     data = None
 
-    def __init__(self, dname: str = None) -> None:
-        """Initializes a dname storage. Optionally initializes from a string."""
+    def __init__(self, dname_str: str = None, dname_wire: bytes = None) -> None:
+        """Initializes a dname storage. Optionally initializes from a string or wire."""
 
         if not KnotDname.SIZE:
             libknot.Knot()
@@ -28,6 +29,10 @@ class KnotDname(object):
             KnotDname.SIZE = libknot.Knot.LIBKNOT.knot_dname_size
             KnotDname.SIZE.restype = ctypes.c_size_t
             KnotDname.SIZE.argtypes = [KnotDname.DnameStorage]
+
+            KnotDname.CHECK = libknot.Knot.LIBKNOT.knot_dname_wire_check
+            KnotDname.CHECK.restype = ctypes.c_int
+            KnotDname.CHECK.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
 
             KnotDname.TO_STR = libknot.Knot.LIBKNOT.knot_dname_to_str
             KnotDname.TO_STR.restype = ctypes.c_char_p
@@ -37,9 +42,19 @@ class KnotDname(object):
             KnotDname.FROM_STR.restype = ctypes.c_char_p
             KnotDname.FROM_STR.argtypes = [KnotDname.DnameStorage, ctypes.c_char_p, ctypes.c_size_t]
 
-        if dname:
+        if dname_str:
             self.data = KnotDname.DnameStorage()
-            if not KnotDname.FROM_STR(self.data, dname.encode('utf-8'), KnotDname.CAPACITY):
+            if not KnotDname.FROM_STR(self.data, dname_str.encode('utf-8'), KnotDname.CAPACITY):
+                raise ValueError
+        elif dname_wire:
+            size = len(dname_wire)
+            if size > KnotDname.CAPACITY:
+                raise ValueError
+            self.data = KnotDname.DnameStorage()
+            ctypes.memmove(self.data, dname_wire, size)
+            start = ctypes.cast(self.data, ctypes.POINTER(ctypes.c_char * size))[0]
+            end = ctypes.cast(self.data, ctypes.POINTER(ctypes.c_char * size))[1]
+            if KnotDname.CHECK(start, end, start) <= 0:
                 raise ValueError
 
     def size(self):
