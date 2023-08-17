54599b6465935c7dad936fac9c1c47eebc0160a2
Daniel Salzman <daniel.salzman@nic.cz>
2023-08-17 21:22:44 +0200

    doc: improve sphinx-panels detection

--- doc/conf.py
+++ doc/conf.py
@@ -27,11 +27,9 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-try:
-    import sphinx_panels
+import importlib.util
+if importlib.util.find_spec("sphinx_panels"):
     extensions = [ 'sphinx_panels' ]
-except:
-    pass
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
