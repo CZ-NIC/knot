ef4738d5c347395938f5f0d2711997829eefab9e
Daniel Salzman <daniel.salzman@nic.cz>
2023-09-11 14:41:36 +0200

    knot_exporter: fix knotd PIDs parsing
    This fixes https://github.com/ghedo/knot_exporter/issues/8#issuecomment-1713565861

--- python/knot_exporter/knot_exporter/knot_exporter.py
+++ python/knot_exporter/knot_exporter/knot_exporter.py
@@ -18,13 +18,12 @@ from prometheus_client.exposition import MetricsHandler
 
 def memory_usage():
     out = dict()
-    pids = subprocess.check_output(['pidof', 'knotd']).split(b' ')
-    for pid in pids:
-        if not pid:
-            continue
-        key = int(pid)
-        out[key] = psutil.Process(key).memory_info()._asdict()['rss']
-    return out
+    try:
+        pids = subprocess.check_output(['pidof', 'knotd']).decode().split()
+        for pid in pids:
+            out[pid] = psutil.Process(int(pid)).memory_info()._asdict()['rss']
+    finally:
+        return out
 
 
 class KnotCollector(object):
