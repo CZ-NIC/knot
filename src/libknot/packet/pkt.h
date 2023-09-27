f55037f6f39d0ddf6debac47fe168d4ab7564a72
Libor Peltan <libor.peltan@nic.cz>
2023-09-27 13:08:26 +0200

    libknot/knot: allow uncompressed wire conversion of rrset that fits to 64k only when compressed

--- src/libknot/packet/pkt.h
+++ src/libknot/packet/pkt.h
@@ -52,6 +52,7 @@ enum {
 	KNOT_PF_NOCANON   = 1 << 5, /*!< Don't canonicalize rrsets during parsing. */
 	KNOT_PF_ORIGTTL   = 1 << 6, /*!< Write RRSIGs with their original TTL. */
 	KNOT_PF_SOAMINTTL = 1 << 7, /*!< Write SOA with its minimum-ttl as TTL. */
+	KNOT_PF_BUFENOUGH = 1 << 8, /*!< The output buffer is big enough for the output. */
 };
 
 typedef struct knot_pkt knot_pkt_t;
