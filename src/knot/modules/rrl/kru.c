7bdd595b7209a180b65c561e0895761167a2f283
Vladimír Čunát <vladimir.cunat@nic.cz>
2023-11-30 09:21:22 +0100

    WIP: switch to a different sketch
    https://en.wikipedia.org/wiki/Count_sketch
    We need signed numbers now, so the weigths got scaled roughly to halves.
    In most respects it's very similar, though.

--- src/knot/modules/rrl/kru.c
+++ src/knot/modules/rrl/kru.c
@@ -1,6 +1,6 @@
 /** @file
 
-KRU is a Count-min sketch with aging
+KRU is a variant of Count sketch with aging
 
 The point is to answer point-queries that estimate if the item has been heavily used recently.
 To give more weight to recent usage, we use aging via exponential decay (simple to compute).
@@ -11,15 +11,14 @@ That has applications for garbage collection of cache and various limiting scena
 ### Choosing parameters
 
 For limiting, `time` is probably in milliseconds from kr_now().
-In case of DECAY_32, we get at most 92M per tick which gives 92G per second.
-Say, if we want p QPS, we add `92G / p` for each query.
+In case of DECAY_32, we get at most 46M per tick which gives 46G per second.
+Say, if we want p QPS, we add `46G / p` for each query.
 
 Tick length (`ticklen_log`) will need to be chosen the same for all users of a given table.
-Smaller resolvers might choose more than a single millisecond to get longer half-life,
-as it's advisable to allow at least several queries per tick.
+Smaller resolvers might choose more than a single millisecond to get longer half-life.
 
-Size (`loads_bits`):
- - The KRU takes 128 bytes * length + some small constants.
+Size (`loads_bits` = log2 length):
+ - The KRU takes 128 bytes * length * TABLE_COUNT + some small constants.
  - The length should probably be at least something like the square of the number of utilized CPUs.
    But this most likely won't be a limiting factor.
  - TODO: more info
@@ -58,7 +57,7 @@ void *memzero(void *s, size_t n)
 struct load_cl {
 	uint32_t time;
 	#define LOADS_LEN 15
-	uint32_t loads[LOADS_LEN];
+	int32_t loads[LOADS_LEN];
 } ALIGNED_CPU_CACHE;
 static_assert(64 == sizeof(struct load_cl), "bad size of struct load_cl");
 
@@ -69,8 +68,8 @@ struct decay_config {
 	/// Exponential decay with half-life of (2 ^ half_life_log) ticks.
 	uint32_t half_life_log;
 	/// Precomputed scaling constants.  Indexed by tick count [1 .. 2^half_life_log - 1],
-	///   contains the corresponding factor of decay (<1, scaled to 2^32 and rounded).
-	uint32_t scales[];
+	///   contains the corresponding factor of decay (<1, scaled to 2^31 and rounded).
+	int32_t scales[];
 };
 typedef const struct decay_config decay_cfg_t;
 
@@ -106,26 +105,27 @@ static void update_time(struct load_cl *l, const uint32_t time_now, decay_cfg_t 
 	const uint32_t load_nonfrac_shift = ticks >> decay->half_life_log;
 	for (int i = 0; i < LOADS_LEN; ++i) {
 		// decay: first do the "fractional part of the bit shift"
-		uint64_t m = (uint64_t)l->loads[i] * decay->scales[decay_frac];
-		uint32_t l1 = (m >> 32) + /*rounding*/((m >> 31) & 1);
+		int64_t m = (int64_t)l->loads[i] * decay->scales[decay_frac];
+		int32_t l1 = m >> 31;
 		// finally the non-fractional part of the bit shift
 		l->loads[i] = l1 >> load_nonfrac_shift;
+		// rounding during signed shift(s) would be too complicated?
 	}
-
 }
 /// Half-life of 32 ticks, consequently forgetting in about 1k ticks.
-/// Experiment: if going by a single tick, fix-point at load 23 after 874 steps,
+/// Experiment: if going by a single tick, after 840 steps +0 (or fixed at -46),
 ///  but accuracy at the beginning of that (first 32 ticks) is very good,
-///  getting from max 2^32 - 1 to 2^31 - 7.  Max. decay per tick is 92032292.
+///  getting from max 2^31 - 1 to 2^30 - 9 or -2^31 to -2^30 - 17.
+///  Max. decay per tick is 46016146.
 const struct decay_config DECAY_32 = {
 	.ticklen_log = 0,
 	.half_life_log = 5,
-	.scales = { // ghci> map (\i -> round(2^32 * 0.5 ** (i/32))) [1..31]
-		0, 4202935003,4112874773,4024744348,3938502376,3854108391,3771522796,
-		3690706840,3611622603,3534232978,3458501653,3384393094,3311872529,
-		3240905930,3171459999,3103502151,3037000500,2971923842,2908241642,
-		2845924021,2784941738,2725266179,2666869345,2609723834,2553802834,
-		2499080105,2445529972,2393127307,2341847524,2291666561,2242560872,2194507417
+	.scales = { // ghci> map (\i -> round(2^31 * 0.5 ** (i/32))) [1..31]
+		0, 2101467502,2056437387,2012372174,1969251188,1927054196,1885761398,
+		1845353420,1805811301,1767116489,1729250827,1692196547,1655936265,
+		1620452965,1585730000,1551751076,1518500250,1485961921,1454120821,
+		1422962010,1392470869,1362633090,1333434672,1304861917,1276901417,
+		1249540052,1222764986,1196563654,1170923762,1145833280,1121280436,1097253708
 	}
 };
 
@@ -133,8 +133,6 @@ const struct decay_config DECAY_32 = {
 struct kru {
 	/// Length of `loads_cls`, stored as binary logarithm.
 	uint32_t loads_bits;
-	/// Optimum: log2(max. number of limited users) - loads_bits - 1
-	uint32_t prob_bits;
 	/// Hashing secret.  Random but shared by all users of the table.
 	SIPHASH_KEY hash_key;  // TODO use or remove
 
@@ -143,12 +141,11 @@ struct kru {
 	struct load_cl load_cls[][TABLE_COUNT];
 };
 
-struct kru *kru_init(uint32_t loads_bits, uint32_t prob_bits)
+struct kru *kru_init(uint32_t loads_bits)
 {
 	struct kru *kru = calloc(1, offsetof(struct kru, load_cls) + sizeof(struct load_cl) * TABLE_COUNT * (1 << loads_bits));
 
 	kru->loads_bits = loads_bits;
-	kru->prob_bits = prob_bits;
 	// hash_key zero-initialized
 
 	return kru;
@@ -160,59 +157,38 @@ void kru_destroy(struct kru *kru) {
 /// Update limiting and return true iff it hit the limit instead.
 bool kru_limited(struct kru *kru, uint64_t hash, uint32_t time_now, uint32_t price)
 {
-	assert(sizeof(hash) * 8 >= TABLE_COUNT * kru->loads_bits + LOADS_LEN * kru->prob_bits);
-	/*
-		Given 64-bit hash + TABLE_COUNT cache-lines of 15 items:
-		prob_bits -> max loads_bits  | opt. heavy-hitter limit (see prob_bits comment)
-		1 -> 24  | 2^26
-		2 -> 17  | 2^20
-		3 -> 9   | 2^13
-		We might just need longer hash, e.g. an array of SIPHASHes.
-	*/
+	assert(sizeof(hash) * 8 >= TABLE_COUNT * (kru->loads_bits + LOADS_LEN));
 
 	// Choose two struct load_cl, i.e. two cache-lines to operate on,
-	// and update their notion of time.
+	// update their notion of time, and copy all their loads.
 	struct load_cl *l[TABLE_COUNT];
+	int32_t l_c[TABLE_COUNT * LOADS_LEN];
 	const uint32_t loads_mask = (1 << kru->loads_bits) - 1;
 	for (int li = 0; li < TABLE_COUNT; ++li) {
 		l[li] = &kru->load_cls[hash & loads_mask][li];
 		hash >>= kru->loads_bits;
 		update_time(l[li], time_now, &DECAY_32);
+		memcpy(&l_c[li * LOADS_LEN], l[li]->loads, sizeof(l[li]->loads));
 	}
 
-	const uint64_t prob_mask = (1 << kru->prob_bits) - 1;
-	const uint32_t limit = -price;
-	// Check if an index indicates that we're under the limit.
-	uint64_t prnd = hash;
-	for (int li = 0; li < TABLE_COUNT; ++li) {
-		for (int i = 0; i < LOADS_LEN; ++i) {
-			unsigned int skip = prnd & prob_mask;
-			prnd >>= kru->prob_bits;
-			if (skip != 0) // TODO: exception to avoid skipping all in a table?
-				continue;
-			if (l[li]->loads[i] < limit)
-				goto under_limit;
+	// Update the copied loads, saturating to max. value.
+	// __builtin_add_overflow: GCC+clang most likely suffice for us
+	// TODO: check that all is OK, maybe use addition with saturation in x86 asm
+	// The estimate is median of values, so we only count how many are over limit.
+	int over_limit = 0;
+	for (int i = 0; i < TABLE_COUNT * LOADS_LEN; ++i) {
+		const int32_t sign = 2 * ((int32_t)hash&1) - 1; // 0 or 1  ->  -1 or 1
+		hash >>= 1;
+		int32_t * const load = &l_c[i];
+		if (__builtin_add_overflow(*load, sign * (int32_t)price, load)) {
+			if (++over_limit > TABLE_COUNT * LOADS_LEN / 2)
+				return true; // TODO: equality?
+			*load = sign * (int32_t)((1ull<<31) - 1);
 		}
 	}
-	return true; // All positions were on the limit or higher.
-under_limit:
-	// Update the loads, saturating to max. value (-1).
-	// We're trying hard to avoid overflow even in case of races.
-	//  __builtin_add_overflow: GCC+clang most likely suffice for us
-	// TODO: check that all is OK, maybe use stdatomic.h
-	//  or addition with saturation might be easy and efficient in x86 asm
-	prnd = hash;
-	for (int li = 0; li < TABLE_COUNT; ++li) {
-		for (int i = 0; i < LOADS_LEN; ++i) {
-			unsigned int skip = prnd & prob_mask;
-			prnd >>= kru->prob_bits;
-			if (skip != 0) // TODO: exception to avoid skipping all in a table?
-				continue;
-			uint32_t * const load = &l[li]->loads[i];
-			if (__builtin_add_overflow(*load, price, load))
-				*load = -1;
-		}
-	}
+	// Not limited, so copy the updated loads back.
+	for (int li = 0; li < TABLE_COUNT; ++li)
+		memcpy(l[li]->loads, &l_c[li * LOADS_LEN], sizeof(l[li]->loads));
 	return false;
 }
 
@@ -221,10 +197,10 @@ under_limit:
 #include <inttypes.h>
 void test_decay32(void)
 {
-	struct load_cl l = { .loads[0] = -1, .time = 0 };
-	for (uint32_t time = 0; time < 1030; ++time) {
+	struct load_cl l = { .loads[0] = (1ull<<31) - 1, .loads[1] = -(1ll<<31), .time = 0 };
+	for (uint32_t time = 0; time < 850; ++time) {
 		update_time(&l, time, &DECAY_32);
-		printf("%3d: %08zx\n", time, (size_t)l.loads[0]);
+		printf("%3d: %08d %08d\n", time, (int)l.loads[0], (int)l.loads[1]);
 	}
 }
 
@@ -285,7 +261,7 @@ void test_stage(struct test_ctx *ctx, uint32_t dur, uint64_t *freq) {
 #define TEST_STAGE(duration, ...) test_stage(&ctx, duration, (uint64_t[]) {__VA_ARGS__});
 
 void test(void) { // TODO more descriptive name
-	struct kru *kru = kru_init(16,2);
+	struct kru *kru = kru_init(16);
 
 	struct test_cat cats[] = {
 		{ "normal",       1,1000  },   // normal queries come from 1000 different addreses indexed 1-1000
@@ -293,7 +269,7 @@ void test(void) { // TODO more descriptive name
 	};
 
 	struct test_ctx ctx = {.kru = kru, .time = 0, .cats = cats, .cnt = sizeof(cats)/sizeof(struct test_cat),
-		.price = 1<<24  // same price for all packets
+		.price = 1<<23  // same price for all packets
 	};
 
 	// in each out of 10 ticks send around 1000 queries from random normal addresses and 500000 from each of the two attackers
