cc89699432225da72a4aeffa4c1ecbb567e4eee9
Daniel Salzman <daniel.salzman@nic.cz>
2023-12-23 20:52:43 +0100

    conf

--- src/knot/conf/schema.c
+++ src/knot/conf/schema.c
@@ -263,6 +263,7 @@ static const yp_item_t desc_xdp[] = {
 	{ C_TCP_IDLE_RESET,       YP_TINT,  YP_VINT = { 1, INT32_MAX, 20, YP_STIME } },
 	{ C_TCP_RESEND,           YP_TINT,  YP_VINT = { 1, INT32_MAX, 5, YP_STIME } },
 	{ C_ROUTE_CHECK,          YP_TBOOL, YP_VNONE },
+	{ C_RING_SIZE,            YP_TINT,  YP_VINT = { 4, 4096, 2048 } },
 	{ C_COMMENT,              YP_TSTR,  YP_VNONE },
 	{ NULL }
 };
