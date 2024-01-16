52b35d06d1da97b3590872a83662355709fe4c3e
Oto Šťáva <oto.stava@nic.cz>
2024-01-16 11:54:49 +0100

    doc: use sphinx_rtd_theme with colors from the logo manual
    This makes the HTML docs look similar to Knot Resolver's

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os, time, logging
 
+import sphinx_rtd_theme
+
 sys.setrecursionlimit(1500)
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -28,8 +30,13 @@ sys.path.insert(0, os.path.abspath('ext'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 import importlib.util
+extensions = []
 if importlib.util.find_spec("sphinx_panels"):
-    extensions = [ 'sphinx_panels' ]
+    extensions.append('sphinx_panels')
+
+theme_major = sphinx_rtd_theme.__version__.partition('.')[0]
+if theme_major == '2':
+    extensions.append('sphinxcontrib.jquery')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -100,7 +107,18 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'theme_html'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_static_path = ['_static']
+html_theme_options = {
+    'logo_only': True,  # if we have a html_logo below, this shows only the logo with no title text
+    # ToC options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+}
+html_logo = '_static/logo-negativ.svg'
+html_style = 'css/main.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -108,7 +126,7 @@ html_theme = 'theme_html'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['.']
+#html_theme_path = ['.']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -119,7 +137,7 @@ html_theme_path = ['.']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'logo.svg'
+#html_logo = 'logo.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
