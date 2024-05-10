1945cd43e82a02b32c2f3d97b67316cf0747145d
Libor Peltan <libor.peltan@nic.cz>
2024-05-10 18:27:57 +0200

    TLS: theoretically more realistic handshake timeout depending on IO timeout

--- src/knot/server/tcp-handler.c
+++ src/knot/server/tcp-handler.c
@@ -396,9 +396,9 @@ int tcp_master(dthread_t *thread)
 
 	/* Initialize TLS context. */
 	if (tls) {
-		// Set the HS timeout to double the RMT IO one.
+		// Set the HS timeout to 8x the RMT IO one as the HS duration can be up to 4*roundtrip.
 		tcp.tls_ctx = knot_tls_ctx_new(handler->server->quic_creds,
-		                               tcp.io_timeout, 2 * tcp.io_timeout, true);
+		                               tcp.io_timeout, 8 * tcp.io_timeout, true);
 		if (tcp.tls_ctx == NULL) {
 			ret = KNOT_ENOMEM;
 			goto finish;
