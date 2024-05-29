1fc2243a6cbc4ac3b5b572f7d7ebeae084a44b07
David Vašek <david.vasek@nic.cz>
2024-05-29 18:10:10 +0200

    libknot/error: add a new error code -- KNOT_EPERM
    The error string should be different from KNOT_EACCES, but it already uses
    the same string.

--- src/libknot/errcode.h
+++ src/libknot/errcode.h
@@ -57,6 +57,7 @@ enum knot_error {
 	KNOT_ERANGE        = -ERANGE,
 	KNOT_EADDRNOTAVAIL = -EADDRNOTAVAIL,
 	KNOT_ENOTDIR       = -ENOTDIR,
+	KNOT_EPERM         = -EPERM,
 
 	KNOT_ERRNO_ERROR   = -500,
 
