4ef9352f9802909733eed959cb59ed45384c4cfa
Daniel Salzman <daniel.salzman@nic.cz>
2024-08-16 19:49:58 +0200

    xx

--- tests/knot/test_confio.c
+++ tests/knot/test_confio.c
@@ -1007,6 +1007,7 @@ static const yp_item_t desc_xdp[] = {
 	{ C_TCP_IDLE_RESET,     YP_TINT,  YP_VNONE },
 	{ C_TCP_RESEND,         YP_TINT,  YP_VNONE },
 	{ C_ROUTE_CHECK,        YP_TBOOL, YP_VNONE },
+	{ C_NEEDS_WAKEUP,       YP_TBOOL, YP_VNONE },
 	{ C_RING_SIZE,          YP_TINT,  YP_VNONE },
 	{ C_BUSYPOLL_BUDGET,    YP_TINT,  YP_VNONE },
 	{ C_BUSYPOLL_TIMEOUT,   YP_TINT,  YP_VNONE },
