5f2f7a24a1e9428b3dce35cbf7e4472fd6b03fd6
Libor Peltan <libor.peltan@nic.cz>
2024-08-31 11:10:47 +0200

    concurrent CTL: bugfix: ctl_globals have to exist separately for each CTL thread

--- src/knot/ctl/process.h
+++ src/knot/ctl/process.h
@@ -19,13 +19,16 @@
 #include "libknot/libknot.h"
 #include "knot/server/server.h"
 
+#define CTL_MAX_CONCURRENT 8 // Number of CTL threads EXCLUDING the main thread which can also process CTL.
+
 /*!
  * Processes incoming control commands.
  *
  * \param[in]  ctl        Control context.
  * \param[in]  server     Server instance.
+ * \param[in]  thread_idx Index of a thread which performs the operation.
  * \param[out] exclusive  All following CTLs shall (not) be processed exclusively by this thread.
  *
  * \return Error code, KNOT_EOK if successful.
  */
-int ctl_process(knot_ctl_t *ctl, server_t *server, bool *exclusive);
+int ctl_process(knot_ctl_t *ctl, server_t *server, int thread_idx, bool *exclusive);
--- src/utils/knotd/main.c
+++ src/utils/knotd/main.c
@@ -48,7 +48,6 @@
 #include "utils/common/params.h"
 
 #define PROGRAM_NAME "knotd"
-#define MAX_CTL_CONCURRENT 8
 
 typedef enum {
 	CONCURRENT_EMPTY = 0,
@@ -68,6 +67,7 @@ typedef struct {
 	pthread_t thread;
 	sigset_t sigmask;
 	int ret;
+	int thread_idx;
 	bool exclusive;
 } concurrent_ctl_ctx_t;
 
@@ -341,6 +341,7 @@ static void init_ctxs(concurrent_ctl_ctx_t *concurrent_ctxs, size_t n_ctxs, serv
 		pthread_mutex_init(&cctx->mutex, NULL);
 		pthread_cond_init(&cctx->cond, NULL);
 		cctx->server = server;
+		cctx->thread_idx = i + 1;
 	}
 }
 
@@ -402,7 +403,7 @@ static void *ctl_process_thread(void *arg)
 		bool exclusive = ctx->exclusive;
 		pthread_mutex_unlock(&ctx->mutex);
 
-		int ret = ctl_process(ctx->ctl, ctx->server, &exclusive);
+		int ret = ctl_process(ctx->ctl, ctx->server, ctx->thread_idx, &exclusive);
 
 		pthread_mutex_lock(&ctx->mutex);
 		ctx->ret = ret;
@@ -468,8 +469,8 @@ static void event_loop(server_t *server, const char *socket, bool daemonize,
 
 	enable_signals();
 
-	concurrent_ctl_ctx_t concurrent_ctxs[MAX_CTL_CONCURRENT] = { 0 };
-	init_ctxs(concurrent_ctxs, MAX_CTL_CONCURRENT, server);
+	concurrent_ctl_ctx_t concurrent_ctxs[CTL_MAX_CONCURRENT] = { 0 };
+	init_ctxs(concurrent_ctxs, CTL_MAX_CONCURRENT, server);
 	bool main_thread_exclusive = false;
 
 	/* Notify systemd about successful start. */
@@ -497,7 +498,7 @@ static void event_loop(server_t *server, const char *socket, bool daemonize,
 			server_update_zones(conf(), server, mode);
 			pthread_rwlock_unlock(&server->ctl_lock);
 		}
-		if (sig_req_stop || cleanup_ctxs(concurrent_ctxs, MAX_CTL_CONCURRENT) == KNOT_CTL_ESTOP) {
+		if (sig_req_stop || cleanup_ctxs(concurrent_ctxs, CTL_MAX_CONCURRENT) == KNOT_CTL_ESTOP) {
 			break;
 		}
 
@@ -516,8 +517,8 @@ static void event_loop(server_t *server, const char *socket, bool daemonize,
 		}
 
 		if (main_thread_exclusive ||
-		    find_free_ctx(concurrent_ctxs, MAX_CTL_CONCURRENT, ctl) == NULL) {
-			ret = ctl_process(ctl, server, &main_thread_exclusive);
+		    find_free_ctx(concurrent_ctxs, CTL_MAX_CONCURRENT, ctl) == NULL) {
+			ret = ctl_process(ctl, server, 0, &main_thread_exclusive);
 			knot_ctl_close(ctl);
 			if (ret == KNOT_CTL_ESTOP) {
 				break;
@@ -525,7 +526,7 @@ static void event_loop(server_t *server, const char *socket, bool daemonize,
 		}
 	}
 
-	finalize_ctxs(concurrent_ctxs, MAX_CTL_CONCURRENT);
+	finalize_ctxs(concurrent_ctxs, CTL_MAX_CONCURRENT);
 
 	if (conf()->cache.srv_dbus_event & DBUS_EVENT_RUNNING) {
 		dbus_emit_running(false);
