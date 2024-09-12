252c830894a8716febdf6549c43d933a9b6722ea
Libor Peltan <libor.peltan@nic.cz>
2024-09-12 12:31:32 +0200

    libknot/TLS: unify flags for TLS and QUIC

--- src/libknot/quic/tls.c
+++ src/libknot/quic/tls.c
@@ -83,7 +83,7 @@ knot_tls_conn_t *knot_tls_conn_new(knot_tls_ctx_t *ctx, int sock_fd)
 	res->fd = sock_fd;
 
 	int ret = knot_tls_session(&res->session, ctx->creds, ctx->priority,
-	                           "\x03""dot", false, ctx->server);
+	                           "\x03""dot", true, ctx->server);
 	if (ret != KNOT_EOK) {
 		goto fail;
 	}
