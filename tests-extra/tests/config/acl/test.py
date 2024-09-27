b1a2fd2f5b067373664e9b997d24e1e4fe2b08d7
Daniel Salzman <daniel.salzman@nic.cz>
2024-09-27 08:35:00 +0200

    x

--- tests-extra/tests/config/acl/test.py
+++ tests-extra/tests/config/acl/test.py
@@ -9,7 +9,7 @@ from dnstest.libknot import libknot
 from dnstest.test import Test
 from dnstest.utils import *
 
-def set_acl(server, actions, addrs=list(), deny=False, cert_key=None, proto=None):
+def set_acl(server, actions, addrs=list(), deny=False, cert_key=None, protos=list()):
 
     ACL_ID = "acl_test"
 
@@ -32,7 +32,7 @@ def set_acl(server, actions, addrs=list(), deny=False, cert_key=None, proto=None
     if deny:
         ctl.send_block(cmd="conf-set", section="acl", identifier=ACL_ID, item="deny", data="on")
         resp = ctl.receive_block()
-    if proto:
+    for proto in protos:
         ctl.send_block(cmd="conf-set", section="acl", identifier=ACL_ID, item="protocol", data=proto)
         resp = ctl.receive_block()
     if cert_key:
@@ -51,20 +51,68 @@ def set_acl(server, actions, addrs=list(), deny=False, cert_key=None, proto=None
     ctl.send(libknot.control.KnotCtlType.END)
     ctl.close()
 
-def send_upd(server, zone, proto, rcode="NOERROR"):
+def send_upd(server, zone, rcode="NOERROR", proto=Proto.TCP):
 
     up = server.update(zone)
     up.add("test", 3600, "A", "1.2.3.4")
     up.send(rcode, proto=proto)
+
+def test_normal(server, zone):
+
+    ZONE = zone.name
+
+    # Purge the ACL rule.
+    set_acl(server, [])
+
+    # Normal queries don't require authorization.
+    resp = server.dig(ZONE, "SOA")
+    resp.check(rcode="NOERROR")
+
+    # Authorized operations are denied by default.
+    resp = server.dig(ZONE, "NOTIFY")
+    resp.check(rcode="NOTAUTH")
+    resp = server.dig(ZONE, "AXFR")
+    resp.check_xfr(rcode="NOTAUTH")
+
+    # Authorize only one action.
+    set_acl(server, ["notify"])
+    resp = server.dig(ZONE, "NOTIFY")
+    resp.check(rcode="NOERROR")
+    resp = server.dig(ZONE, "AXFR")
+    resp.check_xfr(rcode="NOTAUTH")
+
+    # Authorize more actions.
+    set_acl(server, ["notify", "transfer"])
+    resp = server.dig(ZONE, "NOTIFY")
+    resp.check(rcode="NOERROR")
+    resp = server.dig(ZONE, "AXFR")
+    resp.check_xfr(rcode="NOERROR")
+    send_upd(server, zone, "NOTAUTH")
+
+    # Deny explicit action.
+    set_acl(server, ["notify"], deny=True)
+    resp = server.dig(ZONE, "NOTIFY")
+    resp.check(rcode="NOTAUTH")
+
+    # Authorize only some protocols.
+    set_acl(server, ["update"], protos=["tls", "udp"])
+    send_upd(server, zone, "NOERROR", Proto.UDP)
+    send_upd(server, zone, "NOTAUTH", Proto.TCP)
+    send_upd(server, zone, "NOERROR", Proto.TLS)
+
+    # Authorize specific address and protocol.
+    set_acl(server, ["notify"], addrs=[t.addr], protos=["tcp"])
+    resp = server.dig(ZONE, "NOTIFY", udp=False)
+    resp.check(rcode="NOERROR")
+    set_acl(server, ["notify"], addrs=[t.addr], protos=["udp"])
+    resp = server.dig(ZONE, "NOTIFY", udp=False)
+    resp.check(rcode="NOTAUTH")
 
 t = Test(quic=True, tls=True, tsig=False)
 
 master = t.server("knot")
 zones = t.zone_rnd(1, records=5, dnssec=False)
 zones[0].name = zones[0].name.lower()
-zone = zones[0]
-ZONE = zones[0].name
-
 t.link(zones, master)
 
 ctl = libknot.control.KnotCtl()
@@ -73,41 +121,6 @@ t.start()
 
 master.zones_wait(zones)
 
-set_acl(master, [])
-
-resp = master.dig(ZONE, "SOA")
-resp.check(rcode="NOERROR")
-
-resp = master.dig(ZONE, "AXFR")
-resp.check_xfr(rcode="NOTAUTH")
-
-resp = master.dig(ZONE, "NOTIFY")
-resp.check(rcode="NOTAUTH")
-
-set_acl(master, ["notify"])
-resp = master.dig(ZONE, "AXFR")
-resp.check_xfr(rcode="NOTAUTH")
-resp = master.dig(ZONE, "NOTIFY")
-resp.check(rcode="NOERROR")
-
-set_acl(master, ["notify", "transfer"])
-resp = master.dig(ZONE, "AXFR")
-resp.check_xfr(rcode="NOERROR")
-resp = master.dig(ZONE, "NOTIFY")
-resp.check(rcode="NOERROR")
-
-set_acl(master, ["notify"], deny=True)
-resp = master.dig(ZONE, "NOTIFY")
-resp.check(rcode="NOTAUTH")
-
-set_acl(master, ["notify"], deny=True, proto="tcp")
-resp = master.dig(ZONE, "NOTIFY", udp=False)
-resp.check(rcode="NOTAUTH")
-send_upd(master, zone, Proto.TLS, "NOTAUTH")
-
-set_acl(master, ["update"], proto="tls")
-send_upd(master, zone, Proto.UDP, "NOTAUTH")
-send_upd(master, zone, Proto.TCP, "NOTAUTH")
-send_upd(master, zone, Proto.TLS, "NOERROR")
+test_normal(master, zones[0])
 
 t.end()
