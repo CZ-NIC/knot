de9d1f4169e6d881eff0b57a54aa46e95b5e22fc
Jan Doskočil <jan.doskocil@nic.cz>
2024-10-18 11:36:37 +0200

    kxdpgun: stats_period overflow bugfix

--- src/utils/kxdpgun/stats.c
+++ src/utils/kxdpgun/stats.c
@@ -111,8 +111,8 @@ void json_stats_header(const xdp_gun_ctx_t *ctx)
 		jsonw_end(w);
 
 		jsonw_ulong(w, "time_units_per_sec", 1000000000);
-		if (ctx->stats_period > 0) {
-			jsonw_double(w, "stats_interval", ctx->stats_period / 1000.0);
+		if (ctx->stats_period_ns > 0) {
+			jsonw_double(w, "stats_interval", ctx->stats_period_ns / 1000000000.0);
 		}
 		// TODO: timeout
 
