e176ac839f4857ad9054e623bf4b9b35e28cf403
Libor Peltan <libor.peltan@nic.cz>
2025-01-03 13:30:13 +0100

    refactoring: move ctlsocket-related and signal-related code from knotd/main.c

---
+++ src/knot/ctl/threads.h
@@ -0,0 +1,83 @@
+/*  Copyright (C) 2025 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "knot/ctl/process.h"
+
+typedef enum {
+	CONCURRENT_EMPTY = 0,   // fresh cctx without a thread.
+	CONCURRENT_ASSIGNED,    // cctx assigned to process a command.
+	CONCURRENT_RUNNING,     // ctl command is being processed in the thread.
+	CONCURRENT_IDLE,        // command has been processed, waiting for a new one.
+	CONCURRENT_KILLED,      // cctx cleanup has started.
+	CONCURRENT_FINISHED,    // after having been killed, the thread is being joined.
+} concurrent_ctl_state_t;
+
+typedef struct {
+	concurrent_ctl_state_t state;
+	pthread_mutex_t mutex;  // Protects .state.
+	pthread_cond_t cond;
+	knot_ctl_t *ctl;
+	server_t *server;
+	pthread_t thread;
+	int ret;
+	int thread_idx;
+	bool exclusive;
+} concurrent_ctl_ctx_t;
+
+/*!
+ * \brief Initialize CTL thread processing contexts.
+ *
+ * \param concurrent_ctxs    Structures to initialize.
+ * \param n_ctxs             Their number/count.
+ * \param server             Server structure.
+ */
+void ctl_init_ctxs(concurrent_ctl_ctx_t *concurrent_ctxs, size_t n_ctxs, server_t *server);
+
+/*!
+ * \brief Regularly check the state of parallel CTL processing workers.
+ *
+ * \param concurrent_ctxs    Parallel CTL procesing contexts.
+ * \param n_ctxs             Their number/count.
+ *
+ * \retval KNOT_ESTOP   Server shutdown requested.
+ * \retval KNOT_EOK     Otherwise.
+ */
+int ctl_cleanup_ctxs(concurrent_ctl_ctx_t *concurrent_ctxs, size_t n_ctxs);
+
+/*!
+ * \brief De-initialize CTL thread processing contexts.
+ *
+ * \param concurrent_ctxs    Structures to de-initialize.
+ * \param n_ctxs             Their number/count.
+ */
+void ctl_finalize_ctxs(concurrent_ctl_ctx_t *concurrent_ctxs, size_t n_ctxs);
+
+/*!
+ * Find/create a thread processing incomming control commands.
+ *
+ * \param[in]       ctl        Control context.
+ * \param[in]       server     Server instance.
+ * \param[in/out]   excl       CTLs are being processed exclusively by calling thread.
+ * \param[in]       thrid      Calling thread id.
+ * \param[in]       ctxs       CTL threads contexts.
+ * \param[in]       n_ctxs     Number of CTL threads contexts.
+ *
+ * \return Error code, KNOT_EOK if successful, KNOT_CTL_ESTOP if server shutdown desired.
+ */
+int ctl_manage(knot_ctl_t *ctl, server_t *server, bool *excl,
+               int thrid, concurrent_ctl_ctx_t *ctxs, size_t n_ctxs);
---
+++ src/knot/server/signals.c
@@ -0,0 +1,98 @@
+/*  Copyright (C) 2025 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "knot/server/signals.h"
+
+#include <signal.h>
+#include <stdlib.h>
+
+volatile bool signals_req_stop = false;
+volatile bool signals_req_reload = false;
+volatile bool signals_req_zones_reload = false;
+
+struct signal {
+	int signum;
+	bool handle;
+};
+
+/*! \brief Signals used by the server. */
+static const struct signal SIGNALS[] = {
+    { SIGHUP,  true  },  /* Reload server. */
+    { SIGUSR1, true  },  /* Reload zones. */
+    { SIGINT,  true  },  /* Terminate server. */
+    { SIGTERM, true  },  /* Terminate server. */
+    { SIGALRM, false },  /* Internal thread synchronization. */
+    { SIGPIPE, false },  /* Ignored. Some I/O errors. */
+    { 0 }
+};
+
+static void handle_signal(int signum)
+{
+	switch (signum) {
+	case SIGHUP:
+		signals_req_reload = true;
+		break;
+	case SIGUSR1:
+		signals_req_zones_reload = true;
+		break;
+	case SIGINT:
+	case SIGTERM:
+		if (signals_req_stop) {
+			exit(EXIT_FAILURE);
+		}
+		signals_req_stop = true;
+		break;
+	default:
+		/* ignore */
+		break;
+	}
+}
+
+void signals_setup(void)
+{
+	/* Block all signals. */
+	static sigset_t all;
+	sigfillset(&all);
+	sigdelset(&all, SIGPROF);
+	sigdelset(&all, SIGQUIT);
+	sigdelset(&all, SIGILL);
+	sigdelset(&all, SIGABRT);
+	sigdelset(&all, SIGBUS);
+	sigdelset(&all, SIGFPE);
+	sigdelset(&all, SIGSEGV);
+
+	/* Setup handlers. */
+	struct sigaction action = { .sa_handler = handle_signal };
+	for (const struct signal *s = SIGNALS; s->signum > 0; s++) {
+		sigaction(s->signum, &action, NULL);
+	}
+
+	pthread_sigmask(SIG_SETMASK, &all, NULL);
+}
+
+void signals_enable(void)
+{
+	sigset_t mask;
+	sigemptyset(&mask);
+
+	for (const struct signal *s = SIGNALS; s->signum > 0; s++) {
+		if (s->handle) {
+			sigaddset(&mask, s->signum);
+		}
+	}
+
+	pthread_sigmask(SIG_UNBLOCK, &mask, NULL);
+}
