aa129ef2d354566ece3ad5a5a21a6adfb22140e9
Daniel Salzman <daniel.salzman@nic.cz>
2025-01-28 14:34:34 +0100

    knot: prepare for integration of the Redis backend

--- src/knot/server/server.c
+++ src/knot/server/server.c
@@ -9,6 +9,7 @@
 
 #include <assert.h>
 #include <gnutls/x509.h>
+#include <string.h>
 #include <sys/types.h>   // OpenBSD
 #include <netinet/tcp.h> // TCP_FASTOPEN
 #include <sys/resource.h>
@@ -834,16 +835,148 @@ static int configure_sockets(conf_t *conf, server_t *s)
 	return KNOT_EOK;
 }
 
+#ifdef ENABLE_REDIS
+#include "contrib/openbsd/strlcpy.h"
+#include "contrib/strtonum.h"
+#include "redis/knot.h"
+#include "knot/common/hiredis.h"
+
+#define RDB_TIMESTAMP_SIZE 42 // 2x uint64_t as string (2x20) + dash separator + zero byte.
+
+static void rdb_process_event(redisReply *reply, knot_zonedb_t *zone_db,
+                              char since[static RDB_TIMESTAMP_SIZE])
+{
+	redisReply *timestamp = reply->element[0];
+	redisReply *data = reply->element[1];
+	if (data->type != REDIS_REPLY_ARRAY ||
+	    data->elements % 2 != 0 ||
+	    timestamp->len > RDB_TIMESTAMP_SIZE) {
+		goto failed;
+	}
+	strlcpy(since, timestamp->str, RDB_TIMESTAMP_SIZE);
+
+	uint8_t type = 0;
+	uint8_t instance = 0;
+	uint32_t serial = 0;
+	const knot_dname_t *origin = NULL;
+
+	for (int idx = 0; idx < data->elements; ++idx) {
+		redisReply *key = data->element[idx++];
+		redisReply *val = data->element[idx];
+		if (key->type != REDIS_REPLY_STRING) {
+			goto failed;
+		}
+		if (strcmp(key->str, RDB_EVENT_ARG_EVENT) == 0) {
+			if (str_to_u8(val->str, &type) != KNOT_EOK) {
+				goto failed;
+			}
+		} else if (strcmp(key->str, RDB_EVENT_ARG_ORIGIN) == 0) {
+			origin = (const knot_dname_t *)val->str;
+		} else if (strcmp(key->str, RDB_EVENT_ARG_INSTANCE) == 0) {
+			if (str_to_u8(val->str, &instance) != KNOT_EOK) {
+				goto failed;
+			}
+		} else if (strcmp(key->str, RDB_EVENT_ARG_SERIAL) == 0) {
+			if (str_to_u32(val->str, &serial) != KNOT_EOK) {
+				goto failed;
+			}
+		}
+	}
+
+	if (type == 0 || origin == NULL || instance == 0) {
+		goto failed;
+	}
+
+	conf_val_t val = conf_zone_get(conf(), C_ZONE_DB_IN, origin);
+	if (conf_int(&val) != instance) {
+		return;
+	}
+	zone_t *zone = knot_zonedb_find(zone_db, origin);
+	if (zone == NULL) {
+		return;
+	}
+
+	switch (type) {
+	case RDB_EVENT_ZONE:
+	case RDB_EVENT_UPD:
+		zone_events_schedule_now(zone, ZONE_EVENT_LOAD);
+		break;
+	default:
+		break;
+	}
+
+	return;
+failed:
+	log_error("rdb, invalid event parameters");
+}
+
+static void rdb_process_events(redisReply *reply, knot_zonedb_t *zone_db,
+                               char since[static RDB_TIMESTAMP_SIZE])
+{
+	if (reply->type != REDIS_REPLY_ARRAY) {
+		log_error("rdb, unexpected response");
+		return;
+	}
+
+	for (int idx = 0; idx < reply->elements; ++idx) {
+		if (reply->element[idx]->type != REDIS_REPLY_ARRAY) {
+			log_error("rdb, unexpected response");
+			continue;
+		}
+		redisReply *events = reply->element[idx]->element[1];
+		for (int event_idx = 0; event_idx < events->elements; ++event_idx) {
+			rdb_process_event(events->element[event_idx], zone_db, since);
+		}
+	}
+}
+
+static int rdb_listener_run(struct dthread *thread)
+{
+	server_t *s = thread->data;
+
+	redisContext *ctx = NULL;
+	char since[RDB_TIMESTAMP_SIZE] = "$";
+
+	while (thread->state == ThreadActive) {
+		if (ctx == NULL && (ctx = rdb_connect(conf())) == NULL) {
+			log_error("rdb, failed to connect");
+			sleep(2);
+			continue;
+		}
+
+		redisReply *reply = redisCommand(ctx, "XREAD BLOCK %d STREAMS %b %s",
+		                                 2000, RDB_EVENT_KEY, strlen(RDB_EVENT_KEY), since);
+		if (reply == NULL) {
+			sleep(2);
+			log_error("rdb, failed to read events");
+			continue;
+		}
+		if (reply->type == REDIS_REPLY_NIL) {
+			freeReplyObject(reply);
+			continue;
+		}
+
+		knot_zonedb_t *zone_db = s->zone_db;
+		if (zone_db != NULL) {
+			rdb_process_events(reply, zone_db, since);
+		}
+		freeReplyObject(reply);
+	}
+
+	rdb_disconnect(ctx);
+
+	return KNOT_EOK;
+}
+#endif
+
 int server_init(server_t *server, int bg_workers)
 {
 	if (server == NULL) {
 		return KNOT_EINVAL;
 	}
 
-	/* Clear the structure. */
 	memset(server, 0, sizeof(server_t));
 
-	/* Initialize event scheduler. */
 	if (evsched_init(&server->sched, server) != KNOT_EOK) {
 		return KNOT_ENOMEM;
 	}
@@ -854,8 +987,22 @@ int server_init(server_t *server, int bg_workers)
 		return KNOT_ENOMEM;
 	}
 
+#ifdef ENABLE_REDIS
+	conf_val_t rdb = conf_db_param(conf(), C_ZONE_DB_LISTEN);
+	if (rdb.code == KNOT_EOK) {
+		server->rdb_events = dt_create(1, rdb_listener_run, NULL, server);
+		if (server->rdb_events == NULL) {
+			worker_pool_destroy(server->workers);
+			evsched_deinit(&server->sched);
+			return KNOT_ENOMEM;
+		}
+	}
+#endif
+
 	int ret = catalog_update_init(&server->catalog_upd);
 	if (ret != KNOT_EOK) {
+		dt_stop(server->rdb_events);
+		dt_delete(&server->rdb_events);
 		worker_pool_destroy(server->workers);
 		evsched_deinit(&server->sched);
 		return ret;
@@ -911,6 +1058,10 @@ void server_deinit(server_t *server)
 
 	/* Free threads and event handlers. */
 	worker_pool_destroy(server->workers);
+
+	/* Free optional zone DB event thread. */
+	dt_stop(server->rdb_events);
+	dt_delete(&server->rdb_events);
 
 	/* Free zone database. */
 	knot_zonedb_deep_free(&server->zone_db, true);
@@ -1025,6 +1176,9 @@ int server_start(server_t *server, bool answering)
 
 	/* Start workers. */
 	worker_pool_start(server->workers);
+
+	/* Start zone DB event processing. */
+	dt_start(server->rdb_events);
 
 	/* Start evsched handler. */
 	evsched_start(&server->sched);
