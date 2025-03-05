cb8beb5997574d114fb382db8d25c1e7d134f19e
Daniel Salzman <daniel.salzman@nic.cz>
2025-03-05 09:14:52 +0100

    fixup 2

--- src/knot/conf/confio.c
+++ src/knot/conf/confio.c
@@ -15,7 +15,6 @@
  */
 
 #include <assert.h>
-#include <unistd.h>
 
 #include "knot/common/log.h"
 #include "knot/conf/confdb.h"
@@ -59,9 +58,12 @@ static void io_reset_bin(
 
 static bool check_same_thread(void)
 {
-	return conf()->io.txn == NULL || conf()->io.thread_id == gettid();
+	return conf()->io.txn == NULL ||
+	       pthread_equal(conf()->io.thread_id, pthread_self()) != 0;
 }
-#define CHECK_SAME_THREAD() do { if (!check_same_thread()) { return KNOT_TXN_EEXISTS; } } while (0) // FIXME special error code
+#define CHECK_SAME_THREAD do { \
+	if (!check_same_thread()) { return KNOT_TXN_ETHREAD; } \
+} while (0);
 
 int conf_io_begin(
 	bool child)
@@ -73,7 +75,7 @@ int conf_io_begin(
 	} else if (conf()->io.txn == NULL && child) {
 		return KNOT_TXN_ENOTEXISTS;
 	}
-	CHECK_SAME_THREAD();
+	CHECK_SAME_THREAD;
 
 	knot_db_txn_t *parent = conf()->io.txn;
 	knot_db_txn_t *txn = (parent == NULL) ? conf()->io.txn_stack : parent + 1;
@@ -93,7 +95,7 @@ int conf_io_begin(
 	}
 
 	conf()->io.txn = txn;
-	conf()->io.thread_id = gettid();
+	conf()->io.thread_id = pthread_self();
 
 	// Reset master transaction flags.
 	if (!child) {
@@ -115,7 +117,7 @@ int conf_io_commit(
 	    (child && conf()->io.txn == conf()->io.txn_stack)) {
 		return KNOT_TXN_ENOTEXISTS;
 	}
-	CHECK_SAME_THREAD();
+	CHECK_SAME_THREAD;
 
 	knot_db_txn_t *txn = child ? conf()->io.txn : conf()->io.txn_stack;
 
@@ -185,7 +187,7 @@ int conf_io_list(
 	if (conf()->io.txn == NULL && !get_current) {
 		return KNOT_TXN_ENOTEXISTS;
 	}
-	CHECK_SAME_THREAD();
+	CHECK_SAME_THREAD;
 
 	// List schema sections by default.
 	if (key0 == NULL) {
@@ -587,7 +589,7 @@ int conf_io_diff(
 	if (conf()->io.txn == NULL) {
 		return KNOT_TXN_ENOTEXISTS;
 	}
-	CHECK_SAME_THREAD();
+	CHECK_SAME_THREAD;
 
 	// Compare all sections by default.
 	if (key0 == NULL) {
@@ -788,7 +790,7 @@ int conf_io_get(
 	if (conf()->io.txn == NULL && !get_current) {
 		return KNOT_TXN_ENOTEXISTS;
 	}
-	CHECK_SAME_THREAD();
+	CHECK_SAME_THREAD;
 
 	// List all sections by default.
 	if (key0 == NULL) {
@@ -1031,7 +1033,7 @@ int conf_io_set(
 	if (conf()->io.txn == NULL) {
 		return KNOT_TXN_ENOTEXISTS;
 	}
-	CHECK_SAME_THREAD();
+	CHECK_SAME_THREAD;
 
 	// At least key0 must be specified.
 	if (key0 == NULL) {
@@ -1227,7 +1229,7 @@ int conf_io_unset(
 	if (conf()->io.txn == NULL) {
 		return KNOT_TXN_ENOTEXISTS;
 	}
-	CHECK_SAME_THREAD();
+	CHECK_SAME_THREAD;
 
 	// Unset all sections by default.
 	if (key0 == NULL) {
@@ -1581,7 +1583,7 @@ int conf_io_check(
 	if (conf()->io.txn == NULL) {
 		return KNOT_TXN_ENOTEXISTS;
 	}
-	CHECK_SAME_THREAD();
+	CHECK_SAME_THREAD;
 
 	int ret;
 
--- src/libknot/error.c
+++ src/libknot/error.c
@@ -1,4 +1,4 @@
-/*  Copyright (C) 2024 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+/*  Copyright (C) 2025 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -166,6 +166,7 @@ static const struct error errors[] = {
 	/* Transaction errors. */
 	{ KNOT_TXN_EEXISTS,    "too many transactions" },
 	{ KNOT_TXN_ENOTEXISTS, "no active transaction" },
+	{ KNOT_TXN_ETHREAD,    "transaction thread mismatch" },
 
 	/* DNSSEC errors. */
 	{ KNOT_INVALID_PUBLIC_KEY,    "invalid public key" },
