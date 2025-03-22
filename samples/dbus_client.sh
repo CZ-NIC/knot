dd27e00b6e9d3477a7e1602db863d31d4265d420
Daniel Salzman <daniel.salzman@nic.cz>
2025-03-22 11:46:50 +0100

    samples: relicense to GPL-2.0-or-later

--- samples/dbus_client.sh
+++ samples/dbus_client.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Copyright (C) CZ.NIC, z.s.p.o. and contributors
+# SPDX-License-Identifier: GPL-2.0-or-later
+# For more information, see <https://www.knot-dns.cz/>
 
 # This package is needed on Debian derived ditributions: libglib2.0-bin
 
