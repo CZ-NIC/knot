c9cafb76b40e316d0a835f6c4387ed0ea3b81437
Daniel Salzman <daniel.salzman@nic.cz>
2025-05-30 09:17:58 +0200

    fixup! knotd: self-signed cert with SAN instead of Subject: CN

--- src/libknot/quic/tls_common.c
+++ src/libknot/quic/tls_common.c
@@ -7,6 +7,7 @@
 #include <gnutls/crypto.h>
 #include <gnutls/gnutls.h>
 #include <gnutls/x509.h>
+#include <gnutls/x509-ext.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -122,9 +123,14 @@ static int self_signed_cert(gnutls_certificate_credentials_t tls_cert,
 {
 	gnutls_x509_privkey_t privkey = NULL;
 	gnutls_x509_crt_t cert = NULL;
+	gnutls_subject_alt_names_t san = NULL;
+	gnutls_datum_t san_der = { 0 };
 
-	char *hostname = sockaddr_hostname();
-	if (hostname == NULL) {
+	gnutls_datum_t hostname = {
+		.data = (unsigned char *)sockaddr_hostname(),
+		.size = strlen((char *)hostname.data)
+	};
+	if (hostname.data == NULL) {
 		return GNUTLS_E_MEMORY_ERROR;
 	}
 
@@ -136,7 +142,6 @@ static int self_signed_cert(gnutls_certificate_credentials_t tls_cert,
 
 #define CHK(cmd) if ((ret = (cmd)) != GNUTLS_E_SUCCESS) { goto finish; }
 #define NOW_DAYS(days) (time(NULL) + 24 * 3600 * (days))
-#define OID_SUBJECT_ALT_NAME "2.5.29.17"
 
 	CHK(self_key(&privkey, key_file, uid, gid));
 
@@ -145,20 +150,14 @@ static int self_signed_cert(gnutls_certificate_credentials_t tls_cert,
 	CHK(gnutls_x509_crt_set_serial(cert, serial, sizeof(serial)));
 	CHK(gnutls_x509_crt_set_activation_time(cert, NOW_DAYS(-1)));
 	CHK(gnutls_x509_crt_set_expiration_time(cert, NOW_DAYS(10 * 365)));
-	CHK(gnutls_x509_crt_set_issuer_dn_by_oid(cert, GNUTLS_OID_X520_COMMON_NAME, 0, hostname,
-						 strlen(hostname)));
+	CHK(gnutls_x509_crt_set_issuer_dn_by_oid(cert, GNUTLS_OID_X520_COMMON_NAME,
+	                                         0, hostname.data, hostname.size));
 
-	char san_data[255];
-	size_t san_size = sizeof(san_data);
-	CHK(gnutls_x509_crt_set_subject_alt_name(cert, GNUTLS_SAN_DNSNAME, hostname,
-						 strlen(hostname), GNUTLS_FSAN_SET));
-	CHK(gnutls_x509_crt_get_extension_by_oid(cert, OID_SUBJECT_ALT_NAME, 0, san_data, &san_size, NULL));
-	if (san_size > sizeof(san_data)) {
-		ret = KNOT_EINVAL;
-		goto finish;
-	}
-	// "critical" flag is required; see rfc5280 section 4.1.2.6
-	CHK(gnutls_x509_crt_set_extension_by_oid(cert, OID_SUBJECT_ALT_NAME, san_data, san_size, 1));
+	CHK(gnutls_subject_alt_names_init(&san));
+	CHK(gnutls_subject_alt_names_set(san, GNUTLS_SAN_DNSNAME, &hostname, 0));
+	CHK(gnutls_x509_ext_export_subject_alt_names(san, &san_der));
+	CHK(gnutls_x509_crt_set_extension_by_oid(cert, GNUTLS_X509EXT_OID_SAN,
+	                                         san_der.data, san_der.size, 1));
 
 	CHK(gnutls_x509_crt_set_key(cert, privkey));
 	CHK(gnutls_x509_crt_sign2(cert, cert, privkey, GNUTLS_DIG_SHA512, 0));
@@ -166,9 +165,11 @@ static int self_signed_cert(gnutls_certificate_credentials_t tls_cert,
 	ret = gnutls_certificate_set_x509_key(tls_cert, &cert, 1, privkey);
 
 finish:
-	free(hostname);
+	free(hostname.data);
+	gnutls_free(san_der.data);
 	gnutls_x509_crt_deinit(cert);
 	gnutls_x509_privkey_deinit(privkey);
+	gnutls_subject_alt_names_deinit(san);
 
 	return ret;
 }
