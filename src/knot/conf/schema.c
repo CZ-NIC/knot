offsoc/knot-dns

remote: implemented tls-ca and cert-validate options

src/knot/conf/schema.c
@@ -236,6 +236,7 @@ static const yp_item_t desc_server[] = {
 	                                                1232, YP_SSIZE } },
 	{ C_CERT_FILE,            YP_TSTR,  YP_VNONE, YP_FNONE },
 	{ C_KEY_FILE,             YP_TSTR,  YP_VNONE, YP_FNONE },
+	{ C_TLS_CA,               YP_TSTR,  YP_VNONE, YP_FMULTI },
 	{ C_ECS,                  YP_TBOOL, YP_VNONE },
 	{ C_ANS_ROTATION,         YP_TBOOL, YP_VNONE },
 	{ C_AUTO_ACL,             YP_TBOOL, YP_VNONE },
@@ -344,6 +345,7 @@ static const yp_item_t desc_remote[] = {
 	{ C_BLOCK_NOTIFY_XFR, YP_TBOOL, YP_VNONE },
 	{ C_NO_EDNS,          YP_TBOOL, YP_VNONE },
 	{ C_AUTO_ACL,         YP_TBOOL, YP_VBOOL = { true } },
+	{ C_CERT_VALIDATE,    YP_TBOOL, YP_VBOOL = { false }, .misc = { check_cert_validate } },
 	{ C_COMMENT,          YP_TSTR,  YP_VNONE },
 	{ NULL }
 };

src/knot/server/server.c
@@ -551,7 +551,7 @@ static void log_sock_conf(conf_t *conf)
 	}
 }
 
-static int check_file(char *path, char *role)
+static int check_file(const char *path, char *role)
 {
 	if (path == NULL) {
 		return KNOT_EOK;
@@ -579,6 +579,8 @@ static int init_creds(conf_t *conf, server_t *server)
 {
 	char *cert_file = conf_tls(conf, C_CERT_FILE);
 	char *key_file = conf_tls(conf, C_KEY_FILE);
+	list_t ca_files;
+	init_list(&ca_files);
 
 	int ret = check_file(cert_file, "certificate");
 	if (ret != KNOT_EOK) {
@@ -615,16 +617,43 @@ static int init_creds(conf_t *conf, server_t *server)
 		goto failed;
 	}
 
+	bool system_ca = false;
+	conf_val_t val = conf_get(conf, C_SERVER, C_TLS_CA);
+	while (val.code == KNOT_EOK) {
+		const char *cert_auth = conf_str(&val);
+		assert(cert_auth != NULL);
+		if (*cert_auth == '\0') {
+			system_ca = true;
+		} else if ((ret = check_file(cert_auth, "certificate")) != KNOT_EOK) {
+			goto failed;
+		} else {
+			if (ptrlist_add(&ca_files, (char *)cert_auth, NULL) == NULL) {
+				ret = KNOT_ENOMEM;
+				goto failed;
+			}
+		}
+		conf_val_next(&val);
+	}
+
 	if (server->quic_creds == NULL) {
-		server->quic_creds = knot_creds_init(key_file, cert_file, uid, gid);
+		int err;
+		server->quic_creds =
+			knot_creds_init(key_file, cert_file, &ca_files, system_ca, uid, gid, &err);
 		if (server->quic_creds == NULL) {
+			if (err == KNOT_EBADCERT) {
+				log_error(QUIC_LOG "failed to import one or more certificates");
+			}
 			log_error(QUIC_LOG "failed to initialize server credentials");
 			ret = KNOT_ERROR;
 			goto failed;
 		}
 	} else {
-		ret = knot_creds_update(server->quic_creds, key_file, cert_file, uid, gid);
+		ret = knot_creds_update(server->quic_creds, key_file, cert_file, &ca_files,
+					system_ca, uid, gid);
 		if (ret != KNOT_EOK) {
+			if (ret == KNOT_EBADCERT) {
+				log_error(QUIC_LOG "failed to import one or more certificates");
+			}
 			goto failed;
 		}
 	}
@@ -637,6 +666,7 @@ static int init_creds(conf_t *conf, server_t *server)
 
 	ret = KNOT_EOK;
 failed:
+	ptrlist_free(&ca_files, NULL);
 	free(key_file);
 	free(cert_file);
 

CZ-NIC/knot

src/knot/conf/schema.c
@@ -236,6 +236,7 @@ static const yp_item_t desc_server[] = {
 	                                                1232, YP_SSIZE } },
 	{ C_CERT_FILE,            YP_TSTR,  YP_VNONE, YP_FNONE },
 	{ C_KEY_FILE,             YP_TSTR,  YP_VNONE, YP_FNONE },
+	{ C_CA_FILE,              YP_TSTR,  YP_VNONE, YP_FMULTI },
 	{ C_ECS,                  YP_TBOOL, YP_VNONE },
 	{ C_ANS_ROTATION,         YP_TBOOL, YP_VNONE },
 	{ C_AUTO_ACL,             YP_TBOOL, YP_VNONE },
@@ -344,6 +345,7 @@ static const yp_item_t desc_remote[] = {
 	{ C_BLOCK_NOTIFY_XFR, YP_TBOOL, YP_VNONE },
 	{ C_NO_EDNS,          YP_TBOOL, YP_VNONE },
 	{ C_AUTO_ACL,         YP_TBOOL, YP_VBOOL = { true } },
+	{ C_CERT_VALIDATE,    YP_TBOOL, YP_VBOOL = { false }, .misc = { check_cert_validate } },
 	{ C_COMMENT,          YP_TSTR,  YP_VNONE },
 	{ NULL }
 };

src/knot/server/server.c
@@ -551,7 +551,7 @@ static void log_sock_conf(conf_t *conf)
 	}
 }
 
-static int check_file(char *path, char *role)
+static int check_file(const char *path, char *role)
 {
 	if (path == NULL) {
 		return KNOT_EOK;
@@ -579,6 +579,8 @@ static int init_creds(conf_t *conf, server_t *server)
 {
 	char *cert_file = conf_tls(conf, C_CERT_FILE);
 	char *key_file = conf_tls(conf, C_KEY_FILE);
+	list_t ca_files;
+	init_list(&ca_files);
 
 	int ret = check_file(cert_file, "certificate");
 	if (ret != KNOT_EOK) {
@@ -615,16 +617,42 @@ static int init_creds(conf_t *conf, server_t *server)
 		goto failed;
 	}
 
+	bool system_ca = false;
+	conf_val_t val = conf_get(conf, C_SERVER, C_CA_FILE);
+	while (val.code == KNOT_EOK) {
+		const char *ca_file = conf_str(&val);
+		if (*ca_file == '\0') {
+			system_ca = true;
+		} else if ((ret = check_file(ca_file, "ca")) != KNOT_EOK) {
+			goto failed;
+		} else {
+			if (ptrlist_add(&ca_files, (char *)ca_file, NULL) == NULL) {
+				ret = KNOT_ENOMEM;
+				goto failed;
+			}
+		}
+		conf_val_next(&val);
+	}
+
 	if (server->quic_creds == NULL) {
-		server->quic_creds = knot_creds_init(key_file, cert_file, uid, gid);
+		int err;
+		server->quic_creds =
+			knot_creds_init(key_file, cert_file, &ca_files, system_ca, uid, gid, &err);
 		if (server->quic_creds == NULL) {
+			if (err == KNOT_EBADCERT) {
+				log_error(QUIC_LOG "failed to import one or more certificates");
+			}
 			log_error(QUIC_LOG "failed to initialize server credentials");
 			ret = KNOT_ERROR;
 			goto failed;
 		}
 	} else {
-		ret = knot_creds_update(server->quic_creds, key_file, cert_file, uid, gid);
+		ret = knot_creds_update(server->quic_creds, key_file, cert_file, &ca_files,
+					system_ca, uid, gid);
 		if (ret != KNOT_EOK) {
+			if (ret == KNOT_EBADCERT) {
+				log_error(QUIC_LOG "failed to import one or more certificates");
+			}
 			goto failed;
 		}
 	}
@@ -637,6 +665,7 @@ static int init_creds(conf_t *conf, server_t *server)
 
 	ret = KNOT_EOK;
 failed:
+	ptrlist_free(&ca_files, NULL);
 	free(key_file);
 	free(cert_file);
 