9a50dbfc872f624cc4389a1a016b7fd8ee7d41bf
Daniel Salzman <daniel.salzman@nic.cz>
2025-07-21 08:43:36 +0200

    keymgr: support colon separators in keyid

--- src/libdnssec/keyid.c
+++ src/libdnssec/keyid.c
@@ -33,11 +33,16 @@ bool dnssec_keyid_is_valid(const char *id)
 		return false;
 	}
 
-	if (strlen(id) % 2 != 0) {
+	size_t id_len = strlen(id);
+	bool with_colons = (id_len > 3 && id[2] == ':'); // p11tool format XX:XX:XX:<..>:XX
+	if ((id_len - (with_colons ? id_len / 3 : 0)) % 2 != 0) {
 		return false;
 	}
 
 	for (int i = 0; id[i] != '\0'; i++) {
+		if (with_colons && i > 0 && i < id_len - 1 && i % 3 == 2 && id[i] == ':') {
+			continue;
+		}
 		if (!is_xdigit(id[i])) {
 			return false;
 		}
@@ -53,10 +58,16 @@ void dnssec_keyid_normalize(char *id)
 		return;
 	}
 
+	size_t j = 0;
 	for (size_t i = 0; id[i] != '\0'; i++) {
+		if (id[i] == ':') {
+			assert(i % 3 == 2);
+			continue;
+		}
 		assert(id[i] != '\0' && is_xdigit(id[i]));
-		id[i] = knot_tolower(id[i]);
+		id[j++] = knot_tolower(id[i]);
 	}
+	id[j] = '\0';
 }
 
 _public_
