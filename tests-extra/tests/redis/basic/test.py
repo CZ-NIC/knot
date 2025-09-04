d3b9e4784a44c54790a99c59b7a1e1f7bf165384
Libor Peltan <libor.peltan@nic.cz>
2025-09-04 20:36:11 +0200

    tests/redis: SOA serial loop scenario

--- tests-extra/tests/redis/basic/test.py
+++ tests-extra/tests/redis/basic/test.py
@@ -98,4 +98,31 @@ for z in zones:
     resp.check(rcode="NOERROR", nordata="1.2.3.4", rdata="1.2.3.5", ttl=900)
 t.xfr_diff(master, slave, zones)
 
+# SOA serial logic rotation
+serials6 = serials5
+
+for i in range(5):
+    if i == 3:
+        slave.ctl("zone-freeze", wait=True)
+
+    for z in zones:
+        if i == 4:
+            serials6[z.name] += 1
+        else:
+            serials6[z.name] += (1 << 30)
+        serials6[z.name] %= (1 << 32)
+        up = master.update(z)
+        up.add(z.name, 3600, "SOA", "dns1 hostmaster %d 10800 3600 1209600 7200" % (serials6[z.name]))
+        up.add("loop", 3600, "AAAA", "1::%d" % i)
+        up.send()
+    master.zones_wait(zones, serials6, equal=True)
+
+slave.ctl("zone-thaw")
+slave.zones_wait(zones, serials6, equal=True)
+t.xfr_diff(master, slave, zones)
+resp = slave.dig("loop." + zones[0].name, "AAAA")
+resp.check(rcode="NOERROR", rdata="1::1")
+resp.check(rcode="NOERROR", rdata="1::4")
+resp.check_count(5, "AAAA")
+
 t.end()
