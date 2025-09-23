1297a6cc0fef21f35cfa517b5b55d94cd9cea41d
Jan Hák <jan.hak@nic.cz>
2025-09-23 12:52:25 +0200

    fix: failing build of knot/common/stats.c on PowerPC and MIPS

--- src/knot/common/stats.c
+++ src/knot/common/stats.c
@@ -127,8 +127,8 @@ int stats_server(stats_dump_ctr_f fcn, stats_dump_ctx_t *ctx)
 	}
 
 	DUMP_VAL(params, "zone-count", knot_zonedb_size(ctx->server->zone_db));
-	DUMP_VAL(params, "tcp-io-timeout", ctx->server->stats.tcp_io_timeout);
-	DUMP_VAL(params, "tcp-idle-timeout", ctx->server->stats.tcp_idle_timeout);
+	DUMP_VAL(params, "tcp-io-timeout", ATOMIC_GET(ctx->server->stats.tcp_io_timeout));
+	DUMP_VAL(params, "tcp-idle-timeout", ATOMIC_GET(ctx->server->stats.tcp_idle_timeout));
 
 	return KNOT_EOK;
 }
