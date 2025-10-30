9706c26be8102feda6ac4de92d9512676d5ad366
Jan Hák <jan.hak@nic.cz>
2025-10-30 10:14:35 +0100

    lll

--- tests-extra/tests/redis/ha/test.py
+++ tests-extra/tests/redis/ha/test.py
@@ -86,13 +86,7 @@ for z in zones:
 freeze_future.wait()
 time.sleep(5)
 
-# Test no change
-# NOTE for some reason 'database is up-to-date' is somethimes in logfile twice
-# slave.ctl("zone-reload", wait=True)
-# uptodate_log = slave.log_search_count("database is up-to-date")
-# if uptodate_log != len(zones):
-#     set_err("UP-TO-DATE LOGGED %dx" % uptodate_log)
-
+slave.ctl("zone-reload", wait=True)
 
 # Add to DB manually. Slave will diverge from master.
 for z in zones:
@@ -101,62 +95,64 @@ for z in zones:
     r = redis_slave.cli("knot.upd.add", z.name, txn, "example.com. 3600 in soa dns1.example.com. hostmaster.example.com. %d 10800 3600 1209600 7200" % (serials3[z.name] + 1))
     r = redis_slave.cli("knot.upd.add", z.name, txn, "txtadd 3600 A 1.2.3.4")
     r = redis_slave.cli("knot.upd.commit", z.name, txn)
+    compare(r, 'OK', "Unable to commit update")
 
     r = redis_slave.cli("knot.upd.load", z.name, master.zones[z.name].redis_out, str(serials3[z.name]))
     if not "txtadd" in r:
         set_err("NO TXTADD IN UPD")
 
-freeze_future = redis_slave.freeze(15)
-time.sleep(5)
+# freeze_future = redis_slave.freeze(15)
+# time.sleep(5)
 
 serials4 = slave.zones_wait(zones, serials3)
 for z in zones:
     resp = slave.dig("txtadd." + z.name, "A")
     resp.check(rcode="NOERROR", rdata="1.2.3.4")
 
-freeze_future.wait()
+# freeze_future.wait()
+# time.sleep(5)
 
-# Update master with double SOA increment, it shall overwrite with greater serial and different contents.
-for z in zones:
-    up = master.update(z)
-    up.add(z.name, 3600, "SOA", "dns1.example.com. hostmaster.example.com. %d 10800 3600 1209600 7200" % (serials3[z.name] + 2))
-    up.delete("suppnot1", "A", "1.2.3.5")
-    up.add("suppnot1", 900, "A", "1.2.3.5")
-    up.send()
+# # Update master with double SOA increment, it shall overwrite with greater serial and different contents.
+# for z in zones:
+#     up = master.update(z)
+#     up.add(z.name, 3600, "SOA", "dns1.example.com. hostmaster.example.com. %d 10800 3600 1209600 7200" % (serials3[z.name] + 2))
+#     up.delete("suppnot1", "A", "1.2.3.5")
+#     up.add("suppnot1", 900, "A", "1.2.3.5")
+#     up.send()
 
-serials5 = slave.zones_wait(zones, serials4)
-for z in zones:
-    resp = slave.dig("txtadd." + z.name, "A")
-    resp.check(rcode="NXDOMAIN", nordata="1.2.3.4")
-    resp = slave.dig("suppnot1." + z.name, "A")
-    resp.check(rcode="NOERROR", nordata="1.2.3.4", rdata="1.2.3.5", ttl=900)
-t.xfr_diff(master, slave, zones)
+# serials5 = slave.zones_wait(zones, serials4)
+# for z in zones:
+#     resp = slave.dig("txtadd." + z.name, "A")
+#     resp.check(rcode="NXDOMAIN", nordata="1.2.3.4")
+#     resp = slave.dig("suppnot1." + z.name, "A")
+#     resp.check(rcode="NOERROR", nordata="1.2.3.4", rdata="1.2.3.5", ttl=900)
+# t.xfr_diff(master, slave, zones)
 
 # SOA serial logic rotation
-serials6 = serials5
+# serials6 = serials5
 
-for i in range(5):
-    if i == 3:
-        slave.ctl("zone-freeze", wait=True)
+# for i in range(5):
+#     if i == 3:
+#         slave.ctl("zone-freeze", wait=True)
 
-    for z in zones:
-        if i == 4:
-            serials6[z.name] += 1
-        else:
-            serials6[z.name] += (1 << 30)
-        serials6[z.name] %= (1 << 32)
-        up = master.update(z)
-        up.add(z.name, 3600, "SOA", "dns1 hostmaster %d 10800 3600 1209600 7200" % (serials6[z.name]))
-        up.add("loop", 3600, "AAAA", "1::%d" % i)
-        up.send()
-    master.zones_wait(zones, serials6, equal=True)
+#     for z in zones:
+#         if i == 4:
+#             serials6[z.name] += 1
+#         else:
+#             serials6[z.name] += (1 << 30)
+#         serials6[z.name] %= (1 << 32)
+#         up = master.update(z)
+#         up.add(z.name, 3600, "SOA", "dns1 hostmaster %d 10800 3600 1209600 7200" % (serials6[z.name]))
+#         up.add("loop", 3600, "AAAA", "1::%d" % i)
+#         up.send()
+#     master.zones_wait(zones, serials6, equal=True)
 
-slave.ctl("zone-thaw")
-slave.zones_wait(zones, serials6, equal=True)
-t.xfr_diff(master, slave, zones)
-resp = slave.dig("loop." + zones[0].name, "AAAA")
-resp.check(rcode="NOERROR", rdata="1::1")
-resp.check(rcode="NOERROR", rdata="1::4")
-resp.check_count(5, "AAAA")
+# slave.ctl("zone-thaw")
+# slave.zones_wait(zones, serials6, equal=True)
+# t.xfr_diff(master, slave, zones)
+# resp = slave.dig("loop." + zones[0].name, "AAAA")
+# resp.check(rcode="NOERROR", rdata="1::1")
+# resp.check(rcode="NOERROR", rdata="1::4")
+# resp.check_count(5, "AAAA")
 
 t.end()
