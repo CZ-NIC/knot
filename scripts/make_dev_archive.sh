f5f156b9abe905e8c44d0657d680fc0de52a7c4c
Jakub Ružička <jakub.ruzicka@nic.cz>
2025-11-12 13:47:40 +0100

    distro: update to latest apkg compat level 6
    Modify scripts/make-archive.sh to output YAML only to stdout as required
    by latest apkg compat level 6. All stdout was redirected to stderr.

--- scripts/make_dev_archive.sh
+++ scripts/make_dev_archive.sh
@@ -2,19 +2,21 @@
 # Copyright (C) CZ.NIC, z.s.p.o. and contributors
 # SPDX-License-Identifier: GPL-2.0-or-later
 # For more information, see <https://www.knot-dns.cz/>
-
 #
 # Create a development tarball
 #
+# This script is used by apkg to generate archive from current sources.
+# It must only output valid YAML to stdout!
 
 set -o errexit -o nounset -o xtrace
 
 cd "$(dirname ${0})/.."
 
 # configure Knot DNS in order to create archive
-autoreconf -if
-./configure
+autoreconf -if >&2
+./configure >&2
 # create archive and parse output for archive name
 TARDIR=$(make dist 2>&1 | sed -n 's/tardir=\([^ ]\+\).*/\1/p')
 # print created archive name
-ls -1 $TARDIR.tar.*
+archive=$(ls -1 $TARDIR.tar.*)
+echo "archive: '$archive'"
