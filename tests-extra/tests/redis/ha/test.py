89eb02e1a19ddc7dafc58373290542db1d2c10cf
Libor Peltan <libor.peltan@nic.cz>
2025-11-20 16:53:12 +0100

    fixup?

--- tests-extra/tests/redis/ha/test.py
+++ tests-extra/tests/redis/ha/test.py
@@ -29,20 +29,17 @@ redis_slave1.slave_of(redis_master)
 redis_slave2.slave_of(redis_master)
 redis_sentinel.sentinel_of(redis_master, 1)
 
-# Note that redis_master, redis_slave1, redis_slave2 specification helps conn pool work effectively
-master.db_out(zones, [redis_sentinel, redis_master, redis_slave1, redis_slave2], 1)
-slave1.db_in(zones,  [redis_slave1, redis_slave2], 1)
+zone_write_instances = random.choice([  [redis_sentinel], [redis_master, redis_slave1, redis_slave2], [redis_sentinel, redis_master, redis_slave1, redis_slave2] ])
+master.db_out(zones, zone_write_instances, 1)
+slave1.db_in(zones,  [redis_slave1], 1)
 slave2.db_in(zones,  [redis_slave1, redis_slave2], 1)
 
 t.start()
 
-# Give sentinel some time to discover the replicas
-t.sleep(10)
-
 # Initial synchronization
-serial = master.zones_wait(zones)
-slave1.zones_wait(zones)
-slave2.zones_wait(zones)
+serial_init = slave1.zones_wait(zones)
+serial = slave2.zones_wait(zones)
+# serial == serial_init now
 
 # Update master, wait for for replicas
 master.ctl(f"zone-serial-set {ZONE} +1")
@@ -53,21 +50,25 @@ serial = slave2.zones_wait(zones, serial)
 redis_slave1.stop() # Ensure replica2 becomes a new master
 redis_master.stop()
 t.sleep(1)
-for i in range(10):
+for i in range(10): # usualy just 2x
     try:
         master.ctl(f"zone-serial-set {ZONE} +1")
     except Exception:
         t.sleep(2)
         continue
     break
-slave1.zones_wait(zones, serial)
 serial = slave2.zones_wait(zones, serial)
 redis_slave1.start() # Put replica2 to operation
+
+t.sleep(11) # up to 10 seconds till sentinel tells redis_slave1 that redis_slave2 is now the master
 
 # Clog the replica2/new_master and update new master to replica1
 redis_slave2.freeze(15).wait()
 master.ctl(f"zone-serial-set {ZONE} +1")
 slave1.zones_wait(zones, serial)
 serial = slave2.zones_wait(zones, serial)
+
+t.xfr_diff(master, slave1, zones, serial_init)
+t.xfr_diff(master, slave2, zones, serial_init)
 
 t.end()
