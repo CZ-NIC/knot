a2a3cac0be88d1c1db5d5b81e76ed267a53e7e9d
Jan Doskočil <jan.doskocil@nic.cz>
2025-12-09 16:11:22 +0100

    mod-dnstap: CLOCK_REALTIME_COARSE instead of *_REALTIME if possible

--- src/knot/modules/dnstap/dnstap.c
+++ src/knot/modules/dnstap/dnstap.c
@@ -13,6 +13,10 @@
 #include "contrib/time.h"
 #include "knot/include/module.h"
 
+#ifndef CLOCK_REALTIME_COARSE
+#define CLOCK_REALTIME_COARSE CLOCK_REALTIME
+#endif
+
 #define MOD_SINK		"\x04""sink"
 #define MOD_IDENTITY		"\x08""identity"
 #define MOD_VERSION		"\x07""version"
@@ -77,7 +81,7 @@ static knotd_state_t log_message(knotd_state_t state, const knot_pkt_t *pkt,
 	/* Unless we want to measure the time it takes to process each query,
 	 * we can treat Q/R times the same. */
 	struct timespec tv = { 0 };
-	clock_gettime(CLOCK_REALTIME, &tv);
+	clock_gettime(CLOCK_REALTIME_COARSE, &tv);
 
 	/* Determine query / response. */
 	Dnstap__Message__Type msgtype = DNSTAP__MESSAGE__TYPE__AUTH_QUERY;
