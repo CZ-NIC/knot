6481b54a3740025152613b6fab4862156546c88e
Libor Peltan <libor.peltan@nic.cz>
2025-12-28 20:41:46 +0100

    timerDB dump: skip not yet started zones

--- src/knot/zone/timers.c
+++ src/knot/zone/timers.c
@@ -208,8 +208,10 @@ int zone_timers_write(knot_lmdb_db_t *db, const knot_dname_t *zone,
 
 static void txn_zone_write(zone_t *z, knot_lmdb_txn_t *txn)
 {
-	zone_timers_t *t = z->timers_static;
-	txn_write_timers(txn, z->name, t);
+	if (z->started) {
+                zone_timers_t *t = z->timers_static;
+                txn_write_timers(txn, z->name, t);
+	}
 }
 
 int zone_timers_write_all(knot_lmdb_db_t *db, knot_zonedb_t *zonedb)
