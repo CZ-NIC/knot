1aedfc957cb94d16859d419daa893e48dd0c5306
Daniel Salzman <daniel.salzman@nic.cz>
2026-01-06 14:13:23 +0100

    x

--- tests-extra/runtests.py
+++ tests-extra/runtests.py
@@ -157,7 +157,7 @@ def job(job_id, tasks, results, stop):
             tasks.put((test, case, repeat + 1))
 
         test_dir = os.path.join(current_dir, TESTS_DIR, test)
-        case_n = case if params.repeat == 1 else case + " :" + str(repeat)
+        case_n = case if params.repeat == 1 else case + " #" + str(repeat)
         case_str_err = (" * %s/%s" % (test, case_n)).ljust(35)
         case_str_fail = ("%s/%s" % (test, case_n)).ljust(32)
         case_cnt += 1
@@ -170,7 +170,7 @@ def job(job_id, tasks, results, stop):
             continue
 
         try:
-            out_dir = os.path.join(outs_dir, test, case_n.replace(" ", ""))
+            out_dir = os.path.join(outs_dir, test, case_n.replace(" #", ":"))
             log_file = os.path.join(out_dir, "case.log")
 
             os.makedirs(out_dir, exist_ok=True)
