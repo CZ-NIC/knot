7405629bd9a5ecebe546d4f4c2fdd092d4c4d354
Jan Doskočil <jan.doskocil@nic.cz>
2026-01-07 11:08:03 +0100

    libknot/dnssec: add dnssec_key_load_pkcs8_der()
    Load PKCS #8 private key in the DER format.

--- src/libknot/dnssec/key/simple.c
+++ src/libknot/dnssec/key/simple.c
@@ -42,3 +42,29 @@ int dnssec_key_load_pkcs8(dnssec_key_t *key, const dnssec_binary_t *pem)
 
 	return KNOT_EOK;
 }
+
+_public_
+int dnssec_key_load_pkcs8_der(dnssec_key_t *key, const dnssec_binary_t *der)
+{
+	if (!key || !der || !der->data) {
+		return KNOT_EINVAL;
+	}
+
+	if (dnssec_key_get_algorithm(key) == 0) {
+		return KNOT_INVALID_KEY_ALGORITHM;
+	}
+
+	gnutls_privkey_t privkey = NULL;
+	int r = dnssec_der_to_privkey(der, &privkey);
+	if (r != KNOT_EOK) {
+		return r;
+	}
+
+	r = key_set_private_key(key, privkey);
+	if (r != KNOT_EOK) {
+		gnutls_privkey_deinit(privkey);
+		return r;
+	}
+
+	return KNOT_EOK;
+}
