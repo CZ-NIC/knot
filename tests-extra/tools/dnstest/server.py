cfccdeace0b38d2552fa316f4e34dd973c1ebace
David Vašek <david.vasek@nic.cz>
2026-01-12 11:15:49 +0100

    tests-extra: allow a keystore to be shared by different zones

--- tests-extra/tools/dnstest/server.py
+++ tests-extra/tools/dnstest/server.py
@@ -1656,6 +1656,7 @@ class Knot(Server):
                     s.item_type(ci.replace("_", "-"), val)
                 s.end()
 
+        keystores = list()
         have_keystore = False
         for zone in sorted(self.zones):
             z = self.zones[zone]
@@ -1666,6 +1667,9 @@ class Knot(Server):
                 s.begin("keystore")
                 have_keystore = True
             for ks in z.dnssec.keystore:
+                if ks.id in keystores:
+                    continue
+                keystores.append(ks.id)
                 s.id_item("id", ks.id)
                 s.item_type("config", ks.config())
                 s.item_type("backend", ks.backend())
