6bb7725d10e99c3eeb2fd87a6c4349857cc25ed7
Libor Peltan <libor.peltan@nic.cz>
2026-02-13 16:59:14 +0100

    hs bbb

--- src/contrib/hs_tree.c
+++ src/contrib/hs_tree.c
@@ -28,8 +28,7 @@ static size_t node_alloc_size(const hs_node_t *n, hs_tree_t *t) {
 }
 
 static void node_free(hs_node_t *n, hs_tree_t *t) {
-	mm_free(t->mm, n->own_hash);
-	mm_free(t->mm);
+	mm_free(t->mm, n);
 }
 
 static bool node_geq(hs_node_t *n, unsigned depth, hs_tree_t *t, unsigned idx, const uint8_t *hash)
@@ -82,15 +81,27 @@ static int node_rem_idx(hs_node_t **npp, unsigned depth, hs_tree_t *t, unsigned 
 		n->capacity /= 2;
 		size_t after = node_alloc_size(n, t);
 
-		// FIXME
-		*npp = mm_realloc(t->mm, n, after, before);
-		if (*npp == NULL) {
+		n = mm_realloc(t->mm, n, after, before);
+		if (n == NULL) {
 			return -ENOMEM;
 		}
+		*npp = n;
 		t->alloc_size -= (before - after);
 	}
 
 	return 0;
+}
+
+const static hs_node_t node_new_tpl = { .size = 0, .capacity = 2 };
+
+static hs_node_t *node_new(hs_tree_t *t)
+{
+	hs_node_t *n = mm_alloc(t->mm, node_alloc_size(&node_new_tpl, t));
+	if (n != NULL) {
+		n->capacity = 2;
+		n->own_hash = (void *)n + node_alloc_size(n, t) - t->hash_len;
+	}
+	return n;
 }
 
 static int node_rem(hs_node_t **npp, unsigned depth, hs_tree_t *t, const uint8_t *hash)
@@ -123,4 +134,95 @@ int hs_tree_rem(hs_tree_t *t, const uint8_t *hash)
 	}
 
 	return node_rem(&t->root, 0, t, hash);
+
+	// FIXME rehash
 }
+
+static int node_add_idx(hs_node_t **npp, unsigned depth, hs_tree_t *t, unsigned idx, const uint8_t *hash)
+{
+	hs_node_t *n = *npp;
+	if (n->capacity == n->size) {
+                size_t before = node_alloc_size(n, t);
+		n->capacity *= 2;
+                size_t after = node_alloc_size(n, t);
+		n = mm_realloc(t->mm, n, after, before);
+		if (n == NULL) {
+			return -ENOMEM;
+		}
+		*npp = n;
+		t->alloc_size += (after - before);
+	}
+
+	if (n->size > idx) {
+		memmove(n->leaf_childs + idx + 1, n->leaf_childs + idx, sizeof(n->leaf_childs[0]) * (n->size - idx));
+	}
+	n->size++;
+
+        // generate one-item branch nodes down to leaf to fulfill the depth
+	while (!leaf_parent(n, depth, t)) {
+		n->branch_childs[idx] = node_new(t);
+		if (n->branch_childs[idx] == NULL) {
+			return -ENOMEM;
+		}
+
+		npp = &n->branch_childs[idx];
+		n = *npp;
+		idx = 0;
+		n->size = 1;
+	}
+
+	n->leaf_childs[idx] = mm_alloc(t->mm, t->hash_len);
+	if (n->leaf_childs[idx] == NULL) {
+		return -ENOMEM;
+        }
+	memcpy(n->leaf_childs[idx], hash, t->hash_len);
+
+	return 0;
+}
+
+static int node_add(hs_node_t **npp, unsigned depth, hs_tree_t *t, const uint8_t *hash)
+{
+	hs_node_t *n = *npp;
+	unsigned idx = node_bsearch(n, depth, t, hash);
+
+	if (leaf_parent(n, depth, t)) {
+		int cmp = memcmp(n->leaf_childs[idx], hash, t->hash_len);
+		if (cmp == 0) {
+			return -EEXIST;
+		}
+		if (cmp > 0) {
+			assert(idx == 0);
+		} else {
+			idx++;
+		}
+		return node_add_idx(npp, depth, t, idx, hash);
+	} else {
+		int cmp = memcmp(TODO, hash, t->hash_len);
+		if (cmp == 0) {
+			return node_add(&n->branch_childs[idx], depth + 1, t, hash);
+		} else {
+			if (cmp > 0) {
+				assert(idx == 0);
+			} else {
+				idx++;
+			}
+			return node_add_idx(npp, depth, t, idx, hash);
+		}
+	}
+}
+
+int hs_tree_add(hs_tree_t *t, const uint8_t *hash)
+{
+	if (t == NULL || hash == NULL) {
+		return -EINVAL;
+	}
+
+	if (t->root == NULL) {
+		// TODO
+	}
+
+	return node_add(&t->root, 0, t, hash);
+
+	// FIXME rehash
+}
+
