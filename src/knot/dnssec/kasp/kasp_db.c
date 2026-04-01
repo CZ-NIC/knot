ae712b5c24bd3031e6f81246dcf42a315af07a54
David Vašek <david.vasek@nic.cz>
2026-04-01 10:57:27 +0200

    zone-keys: in kdnssec_delete_from_keystores(), allow turning off error logging

--- src/knot/dnssec/kasp/kasp_db.c
+++ src/knot/dnssec/kasp/kasp_db.c
@@ -507,7 +507,7 @@ int kasp_db_delete_keys(knot_lmdb_db_t *db, const knot_dname_t *zone_name,
 				ret = make_trash_key(&txn, &txn.cur_key, &txn.cur_val, delay);
 			} else {
 				ret = kdnssec_delete_from_keystores(keystores, key_id,
-				                                    zone_name, true);
+				                                    zone_name, true, false);
 				ret = (ret == KNOT_ENOENT) ? KNOT_EOK : ret;
 				// Note: if error, it isn't sure that there still is a key to delete.
 			}
@@ -566,7 +566,7 @@ int kasp_db_delete_trash(knot_lmdb_db_t *db, const knot_dname_t *zone_name, char
 	if (key_id != NULL) {
 		// Remove just one trash key.
 		// In this case, zone_name is ignored.
-		ret = kdnssec_delete_from_keystores(keystores, key_id, NULL, true);
+		ret = kdnssec_delete_from_keystores(keystores, key_id, NULL, true, false);
 		ret = (ret == KNOT_ENOENT) ? KNOT_EOK : ret;
 		if (ret == KNOT_EOK) {
 			MDB_val prefix = make_key_str(KASPDBKEY_TRASH, NULL, key_id);
@@ -717,7 +717,8 @@ int kasp_db_sweep_keys(knot_lmdb_db_t *db, sweep_cb keep_zone, void *cb_data)
 
 			assert(count > 0 || is_trash_related(&txn.cur_key));
 			if (count < 2) {
-				ret = kdnssec_delete_from_keystores(keystores, key_id, NULL, true);
+				ret = kdnssec_delete_from_keystores(keystores, key_id, NULL,
+				                                    true, false);
 				ret = (ret == KNOT_ENOENT) ? KNOT_EOK : ret;
 				if (ret != KNOT_EOK) {
 					// Note: it isn't sure that there still is a key to delete.
