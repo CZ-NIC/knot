d50a5f5b21adbdcf37e7e2ffe44f8f504f435664
Daniel Salzman <daniel.salzman@nic.cz>
2026-07-04 19:18:07 +0200

    contrib: add embedded LMDB 0.9.35
    This helps with:
    - database migration to LMDB 1.0.x
    - occasional debugging

---
+++ src/contrib/lmdb/mute_warnings.h
@@ -0,0 +1,12 @@
+/*  Copyright (C) CZ.NIC, z.s.p.o. and contributors
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  For more information, see <https://www.knot-dns.cz/>
+ */
+
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#if __GNUC__ > 4
+#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
+#pragma GCC diagnostic ignored "-Wshadow"
