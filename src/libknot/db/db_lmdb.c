63807de1e566d0d170e9f481d2d37351c2589c7f
Daniel Salzman <daniel.salzman@nic.cz>
2026-07-04 20:40:52 +0200

    libknot/db: replace mdb_drop() with mdb_cursor_del() for each key in lmdb.clear()
    This fixes compatibility with LMDB 1.0.0, where mdb_drop() cannot be followed
    by further additions within the same transaction. Otherwise, mdb_commit() fails
    with "MDB_BAD_TXN: Transaction must abort, has a child, or is invalid".

--- src/libknot/db/db_lmdb.c
+++ src/libknot/db/db_lmdb.c
@@ -305,8 +305,25 @@ static int clear(knot_db_txn_t *txn)
 {
 	struct lmdb_env *env = txn->db;
 
-	int ret = mdb_drop(txn->txn, env->dbi, 0);
+	MDB_cursor *cursor = NULL;
+	int ret = mdb_cursor_open(txn->txn, env->dbi, &cursor);
 	if (ret != MDB_SUCCESS) {
+		return lmdb_error_to_knot(ret);
+	}
+
+	MDB_val mdb_key, mdb_val;
+	ret = mdb_cursor_get(cursor, &mdb_key, &mdb_val, MDB_FIRST);
+	while (ret == MDB_SUCCESS) {
+		ret = mdb_cursor_del(cursor, 0);
+		if (ret != MDB_SUCCESS) {
+			break;
+		}
+		ret = mdb_cursor_get(cursor, &mdb_key, &mdb_val, MDB_NEXT);
+	}
+
+	mdb_cursor_close(cursor);
+
+	if (ret != MDB_NOTFOUND && ret != MDB_SUCCESS) {
 		return lmdb_error_to_knot(ret);
 	}
 
