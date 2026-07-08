29acaeaba94de7af39143921abf9d777886d217a
Daniel Salzman <daniel.salzman@nic.cz>
2026-07-08 20:30:41 +0200

    tests-extra: improve bind_version() to accept newer extended string

--- tests-extra/tools/dnstest/server.py
+++ tests-extra/tools/dnstest/server.py
@@ -1120,7 +1120,13 @@ class Bind(Server):
         self.ctl("sync%s" % zone_name, wait=wait)
 
     def bind_version(self):
-        return tuple(map(int, run([self.daemon_bin, '-v'], stdout=PIPE, stderr=PIPE, text=True).stdout.replace('BIND ', '').split('-')[0].split('.')))
+        version = (
+            run([self.daemon_bin, '-v'], stdout=PIPE, stderr=PIPE, text=True)
+            .stdout.removeprefix('BIND ')
+            .split()[0]          # "9.20.24"
+            .split('-')[0]       # handles "-RedHat" suffixes
+        )
+        return tuple(map(int, version.split('.')))
 
     def key_timer_param(self, timer_name, timer_val):
         ktpmap = { 'publish': '-P', 'active': '-A', 'retire': '-I', 'revoke': '-R', 'remove': '-D' }
