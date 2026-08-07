c6dce1fde5db537922dc52792f2d9354abfa754a
Libor Peltan <libor.peltan@nic.cz>
2026-08-07 11:44:59 +0200

    tests/jitter: also check some rrsig-refresh limits handling

--- tests-extra/tests/dnssec/jitter/test.py
+++ tests-extra/tests/dnssec/jitter/test.py
@@ -11,7 +11,7 @@ from collections import Counter
 t = Test()
 
 master = t.server("knot")
-zones = t.zone_rnd(16, records=1, dnssec=False)
+zones = t.zone_rnd(16, records=1, ttl=8, dnssec=False)
 for z in zones:
     z.update_soa(serial=1, refresh=600, retry=100, expire=3600)
 t.link(zones, master)
@@ -25,6 +25,7 @@ master.dnssec(zones).rrsig_refresh = 5
 master.dnssec(zones).rrsig_pre_refresh = 1
 master.dnssec(zones).dnssec_jitter = 12
 master.dnssec(zones).zone_max_ttl = 1
+master.dnssec(zones).dnskey_ttl = 1
 
 def wait_for_any(server, zns, rrtype, callback, arg):
     for i in range(100):
@@ -67,5 +68,25 @@ wait_for_any(master, zones, "DNSKEY", has_dnskeys, 3)
 t.sleep(6)
 dnskeys = get_dnskeys(master, zones)
 check_jitter(dnskeys, zones, 2, 3, "ZSK lifetime")
+
+master.dnssec(zones).rrsig_pre_refresh = 13
+master.gen_confile()
+try:
+   master.reload()
+   set_err("conf accepts refresh+prerefresh+jitter>=lifetime")
+except:
+    pass
+
+t.sleep(1)
+master.dnssec(zones).rrsig_pre_refresh = 12
+master.dnssec(zones).zone_max_ttl = None # effectively computes the maximum TTL from zonefile, which is 8
+master.gen_confile()
+master.reload()
+t.sleep(1)
+warnings = master.log_search_count("RRSIG refresh lower")
+detail_log(str(warnings))
+threshold = len(zones) * 15 / 100
+isset(warnings > threshold, "low refresh warnings some")
+isset(warnings < len(zones) - threshold, "low refresh warnings not all")
 
 t.end()
