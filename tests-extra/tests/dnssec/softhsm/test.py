bb4aef569d45921f31ffe4f399b5b71013c1f398
Daniel Salzman <daniel.salzman@nic.cz>
2026-08-09 19:09:23 +0200

    tests-extra: call keymgr keystore test if not ASAN

--- tests-extra/tests/dnssec/softhsm/test.py
+++ tests-extra/tests/dnssec/softhsm/test.py
@@ -53,9 +53,12 @@ knot2.zone_wait(zone, serial)
 resp = knot2.dig(zone[0].name, "DNSKEY")
 resp.cmp(knot1)
 
-# Run keystore test and benchmark
-_, out, _ = Keymgr.run_check(knot1.confile, "keys1", "keystore-test", env=keys1.env())
-isset("PKCS #11" in out, "keystore-test")
+# Run keystore test (only with Valgrind due to leaks in Ed25518 Ed448)
+if knot1.valgrind:
+    _, out, _ = Keymgr.run_check(knot1.confile, "keys1", "keystore-test", env=keys1.env())
+    isset("PKCS #11" in out, "keystore-test")
+
+# Run keystore benchmark
 _, out, _ = Keymgr.run_check(knot1.confile, "keys1", "keystore-bench", "1", "ECDSAP256SHA256", env=keys1.env())
 isset("ECDSAP256SHA256" in out, "keystore-bench")
 
