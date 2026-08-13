8360bdb8839d81b39b358195439b4fc69fc0056e
Daniel Salzman <daniel.salzman@nic.cz>
2026-08-13 09:28:39 +0200

    yptrafo: mute warning 'addr_len' may be used uninitialized (gcc 15.2.0, -Og)

--- src/libknot/yparser/yptrafo.c
+++ src/libknot/yparser/yptrafo.c
@@ -1258,6 +1258,7 @@ struct sockaddr_storage yp_addr_noport(
 	// Set address.
 	switch (type) {
 	case ADDR_TYPE_UNIX:
+		addr_len = 0;
 		sockaddr_set(&ss, AF_UNIX, (char *)data, 0);
 		break;
 	case ADDR_TYPE_IPV4:
