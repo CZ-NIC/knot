9f5d87e3feaf8faf36288901168e0b9085e825c1
Daniel Salzman <daniel.salzman@nic.cz>
2026-08-16 17:22:15 +0200

    onlinesign: check for truncated response - return NOERROR instead of SERVFAIL

--- src/knot/modules/onlinesign/onlinesign.c
+++ src/knot/modules/onlinesign/onlinesign.c
@@ -329,7 +329,8 @@ static knotd_in_state_t sign_section(knotd_in_state_t state, knot_pkt_t *pkt,
 		int r = knot_pkt_put(pkt, KNOT_COMPR_HINT_NONE, rrsig, KNOT_PF_FREE);
 		if (r != KNOT_EOK) {
 			knot_rrset_free(rrsig, &pkt->mm);
-			state = KNOTD_IN_STATE_ERROR;
+			state = (r == KNOT_ESPACE ? KNOTD_IN_STATE_TRUNC :
+			                            KNOTD_IN_STATE_ERROR);
 			break;
 		}
 	}
@@ -353,7 +354,8 @@ static knotd_in_state_t synth_authority(knotd_in_state_t state, knot_pkt_t *pkt,
 		int r = knot_pkt_put(pkt, KNOT_COMPR_HINT_NONE, nsec, KNOT_PF_FREE);
 		if (r != DNSSEC_EOK) {
 			knot_rrset_free(nsec, &pkt->mm);
-			return KNOTD_IN_STATE_ERROR;
+			return (r == KNOT_ESPACE ? KNOTD_IN_STATE_TRUNC :
+			                           KNOTD_IN_STATE_ERROR);
 		}
 	}
 
@@ -538,7 +540,8 @@ static knotd_in_state_t synth_answer(knotd_in_state_t state, knot_pkt_t *pkt,
 		int r = knot_pkt_put(pkt, KNOT_COMPR_HINT_QNAME, dnskey, KNOT_PF_FREE);
 		if (r != DNSSEC_EOK) {
 			knot_rrset_free(dnskey, &pkt->mm);
-			return KNOTD_IN_STATE_ERROR;
+			return (r == KNOT_ESPACE ? KNOTD_IN_STATE_TRUNC :
+			                           KNOTD_IN_STATE_ERROR);
 		}
 		state = KNOTD_IN_STATE_HIT;
 	}
@@ -552,7 +555,8 @@ static knotd_in_state_t synth_answer(knotd_in_state_t state, knot_pkt_t *pkt,
 		int r = knot_pkt_put(pkt, KNOT_COMPR_HINT_QNAME, dnskey, KNOT_PF_FREE);
 		if (r != DNSSEC_EOK) {
 			knot_rrset_free(dnskey, &pkt->mm);
-			return KNOTD_IN_STATE_ERROR;
+			return (r == KNOT_ESPACE ? KNOTD_IN_STATE_TRUNC :
+			                           KNOTD_IN_STATE_ERROR);
 		}
 		state = KNOTD_IN_STATE_HIT;
 	}
@@ -566,7 +570,8 @@ static knotd_in_state_t synth_answer(knotd_in_state_t state, knot_pkt_t *pkt,
 		int r = knot_pkt_put(pkt, KNOT_COMPR_HINT_QNAME, ds, KNOT_PF_FREE);
 		if (r != DNSSEC_EOK) {
 			knot_rrset_free(ds, &pkt->mm);
-			return KNOTD_IN_STATE_ERROR;
+			return (r == KNOT_ESPACE ? KNOTD_IN_STATE_TRUNC :
+			                           KNOTD_IN_STATE_ERROR);
 		}
 		state = KNOTD_IN_STATE_HIT;
 	}
@@ -580,7 +585,8 @@ static knotd_in_state_t synth_answer(knotd_in_state_t state, knot_pkt_t *pkt,
 		int r = knot_pkt_put(pkt, KNOT_COMPR_HINT_QNAME, nsec, KNOT_PF_FREE);
 		if (r != DNSSEC_EOK) {
 			knot_rrset_free(nsec, &pkt->mm);
-			return KNOTD_IN_STATE_ERROR;
+			return (r == KNOT_ESPACE ? KNOTD_IN_STATE_TRUNC :
+			                           KNOTD_IN_STATE_ERROR);
 		}
 
 		state = KNOTD_IN_STATE_HIT;
