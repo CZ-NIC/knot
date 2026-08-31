34d7f9f77987493add0942cc2ba8f3b4e1fda80c
Daniel Salzman <daniel.salzman@nic.cz>
2026-08-31 14:47:15 +0200

    knotc: fix compilation on SmartOS
    fixes #979

--- src/utils/knotc/interactive.c
+++ src/utils/knotc/interactive.c
@@ -359,11 +359,16 @@ static void path_lookup(EditLine *el, const char *str, bool dirsonly)
 	struct stat sb;
 	for (int i = 0; i < nnames; ++i) {
 		const struct dirent *it = namelist[i];
+#ifdef __sun
+		strlcpy(base, it->d_name, PATH_MAX - (size_t)(base - path));
+		bool is_dir = !stat(path, &sb) && S_ISDIR(sb.st_mode);
+#else
 		bool is_dir = (it->d_type == DT_DIR);
 		if (it->d_type == DT_LNK) {
 			strlcpy(base, it->d_name, PATH_MAX - (size_t)(base - path));
 			is_dir = !stat(path, &sb) && S_ISDIR(sb.st_mode);
 		}
+#endif
 		if ((!dirsonly || is_dir) &&
 		    (strcmp(it->d_name, ".") && strcmp(it->d_name, ".."))) {
 			char buf[NAME_MAX + 2];  // Max. name length + slash + terminator.
