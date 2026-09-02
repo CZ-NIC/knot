f7adb908e9fbe69fd0f8a7b3721153f583679638
Libor Peltan <libor.peltan@nic.cz>
2026-09-02 14:46:50 +0200

    fixup! tests: reproducer of block in startup with catalog and big zone

--- tests-extra/tests/catalog/load_ctl/test.py
+++ tests-extra/tests/catalog/load_ctl/test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
-'''Test of chained catalog interpreter and generator.'''
+'''Test of mutual blocking of catalog processing and big zone signing.'''
 
 from dnstest.utils import *
 from dnstest.test import Test
 import glob
 import random
 import shutil
+import threading
 
 t = Test(tsig=False) # TSIG prevents zone_wait(catz)
 
@@ -30,11 +31,16 @@ master.dnssec(bigz).nsec3 = True
 master.dnssec(bigz).signing_threads = 1
 master.dnssec(bigz).algorithm = "RSASHA512"
 
+master.conf_zone(bigz).zonefile_sync = -1
+
 if not master.valgrind:
     master.dnssec(bigz).zsk_size = 4096
 
-t.start()
-master.zone_wait(catz)
+def tstart(pt):
+    pt.start()
+threading.Thread(target=tstart, args=(t,)).start()
+
+cs = master.zone_wait(catz)
 s = master.zone_wait(smallz)
 
 confsock = master.ctl_sock_rnd()
@@ -59,12 +65,39 @@ resp.check(rcode="REFUSED") # otherwise test failure, signing too fast
 
 master.ctl("reload", custom_parm=confsock) # blocks until catalog is populated and earlier zone-reload processed
 
-master.zone_wait(bigz)
+bs = master.zone_wait(bigz)
 t.sleep(1)
 resp = master.dig("records.com.", "SOA")
 resp.check(rcode="SERVFAIL")
 
 resp = master.dig("dewjhdlkwjd.%s" % Z, "A")
+resp.check(rcode="NXDOMAIN", nordata="1.1.1.1")
+
+master.zones[bigz[0].name].zfile.update_rnd()
+master.zones[catz[0].name].zfile.update_soa()
+master.ctl("zone-reload")
+
+master.zone_wait(catz, cs)
+
+master.ctl("zone-status", custom_parm=confsock)
+
+master.ctl("zone-begin %s" % Z, custom_parm=confsock)
+master.ctl("zone-set %s djeljdlkejeee 3600 A 1.1.1.1" % Z, custom_parm=confsock)
+master.ctl("zone-commit %s" % Z, custom_parm=confsock)
+
+resp = master.dig("djeljdlkejeee.%s" % Z, "A")
+resp.check(rcode="NOERROR", rdata="1.1.1.1")
+
+master.zones[Z].zfile.update_soa(serial=(s+4))
+master.ctl("zone-reload %s" % Z, custom_parm=confsock)
+
+t.sleep(25)
+master.ctl("reload", custom_parm=confsock) # blocks until catalog is populated and earlier zone-reload processed
+
+master.zone_wait(bigz, bs)
+t.sleep(1)
+
+resp = master.dig("djeljdlkejeee.%s" % Z, "A")
 resp.check(rcode="NXDOMAIN", nordata="1.1.1.1")
 
 t.end()
