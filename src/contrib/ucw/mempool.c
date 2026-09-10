befa4fc36a6e321eda83b3fbe72a21cc882ae4f6
Daniel Salzman <daniel.salzman@nic.cz>
2026-09-10 18:46:29 +0200

    mempool: replace bzero() with general memzero()

--- src/contrib/ucw/mempool.c
+++ src/contrib/ucw/mempool.c
@@ -17,6 +17,7 @@
 #include <assert.h>
 #include "contrib/macros.h"
 #include "contrib/memcheck.h"
+#include "contrib/string.h"
 #include "contrib/ucw/mempool.h"
 
 #pragma GCC diagnostic ignored "-Wpointer-arith"
@@ -246,7 +247,7 @@ mp_stats_chain(struct mempool *pool, struct mempool_chunk *chunk, struct mempool
 void
 mp_stats(struct mempool *pool, struct mempool_stats *stats)
 {
-	bzero(stats, sizeof(*stats));
+	memzero(stats, sizeof(*stats));
 	mp_stats_chain(pool, pool->state.last[0], stats, 0);
 	mp_stats_chain(pool, pool->state.last[1], stats, 1);
 	mp_stats_chain(pool, pool->unused, stats, 2);
